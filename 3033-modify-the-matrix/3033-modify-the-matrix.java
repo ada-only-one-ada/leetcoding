451617a3bf45dd6a075b32085b1ed821818fddb9
yuqinggongyg <122472773+yuqinggongyg@users.noreply.github.com>
2024-02-12 00:49:18 -0800

    Time: 1 ms (100.00%), Space: 45.5 MB (50.00%) - LeetHub

--- 3033-modify-the-matrix/3033-modify-the-matrix.java
+++ 3033-modify-the-matrix/3033-modify-the-matrix.java
@@ -1,6 +1,5 @@
 class Solution {
     public int[][] modifiedMatrix(int[][] matrix) {
-        int[][] ans = new int[matrix.length][matrix[0].length];
         int[] maxInCol = new int[matrix[0].length];
         
         for (int col = 0; col < matrix[0].length; col++) {
@@ -11,16 +10,16 @@ class Solution {
             maxInCol[col] = max;
         }
         
-        for (int row = 0; row < ans.length; row++) {
-            for (int col = 0; col < ans[0].length; col++) {
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[0].length; col++) {
                 if (matrix[row][col] != -1) {
-                    ans[row][col] = matrix[row][col];
+                    continue;
                 } else {
-                    ans[row][col] = maxInCol[col];
+                    matrix[row][col] = maxInCol[col];
                 }
             }
         }
         
-        return ans;
+        return matrix;
     }
 }
