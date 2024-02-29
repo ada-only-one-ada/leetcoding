98f66082da16f34440f07d9008f3b0ac428d9baf
yuqinggongyg <122472773+yuqinggongyg@users.noreply.github.com>
2024-02-28 20:19:37 -0800

    Time: 1 ms (69.74%), Space: 44.1 MB (54.96%) - LeetHub

---
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        if (root == null) return -1;
+        int res = -1;
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (i == 0) {
+                    res= node.val;
+                }
+                
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+        }
+        return res;
+    
+    }
+}
