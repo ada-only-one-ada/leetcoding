7696643add6156990e1c0ee63729d07b82d53479
yuqinggongyg <122472773+yuqinggongyg@users.noreply.github.com>
2024-03-13 12:28:42 -0700

    Time: 51 ms (79.73%), Space: 52 MB (17.26%) - LeetHub

---
+++ 2677-chunk-array/2677-chunk-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+const chunk = function(arr, size) {
+    return arr.reduce((chunkedArray, element) => {
+        const lastChunk = chunkedArray[chunkedArray.length - 1];
+
+        if (!lastChunk || lastChunk.length === size) {
+            chunkedArray.push([element]);
+        } else {
+            lastChunk.push(element);
+        }
+        return chunkedArray;
+    }, []);
+};
