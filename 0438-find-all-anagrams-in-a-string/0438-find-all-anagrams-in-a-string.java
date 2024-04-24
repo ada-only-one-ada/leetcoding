3450150aaf7d19d035efdd7764430c7d6803c06b
yuqinggongyg <122472773+yuqinggongyg@users.noreply.github.com>
2024-04-24 13:35:55 -0700

    Time: 37 ms (40.13%), Space: 44.4 MB (93.05%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,28 +1,29 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> res = new ArrayList<>();
+        if (p.length() > s.length()) return res;
+
         Map<Character, Integer> map = new HashMap<>();
         for (char c : p.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
 
-        int found = 0;
+        int required = p.length();
         int left = 0;
         for (int right = 0; right < s.length(); right++) {
             char curr = s.charAt(right);
-
-            if (map.containsKey(curr) && map.get(curr) > 0) {
-                found++;
+            if (map.containsKey(curr) && map.get(curr) >= 1) {
+                required--;
             }
             map.put(curr, map.getOrDefault(curr, 0) - 1);
 
             if (right - left + 1 == p.length()) {
-                if (found == p.length()) {
-                    res.add(left);
-                }
+                if (required == 0) res.add(left);
 
-                if (map.get(s.charAt(left)) >= 0) found--;
-                map.put(s.charAt(left), map.get(s.charAt(left)) + 1);
+                char remove = s.charAt(left);
+                if (map.get(remove) >= 0) required++;
+
+                map.put(remove, map.get(remove) + 1);
                 left++;
             }
         }
