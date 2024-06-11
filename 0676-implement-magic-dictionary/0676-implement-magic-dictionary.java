90932bb0c35b405e70f1648074dcc9cd5c493622
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-06-10 22:37:58 -0700

    Time: 25 ms (90.52%), Space: 45.5 MB (70.63%) - LeetHub

--- 0676-implement-magic-dictionary/0676-implement-magic-dictionary.java
+++ 0676-implement-magic-dictionary/0676-implement-magic-dictionary.java
@@ -9,7 +9,6 @@ class TrieNode {
 
 class MagicDictionary {
     TrieNode root;
-
     public MagicDictionary() {
         root = new TrieNode();    
     }
@@ -31,32 +30,33 @@ class MagicDictionary {
         for (String word: dictionary) {
             insertNode(root, word);
         }
-        
     }
 
     public boolean searchHelper(TrieNode node, String searchWord, int index, int diff) {
+        // 字符全部遍历完
         if (index == searchWord.length()) {
             return diff == 1 && node.isValid;
         }
 
+        // 字典树是存在该字符, 接着找下一个
         if (node.children[searchWord.charAt(index) - 'a'] != null) {
             if (searchHelper(node.children[searchWord.charAt(index) - 'a'], searchWord, index + 1, diff)) {
                 return true;
             }
         } 
-            if (diff > 0) return false;
-            for (int child = 0; child < 26; child++) {
-                if (child != searchWord.charAt(index) - 'a' && node.children[child] != null) {
-                    if (searchHelper(node.children[child], searchWord, index + 1, diff + 1)) {
-                        return true;
-                    }
+        
+        // 字典树不存在该字符, 且之前替换过了，返回false
+        if (diff > 0) return false;
+
+        // 字典树不存在该字符, 之前没有替换过
+        // 枚举当前结点的所有子结点进行遍历，不能是当前的字符，不然字符一样不算替换
+        for (int child = 0; child < 26; child++) {
+            if (child != searchWord.charAt(index) - 'a' && node.children[child] != null) {
+                if (searchHelper(node.children[child], searchWord, index + 1, diff + 1)) {
+                    return true;
                 }
             }
-
-
-        
-
-        
+        }
 
         return false;
     }
