5cfcd0450c7ac476f69cdcd5553d21ef61c7c76f
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-06-17 15:26:32 -0700

    Time: 3 ms (53.16%), Space: 46.8 MB (99.01%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
@@ -1,25 +1,36 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        int n = nums.length;
-        int left = 0, right = 0;
+        int res = 0;
+       
+        int leftCount = 1; // 没有前导零本身就是一个subarray
         int sum = 0;
-        int ans = 0;
-        int leftCnt = 1;
-        while(right < n){
+
+        int left = 0;
+        for (int right = 0; right < nums.length; right++) {
             sum += nums[right];
-            if(sum > goal) {
-                leftCnt = 1;
+
+            if (sum > goal) {
+                leftCount = 1;
             }
-            while(sum > goal && left < right){
-                sum -= nums[left++];
-            }
-            while(sum == goal && left < right && nums[left] == 0){
-                leftCnt++;
+
+            // 确保我们始终在检查一个和不大于 goal 的窗口
+            while (sum > goal && left < right) {
+                sum -= nums[left];
                 left++;
             }
-            if(sum == goal) ans += leftCnt;
-            right++;
+
+            // 处理前导零：
+            // 当窗口的和 sum 恰好等于 goal 时，说明找到了一个满足条件的子数组
+            // 如果左边界元素为 0，意味着左边界可以继续向右扩展，且扩展过程中子数组的和仍然等于 goal
+            // 因此 leftCount 会增加，这样的扩展会产生多个满足条件的子数组
+            while (left < right && nums[left] == 0) {
+                leftCount++;
+                left++;
+            }
+           
+            if (sum == goal) res += leftCount;
         }
-        return ans;
+
+        return res;
     }
 }
