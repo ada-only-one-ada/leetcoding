6eee71e054f86303a43544da7b57920dea11d6eb
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-07-28 19:13:29 +0800

    Time: 1 ms (98.8%), Space: 44.6 MB (69.88%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,7 +1,9 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         List<int[]> list = new ArrayList<>();
-      
+        if (intervals.length == 0) {
+            return new int[][]{newInterval};
+        }
         
         int start = newInterval[0];
         int end = newInterval[1];
