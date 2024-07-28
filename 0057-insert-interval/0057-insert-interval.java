1b38c21ee799f6a1344788e00589e58c5d2a7950
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-07-28 19:29:46 +0800

    Time: 1 ms (98.8%), Space: 44.7 MB (53.33%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -10,14 +10,22 @@ class Solution {
         int end = newInterval[1];
 
         int i = 0;
+        // 正确加入:
+        //【start，end】->【newStart，newEnd】，end是小于新的start
+        // 如果当前end是小于新start的，可以把当前interval加入res
         while (i < intervals.length && intervals[i][1] < start) {
             list.add(intervals[i]);
             i++;
         }
+
+        // 更新重叠中的起始点
         if (i < intervals.length) {
             start = Math.min(intervals[i][0], start);
         }
         
+        // 错误更新:
+        //【newStart，newEnd】->【start，end】，新end要小于当前start
+        // 如果新end大于等于当前start的话，意味着有重叠，要更新
         while (i < intervals.length && intervals[i][0] <= end) {
             end = Math.max(intervals[i][1], end);
             i++;
@@ -25,6 +33,7 @@ class Solution {
         
         list.add(new int[]{start, end});
 
+        // 后面还剩下一些不重叠的要记得加入
         while (i < intervals.length) {
             list.add(intervals[i]);
             i++;
