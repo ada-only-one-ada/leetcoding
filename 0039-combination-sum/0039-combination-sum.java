fcc3ff724dcf29ced113e1cc23e96b98ad8dfb1d
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-03 14:24:00 +0800

    Time: 2 ms (84.29%), Space: 44.3 MB (77.78%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtracking(candidates, target, res, new ArrayList<>(), 0, 0);
+        return res;
+    }
+
+    public void backtracking(int[] candidates, int target, List<List<Integer>> res, List<Integer> currRes, int sum, int index) {
+        if (sum >= target) {
+            if (sum == target) {
+                res.add(new ArrayList<>(currRes));
+            }
+            return;
+        }
+
+        for (int i = index; i < candidates.length; i++) {
+            if (currRes.size() == 0 && i > 0 && candidates[i] == candidates[i - 1]) continue;
+            currRes.add(candidates[i]);
+            sum += candidates[i];
+            backtracking(candidates, target, res, currRes, sum, i);
+            currRes.remove(currRes.size() - 1);
+            sum -= candidates[i];
+        }
+    }
+}
