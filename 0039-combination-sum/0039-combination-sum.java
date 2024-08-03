8c0164383cece73665f6f64f8101bdd96be6004a
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-03 14:39:36 +0800

    Time: 2 ms (84.29%), Space: 44.3 MB (77.78%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -16,6 +16,7 @@ class Solution {
         for (int i = index; i < candidates.length; i++) {
             currRes.add(candidates[i]);
             sum += candidates[i];
+            // i可以再取，但不可以往前取，所以start记录从每次开始的位置
             backtracking(candidates, target, res, currRes, sum, i);
             currRes.remove(currRes.size() - 1);
             sum -= candidates[i];
