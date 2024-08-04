e69f884828b6a22302b58eaa932a390ef4afbc26
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-05 06:42:36 +0800

    Time: 0 ms (100%), Space: 41.9 MB (15.8%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,6 +1,7 @@
 class Solution {
     public void sortColors(int[] nums) {
         int left = 0;
+        // right pointer will find all 0's and move them to the left
         for (int right = 0; right < nums.length; right++) {
             if (nums[right] == 0) {
                 swap(nums, left, right);
@@ -8,6 +9,7 @@ class Solution {
             }
         }
 
+        // right pointer will find all 1's and move them to the left
         for (int right = left; right < nums.length; right++) {
             if (nums[right] == 1) {
                 swap(nums, left, right);
