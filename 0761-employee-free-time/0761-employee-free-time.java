5cd8323908e40b28e38383baea29dbefd6055759
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-05 16:16:32 -0700

    Time: 18 ms (19.39%), Space: 45.9 MB (65.17%) - LeetHub

---
+++ 0761-employee-free-time/0761-employee-free-time.java
@@ -0,0 +1,45 @@
+/*
+// Definition for an Interval.
+class Interval {
+    public int start;
+    public int end;
+
+    public Interval() {}
+
+    public Interval(int _start, int _end) {
+        start = _start;
+        end = _end;
+    }
+};
+*/
+
+class Solution {
+    public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
+        Queue<Integer> startQueue = new PriorityQueue<>();
+        Queue<Integer> endQueue = new PriorityQueue<>();
+
+        for (List<Interval> s: schedule) {
+            for (Interval i: s) {
+                int start = i.start;
+                int end = i.end;
+
+                startQueue.add(start);
+                endQueue.add(end);
+            }   
+        }
+
+        List<Interval> res = new ArrayList<>();
+        startQueue.poll();
+        while (!startQueue.isEmpty()) {
+            if (endQueue.peek() < startQueue.peek()) {
+                res.add(new Interval(endQueue.poll(), startQueue.poll()));
+            } else {
+                startQueue.poll();
+                endQueue.poll();
+            }
+        }
+
+        return res;
+
+    }
+}
