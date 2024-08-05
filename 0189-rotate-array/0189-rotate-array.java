7bab8f673b97d25bcf35056ee198c81ba6c588d4
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-05 14:07:37 -0700

    Time: 0 ms (100%), Space: 57.2 MB (59.77%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,9 +1,12 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         k = k % nums.length;
-        
+
+        // 反转整个字符串
         reverse(nums, 0, nums.length - 1);
+        // 反转 前 k-1 的子串
         reverse(nums, 0, k - 1);
+        // 反转 k 到末尾的子串
         reverse(nums, k, nums.length - 1);
     }
 
