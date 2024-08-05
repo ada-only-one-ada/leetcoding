b0d819edc7fbcbf19d8e92d3134e2e739c2c988f
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-05 15:22:30 -0700

    Time: 25 ms (47.96%), Space: 46.4 MB (52.95%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -2,17 +2,21 @@ class Solution {
     public int subarraySum(int[] nums, int k) {
         int count = 0;
         Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1); 
+        // [1,1,1],
+        // prefixSum分别是 1，2，3
+        // 要找 -1，0，1 （要减去的前缀和区间）
+        // 比如当前prefixSum是3，我们要去掉前缀和为1的区间
 
         int prefixSum = 0;
         for (int i = 0; i < nums.length; i++) {
-            map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
             prefixSum += nums[i];
 
             if (map.containsKey(prefixSum - k)) {
                 count += map.get(prefixSum - k);
             }
 
-            
+            map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
         }
 
         return count;
