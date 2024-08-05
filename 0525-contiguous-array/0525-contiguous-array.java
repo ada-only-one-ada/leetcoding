520ff1eb59d5a37c5825248c1cc1924977fb1f04
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-05 15:03:55 -0700

    Time: 23 ms (83.39%), Space: 54.9 MB (65.48%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.java
+++ 0525-contiguous-array/0525-contiguous-array.java
@@ -4,7 +4,7 @@ class Solution {
         map.put(0, -1); // [0, 1, 1], [0, 1]
         int maxLen = 0;
 
-        
+        // [2, xxxxx, 2]: prefix 为 2 出现了两次，那么中间肯定是有0有1数量相等抵消了
         int prefixSum = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == 0) {
