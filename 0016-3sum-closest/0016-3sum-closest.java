3b6005afb614d96c54458743d1d279515db5742f
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-06 11:03:11 -0700

    Time: 18 ms (13.32%), Space: 42.8 MB (90.4%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -2,24 +2,32 @@ class Solution {
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
         int res = Integer.MAX_VALUE;
+        int minDiff = Integer.MAX_VALUE;
 
         int i1 = 0;
         while (i1 <= nums.length - 3) {
             int i2 = i1 + 1;
             int i3 = nums.length - 1;
-
-            while (i2 < i3) {
+            while (i2 <= nums.length - 2 && i2 < i3) {
                 int sum = nums[i1] + nums[i2] + nums[i3];
-                if (Math.abs(sum - target) < Math.abs(res - target)) {
-                    res = sum;
-                } 
-
-                if (sum > target) {
-                    i3--;
-                    while (i2 < i3 && nums[i3] == nums[i3 + 1]) i3--;
-                } else {
+                if (sum == target) {
+                    return sum;
+                } else if (sum < target) {
+                    int diff = Math.abs(sum - target);
+                    if (diff < minDiff) {
+                        minDiff = diff;
+                        res = sum;
+                    }
                     i2++;
                     while (i2 < i3 && nums[i2] == nums[i2 - 1]) i2++;
+                } else if (sum > target) {
+                    int diff = Math.abs(sum - target);
+                    if (diff < minDiff) {
+                        minDiff = diff;
+                        res = sum;
+                    }
+                    i3--;
+                    while (i2 < i3 && nums[i3] == nums[i3 + 1]) i3--;
                 }
             }
 
