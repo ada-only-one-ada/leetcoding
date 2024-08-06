13679761ce56638d30e94657627154d3c900a871
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-06 15:24:58 -0700

    Time: 1 ms (100%), Space: 42.2 MB (76.65%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -6,6 +6,7 @@ class Solution {
         while (i < s.length() && s.charAt(i) == ' ') {
             i++;
         }
+
         if (i == s.length()) return 0;
 
         int sign = 1;
@@ -18,14 +19,14 @@ class Solution {
 
         int MAX = Integer.MAX_VALUE;
         int MIN = Integer.MIN_VALUE;
-       
         int res = 0;
+
         while (i < s.length()) {
             int curr = s.charAt(i) - '0';
-            if (curr < 0 || curr > 9) break;
+            if (curr < 0 || curr > 9) break; // "1337c0d3" return 1337
 
             if (res > MAX / 10 || (res == MAX / 10 && curr > MAX % 10)) {
-                return sign == 1? MAX: MIN;
+                return sign == 1? MAX : MIN;
             }
 
             res = res * 10 + curr;
