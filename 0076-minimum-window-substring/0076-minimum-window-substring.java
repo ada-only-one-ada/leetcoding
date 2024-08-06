b6072c979649b7cab007195d69962bac0af2f519
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-06 14:52:34 -0700

    Time: 14 ms (67.75%), Space: 45 MB (49.01%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,47 +1,38 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int minLen = s.length() + 1;
-        int start = 0;
-        int end = s.length() - 1;
-
         Map<Character, Integer> map = new HashMap<>();
         for (char c: t.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
 
         int toFind = map.size();
+        int start = 0;
+        int end = s.length() + 1;
 
         int left = 0;
         for (int right = 0; right < s.length(); right++) {
             char c = s.charAt(right);
+            map.put(c, map.getOrDefault(c, 0) - 1);
 
-            if (map.containsKey(c)) {
-                map.put(c, map.get(c) - 1);
-                
-                if (map.get(c) == 0) {
-                    toFind--;
-                }
+            if (map.get(c) == 0) {
+                toFind--;
             }
 
             while (toFind == 0) {
-                if (right - left + 1 < minLen) {
-                    minLen = right - left + 1;
+                if (right - left < end - start) {
                     start = left;
                     end = right;
                 }
-                
-                if (map.containsKey(s.charAt(left))) {
-                    if (map.get(s.charAt(left)) == 0) {
-                        toFind++;
-                    }
 
-                    map.put(s.charAt(left), map.get(s.charAt(left)) + 1);    
+                if (map.get(s.charAt(left)) == 0) {
+                    toFind++;
                 }
+
+                map.put(s.charAt(left), map.get(s.charAt(left)) + 1);
                 left++;
-            }
+            }            
         }
 
-        return minLen == s.length() + 1? "" : s.substring(start, end + 1);
-    
+        return end == s.length() + 1? "" :s.substring(start, end + 1);
     }
 }
