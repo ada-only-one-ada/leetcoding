20dcbfbf31c291bd0d597745338fd1bbc2aa0622
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-06 11:26:45 -0700

    Time: 61 ms (16.8%), Space: 73.2 MB (79.63%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -15,7 +15,9 @@ class Solution {
             int currEnd = intervals[i][1];
 
             if (currStart < prevEnd) {
+                // 有重叠，必然得去掉一个，所以count++
                 count++;          
+                // 如果当前这个更早结束，那就去掉之前那个，因为晚结束的可能会覆盖到后面的
                 prevEnd = Math.min(prevEnd, currEnd);
             } else {
                 prevEnd = currEnd;
