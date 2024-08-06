222b3e2bcd7da8314c9e9d3c22c54fb7ee01f39c
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-06 15:01:24 -0700

    Time: 25 ms (28.46%), Space: 45.4 MB (22.81%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,38 +1,45 @@
 class Solution {
     public String minWindow(String s, String t) {
+        int start = -1;
+        int end = -1;
+        int minLen = s.length() + 1;
+      
         Map<Character, Integer> map = new HashMap<>();
-        for (char c: t.toCharArray()) {
+        for (char c : t.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
 
-        int toFind = map.size();
-        int start = 0;
-        int end = s.length() + 1;
+        int found = 0;
+        Map<Character, Integer> count = new HashMap<>();
 
         int left = 0;
         for (int right = 0; right < s.length(); right++) {
             char c = s.charAt(right);
-            map.put(c, map.getOrDefault(c, 0) - 1);
+            count.put(c, count.getOrDefault(c, 0) + 1);
 
-            if (map.get(c) == 0) {
-                toFind--;
+            // wrong: if (map.containsKey(curr) && count.get(curr) == map.get(curr))  can't use == here.
+            //if (map.containsKey(curr) && count.get(curr).intValue() == map.get(curr).intValue())
+            if (map.containsKey(c) && count.get(c).equals(map.get(c))) {
+                found++;
             }
 
-            while (toFind == 0) {
-                if (right - left < end - start) {
+            while (found == map.size()) {
+                if (right - left + 1 < minLen) {
                     start = left;
                     end = right;
+                    minLen = right - left + 1;
                 }
 
-                if (map.get(s.charAt(left)) == 0) {
-                    toFind++;
+                char remove = s.charAt(left);
+                if (map.containsKey(remove) && count.get(remove).equals(map.get(remove))) {
+                    found--;
                 }
-
-                map.put(s.charAt(left), map.get(s.charAt(left)) + 1);
+                
+                count.put(remove, count.get(remove) - 1);
                 left++;
-            }            
+            }
         }
 
-        return end == s.length() + 1? "" :s.substring(start, end + 1);
+        return start == -1? "" : s.substring(start, end + 1);
     }
 }
