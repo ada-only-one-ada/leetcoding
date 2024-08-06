5b8ef276f14a9d33c92f8d678fd9f07fb995f837
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-06 12:13:28 -0700

    Time: 9 ms (21.44%), Space: 43.7 MB (85.12%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,20 +1,23 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        // s consists of English letters, digits, symbols and spaces.
-        int[] freq = new int[128];
-        int res = 0;
-
+        int maxLen = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        
         int left = 0;
-        for (int right = 0; right < s.length(); right++){
-            while (freq[s.charAt(right)] == 1){
-                freq[s.charAt(left)]--;
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+
+            while (map.containsKey(c) && map.get(c) == 1) {
+                map.put(s.charAt(left), map.get(s.charAt(left)) - 1);
+                //if (map.get(s.charAt(left)) == 0) map.remove(s.charAt(left));
                 left++;
             }
 
-            freq[s.charAt(right)]++;
-            res = Math.max(res, right-left+1);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+            // here no need to set (right - left + 1 == map.size() since it could be key: 0
+            maxLen = Math.max(maxLen, right - left + 1);
         }
-
-        return res;
+        
+        return maxLen;
     }
 }
