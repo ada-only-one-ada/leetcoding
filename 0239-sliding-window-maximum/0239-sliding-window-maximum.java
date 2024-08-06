2d4740a4c5b8790fd0992f65f1b3c7e08f11030b
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-06 10:46:04 -0700

    Time: 32 ms (50.41%), Space: 62.2 MB (20.39%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -3,25 +3,23 @@ class Solution {
         int[] res = new int[nums.length - k + 1];
         int index = 0;
 
-        Queue<int[]> queue = new PriorityQueue<>((a, b) -> (b[1] - a[1]));
+        Deque<int[]> queue = new ArrayDeque<>();
+
         for (int i = 0; i < nums.length; i++) {
-            // 前面的out of index的数字刚好是最大时，要remove掉
             while (!queue.isEmpty() && i - queue.peek()[0] >= k) {
                 queue.poll();
             }
-            
-            // 当前的数字反而更大，且当前的index是更大的，所以前面的那个更小的永远不会再成为答案了
-            while (!queue.isEmpty() && nums[i] > queue.peek()[1]) {
-                queue.poll();
-            }
+
+            while (!queue.isEmpty() && nums[i] > queue.getLast()[1]) {
+                queue.removeLast();
+            }  
 
             queue.add(new int[]{i, nums[i]});
-
-            if (i >= k - 1) {
-                res[index++] = queue.peek()[1];
+            if (i >= k-1) {
+                res[index++] = queue.getFirst()[1];
             }
         }
-        
+
         return res;
     }
 }
