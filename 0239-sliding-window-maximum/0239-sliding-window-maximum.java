c9cbd019729122f1e93e62d8824cd0494510cc49
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-06 08:26:48 -0700

    Time: 149 ms (9.24%), Space: 59.1 MB (48.97%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -3,20 +3,20 @@ class Solution {
         int[] res = new int[nums.length - k + 1];
         int index = 0;
 
-        Queue<int[]> queue = new PriorityQueue<>((a, b) -> (b[0] - a[0]));
+        Queue<int[]> queue = new PriorityQueue<>((a, b) -> (b[1] - a[1]));
         for (int i = 0; i < nums.length; i++) {
-            while (!queue.isEmpty() && queue.peek()[1] + k <= i) {
+            while (!queue.isEmpty() && queue.peek()[0] + k <= i) {
                 queue.poll();
             }
 
-            while (!queue.isEmpty() && nums[i] > queue.peek()[0]) {
+            while (!queue.isEmpty() && nums[i] > queue.peek()[1]) {
                 queue.poll();
             }
 
-            queue.add(new int[]{nums[i], i});
+            queue.add(new int[]{i, nums[i]});
 
             if (i >= k - 1) {
-                res[index++] = queue.peek()[0];
+                res[index++] = queue.peek()[1];
             }
         }
         
