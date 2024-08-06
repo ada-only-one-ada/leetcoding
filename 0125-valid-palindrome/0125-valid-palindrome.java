16698a0a042b1aaf9c67ca650a43636ce5999988
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-06 11:32:50 -0700

    Time: 3 ms (73.32%), Space: 42.6 MB (93.76%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            while (left < right && (!Character.isLetter(s.charAt(left)) && !Character.isDigit(s.charAt(left)))) {
+                left++;
+            }
+
+            while (left < right && (!Character.isLetter(s.charAt(right)) && !Character.isDigit(s.charAt(right)))) {
+                right--;
+            }
+
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            } 
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
