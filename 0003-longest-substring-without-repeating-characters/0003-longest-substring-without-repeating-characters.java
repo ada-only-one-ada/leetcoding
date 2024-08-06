db7c87627f881763214b21c2b2ff92aceaaca5d4
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-06 12:14:15 -0700

    Time: 8 ms (25.46%), Space: 43.6 MB (86.67%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -9,12 +9,10 @@ class Solution {
 
             while (map.containsKey(c) && map.get(c) == 1) {
                 map.put(s.charAt(left), map.get(s.charAt(left)) - 1);
-                //if (map.get(s.charAt(left)) == 0) map.remove(s.charAt(left));
                 left++;
             }
 
             map.put(c, map.getOrDefault(c, 0) + 1);
-            // here no need to set (right - left + 1 == map.size() since it could be key: 0
             maxLen = Math.max(maxLen, right - left + 1);
         }
         
