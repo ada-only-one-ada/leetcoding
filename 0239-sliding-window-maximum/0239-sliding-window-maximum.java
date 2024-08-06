9766859c1e58acb34539fc36e8634f62b9d59ec1
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-06 10:43:19 -0700

    Time: 150 ms (9.23%), Space: 59.2 MB (48.97%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -5,10 +5,12 @@ class Solution {
 
         Queue<int[]> queue = new PriorityQueue<>((a, b) -> (b[1] - a[1]));
         for (int i = 0; i < nums.length; i++) {
+            // 前面的out of index的数字刚好是最大时，要remove掉
             while (!queue.isEmpty() && i - queue.peek()[0] >= k) {
                 queue.poll();
             }
-
+            
+            // 当前的数字反而更大，且当前的index是更大的，所以前面的那个更小的永远不会再成为答案了
             while (!queue.isEmpty() && nums[i] > queue.peek()[1]) {
                 queue.poll();
             }
