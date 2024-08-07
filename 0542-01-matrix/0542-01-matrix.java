e26d1c18047e99c3b60a5c91b522f71d00338a6c
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-07 13:15:14 -0700

    Time: 8 ms (92.91%), Space: 48.2 MB (10.82%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,38 +1,46 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        int rowNum = mat.length;
-        int colNum = mat[0].length;
-
+        int[][] res = new int[mat.length][mat[0].length];
         for (int row = 0; row < mat.length; row++) {
             for (int col = 0; col < mat[0].length; col++) {
-                if (mat[row][col] == 0) continue;
-  
-                int minDis = Integer.MAX_VALUE;
-                if (row > 0) minDis = Math.min(minDis, mat[row - 1][col]);
-                if (col > 0) minDis = Math.min(minDis, mat[row][col - 1]);
-
-                if (minDis != Integer.MAX_VALUE) {
-                    mat[row][col] = 1 + minDis;
-                } else {
-                    mat[row][col] = Integer.MAX_VALUE;
+                // 如果等於1，说明我们要去求这个cell到最近的一个0的cell的距离，先把距离设置成一个无限大的值
+                if (mat[row][col] == 1) {
+                    // use Integer.MAX_VALUE then add one will cause overflow, we can use Integer.MAX_VALUE - 1
+                    res[row][col] = mat.length * mat[0].length + 1;
                 }
             }
         }
 
-        for (int row = rowNum - 1; row >= 0; row--) {
-            for (int col = colNum - 1; col >= 0; col--) {
-                if (mat[row][col] == 0) continue;
-
-                int minDis = mat[row][col];
-                if (row < rowNum - 1) minDis = Math.min(minDis, mat[row + 1][col]);
-                if (col < colNum - 1) minDis = Math.min(minDis, mat[row][col + 1]);
-
+        // 起点在left - top
+        for (int row = 0; row < res.length; row++) {
+            for (int col = 0; col < res[0].length; col++) {
+                // 是0的话表示这个位置本身是0，不用求
+                if (res[row][col] == 0) continue;
                 
-                    mat[row][col] = Math.min(1 + minDis, mat[row][col]);
-                
+                // 否则求距离，初始距离是剛才設置的无限大的一个值
+                int distance = res[row][col];
+                if (row > 0) distance = Math.min(distance, res[row - 1][col]); // upper
+                if (col > 0) distance = Math.min(distance, res[row][col - 1]); // left
+                // 从无限大 -> update到另一个更小的距离 （到达上一个点所需要的距离，再加1个）
+                res[row][col] = 1 + distance;
             }
         }
 
-        return mat;
+        // 起点在bottom - right
+        for (int row = res.length - 1; row >= 0; row--) {
+            for (int col = res[0].length - 1; col >= 0; col--) {
+                if (res[row][col] == 0) continue;
+
+                // 这个距离不是无限大的那个，是上一次loop中更新的距离
+                int distance = res[row][col];
+                if (row < res.length - 1) distance = Math.min(distance, res[row + 1][col]);
+                if (col < res[0].length - 1) distance = Math.min(distance, res[row][col + 1]);
+
+                // 比较左上出发快还是右下出发快
+                res[row][col] = Math.min(res[row][col] , 1 + distance);
+            }
+        }
+        
+        return res;
     }
 }
