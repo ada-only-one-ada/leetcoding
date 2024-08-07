e3097b17147c8685ddb4a757770e1cac1cef8e9e
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-07 16:56:51 -0700

    Time: 1 ms (98.62%), Space: 44.3 MB (95.94%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if (ransomNote.length() > magazine.length()) return false;
+
+        int[] freq = new int[26];
+        for (char c: ransomNote.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        for (char c: magazine.toCharArray()) {
+            freq[c - 'a']--;
+        }
+
+        for (int f: freq) {
+            if (f > 0) return false;
+        }
+
+        return true;
+    }
+}
