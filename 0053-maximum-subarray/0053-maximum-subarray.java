9a6ba26b589446b89ccf74273730d35c554c7d35
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-07 11:49:26 -0700

    Time: 2 ms (6.29%), Space: 58 MB (5.09%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,13 +1,15 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int[] dp = new int[nums.length];
-        
-        int maxSum = nums[0];
-        dp[0] = nums[0]; 
-        
+        /* dp[i]: 以nums[i]为结尾的maximum subarray
+           情况1: 延续前面的和，加上当前num，dp[i] = dp[i-1] + num
+           情况2: 前面的和都不要了，从当前num开始计算，dp[i] = num
+        */ 
+
+        int[] dp = new int[nums.length + 1];
+        int maxSum = Integer.MIN_VALUE;
+
         for (int i = 1; i < dp.length; i++) {
-            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
-            
+            dp[i] = Math.max(dp[i - 1] + nums[i - 1], nums[i - 1]);
             maxSum = Math.max(maxSum, dp[i]);
         }
 
