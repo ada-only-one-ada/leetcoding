921f834404a993b25d1061f6648cbf1d915002d8
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-08 11:52:17 -0700

    Time: 0 ms (100%), Space: 40.8 MB (98.54%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -4,9 +4,14 @@ class Solution {
 
         for (int i = 1; i < strs.length; i++) {
             String s = strs[i];
-            // 这里不是-1，比如c和acc没有公共前缀
+            /* 
+               这里不是-1，比如c和acc没有公共前缀
+               ["c","acc","ccc"], not return "c" since it is not prefix!
+               should not be -1, should be 0!
+            */
             while (s.indexOf(candidate) != 0) {
                 candidate = candidate.substring(0, candidate.length() - 1);
+                if (candidate.length() == 0) break;
             }
         }
 
