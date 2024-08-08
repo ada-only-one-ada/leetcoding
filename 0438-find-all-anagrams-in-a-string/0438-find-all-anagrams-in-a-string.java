91932c0d62290871e781239a4469f71472c8aae3
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-08 15:14:33 -0700

    Time: 28 ms (45.05%), Space: 44.5 MB (90%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,29 +1,31 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> res = new ArrayList<>();
-        if (p.length() > s.length()) return res;
 
         Map<Character, Integer> map = new HashMap<>();
-        for (char c : p.toCharArray()) {
+        for (char c: p.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
+        int pCharNum = map.size();
+        int toFind = map.size();
 
-        int required = p.length();
         int left = 0;
         for (int right = 0; right < s.length(); right++) {
-            char curr = s.charAt(right);
-            if (map.containsKey(curr) && map.get(curr) >= 1) {
-                required--;
+            char c = s.charAt(right);
+            map.put(c, map.getOrDefault(c, 0) - 1);
+         
+            if (map.get(c) == 0) {
+                toFind--;
             }
-            map.put(curr, map.getOrDefault(curr, 0) - 1);
 
-            if (right - left + 1 == p.length()) {
-                if (required == 0) res.add(left);
+            while (right - left + 1 == p.length()) {
+                if (toFind == 0 ) {
+                    res.add(left);
+                }
 
-                char remove = s.charAt(left);
-                if (map.get(remove) >= 0) required++;
-
-                map.put(remove, map.get(remove) + 1);
+                if (map.get(s.charAt(left)) == 0) toFind++;
+                map.put(s.charAt(left), map.get(s.charAt(left)) + 1);
+                
                 left++;
             }
         }
