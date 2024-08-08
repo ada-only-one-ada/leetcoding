01cf54b0082e6f76f4c87a6f7573f7b6fe8fa116
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-08 11:49:23 -0700

    Time: 0 ms (100%), Space: 41.5 MB (40.52%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -4,6 +4,7 @@ class Solution {
 
         for (int i = 1; i < strs.length; i++) {
             String s = strs[i];
+            // 这里不是-1，比如c和acc没有公共前缀
             while (s.indexOf(candidate) != 0) {
                 candidate = candidate.substring(0, candidate.length() - 1);
             }
