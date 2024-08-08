d8d4868080ae533770e26287e38a4c3a81278221
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-08 11:53:19 -0700

    Time: 0 ms (100%), Space: 40.9 MB (98.54%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,20 +1,14 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        String candidate = strs[0];
-
-        for (int i = 1; i < strs.length; i++) {
-            String s = strs[i];
-            /* 
-               这里不是-1，比如c和acc没有公共前缀
-               ["c","acc","ccc"], not return "c" since it is not prefix!
-               should not be -1, should be 0!
-            */
-            while (s.indexOf(candidate) != 0) {
-                candidate = candidate.substring(0, candidate.length() - 1);
-                if (candidate.length() == 0) break;
-            }
+        Arrays.sort(strs);
+      
+        String first = strs[0];
+        String last = strs[strs.length - 1];
+        
+        while (last.indexOf(first) != 0){
+            first = first.substring(0, first.length() - 1);
         }
-
-        return candidate;
+        
+        return first;
     }
 }
