a2396e7aaa6c3745a815d01ae4c018ed4b91f401
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-15 15:06:10 -0700

    Time: 2 ms (9.94%), Space: 44.6 MB (27.53%) - LeetHub

--- 0545-boundary-of-binary-tree/0545-boundary-of-binary-tree.java
+++ 0545-boundary-of-binary-tree/0545-boundary-of-binary-tree.java
@@ -2,18 +2,19 @@ class Solution {
     public List<Integer> boundaryOfBinaryTree(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         if (root == null) return res;
-        if (!isLeaf(root)) res.add(root.val);
+        if (!isLeaf(root)) res.add(root.val); // 根节点先加
 
-        addLeftBoundary(root.left, res);
-        addLeave(root, res);
-        addRightBoundary(root.right, res);
-        return res;
+        addLeftBoundary(root.left, res); // 加入左边界list
+        addLeave(root, res); // 加入叶子边界list
+        addRightBoundary(root.right, res); // 加入右边界list
+        return res; 
     }
 
     public boolean isLeaf(TreeNode node) {
         return node != null && node.left == null && node.right == null;
     }
 
+    // pre-order：根-左-右
     public void addLeftBoundary(TreeNode node, List<Integer> res) {
         while (node != null) {
             if (!isLeaf(node)) res.add(node.val);
@@ -26,6 +27,7 @@ class Solution {
         }
     }
 
+    // pre-order：根-右-左，先找到的后加入，逆序
     public void addRightBoundary(TreeNode node, List<Integer> res) {
         List<Integer> reverseList = new ArrayList<>();
         while (node != null) {
@@ -43,6 +45,7 @@ class Solution {
         }
     }
 
+    // pre-order：根-左-右
     public void addLeave(TreeNode node, List<Integer> res) {
         if (node == null) return;
         if (isLeaf(node)) res.add(node.val);
