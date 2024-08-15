7aca4c0f4f1e383a01db09e84ec4eead4b7b17d6
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-15 15:04:08 -0700

    Time: 2 ms (9.94%), Space: 44.5 MB (62.03%) - LeetHub

--- 0545-boundary-of-binary-tree/0545-boundary-of-binary-tree.java
+++ 0545-boundary-of-binary-tree/0545-boundary-of-binary-tree.java
@@ -2,10 +2,10 @@ class Solution {
     public List<Integer> boundaryOfBinaryTree(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         if (root == null) return res;
-
         if (!isLeaf(root)) res.add(root.val);
+
         addLeftBoundary(root.left, res);
-        addLeaf(root, res);
+        addLeave(root, res);
         addRightBoundary(root.right, res);
         return res;
     }
@@ -14,12 +14,9 @@ class Solution {
         return node != null && node.left == null && node.right == null;
     }
 
-    // pre-order traversal: root-left-right
     public void addLeftBoundary(TreeNode node, List<Integer> res) {
         while (node != null) {
-            if (!isLeaf(node)) {
-                res.add(node.val);
-            }
+            if (!isLeaf(node)) res.add(node.val);
 
             if (node.left != null) {
                 node = node.left;
@@ -29,25 +26,10 @@ class Solution {
         }
     }
 
-    // pre-order traversal: root-left-right
-    public void addLeaf(TreeNode node, List<Integer> res) {
-        if (node == null) return;
-
-        if (isLeaf(node)) {
-            res.add(node.val);
-        }
-
-        addLeaf(node.left, res);
-        addLeaf(node.right, res);
-    }    
-
-    // pre-order traversal: root-right-left
     public void addRightBoundary(TreeNode node, List<Integer> res) {
-        List<Integer> temp = new ArrayList<>();
+        List<Integer> reverseList = new ArrayList<>();
         while (node != null) {
-            if (!isLeaf(node)) {
-                temp.add(node.val);
-            }
+            if (!isLeaf(node)) reverseList.add(0, node.val);
 
             if (node.right != null) {
                 node = node.right;
@@ -56,9 +38,16 @@ class Solution {
             }
         }
 
-        // 先找到的后加入res，所以是reverse加。注意这里不是index 0的位置，可能res前面已经有了其他left，leaf元素了
-        for (int i = temp.size() - 1; i >= 0; i--) {
-            res.add(temp.get(i));
+        for (int val: reverseList) {
+            res.add(val);
         }
+    }
+
+    public void addLeave(TreeNode node, List<Integer> res) {
+        if (node == null) return;
+        if (isLeaf(node)) res.add(node.val);
+
+        addLeave(node.left, res);
+        addLeave(node.right, res);   
     }
 }
