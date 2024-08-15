2a881bd748522617b966197258c2e81d4b288619
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-15 14:35:25 -0700

    Time: 1 ms (74.75%), Space: 44.4 MB (78.73%) - LeetHub

--- 0545-boundary-of-binary-tree/0545-boundary-of-binary-tree.java
+++ 0545-boundary-of-binary-tree/0545-boundary-of-binary-tree.java
@@ -1,52 +1,62 @@
 class Solution {
     public List<Integer> boundaryOfBinaryTree(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        if (root == null) return result;
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
 
-        // Add the root value if it's not a leaf node.
-        if (!isLeaf(root)) result.add(root.val);
+        if (!isLeafNode(root)) res.add(root.val);
 
-        // Add the left boundary (excluding leaf and root).
-        addLeftBoundary(root.left, result);
-
-        // Add leaf nodes.
-        addLeaves(root, result);
-
-        // Add the right boundary (excluding leaf and root) in reverse.
-        addRightBoundary(root.right, result);
-
-        return result;
+        addLeftBoundary(root.left, res);
+        addLeafBoundary(root, res);
+        addRightBoundary(root.right, res);
+        return res;
     }
 
-    private void addLeftBoundary(TreeNode node, List<Integer> res) {
-        while (node != null) {
-            if (!isLeaf(node)) res.add(node.val);
-            if (node.left != null) node = node.left;
-            else node = node.right;
-        }
+    public boolean isLeafNode(TreeNode node) {
+        return node != null && node.left == null && node.right == null;
     }
 
-    private void addLeaves(TreeNode node, List<Integer> res) {
+    // pre-order traversal: root-left-right
+    public void addLeafBoundary(TreeNode node, List<Integer> res) {
         if (node == null) return;
-        if (isLeaf(node)) res.add(node.val);
-        addLeaves(node.left, res);
-        addLeaves(node.right, res);
-    }
 
-    private void addRightBoundary(TreeNode node, List<Integer> res) {
-        List<Integer> tmp = new ArrayList<>();
+        if (isLeafNode(node)) {
+            res.add(node.val);
+        }
+
+        addLeafBoundary(node.left, res);
+        addLeafBoundary(node.right, res);
+    }    
+
+    public void addLeftBoundary(TreeNode node, List<Integer> res) {
         while (node != null) {
-            if (!isLeaf(node)) tmp.add(node.val);
-            if (node.right != null) node = node.right;
-            else node = node.left;
-        }
-        // Add in reverse order.
-        for (int i = tmp.size() - 1; i >= 0; i--) {
-            res.add(tmp.get(i));
+            if (!isLeafNode(node)) {
+                res.add(node.val);
+            }
+
+            if (node.left != null) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
         }
     }
 
-    private boolean isLeaf(TreeNode node) {
-        return (node.left == null && node.right == null);
+    public void addRightBoundary(TreeNode node, List<Integer> res) {
+        List<Integer> temp = new ArrayList<>();
+        while (node != null) {
+            if (!isLeafNode(node)) {
+                temp.add(node.val);
+            }
+
+            if (node.right != null) {
+                node = node.right;
+            } else {
+                node = node.left;
+            }
+        }
+
+        for (int i = temp.size() - 1; i >= 0; i--) {
+            res.add(temp.get(i));
+        }
     }
 }
