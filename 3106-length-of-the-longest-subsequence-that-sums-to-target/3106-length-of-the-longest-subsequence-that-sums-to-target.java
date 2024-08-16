dd5857a7115f10e559aa0eb5fbcf7e9d530384bc
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-16 11:27:01 -0700

    Time: 133 ms (73.39%), Space: 55 MB (46.34%) - LeetHub

--- 3106-length-of-the-longest-subsequence-that-sums-to-target/3106-length-of-the-longest-subsequence-that-sums-to-target.java
+++ 3106-length-of-the-longest-subsequence-that-sums-to-target/3106-length-of-the-longest-subsequence-that-sums-to-target.java
@@ -3,16 +3,21 @@ class Solution {
         if (nums.size() == 0) return -1;
 
         int[][] dp = new int[nums.size() + 1][target + 1];
+        // 前0个元素，不可能有等于target和的序列
         Arrays.fill(dp[0], -1);
+        // 除非target和就等于0
         dp[0][0] = 0;
 
         for (int i = 1; i <= nums.size(); i++) {
             for (int currTarget = 0; currTarget <= target; currTarget++) {
+                // 当前元素
                 int num = nums.get(i - 1);
+        
+                // 1. 不拿当前元素
                 dp[i][currTarget] = dp[i - 1][currTarget];
-
+                // 2. 拿当前元素 (满足条件时), 也有可能不是最长的，所以要考虑不拿的时候最长
                 if (currTarget >= num && dp[i - 1][currTarget - num] != -1) {
-                    dp[i][currTarget] = Math.max(dp[i - 1][currTarget], dp[i - 1][currTarget - num] + 1);
+                    dp[i][currTarget] = Math.max(dp[i][currTarget], dp[i - 1][currTarget - num] + 1);
                 }
             }
         }
