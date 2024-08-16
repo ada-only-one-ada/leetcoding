a77e06f42f821fb3caf555889edced134e0ace5d
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-16 11:36:46 -0700

    Time: 26 ms (88.91%), Space: 44.2 MB (96.45%) - LeetHub

--- 3106-length-of-the-longest-subsequence-that-sums-to-target/3106-length-of-the-longest-subsequence-that-sums-to-target.java
+++ 3106-length-of-the-longest-subsequence-that-sums-to-target/3106-length-of-the-longest-subsequence-that-sums-to-target.java
@@ -3,11 +3,15 @@ class Solution {
         if (nums.size() == 0) return -1;
 
         int[] dp = new int[target + 1];
+        // Initial fill with -1, indicating unreachable states
         Arrays.fill(dp, -1);
+        // Base case: a sum of 0 is achievable with 0 elements
         dp[0] = 0;
 
         for (int num: nums) {
-            for (int currTarget = target; currTarget >= num; currTarget--) {        
+            // Reverse to avoid reuse in the same item iteration
+            for (int currTarget = target; currTarget >= num; currTarget--) {    
+                // Check if the (currTarget - num) is achievable    
                 if (dp[currTarget - num] != -1) {
                     dp[currTarget] = Math.max(dp[currTarget], dp[currTarget - num] + 1);
                 }
