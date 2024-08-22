3b48c3eec40c6b1e24cf542aa1ebd7c1c209f3ce
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-22 14:00:10 -0700

    Time: 23 ms (57.91%), Space: 62 MB (36.43%) - LeetHub

--- 3231-minimum-number-of-coins-to-be-added/3231-minimum-number-of-coins-to-be-added.java
+++ 3231-minimum-number-of-coins-to-be-added/3231-minimum-number-of-coins-to-be-added.java
@@ -1,25 +1,28 @@
 class Solution {
     public int minimumAddedCoins(int[] coins, int target) {
-        int count = 0; 
-        double currSum = 0;
-        Arrays.sort(coins); // 对可用硬币排序，确保从小到大处理硬币
-        int coinIndex = 0;
+        Arrays.sort(coins); // 将硬币面值数组 coins 升序排序
+        int count = 0; // 记录需要添加到数组中的硬币数量
+        int currentSum = 0; // 当前可取得的最大金额
 
-        // 对每个从 1 到 target 的目标金额 currTarget 进行迭代，检查是否能用现有硬币凑成该金额
-        for (int currTarget = 1; currTarget <= target; currTarget++) {
-            // 对于当前目标金额，检查是否可以用现有硬币 加上 之前的总和 来达到或者超过这个目标
-            // 只要还有可用的硬币，且当前硬币值 小于 当前目标值，就可以拿来用
-            while (coinIndex < coins.length && coins[coinIndex] <= currTarget) {
-                currSum += coins[coinIndex];
-                coinIndex++;
-            }
-
-            // 如果当前所有可用硬币加起来，都小于当前目标值，那么就是凑不到当前目标
-            if (currSum < currTarget) {
-                // 添加一个面值为 currTarget的硬币
-                currSum += currTarget;
+        for (int coin : coins) {
+            while (currentSum + 1 < coin && currentSum < target) {
+                // 如果当前可取得的最大金额加1小于当前硬币面值且小于目标金额
+                // 则需要添加硬币面值为 currentSum + 1 的硬币
+                currentSum += currentSum + 1;
                 count++;
             }
+            // 更新当前可取得的最大金额
+            currentSum += coin;
+            if (currentSum >= target) {
+                // 如果当前可取得的最大金额已经大于或等于目标金额，则无需继续遍历硬币
+                break;
+            }
+        }
+
+        // 继续添加硬币直到当前可取得的最大金额大于等于目标金额
+        while (currentSum < target) {
+            currentSum += currentSum + 1;
+            count++;
         }
 
         return count;
