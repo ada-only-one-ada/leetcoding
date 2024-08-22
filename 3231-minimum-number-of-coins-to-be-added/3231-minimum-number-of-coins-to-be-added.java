dc1445859a4003a0028b3354e6e2f9932ec06032
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-22 14:11:06 -0700

    Time: 23 ms (57.91%), Space: 61.6 MB (79.1%) - LeetHub

--- 3231-minimum-number-of-coins-to-be-added/3231-minimum-number-of-coins-to-be-added.java
+++ 3231-minimum-number-of-coins-to-be-added/3231-minimum-number-of-coins-to-be-added.java
@@ -5,15 +5,17 @@ class Solution {
         int currentSum = 0; // 当前可取得的最大金额
 
         for (int coin : coins) {
-            while (currentSum + 1 < coin) {
-                // 下一个要获取的 目标金额 是 currentSum + 1
-                // 如果当前硬币面值 小于 目标金额
-                // 则需要添加硬币面值为 currentSum + 1 的硬币
+            while (coin > currentSum + 1) {
+                // 要获取的 当前目标金额 是 currentSum + 1
+                // 如果当前硬币面值 大于 当前目标金额
+                // 说明 下一个目标金额，无法通过 currentSum 和当前硬币凑到，因为太大了
+                // 则需要添加硬币面值为 当前目标金额 的硬币
                 currentSum += currentSum + 1;
                 count++;
             }
-            // 更新当前可取得的最大金额
-            currentSum += coin;
+
+            currentSum += coin; // 更新当前可取得的最大金额
+            
             if (currentSum >= target) {
                 // 如果当前可取得的最大金额已经大于或等于目标金额，则无需继续遍历硬币
                 break;
