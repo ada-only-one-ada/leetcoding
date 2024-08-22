1a961cca897c8a6d659a98f744f86c28a8937703
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-22 14:08:11 -0700

    Time: 22 ms (90.28%), Space: 62.3 MB (18%) - LeetHub

--- 3231-minimum-number-of-coins-to-be-added/3231-minimum-number-of-coins-to-be-added.java
+++ 3231-minimum-number-of-coins-to-be-added/3231-minimum-number-of-coins-to-be-added.java
@@ -1,12 +1,13 @@
 class Solution {
     public int minimumAddedCoins(int[] coins, int target) {
         Arrays.sort(coins); // 将硬币面值数组 coins 升序排序
-        int count = 0; // 记录需要添加到数组中的硬币数量
+        int count = 0; // 记录需要添加的新硬币数量
         int currentSum = 0; // 当前可取得的最大金额
 
         for (int coin : coins) {
-            while (currentSum + 1 < coin && currentSum < target) {
-                // 如果当前可取得的最大金额加1小于当前硬币面值且小于目标金额
+            while (currentSum + 1 < coin) {
+                // 下一个要获取的 目标金额 是 currentSum + 1
+                // 如果当前硬币面值 小于 目标金额
                 // 则需要添加硬币面值为 currentSum + 1 的硬币
                 currentSum += currentSum + 1;
                 count++;
