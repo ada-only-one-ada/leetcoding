ef1c4dee184f77e2e61726b28246c9890792489b
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-22 14:54:45 -0700

    Time: 1 ms (75.08%), Space: 42.9 MB (44.83%) - LeetHub

--- 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.java
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.java
@@ -1,26 +1,23 @@
-import java.util.HashMap;
-import java.util.Map;
-
 public class Codec {
     private Map<String, String> map = new HashMap<>();
     private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     private int counter = 0;
+
+    // convert an integer ID to a base-62 encoded string
+    private String idToShortURL(int n) {
+        StringBuilder shortUrl = new StringBuilder();
+        while (n > 0) {
+            shortUrl.append(ALPHABET.charAt(n % 69));
+            n /= 69;
+        }
+        return shortUrl.reverse().toString();
+    }
 
     // Encodes a URL to a shortened URL.
     public String encode(String longUrl) {
         String shortUrl = idToShortURL(counter++); // Increment counter for each new URL
         map.put(shortUrl, longUrl);
         return shortUrl;
-    }
-
-    // Helper method to convert an integer ID to a base-62 encoded string
-    private String idToShortURL(int n) {
-        StringBuilder shortUrl = new StringBuilder();
-        while (n > 0) {
-            shortUrl.append(ALPHABET.charAt(n % 62));
-            n /= 62;
-        }
-        return shortUrl.reverse().toString();
     }
 
     // Decodes a shortened URL to its original URL.
