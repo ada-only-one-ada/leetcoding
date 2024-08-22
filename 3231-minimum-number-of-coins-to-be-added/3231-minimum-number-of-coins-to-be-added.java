aa16b21dc0fadeac85743e006bf33e4d444c73d3
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-22 13:48:37 -0700

    Time: 28 ms (5.23%), Space: 61.7 MB (79.1%) - LeetHub

--- 3231-minimum-number-of-coins-to-be-added/3231-minimum-number-of-coins-to-be-added.java
+++ 3231-minimum-number-of-coins-to-be-added/3231-minimum-number-of-coins-to-be-added.java
@@ -2,16 +2,21 @@ class Solution {
     public int minimumAddedCoins(int[] coins, int target) {
         int count = 0; 
         double currSum = 0;
-        Arrays.sort(coins); 
+        Arrays.sort(coins); // 对可用硬币排序，确保从小到大处理硬币
         int coinIndex = 0;
 
+        // 对每个从 1 到 target 的目标金额 currTarget 进行迭代，检查是否能用现有硬币凑成该金额
         for (int currTarget = 1; currTarget <= target; currTarget++) {
+            // 对于当前目标金额，检查是否可以用现有硬币 加上 之前的总和 来达到或者超过这个目标
+            // 只要还有可用的硬币，且当前硬币值 小于 当前目标值，就可以拿来用
             while (coinIndex < coins.length && coins[coinIndex] <= currTarget) {
                 currSum += coins[coinIndex];
                 coinIndex++;
             }
 
+            // 如果当前所有可用硬币加起来，都小于当前目标值，那么就是凑不到当前目标
             if (currSum < currTarget) {
+                // 添加一个面值为 currTarget的硬币
                 currSum += currTarget;
                 count++;
             }
