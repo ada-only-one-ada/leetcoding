b68b86d105c26ca0ac2eb36e7528e6b2ad536ac3
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-23 17:04:29 -0700

    Time: 171 ms (35.59%), Space: 44.8 MB (62.88%) - LeetHub

---
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int[] closestPrimes(int left, int right) {
+        if (right - left < 2) return new int[]{-1, -1};
+
+        boolean[] isPrime = new boolean[right + 1];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = false;
+        isPrime[1] = false;
+        
+        for (int num = 2; num <= right; num++) {
+            if (isPrime[num]) {
+                for (int multiple = 2; multiple * num <= right; multiple++) {
+                    isPrime[num * multiple] = false;
+                }
+            }
+        }
+
+        int firstPrime = left;
+        while (firstPrime <= right && !isPrime[firstPrime]) {
+            firstPrime++;
+        }
+
+        int lastPrime = right;
+        while (lastPrime >= left && !isPrime[lastPrime]) {
+            lastPrime--;
+        }
+
+        if (lastPrime <= firstPrime) {
+            return new int[]{-1, -1}; 
+        }
+
+        int minGap = Integer.MAX_VALUE;
+        int res1 = -1;
+        int res2 = -1;
+
+        int start = firstPrime;
+        for (int end = firstPrime + 1; end <= lastPrime; end++) {
+            if (!isPrime[end]) continue;
+
+            while (start < end) {
+                if (isPrime[start] && end - start < minGap) {
+                    minGap = end - start;
+                    res1 = start;
+                    res2 = end;
+                }
+
+                start++;
+            }
+            
+        }
+         
+        return new int[]{res1, res2};
+    }
+}
