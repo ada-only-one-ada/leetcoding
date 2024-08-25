5e65813fbca8ae033bf77daa406664de2b1a620c
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-24 19:00:02 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
@@ -1,39 +1,46 @@
 class Solution {
     public int[] closestPrimes(int left, int right) {
-        if (right - left < 2) return new int[]{-1, -1};
+        if (right - left <= 2) return new int[]{-1, -1};
 
         boolean[] isPrime = new boolean[right + 1];
         Arrays.fill(isPrime, true);
-        isPrime[0] = false;
-        isPrime[1] = false;
         
-        // num = 2; num <= right; num++
-        for (int num = 2; num * num <= right; num++) {
+        for (int num = 2; num < right; num++) {
             if (isPrime[num]) {
-                // multiple = 2; multiple * num <= right; multiple++
-                for (int multiple = num; multiple * num <= right; multiple++) {
+                for (int multiple = 2; multiple * num < right; multiple++) {
                     isPrime[num * multiple] = false;
                 }
             }
         }
 
+        int firstPrime = left + 1;
+        while (firstPrime < isPrime.length && !isPrime[firstPrime]) {
+            firstPrime++;
+        }
+
+        int lastPrime = right - 1;
+        while (lastPrime >= 0 && !isPrime[lastPrime]) {
+            lastPrime--;
+        }
+
+        if (lastPrime <= firstPrime) {
+            return new int[]{-1, -1}; 
+        }
+
         int minGap = Integer.MAX_VALUE;
-        int[] res = new int[]{-1, -1};
+        int res1 = -1;
+        int res2 = -1;
 
+        int start = firstPrime;
+        for (int end = start+ 1; end < lastPrime; end++) {
+            if (!isPrime[end]) continue;
 
-        int prevPrime = -1;
-        for (int num = left; num <= right; num++) {
-            if (isPrime[num]) {
-                if (prevPrime != -1 && num - prevPrime < minGap) {
-                    minGap = num - prevPrime;
-                    res[0] = prevPrime;
-                    res[1] = num;
-                }
-                
-                prevPrime = num;
+            if (end - start < minGap) {
+                res1 = start;
+                res2 = end;
             }
         }
          
-        return res;
+        return new int[]{res1, res2};
     }
-} 
+}
