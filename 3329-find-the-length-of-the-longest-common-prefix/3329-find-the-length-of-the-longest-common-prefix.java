134dd1563e0012f8784db697122d106d5f2fb632
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-25 13:43:43 -0700

    Time: 67 ms (57.67%), Space: 56.3 MB (37.49%) - LeetHub

--- 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.java
+++ 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.java
@@ -1,5 +1,6 @@
 class Solution {
     public int longestCommonPrefix(int[] arr1, int[] arr2) {
+        // 把 arr1 的所有前缀丢到一个哈希集合中，然后遍历 arr2 的所有前缀，统计在哈希集合中的最大公共前缀，最后拿到它的长度
         Set<Integer> allPrefixInArr1 = new HashSet<>();
         for (int num: arr1) {
             for (int currPrefix = num; currPrefix > 0; currPrefix /= 10) {
@@ -11,6 +12,7 @@ class Solution {
         for (int num: arr2) {
             for (int currPrefix = num; currPrefix > 0; currPrefix /= 10) {
                 if (allPrefixInArr1.contains(currPrefix)) {
+                    // 取数值的最大值，因为数值越大长度越长
                     maxPrefix = Math.max(maxPrefix, currPrefix);
                 }
             }
