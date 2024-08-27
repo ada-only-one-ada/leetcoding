71ef72c061605bb8f0c9e44022c360e84041a27b
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-27 13:46:45 -0700

    Time: 1 ms (69.95%), Space: 42.5 MB (32.59%) - LeetHub

--- 1196-filling-bookcase-shelves/1196-filling-bookcase-shelves.java
+++ 1196-filling-bookcase-shelves/1196-filling-bookcase-shelves.java
@@ -1,29 +1,29 @@
 class Solution {
     public int minHeightShelves(int[][] books, int shelfWidth) {
-        int n = books.length;
-        int[] dp = new int[n + 1];
+        int[] dp = new int[books.length + 1];
         Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[0] = 0;
+        dp[0] = 0;    // 第0本书前没有书，最小高度为0
 
-        for (int i = 1; i <= n; i++) {
-            int currWidth = 0;
-            int currMaxHeight = 0;
-            int j = i;
+        // 前 i 本书 所需要的最小高度
+        for (int i = 1; i < dp.length; i++) {
+            int[] book = books[i - 1];
+            int currWidth = book[0];
+            int currHeight = book[1];
+            
+            // 新建一个书架 
+            dp[i] = dp[i - 1] + currHeight;
 
-            while (j >= 1) {
-                currWidth += books[j - 1][0];  // 累加当前书的宽度
-                if (currWidth > shelfWidth) {  // 如果当前宽度超出书架宽度
-                    break;  // 跳出循环，不能继续在这一层上放置书籍
-                }
-                // 更新当前层的最大高度
-                currMaxHeight = Math.max(currMaxHeight, books[j - 1][1]);
-                // 更新dp数组，考虑在j-1处结束前一层书架，开始新的一层
-                dp[i] = Math.min(dp[i], dp[j - 1] + currMaxHeight);
-
-                j--;  // 继续考虑前一本书是否加入当前层
+            // 尝试放到之前 可行的 前 j 本书所需要的最小高度
+            for (int j = i - 1; j >= 1; j--) {
+                currWidth += books[j - 1][0];
+                if (currWidth > shelfWidth) break;
+                
+                currHeight = Math.max(currHeight, books[j - 1][1]);
+                dp[i] = Math.min(dp[i], dp[j - 1] + currHeight); //为什么这里改成j-1是对的？
             }
         }
 
-        return dp[n];  // 返回全部书籍放置完成后的最小总高度。
+        return dp[books.length];
+
     }
 }
