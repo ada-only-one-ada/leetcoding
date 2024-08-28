2f9c6d61f60e316a8ada69a2d82219f9658135aa
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-28 16:35:00 -0700

    Time: 1 ms (50.2%), Space: 44.8 MB (19.01%) - LeetHub

--- 0772-construct-quad-tree/0772-construct-quad-tree.java
+++ 0772-construct-quad-tree/0772-construct-quad-tree.java
@@ -59,9 +59,10 @@ class Solution {
         // 四个一样的组成的大 leaf
         if (topLeft.isLeaf && topRight.isLeaf && bottomLeft.isLeaf && bottomRight.isLeaf &&
         topLeft.val == topRight.val && topLeft.val == bottomLeft.val && topLeft.val == bottomRight.val) {
-            return new Node(topLeft.val, true); 
+            return new Node(grid[i][j] == 1, true); 
         }
 
+        // 不是 leaf，值为true 或者 false 都可以
         Node root = new Node(false, false);  // new Node(true, false);
         root.topLeft = topLeft;
         root.topRight = topRight;
