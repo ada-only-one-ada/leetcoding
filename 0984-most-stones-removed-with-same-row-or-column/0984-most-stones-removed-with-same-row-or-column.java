63dd2675be05a72b1bf78816d4958530f02b48b9
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-29 00:53:09 -0700

    Time: 26 ms (42.37%), Space: 44.9 MB (62.46%) - LeetHub

--- 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.java
+++ 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.java
@@ -1,58 +1,63 @@
+import java.util.*;
+
 class Solution {
-    int[] parent;
-    int[] rank;
-    int count;
-
+    private Map<Integer, Integer> parent = new HashMap<>();
+    private Map<Integer, Integer> rank = new HashMap<>();
+    
     public int removeStones(int[][] stones) {
-        parent = new int[stones.length];
-        rank = new int[stones.length];
-        count = stones.length;
+        int id = 0; // unique identifier for each row and column
+        Map<Integer, Integer> map = new HashMap<>(); // maps original index to unique id
 
-        for (int i = 0; i < stones.length; i++) {
-            parent[i] = i;
-            rank[i] = 1;
-        }
-
-        for (int i = 0; i < stones.length; i++) {
-            for (int j = i + 1; j < stones.length; j++) {
-                // 如果 石头i 和 石头j 在同一行/列，连通它们
-                if (stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) {
-                    if (connect(i, j)) {
-                        // 单独自己的集合少了一个
-                        count--;
-                    };
-                }
+        // Assign unique ids
+        for (int[] stone : stones) {
+            if (!map.containsKey(stone[0])) {
+                map.put(stone[0], id++);
+            }
+            if (!map.containsKey(~stone[1])) { // Using ~stone[1] to differentiate columns from rows
+                map.put(~stone[1], id++);
             }
         }
 
-        // 本来一共有10个石头，分成了三堆，那么每堆至少保留一个，总共保留三个
-        // 去掉的就是 10 - 3 = 7个，这里求的是去掉的个数
-        return stones.length - count;
-    }
-
-    public int find(int stone) {
-        int p = parent[stone];
-        if (parent[p] != p) {
-            return find(p);
+        // Perform union on each stone's row and column
+        for (int[] stone : stones) {
+            connect(map.get(stone[0]), map.get(~stone[1]));
         }
 
-        return p;
-    }
-
-    public boolean connect(int i, int j) {
-        int pi = find(i);
-        int pj = find(j);
-
-        if (pi == pj) return false;
-
-        if (rank[pi] >= rank[pj]) {
-            rank[pi] += rank[pj];
-            parent[pj] = pi;
-        } else {
-            rank[pj] += rank[pi];
-            parent[pi] = pj;
+        // Find how many unique components there are
+        Set<Integer> uniqueParents = new HashSet<>();
+        for (Integer key : map.keySet()) {
+            uniqueParents.add(find(map.get(key)));
         }
 
-        return true;
+        // The number of stones minus the number of unique components
+        return stones.length - uniqueParents.size();
+    }
+
+    private int find(int x) {
+        if (!parent.containsKey(x)) {
+            parent.put(x, x);
+            rank.put(x, 1);
+        }
+        if (x != parent.get(x)) {
+            parent.put(x, find(parent.get(x))); // Path compression
+        }
+        return parent.get(x);
+    }
+
+    private void connect(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+
+        if (rootX != rootY) {
+            // Union by rank
+            if (rank.get(rootX) > rank.get(rootY)) {
+                parent.put(rootY, rootX);
+            } else if (rank.get(rootX) < rank.get(rootY)) {
+                parent.put(rootX, rootY);
+            } else {
+                parent.put(rootY, rootX);
+                rank.put(rootX, rank.get(rootX) + 1);
+            }
+        }
     }
 }
