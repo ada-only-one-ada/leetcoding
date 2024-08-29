a17fe27505192cf4d5f15d22e9922c656ccd15a1
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-29 13:10:57 -0700

    Time: 20 ms (60.41%), Space: 45.4 MB (13.68%) - LeetHub

--- 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.java
+++ 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.java
@@ -1,11 +1,11 @@
 class Solution {
-    // 使用两个哈希表存储每个点的父节点和秩
     Map<Integer, Integer> parent = new HashMap<>();
     Map<Integer, Integer> rank = new HashMap<>();
 
+    // 主要的功能方法，用于移除石头
     public int removeStones(int[][] stones) {
-        // 设置一个偏移量来区分行和列的标识
-        int rowOffset = 10000; // 最大的row和col是10^4
+        // 行偏移量，用于区分行和列，最大的row和col是10^4
+        int rowOffset = 10000; 
 
         // 初始化并查集数据结构
         for (int[] stone : stones) {
@@ -17,16 +17,24 @@ class Solution {
             rank.putIfAbsent(colKey, 0);
         }
 
-        // 合并每个石头的行和列
+        // 遍历所有石头，并进行合并每个石头的行和列
         for (int[] stone : stones) {
             connect(stone[0] + rowOffset, stone[1]);
         }
 
-        // 使用一个集合来统计所有不同的根节点，即连通分量的数量
+        // 使用HashSet计算不同的根节点数量，代表不同的连通分量
+        
         Set<Integer> set = new HashSet<>();
         for (int stone : parent.keySet()) {
-            set.add(find(stone)); // 查找每个节点的根节点
+            set.add(find(stone)); // 查找每个节点的根节点，row和col是分别的节点
         }
+
+        /*
+        for (int[] stone : stones) {
+            set.add(find(stone[0] + rowOffset)); // 找到行的根节点
+            set.add(find(stone[1])); // 找到列的根节点
+        }
+        */
 
         // 总石头数减去连通分量的数量，即为最大可移除石头数
         return stones.length - set.size();
