ec2a0efd00ec71fb1c08c72ee59ca2bc835b80b9
Adagggggg <122472773+Adagggggg@users.noreply.github.com>
2024-08-30 16:27:32 -0700

    Time: 65 ms (72.34%), Space: 57.2 MB (51.6%) - LeetHub

--- 3265-maximum-good-subarray-sum/3265-maximum-good-subarray-sum.java
+++ 3265-maximum-good-subarray-sum/3265-maximum-good-subarray-sum.java
@@ -2,29 +2,28 @@ class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
         long res = Long.MIN_VALUE;
         Map<Integer, Integer> map = new HashMap<>();
-        long[] prefixSum = new long[nums.length];
-        prefixSum[0] = nums[0];
+        long[] prefixSum = new long[nums.length + 1];
 
         for (int i = 0; i < nums.length; i++) {
             if (i > 0) {
-                prefixSum[i] = prefixSum[i - 1] + nums[i];
+                prefixSum[i] = prefixSum[i - 1] + nums[i - 1];
             }
             
             if (map.containsKey(nums[i] - k)) {
-                long subarraySum = prefixSum[i] - prefixSum[map.get(nums[i] - k)] + nums[map.get(nums[i] - k)];
-                res = Math.max(res, subarraySum);
+                long subarraySum = prefixSum[i] - prefixSum[map.get(nums[i] - k)];
+                res = Math.max(res, subarraySum + nums[i]);
             } 
             
             if (map.containsKey(nums[i] + k)) {
-                long subarraySum = prefixSum[i] - prefixSum[map.get(nums[i] + k)] + nums[map.get(nums[i] + k)];
-                res = Math.max(res, subarraySum);
+                long subarraySum = prefixSum[i] - prefixSum[map.get(nums[i] + k)];
+                res = Math.max(res, subarraySum + nums[i]);
             }
 
             if (!map.containsKey(nums[i])) {
                 map.put(nums[i], i);
             } else {
-                long subarraySum = prefixSum[i] - prefixSum[map.get(nums[i])] + nums[map.get(nums[i])];
-                if (nums[i] > subarraySum) {
+                long subarraySum = prefixSum[i] - prefixSum[map.get(nums[i])];
+                if (nums[i] > subarraySum + nums[i]) {
                     map.put(nums[i], i);
                 }
             }
