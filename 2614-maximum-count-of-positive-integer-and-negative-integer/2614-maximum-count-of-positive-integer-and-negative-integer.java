76bcce870e168719081a6ad35d8a11537fc209fe
zxcv5678 <122472773+zxcv5678@users.noreply.github.com>
2024-09-11 14:48:24 -0700

    Time: 0 ms (100%), Space: 44.8 MB (51.73%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.java
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.java
@@ -1,36 +1,43 @@
 class Solution {
     public int maximumCount(int[] nums) {
-        int start = 0;
-        int end = nums.length - 1;
-        // 找第一个正数
-        while (start < end) {
-            int mid = start + (end - start) / 2;
-            // 当前数字大于0，可能是第一个正数，或者之前也有
+        // 找第一个出现的正数
+        int left = 0;
+        int right = nums.length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            // 当前数字是正数，说明第一个正数肯定是当前或之前
             if (nums[mid] > 0) {
-                end = mid;
-            } else {
-                // 当前小于等于0，第一个正数肯定在当前之后
-                start = mid + 1;
+                right = mid;
+            // 当前数字不是正数，说明第一个正数肯定在之后
+            } else if (nums[mid] <= 0) {
+                left = mid + 1;
             }
         }
-        
-        int pos = start >= 0 && nums[start] > 0? nums.length - start : 0;
+        int firstPosIndex = left;
+        int numOfPosNums = 0;
+        if (firstPosIndex >= 0 && firstPosIndex < nums.length && nums[firstPosIndex] > 0) {
+            numOfPosNums = nums.length - firstPosIndex;
+        }
 
-        // 找最后一个负数
-        start = 0;
-        end = nums.length - 1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            // 当前数字小于0，可能是最后一个负数，也可能之后还有
+        // 找最后一个出现的负数
+        left = 0;
+        right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            // 当前数字是负数，说明最后一个负数肯定在当前或之后
             if (nums[mid] < 0) {
-                start = mid + 1; // here prevent same [mid] [start]
-            // 当前数字大于0等于0，说明最后一个负数肯定在之前
-            } else {
-                end = mid - 1;
+                left = mid + 1; 
+            // 当前数字不是负数，说明最后一个负数肯定在之前
+            } else if (nums[mid] >= 0) {
+                right = mid - 1;
             }
         }
+        int lastNegIndex = left - 1;
+        int numOfNegNums = 0;
+        if (lastNegIndex >= 0 && lastNegIndex < nums.length && nums[lastNegIndex] < 0) {
+            numOfNegNums = lastNegIndex + 1;
+        }
 
-        int neg = start - 1 >= 0 && nums[start - 1] < 0? start: 0;
-        return Math.max(pos, neg);
+        return Math.max(numOfPosNums, numOfNegNums);
     }
 }
