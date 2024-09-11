fa316f1e982fcbddc9ef0443d32e0f0d62267c7d
zxcv5678 <122472773+zxcv5678@users.noreply.github.com>
2024-09-11 16:07:01 -0700

    Time: 54 ms (14.29%), Space: 45.8 MB (15.08%) - LeetHub

---
+++ 2615-make-number-of-distinct-characters-equal/2615-make-number-of-distinct-characters-equal.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isItPossible(String word1, String word2) {
+        Map<Character, Integer> map1 = new HashMap<>();
+        Map<Character, Integer> map2 = new HashMap<>();
+        for (char c: word1.toCharArray()) map1.put(c, map1.getOrDefault(c, 0) + 1);
+        for (char c: word2.toCharArray()) map2.put(c, map2.getOrDefault(c, 0) + 1);
+        
+        for (char key1: map1.keySet()) {
+            for (char key2: map2.keySet()) {
+                if (key1 == key2) {
+                    if (map1.size() == map2.size()) return true;
+                } else if (key1 != key2) {
+                    int newMap1Size = map1.size();
+                    if (!map1.containsKey(key2)) newMap1Size++;
+                    if (map1.get(key1) == 1) newMap1Size--;
+
+                    int newMap2Size = map2.size();
+                    if (!map2.containsKey(key1)) newMap2Size++;
+                    if (map2.get(key2) == 1) newMap2Size--;
+                    
+                    if (newMap1Size == newMap2Size) return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
