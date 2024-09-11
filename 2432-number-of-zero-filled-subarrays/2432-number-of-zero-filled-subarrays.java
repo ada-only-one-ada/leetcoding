b8a9b96523f055a6eee48c31daa2613f60f021e4
zxcv5678 <122472773+zxcv5678@users.noreply.github.com>
2024-09-11 00:53:10 -0700

    Time: 7 ms (6.22%), Space: 64.5 MB (5.39%) - LeetHub

--- 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.java
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.java
@@ -1,18 +1,18 @@
 class Solution {
     public long zeroFilledSubarray(int[] nums) {
+        long res = 0;
         long[] count = new long[nums.length];
+
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == 0) {
                 count[i] = 1;
             }
-        }
-
-        long res = 0;
-        for (int i = 0; i < count.length; i++) {
             if (i == 0 || count[i] == 0) continue;
             count[i] += count[i - 1];
         }
 
+      
+     
         long sum = 0;
         for (int i = 0; i < count.length; i++) {
             sum += count[i];
@@ -22,7 +22,7 @@ class Solution {
                 sum = 0;
             }
         }
-        
+
         if (sum != 0) res += sum;
 
         return res;
