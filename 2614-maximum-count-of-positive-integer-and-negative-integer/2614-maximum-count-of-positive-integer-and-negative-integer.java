9b18023b275f9223d997ac7a323930dc2b245c61
zxcv5678 <122472773+zxcv5678@users.noreply.github.com>
2024-09-11 10:26:58 -0700

    Time: 0 ms (100%), Space: 45 MB (26.57%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.java
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.java
@@ -5,7 +5,7 @@ class Solution {
         // 找第一个正数
         while (start < end) {
             int mid = start + (end - start) / 2;
-            // 当前数字大于0，第一个正数可能是当前或之前的
+            // 当前数字大于0，可能是第一个正数，或者之前也有
             if (nums[mid] > 0) {
                 end = mid;
             } else {
@@ -23,7 +23,7 @@ class Solution {
             int mid = start + (end - start) / 2;
             // 当前数字小于0，可能是最后一个负数，也可能之后还有
             if (nums[mid] < 0) {
-                start = mid + 1;
+                start = mid + 1; // here prevent same [mid] [start]
             // 当前数字大于0等于0，说明最后一个负数肯定在之前
             } else {
                 end = mid - 1;
