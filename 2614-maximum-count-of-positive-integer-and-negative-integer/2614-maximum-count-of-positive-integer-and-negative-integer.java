1630ab8871faf6de17cbf2a0d120ca67fff3a3b9
zxcv5678 <122472773+zxcv5678@users.noreply.github.com>
2024-09-11 14:53:24 -0700

    Time: 0 ms (100%), Space: 45.1 MB (26.5%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.java
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.java
@@ -3,7 +3,7 @@ class Solution {
         // 找第一个出现的正数
         int left = 0;
         int right = nums.length;
-        while (left < right) {
+        while (left + 1 < right) {
             int mid = left + (right - left) / 2;
             // 当前数字是正数，说明第一个正数肯定是当前或之前
             if (nums[mid] > 0) {
@@ -13,29 +13,35 @@ class Solution {
                 left = mid + 1;
             }
         }
-        int firstPosIndex = left;
+
         int numOfPosNums = 0;
-        if (firstPosIndex >= 0 && firstPosIndex < nums.length && nums[firstPosIndex] > 0) {
-            numOfPosNums = nums.length - firstPosIndex;
+        if (right >= 0 && right < nums.length && nums[right] > 0) {
+            numOfPosNums = nums.length - right;
+        }
+        if (left >= 0 && left < nums.length && nums[left] > 0) {
+            numOfPosNums = nums.length - left;
         }
 
         // 找最后一个出现的负数
         left = 0;
-        right = nums.length - 1;
-        while (left <= right) {
+        right = nums.length;
+        while (left + 1 < right) {
             int mid = left + (right - left) / 2;
             // 当前数字是负数，说明最后一个负数肯定在当前或之后
             if (nums[mid] < 0) {
-                left = mid + 1; 
+                left = mid; 
             // 当前数字不是负数，说明最后一个负数肯定在之前
             } else if (nums[mid] >= 0) {
                 right = mid - 1;
             }
         }
-        int lastNegIndex = left - 1;
+        
         int numOfNegNums = 0;
-        if (lastNegIndex >= 0 && lastNegIndex < nums.length && nums[lastNegIndex] < 0) {
-            numOfNegNums = lastNegIndex + 1;
+        if (left >= 0 && left < nums.length && nums[left ] < 0) {
+            numOfNegNums = left  + 1;
+        }
+        if (right >= 0 && right < nums.length && nums[right] < 0) {
+            numOfNegNums = right + 1;
         }
 
         return Math.max(numOfPosNums, numOfNegNums);
