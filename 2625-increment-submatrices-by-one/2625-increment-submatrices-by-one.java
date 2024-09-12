a455805eff30876d9bba0f03733a95baa94e4c9b
zxcv5678 <122472773+zxcv5678@users.noreply.github.com>
2024-09-11 18:11:15 -0700

    Time: 9 ms (86.78%), Space: 56.2 MB (51.15%) - LeetHub

--- 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.java
+++ 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.java
@@ -1,15 +1,28 @@
 class Solution {
     public int[][] rangeAddQueries(int n, int[][] queries) {
-        int[][] grid = new int[n][n];
+        int[][] diff = new int[n][n];
 
         for (int[] query: queries) {
-            for (int row = query[0]; row <= query[2]; row++) {
-                for (int col = query[1]; col <= query[3]; col++) {
-                    grid[row][col] += 1;
-                }
+            int row1 = query[0], row2 = query[2], col1 = query[1], col2 = query[3];
+            diff[row1][col1] += 1;
+
+            if (row2 + 1 < n) diff[row2 + 1][col1] -= 1;
+            if (col2 + 1 < n) diff[row1][col2 + 1] -= 1;
+            if (row2 + 1 < n && col2 + 1 < n) diff[row2 + 1][col2 + 1] += 1;
+        }
+
+        for (int row = 0; row < n; row++) {
+            for (int col = 1; col < n; col++) {
+                diff[row][col] += diff[row][col - 1];
             }
         }
 
-        return grid;
+        for (int col = 0; col < n; col++) {
+            for (int row = 1; row < n; row++) {
+                diff[row][col] += diff[row - 1][col];
+            }
+        }
+
+        return diff;
     }
 }
