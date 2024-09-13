73f90e9d2a37b8652ea165fdcefc7ebb15ae5688
zxcv5678 <122472773+zxcv5678@users.noreply.github.com>
2024-09-13 16:01:56 -0700

    Time: 56 ms (72.77%), Space: 61.7 MB (8.92%) - LeetHub

--- 2626-count-the-number-of-good-subarrays/2626-count-the-number-of-good-subarrays.java
+++ 2626-count-the-number-of-good-subarrays/2626-count-the-number-of-good-subarrays.java
@@ -1,7 +1,7 @@
 class Solution {
     public long countGood(int[] nums, int k) {
         Map<Integer, Integer> count = new HashMap<>();
-        long ans = 0;
+        long res = 0;
         int start = 0;
         long pairCount = 0; // This will track the number of valid pairs in the current window
 
@@ -11,18 +11,22 @@ class Solution {
             if (count.containsKey(num)) {
                 pairCount += count.get(num);
             }
+
             // Include this number in the count
             count.put(num, count.getOrDefault(num, 0) + 1);
 
             // Check if the current number of pairs is at least k
             while (pairCount >= k) {
-                ans += nums.length - end; // All subarrays starting from 'start' to 'end' and ending at 'end' to 'nums.length - 1' are valid
-                count.put(nums[start], count.get(nums[start]) - 1); // Move the start forward, reducing count of the start element
-                pairCount -= count.get(nums[start]); // Reduce the pairs count as we're removing one occurrence of nums[start]
+                // All subarrays starting from 'start' to 'end' and ending at 'end' to 'nums.length - 1' are valid
+                res += nums.length - end; 
+                // Move the start forward, reducing count of the start element
+                count.put(nums[start], count.get(nums[start]) - 1); 
+                // Reduce the pairs count as we're removing one occurrence of nums[start]
+                pairCount -= count.get(nums[start]); 
                 start++;
             }
         }
-        return ans;
-    
+
+        return res;
     }
 }
