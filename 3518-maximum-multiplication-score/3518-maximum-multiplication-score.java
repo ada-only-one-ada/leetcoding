4eef0521dc1c5c1f01ba4ae309410eeac5db4968
zxcv5678 <122472773+zxcv5678@users.noreply.github.com>
2024-09-15 01:34:45 -0700

    Time: 35 ms (100%), Space: 63 MB (100%) - LeetHub

--- 3518-maximum-multiplication-score/3518-maximum-multiplication-score.java
+++ 3518-maximum-multiplication-score/3518-maximum-multiplication-score.java
@@ -9,27 +9,27 @@ class Solution {
         dp[0][0] = (long) b[0] * a[0];
 
         for (int i = 1; i < b.length; i++) {
-            dp[i][0] = Math.max(dp[i - 1][0], (long) b[i] * a[0]);
-            // For dp[i][1]
-            if (dp[i - 1][0] != Long.MIN_VALUE) {
-                dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + (long) b[i] * a[1]);
+            dp[i][0] = Math.max(dp[i-1][0], (long)b[i] * a[0]);
+
+            if (dp[i-1][0] != Long.MIN_VALUE) {
+                dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] + (long)b[i] * a[1]);
             } else {
-                dp[i][1] = dp[i - 1][1];
+                dp[i][1] = dp[i-1][1];
             }
-            // For dp[i][2]
-            if (dp[i - 1][1] != Long.MIN_VALUE) {
-                dp[i][2] = Math.max(dp[i - 1][2], dp[i - 1][1] + (long) b[i] * a[2]);
+            
+            if (dp[i-1][1] != Long.MIN_VALUE) {
+                dp[i][2] = Math.max(dp[i-1][2], dp[i-1][1] + (long)b[i] * a[2]);
             } else {
-                dp[i][2] = dp[i - 1][2];
+                dp[i][2] = dp[i-1][2];
             }
-            // For dp[i][3]
-            if (dp[i - 1][2] != Long.MIN_VALUE) {
-                dp[i][3] = Math.max(dp[i - 1][3], dp[i - 1][2] + (long) b[i] * a[3]);
+
+            if (dp[i-1][2] != Long.MIN_VALUE) {
+                dp[i][3] = Math.max(dp[i-1][3], dp[i-1][2] + (long)b[i] * a[3]);
             } else {
-                dp[i][3] = dp[i - 1][3];
+                dp[i][3] = dp[i-1][3];
             }
         }
 
-        return dp[b.length - 1][3];
+        return dp[dp.length - 1][3];
     }
 }
