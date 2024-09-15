a1a2d97c2037392414eeeba10f1e8ff1dd6b9d90
zxcv5678 <122472773+zxcv5678@users.noreply.github.com>
2024-09-15 13:48:25 -0700

    Time: 16 ms (100%), Space: 45.2 MB (100%) - LeetHub

---
+++ 3558-find-a-safe-walk-through-a-grid/3558-find-a-safe-walk-through-a-grid.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean findSafeWalk(List<List<Integer>> grid, int health) {
+        int[][] directions = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+        int row = grid.size();
+        int col = grid.get(0).size();     
+
+        Queue<int[]> queue = new PriorityQueue<>((a, b) -> {
+            return b[2] - a[2];
+        });   
+       
+        int[][] maxHealth = new int[row][col];
+        for (int[] r : maxHealth) {
+            Arrays.fill(r, Integer.MIN_VALUE);
+        }
+
+        queue.offer(new int[]{row - 1, col - 1, health - grid.get(row - 1).get(col - 1)});
+        maxHealth[row - 1][col - 1] = health - grid.get(row - 1).get(col - 1);
+
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            int currRow = curr[0];
+            int currCol = curr[1];
+            int currHealth = curr[2];
+
+            if (currRow == 0 && currCol == 0 && currHealth >= 1) {
+                return true;
+            }
+
+            for (int[] direction: directions) {
+                int nextRow = currRow + direction[0];
+                int nextCol = currCol + direction[1];
+
+                if (nextRow < 0 || nextRow >= row || nextCol < 0 || nextCol >= col) continue;
+
+                int nextHealth = currHealth - grid.get(nextRow).get(nextCol);
+                if (nextHealth <= maxHealth[nextRow][nextCol]) continue;
+
+                queue.offer(new int[]{nextRow, nextCol, nextHealth});
+                maxHealth[nextRow][nextCol] = nextHealth;
+            }  
+        }
+
+        return false;
+    }
+}
