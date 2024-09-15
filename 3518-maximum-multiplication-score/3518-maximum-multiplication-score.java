5cfda9e31d258b8d16097632c0873f6decba2582
zxcv5678 <122472773+zxcv5678@users.noreply.github.com>
2024-09-15 01:51:52 -0700

    Time: 40 ms (100%), Space: 63.5 MB (100%) - LeetHub

--- 3518-maximum-multiplication-score/3518-maximum-multiplication-score.java
+++ 3518-maximum-multiplication-score/3518-maximum-multiplication-score.java
@@ -10,7 +10,8 @@ class Solution {
 
         for (int i = 1; i < b.length; i++) {
             dp[i][0] = Math.max(dp[i-1][0], (long)b[i] * a[0]);
-            
+            // 之前的0没选过，当前的1只能选之前的1
+            // 之前的0选过，当前的1可以选之前的1，或者之前的0 加 现在的1 重新选
             dp[i][1] = dp[i-1][0] == Long.MIN_VALUE? dp[i-1][1] : Math.max(dp[i-1][1], dp[i-1][0] + (long)b[i] * a[1]);
             dp[i][2] = dp[i-1][1] == Long.MIN_VALUE? dp[i-1][2] : Math.max(dp[i-1][2], dp[i-1][1] + (long)b[i] * a[2]);
             dp[i][3] = dp[i-1][2] == Long.MIN_VALUE? dp[i-1][3] : Math.max(dp[i-1][3], dp[i-1][2] + (long)b[i] * a[3]);
