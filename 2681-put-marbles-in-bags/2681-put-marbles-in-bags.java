78deeaef4d98b551c980706ac9b9e42f279d41af
zxcv5678 <122472773+zxcv5678@users.noreply.github.com>
2024-09-16 23:52:29 -0700

    Time: 35 ms (38.89%), Space: 60.4 MB (81.48%) - LeetHub

--- 2681-put-marbles-in-bags/2681-put-marbles-in-bags.java
+++ 2681-put-marbles-in-bags/2681-put-marbles-in-bags.java
@@ -1,28 +1,23 @@
 class Solution {
     public long putMarbles(int[] weights, int k) {
-        if (weights.length < k) return -1; // 检查输入是否有效
+        if (weights.length < k) return -1;
 
-        // 计算所有相邻元素对的和
-        int[] adjSum = new int[weights.length - 1];
+        long[] adjSum = new long[weights.length - 1];
         for (int i = 0; i < weights.length - 1; i++) {
             adjSum[i] = weights[i] + weights[i + 1];
         }
 
-        // 对相邻元素对的和进行排序
         Arrays.sort(adjSum);
-
-        // 计算最大的 k-1 个相邻和的总和
-        long maxScore = weights[0] + weights[weights.length - 1]; // 初始化为首尾元素的和
-        for (int i = adjSum.length - 1; i >= adjSum.length - (k - 1); i--) {
-            maxScore += adjSum[i];
-        }
-
-        // 计算最小的 k-1 个相邻和的总和
-        long minScore = weights[0] + weights[weights.length - 1]; // 初始化为首尾元素的和
+        long minSum = weights[0] + weights[weights.length - 1];
         for (int i = 0; i < k - 1; i++) {
-            minScore += adjSum[i];
+            minSum += adjSum[i];
         }
 
-        return maxScore - minScore;
+        long maxSum = weights[0] + weights[weights.length - 1];
+        for (int i = adjSum.length - 1; i >= adjSum.length - (k - 1); i--) {
+            maxSum += adjSum[i];
+        }
+
+        return maxSum - minSum;
     }
 }
