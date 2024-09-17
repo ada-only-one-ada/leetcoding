a086f67cc8af36e2ac813ef195ba09a35b323fba
zxcv5678 <122472773+zxcv5678@users.noreply.github.com>
2024-09-17 12:04:19 -0700

    Time: 39 ms (24.69%), Space: 63.2 MB (32.1%) - LeetHub

--- 2681-put-marbles-in-bags/2681-put-marbles-in-bags.java
+++ 2681-put-marbles-in-bags/2681-put-marbles-in-bags.java
@@ -19,11 +19,20 @@ class Solution {
         }
 
         // 计算成本最大的 k-1 段，初始值加上了第一个和最后一个的成本
+        // 比如 最大收益：1 3 | 5 | 2，
+        //                      7 这里的 7 包括了最后一个2，我们再加一次2即可
+        //                  8 这里的 1 只需要加1次，因为结尾是3，8里面包括了
+
+        // 比如 最小收益：1 ｜ 3  5 | 2，
+        //                4 这里的4 包括了第一个1，我们再加一次1即可
+        
         long maxSum = weights[0] + weights[weights.length - 1];
         for (int i = adjSum.length - 1; i >= adjSum.length - (k - 1); i--) {
             maxSum += adjSum[i];
         }
 
+        System.out.println(minSum);
+        System.out.println(maxSum);
         return maxSum - minSum;
     }
 }
