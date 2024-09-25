09223c92051e951e99644c84cd6a5843f7711265
zxcv5678 <122472773+zxcv5678@users.noreply.github.com>
2024-09-24 21:05:11 -0700

    Time: 5 ms (24.24%), Space: 45.3 MB (65.78%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -5,6 +5,8 @@ class Solution {
         int left = 0;
         int right = s.length() - 1;
         while (left < right) {
+            // 这里不用记录改变过没有，因为一旦遇到不相等的字符，我们只能选择去掉左边/右边的再看是否全等
+            // 此时如果不全等的话就return false了，也就没有下一次的删除操作了（which means used might increase）
             if (s.charAt(left) != s.charAt(right)) { 
                 return helper(s.substring(left, right)) || helper(s.substring(left + 1, right + 1));
             }
