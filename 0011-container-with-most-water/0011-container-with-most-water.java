f03594ca33caa3572c99e8a6bb949042c6ae5224
zxcv5678 <122472773+zxcv5678@users.noreply.github.com>
2024-09-25 16:28:58 -0700

    Time: 5 ms (70.55%), Space: 58.1 MB (19.38%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maxArea(int[] height) {
-        int max = 0;
+        int big = 0;
         int leftIndex = 0;
         int rightIndex = height.length - 1;
 
@@ -9,14 +9,14 @@ class Solution {
             int rightHeight = height[rightIndex];
 
             int currArea = Math.min(leftHeight, rightHeight) * (rightIndex - leftIndex);
-            max = Math.max(max, currArea);
+             big  = Math.max( big , currArea);
             if (leftHeight >= rightHeight) {
                 rightIndex--;
             } else {
                 leftIndex++;
             }
         }
-        // this is tests
-        return max;
+       
+        return  big ;
     }
 }
