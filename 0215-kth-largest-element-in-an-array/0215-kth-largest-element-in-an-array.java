7b3b74e66be4b95cd0784c70944d1151fc59caad
zxcv5678 <122472773+zxcv5678@users.noreply.github.com>
2024-09-24 21:15:55 -0700

    Time: 11 ms (95.53%), Space: 57.9 MB (50.69%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,27 +1,22 @@
 class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        int maxValue = Integer.MIN_VALUE;
-        int minValue = Integer.MAX_VALUE;
-
-        for (int num: nums) {
-            maxValue = Math.max(maxValue, num);
-            minValue = Math.min(minValue, num);
-        }
-
-        int[] count = new int[maxValue - minValue + 1];
-        for (int num: nums) {
-            count[num - minValue]++;
-        }
-
-        for (int num = count.length - 1; num >= 0; num--) {
-            if (count[num] == 0) continue;
-
-            k -= count[num];
-            if (k <= 0) {
-                return num + minValue;
+    int quickselect(int[] nums, int l, int r, int k) {
+        if (l == r) return nums[k];
+        int x = nums[l], i = l - 1, j = r + 1;
+        while (i < j) {
+            do i++; while (nums[i] < x);
+            do j--; while (nums[j] > x);
+            if (i < j){
+                int tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = tmp;
             }
         }
-
-        return -1;
+        if (k <= j) return quickselect(nums, l, j, k);
+        else return quickselect(nums, j + 1, r, k);
+    }
+    public int findKthLargest(int[] _nums, int k) {
+        int n = _nums.length;
+        return quickselect(_nums, 0, n - 1, n - k);
     }
 }
+
