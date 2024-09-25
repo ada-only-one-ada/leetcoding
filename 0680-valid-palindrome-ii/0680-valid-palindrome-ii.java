535cd52cf2f17b7cf7d41a213675b26df345b84a
zxcv5678 <122472773+zxcv5678@users.noreply.github.com>
2024-09-24 21:03:40 -0700

    Time: 5 ms (24.24%), Space: 45.4 MB (46.29%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -4,10 +4,8 @@ class Solution {
 
         int left = 0;
         int right = s.length() - 1;
-        int used = 0;
         while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
-                
+            if (s.charAt(left) != s.charAt(right)) { 
                 return helper(s.substring(left, right)) || helper(s.substring(left + 1, right + 1));
             }
 
