424114aaa02c0c888e52245db899005cc5f30668
zxcv5678 <122472773+zxcv5678@users.noreply.github.com>
2024-09-24 21:50:48 -0700

    Time: 11 ms (95.53%), Space: 58.4 MB (40.27%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -9,7 +9,8 @@ class Solution {
         int pivot = nums[left];
 
         while (i <= j) {
-            while (i <= j && nums[i] < pivot) i++;
+            while (i <= j && nums[i] < pivot) i++;  // 这里不能是<=
+            // 比如【3，3，3，3】在上面这个loop里，i会到数组长度，j不变，在上面这行就退出整个 whileloop了
             while (i <= j && nums[j] > pivot) j--;
 
             if (i <= j) {
@@ -21,6 +22,7 @@ class Solution {
             }  
         }
 
+        // 在这里的时候，left没变，j也没变，造成stack overflow了
         if (kSmallest <= j) return quickSelect(nums, left, j, kSmallest);
         if (kSmallest >= i) return quickSelect(nums, i, right, kSmallest);
         return nums[kSmallest];
