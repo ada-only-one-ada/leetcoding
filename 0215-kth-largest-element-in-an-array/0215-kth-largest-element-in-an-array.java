9dc10b7cfd519929c02a749b6a8d22ae7d44f4fd
zxcv5678 <122472773+zxcv5678@users.noreply.github.com>
2024-09-24 21:17:43 -0700

    Time: 11 ms (95.53%), Space: 57.1 MB (88.78%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,22 +1,32 @@
 class Solution {
-    int quickselect(int[] nums, int l, int r, int k) {
-        if (l == r) return nums[k];
-        int x = nums[l], i = l - 1, j = r + 1;
-        while (i < j) {
-            do i++; while (nums[i] < x);
-            do j--; while (nums[j] > x);
-            if (i < j){
-                int tmp = nums[i];
+    // Function to find the k-th largest element using quickselect
+    public int findKthLargest(int[] nums, int k) {
+        int n = nums.length;
+        return quickselect(nums, 0, n - 1, n - k);
+    }
+
+    // Helper method for quickselect algorithm
+    private int quickselect(int[] nums, int left, int right, int k) {
+        // Using the first element as the pivot
+        int pivot = nums[left];
+        int i = left;
+        int j = right;
+        // Partitioning the array
+        while (i <= j) {
+            while (i <= j && nums[i] < pivot) i++;
+            while (i <= j && nums[j] > pivot) j--;
+            if (i <= j) {
+                // Swap elements at i and j
+                int temp = nums[i];
                 nums[i] = nums[j];
-                nums[j] = tmp;
+                nums[j] = temp;
+                i++;
+                j--;
             }
         }
-        if (k <= j) return quickselect(nums, l, j, k);
-        else return quickselect(nums, j + 1, r, k);
-    }
-    public int findKthLargest(int[] _nums, int k) {
-        int n = _nums.length;
-        return quickselect(_nums, 0, n - 1, n - k);
+        // Determine which part to recursively call quickselect on
+        if (k <= j) return quickselect(nums, left, j, k);
+        if (k >= i) return quickselect(nums, i, right, k);
+        return nums[k];
     }
 }
-
