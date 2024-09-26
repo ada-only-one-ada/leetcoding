8b026a12202ad998aa4c621dee8130864999bf7f
zxcv5678 <122472773+zxcv5678@users.noreply.github.com>
2024-09-25 23:55:39 -0700

    Time: 0 ms (100%), Space: 42.2 MB (73.24%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,28 +1,22 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        if (nums.length == 1) return 0;
-        int[] newNums = new int[nums.length + 2];
-        newNums[0] = newNums[newNums.length - 1] = Integer.MIN_VALUE;
-        for (int i = 0; i < nums.length; i++) {
-            newNums[i + 1] = nums[i];
-        }
-        nums = newNums;
+        int left = 0;
+        int right = nums.length - 1;
 
-        int start = 0;
-        int end = nums.length;
-
-        while (start < end) {
-            int mid = start + (end - start) / 2;
-            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
-                return mid - 1;
-            } else if (nums[mid] < nums[mid + 1]) {
-                start = mid;
-            } else {
-                end = mid;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid-1] < nums[mid] && nums[mid] > nums[mid+1]) {
+                return mid;
+                // min 1 2 min,说明大数在右边
+            } else if (nums[mid+1] > nums[mid]) {
+                left = mid;
+                // min 8 5 min，说明大数在左边
+            } else if (nums[mid-1] > nums[mid]) {
+                right = mid;
             }
-            
-        } 
+        }
 
-        return -1;
+        if (nums[left] > nums[right]) return left;
+        return right;
     }
 }
