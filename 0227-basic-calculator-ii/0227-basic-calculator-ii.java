ff7706e34eb1951815ae02db8c069787058320ed
zxcv5678 <122472773+zxcv5678@users.noreply.github.com>
2024-09-26 12:31:25 -0700

    Time: 23 ms (57.06%), Space: 46.4 MB (45.15%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -1,39 +1,34 @@
 class Solution {
     public int calculate(String s) {
-        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> stack = new Stack();
 
-        int i = 0;
-        char prevSign = ' ';
-        while (i < s.length()) {
+        int value = 0;
+        char prevSign = '+';
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-
-            // 遇到数字了
-            if (Character.isDigit(c)) {
-                int num = 0;
-                // 就要把这个数字处理到底
-                while (i < s.length() && Character.isDigit(s.charAt(i))) {
-                    num = 10 * num + (s.charAt(i) - '0');
-                    i++;
-                }
-
-                // 结合之前的prevSign，把这个数字收获结果
-                if (prevSign == '-') {
-                    stack.push(-num);
-                } else if (prevSign == '*') {
-                    stack.push(stack.pop() * num);
-                } else if (prevSign == '/') {
-                    stack.push(stack.pop() / num);
-                } else {
-                    stack.push(num);
+            
+            if (Character.isDigit(c) || i == s.length() - 1) {
+                if (c != ' ') value = value * 10 + (c - '0');        
+                if (i == s.length() - 1) {
+                    if (prevSign == '+') stack.push(value);
+                    else if (prevSign == '-') stack.push(-value);
+                    else if (prevSign == '*') stack.push(stack.pop() * value);
+                    else if (prevSign == '/') stack.push(stack.pop() / value);
                 }
             } else {
-                if (c != ' ') prevSign = c;
-                i++;
+                if (c == '+' || c == '-' || c == '*' || c == '/') {
+                    if (prevSign == '+') stack.push(value);
+                    else if (prevSign == '-') stack.push(-value);
+                    else if (prevSign == '*') stack.push(stack.pop() * value);
+                    else if (prevSign == '/') stack.push(stack.pop() / value);
+
+                    value = 0;
+                    prevSign = c;
+                }
             }
         }
 
         int res = 0;
-
         while (!stack.isEmpty()) {
             res += stack.pop();
         }
