29857befa0fc99e569c4c6971bb8aea5269e5b5d
zxcv5678 <122472773+zxcv5678@users.noreply.github.com>
2024-09-26 00:01:55 -0700

    Time: 0 ms (100%), Space: 42 MB (86.43%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -3,15 +3,16 @@ class Solution {
         int left = 0;
         int right = nums.length - 1;
 
+        // 保留至少两个数字，防止边界问题
         while (left + 1 < right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid-1] < nums[mid] && nums[mid] > nums[mid+1]) {
+            if (nums[mid - 1] < nums[mid] && nums[mid] > nums[mid + 1]) {
                 return mid;
                 // min...1,2...min，说明大数在右边
-            } else if (nums[mid+1] > nums[mid]) {
+            } else if (nums[mid + 1] > nums[mid]) {
                 left = mid;
                 // min...8,5...min，说明大数在左边
-            } else if (nums[mid-1] > nums[mid]) {
+            } else if (nums[mid - 1] > nums[mid]) {
                 right = mid;
             }
         }
