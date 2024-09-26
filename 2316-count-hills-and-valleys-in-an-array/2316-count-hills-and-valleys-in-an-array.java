482aba6196bebbdfdba7aad25c4491763118b4f0
zxcv5678 <122472773+zxcv5678@users.noreply.github.com>
2024-09-25 17:29:58 -0700

    Time: 0 ms (100%), Space: 41.6 MB (31.74%) - LeetHub

--- 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.java
+++ 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.java
@@ -1,23 +1,33 @@
 class Solution {
     public int countHillValley(int[] nums) {
-        boolean prevIncreasing = false;
-        boolean prevDecreasing = false;
+        // 两个variable记录之前山峰的状态
+        boolean prevIncrease = false;
+        boolean prevDecrease = false;
 
         int res = 0;
 
+        // 不用考虑等于的情况，因为等于的情况，这两个variable都不会更新
         for (int i = 1; i < nums.length ; i++) {
+            // 现在是上升的
             if (nums[i] > nums[i - 1]) {
-                if (prevDecreasing) {
+                // 之前是下降的
+                if (prevDecrease) {
+                    // 说明前一个是低谷
                     res++;
                 }
-                prevIncreasing = true;
-                prevDecreasing = false;
+                // 更新状态
+                prevIncrease = true;
+                prevDecrease = false;
+            // 现在是下降的
             } else if (nums[i] < nums[i - 1]) {
-                if (prevIncreasing) {
+                // 之前是上升的
+                if (prevIncrease) {
+                    // 说明前一个是山顶
                     res++;
                 }
-                prevDecreasing = true;
-                prevIncreasing = false;
+                // 更新状态
+                prevDecrease = true;
+                prevIncrease = false;
             }
         }
         return res;
