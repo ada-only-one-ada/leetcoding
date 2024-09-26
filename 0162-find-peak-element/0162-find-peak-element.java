b911406bfad26c887906b2d39ac4a861776f2cf9
zxcv5678 <122472773+zxcv5678@users.noreply.github.com>
2024-09-26 00:09:43 -0700

    Time: 0 ms (100%), Space: 42.7 MB (13.76%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,12 +1,12 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        // [-2147483648] expect 0
         int left = 0;
         int right = nums.length;
 
         while (left < right) {
             int mid = left + (right - left) / 2;
-
+               
+            // [1, 2]
             if (nums[mid] > getValue(mid - 1, nums) && nums[mid] > getValue(mid + 1, nums)) {
                 return mid;
             } else if (nums[mid] < getValue(mid + 1, nums)) {
@@ -16,6 +16,7 @@ class Solution {
             }
         }
 
+        // [-2147483648] expect 0
         return left;
     }
 
