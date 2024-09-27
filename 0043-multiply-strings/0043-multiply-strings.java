dbb349592a8034001f06735c1ee5b85ff8e1dc4d
zxcv5678 <122472773+zxcv5678@users.noreply.github.com>
2024-09-26 17:11:49 -0700

    Time: 15 ms (8.73%), Space: 45.1 MB (8.01%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.java
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -35,25 +35,22 @@ class Solution {
 
     public String additionHelper(String s1, String s2) {
         StringBuilder sb = new StringBuilder();
-        int prev = 0;
+        int carry = 0;
 
         int i1 = s1.length() - 1;
         int i2 = s2.length() - 1;
-        while (i1 >= 0 || i2 >= 0) {
-            int digit1 = 0;
-            int digit2 = 0;
-            
-            if (i1 >= 0) digit1 = s1.charAt(i1) - '0';
-            if (i2 >= 0) digit2 = s2.charAt(i2) - '0';
+        while (i1 >= 0 || i2 >= 0 || carry != 0) {
+            int digit1 = i1 >= 0? s1.charAt(i1) - '0' : 0;
+            int digit2 = i2 >= 0? s2.charAt(i2) - '0' : 0;
 
-            int currSum = digit1 + digit2 + prev;
-            sb.append(currSum % 10);
-            prev = currSum / 10;
+            int sum = digit1 + digit2 + carry;
+            sb.append(sum % 10);
+            carry = sum / 10;
+         
             i1--;
             i2--;
         }
 
-        if (prev != 0) sb.append(String.valueOf(prev));
         return sb.reverse().toString();
     } 
 }
