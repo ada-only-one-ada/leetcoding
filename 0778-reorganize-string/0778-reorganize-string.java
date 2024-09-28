15b2c655be75c1e581ea0ef13a1a8d2d371a5c4e
123-qwertyuio <122472773+123-qwertyuio@users.noreply.github.com>
2024-09-28 00:44:59 -0700

    Time: 4 ms (51.59%), Space: 41.8 MB (28.34%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.java
+++ 0778-reorganize-string/0778-reorganize-string.java
@@ -22,6 +22,7 @@ class Solution {
             // 加2个字母，就可以重复加第一个，比如：ab 接着又可以加 a 了
             List<Integer> waitingList = new ArrayList<>();
             for (int i = 1; i <= 2; i++) {
+                if (waitingList.isEmpty() && queue.isEmpty()) break;
                 if (!queue.isEmpty()) {
                     int index = queue.poll();
                     res.append((char)(index + 'a'));
