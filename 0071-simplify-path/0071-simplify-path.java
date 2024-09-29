8eb1ad58ee3de15e652067bfabdbd0507192d89c
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-09-29 14:44:58 -0700

    Time: 4 ms (86.38%), Space: 43.3 MB (62.82%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -3,19 +3,16 @@ class Solution {
         Stack<String> stack = new Stack<>();
         StringBuilder temp = new StringBuilder();
         int numOfDots = 0;
-        path += '/'; // 这样可以确保最后一个 local path也可以被收获
+        path += '/';
 
         for (int i = 0; i < path.length(); i++) {
             char curr = path.charAt(i);
 
             if (curr == '/') {
-                // 重复的 '/' 可以跳过
                 if (i > 0 && path.charAt(i - 1) == '/') continue; 
 
-                // 全是 dots 且只有两个 dots 且 stack 不为空，上一级path可以移除
                 if (numOfDots == 2 && numOfDots == temp.length() && !stack.isEmpty()) {
                     stack.pop();
-                // 全是 dots 且 dots 数量多于 3个，或者不全是 dots的情况，加入 stack
                 } else if (numOfDots >= 3 || numOfDots != temp.length()) {
                     stack.push(temp.toString());
                 }
@@ -28,17 +25,9 @@ class Solution {
             } 
         }
 
-        /*
-        if (numOfDots == 2 && numOfDots == temp.length() && !stack.isEmpty()) {
-            stack.pop();
-        } else if (numOfDots >= 3 || numOfDots != temp.length()) {
-            stack.push(temp.toString());
-        }
-        */
-
         StringBuilder res = new StringBuilder();
-        while (!stack.isEmpty()) {
-            res.insert(0, "/" + stack.pop());
+        for (String s: stack) {
+            res.append("/" + s);
         }
 
         return res.length() == 0? "/" : res.toString();
