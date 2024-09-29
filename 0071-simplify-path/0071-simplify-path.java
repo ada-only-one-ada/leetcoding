fc38d4c72f799252f097da36d3f08c9e1d9f634e
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-09-29 14:12:26 -0700

    Time: 16 ms (5.15%), Space: 43.3 MB (51.62%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,52 @@
+class Solution {
+    public String simplifyPath(String path) {
+        if (path.length() == 1 && path.charAt(0) == '/') return path;
+
+        Stack<String> stack = new Stack<>();
+        StringBuilder temp = new StringBuilder();
+        int numOfDots = 0;
+
+        for (int i = 0; i < path.length(); i++) {
+            char curr = path.charAt(i);
+
+            if (curr != '/') {
+                temp.append(curr);
+                if (curr == '.') numOfDots++;
+            } else if (curr == '/') {
+                if (i > 0 && path.charAt(i - 1) == '/') continue;
+
+                if (numOfDots == temp.length()) {
+                    if (numOfDots == 2 && !stack.isEmpty()) {
+                        stack.pop();
+                    } else if (numOfDots >= 3) {
+                        stack.push(temp.toString());
+                    }
+                } else {
+                    stack.push(temp.toString());
+                }
+                temp.setLength(0);
+                numOfDots = 0;
+            }
+        }
+
+
+        if (numOfDots == temp.length()) {
+            if (numOfDots == 2 && !stack.isEmpty()) {
+                stack.pop();
+            } else if (numOfDots == 3) {
+                stack.push("...");
+            }
+        } else {
+            stack.push(temp.toString());
+        }
+
+        StringBuilder res = new StringBuilder();
+        while (!stack.isEmpty()) {
+            System.out.println(stack.peek() + " ");
+            res.insert(0, stack.pop());
+            res.insert(0, "/");
+        }
+
+        return res.length() == 0? "/" : res.toString();
+    }
+}
