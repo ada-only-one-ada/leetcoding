3f555f57257d6ddd7331f4252bb9ecb1021d91f7
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-09-29 15:15:08 -0700

    Time: 6 ms (33.91%), Space: 43 MB (85.21%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,37 +1,26 @@
 class Solution {
     public String simplifyPath(String path) {
-        Stack<String> stack = new Stack<>();
-        StringBuilder temp = new StringBuilder();
-        int numOfDots = 0;
-        path += '/';
+        // String[] paths = path.split('/');
+        // error: incompatible types: char cannot be converted to String
+        String[] paths = path.split("/");
+        Stack<String> stack = new Stack();
 
-        for (int i = 0; i < path.length(); i++) {
-            char curr = path.charAt(i);
+        for (int i = 0; i < paths.length; i++) {
+            // String currPath = paths[i].trim();
+            String currPath = paths[i];
+            // 比如连续的'//'，split以后会变成[]空的
+            // 妈的，string用双引号，草！
+            if (currPath.length() == 0 || currPath.equals(".")) continue;
 
-            if (curr == '/') {
-                if (i > 0 && path.charAt(i - 1) == '/') continue; 
-
-                if (numOfDots == 2 && numOfDots == temp.length() && !stack.isEmpty()) {
-                    stack.pop();
-                } else if (numOfDots >= 3 || numOfDots != temp.length()) {
-                    stack.push(temp.toString());
-                }
-
-                temp.setLength(0);
-                numOfDots = 0;
+            if (currPath.equals("..") && !stack.isEmpty()) {
+                stack.pop();
             } else {
-                temp.append(curr);
-                if (curr == '.') numOfDots++;
-            } 
+                if (!currPath.equals("..")) {
+                    stack.push(currPath);
+                }
+            }
         }
         
-        // return "/" + String.join("/", stack);
-
-        String res = "";
-        for (String s: stack) {
-            res += "/" + s;
-        }
-
-        return res.length() == 0? "/" : res;
+        return "/" + String.join("/", stack);
     }
 }
