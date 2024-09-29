f0105baf79ff5830f1c98ad6c29f22eefdb2e3ba
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-09-29 12:57:13 -0700

    Time: 0 ms (100%), Space: 41.4 MB (98.91%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -3,10 +3,8 @@ class Solution {
         if (n == 0) return 1;
         if (n == 1) return x;
         if (n == -1) return 1 / x;
-
-        double half = myPow(x, n / 2);
         
-        if (n % 2 == 0) return half * half;
-        return half * half * myPow(x, n % 2);
+        double half = myPow(x, n / 2);
+        return half * half * myPow(x, n % 2); // 每一次递归都有可能出现odd，不只是一次
     }
 }
