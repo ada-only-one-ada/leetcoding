43533310a71a78a4a930f09feb92b3e884c37b07
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-09-29 13:02:41 -0700

    Time: 19 ms (99.16%), Space: 46.4 MB (54.02%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,20 +1,23 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int count = 0;
-        int[] sum = new int[nums.length + 1];
-       
-        for (int i = 1; i < sum.length; i++) {
-            sum[i] = sum[i - 1] + nums[i - 1];
-          
-            if (sum[i] == k) count++;
-           
-            for (int j = 1; j < i; j++) {
-                if (sum[i] - sum[j] == k) {
-                    count++;
-                }
-            }
+        int[] prefix = new int[nums.length];
+        prefix[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            prefix[i] = prefix[i - 1] + nums[i];
         }
 
-        return count;
+        int res = 0;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1); 
+        for (int i = 0; i < prefix.length; i++) {
+            if (map.containsKey(prefix[i] - k)) {
+                res += map.get(prefix[i] - k);
+            } 
+
+            map.put(prefix[i], map.getOrDefault(prefix[i], 0) + 1);
+        }
+
+        return res;
     }
 }
