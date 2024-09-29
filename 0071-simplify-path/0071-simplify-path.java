75f14ef815daaa6d333699f7478cdc317111ced6
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-09-29 15:14:30 -0700

    Time: 9 ms (15.68%), Space: 44.8 MB (10.37%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,25 +1,37 @@
 class Solution {
     public String simplifyPath(String path) {
-        // String[] paths = path.split('/');
-        // error: incompatible types: char cannot be converted to String
-        String[] paths = path.split("/");
-        Stack<String> stack = new Stack();
+        Stack<String> stack = new Stack<>();
+        StringBuilder temp = new StringBuilder();
+        int numOfDots = 0;
+        path += '/';
 
-        for (int i = 0; i < paths.length; i++) {
-            String currPath = paths[i];
-            // 比如连续的'//'，split以后会变成[]空的
-            // 妈的，string用双引号，草！
-            if (currPath.length() == 0 || currPath.equals(".")) continue;
+        for (int i = 0; i < path.length(); i++) {
+            char curr = path.charAt(i);
 
-            if (currPath.equals("..") && !stack.isEmpty()) {
-                stack.pop();
-            } else {
-                if (!currPath.equals("..")) {
-                    stack.push(currPath);
+            if (curr == '/') {
+                if (i > 0 && path.charAt(i - 1) == '/') continue; 
+
+                if (numOfDots == 2 && numOfDots == temp.length() && !stack.isEmpty()) {
+                    stack.pop();
+                } else if (numOfDots >= 3 || numOfDots != temp.length()) {
+                    stack.push(temp.toString());
                 }
-            }
+
+                temp.setLength(0);
+                numOfDots = 0;
+            } else {
+                temp.append(curr);
+                if (curr == '.') numOfDots++;
+            } 
         }
         
-        return "/" + String.join("/", stack);
+        // return "/" + String.join("/", stack);
+
+        String res = "";
+        for (String s: stack) {
+            res += "/" + s;
+        }
+
+        return res.length() == 0? "/" : res;
     }
 }
