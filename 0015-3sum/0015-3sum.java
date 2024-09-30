440df7d64674276216583a34977a9b02348f1c58
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-09-30 00:46:10 -0700

    Time: 33 ms (40.84%), Space: 51.1 MB (86.77%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,25 +1,22 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-
         Arrays.sort(nums);
+      
+        // lock住i1
         int i1 = 0;
         while (i1 <= nums.length - 3) {
             int i2 = i1 + 1;
             int i3 = nums.length - 1;
-            
-            while (i2 <= nums.length - 2&& i2 < i3) {
+
+            while (i2 < i3) {
                 int sum = nums[i1] + nums[i2] + nums[i3];
                 if (sum == 0) {
-                    List<Integer> validRes = new ArrayList<>();
-                    validRes.add(nums[i1]);
-                    validRes.add(nums[i2]);
-                    validRes.add(nums[i3]);
-                    res.add(validRes);
+                    res.add(Arrays.asList(nums[i1], nums[i2], nums[i3]));  
                     i2++;
                     i3--;
-                    while (i2 <= nums.length - 2&& nums[i2] == nums[i2 - 1]) i2++;
-                    while (i2 <= nums.length - 2&& nums[i3] == nums[i3 + 1]) i3--;
+                    while (i2 < i3 && nums[i2] == nums[i2 - 1]) i2++;
+                    while (i2 < i3 && nums[i3] == nums[i3 + 1]) i3--;
                 } else if (sum < 0) {
                     i2++;
                 } else if (sum > 0) {
@@ -30,7 +27,7 @@ class Solution {
             i1++;
             while (i1 <= nums.length - 3 && nums[i1] == nums[i1 - 1]) i1++;
         }
-
+        
         return res;
     }
 }
