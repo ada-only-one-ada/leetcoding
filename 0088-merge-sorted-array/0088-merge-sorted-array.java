0c0e79ef2fcf4a5a108677ea82a2e115ea718449
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-09-29 21:13:58 -0700

    Time: 0 ms (100%), Space: 42.1 MB (63.04%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i1 = m - 1;
+        int i2 = n - 1;
+        int index = m + n - 1;
+
+        while (i1 >= 0 && i2 >= 0) {
+            if (nums1[i1] >= nums2[i2]) {
+                nums1[index--] = nums1[i1--];
+            } else {
+                nums1[index--] = nums2[i2--];
+            }
+        }
+
+        while (i1 >= 0 && index >= 0) {
+            nums1[index--] = nums1[i1--];
+        }
+
+        while (i2 >= 0 && index >= 0) {
+            nums1[index--] = nums2[i2--];
+        }
+    }
+}
