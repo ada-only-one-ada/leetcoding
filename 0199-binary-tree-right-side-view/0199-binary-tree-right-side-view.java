43f6f8c63c1bd0b8a69f949b5c52ad2acd151b2f
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-01 16:16:54 -0700

    Time: 0 ms (100%), Space: 41.8 MB (90.63%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -1,28 +1,29 @@
 class Solution {
     List<Integer> res = new ArrayList<>();
-    
     public List<Integer> rightSideView(TreeNode root) {
         if (root == null) return res;
-        helper(root, 0);
+        dfs(root, 0);
         return res;
     }
 
-    public void helper(TreeNode root, int currHeight) {
+    public void dfs(TreeNode root, int currHeight) {
         if (root == null) return;
 
-        // 在新的层级首次访问时添加节点值
         if (currHeight == res.size()) {
             res.add(root.val);
         }
 
         // 首先尝试遍历右子树
         if (root.right != null) {
-            helper(root.right, currHeight + 1);
+            dfs(root.right, currHeight + 1);
+        } 
+        
+        // 再尝试遍历左子树
+        if (root.left != null ) {
+            dfs(root.left, currHeight + 1);
         }
-        // 如果右子树不存在，转向左子树
-        if (root.right == null || getHeight(root.left) > getHeight(root.right)) {
-            helper(root.left, currHeight + 1);
-        }
+
+        
     }
 
     public int getHeight(TreeNode root) {
