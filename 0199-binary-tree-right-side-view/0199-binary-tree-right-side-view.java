1422c1850fab64851d5ab24cdf06707e57d76187
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-01 16:04:46 -0700

    Time: 0 ms (100%), Space: 42.5 MB (9.69%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -1,24 +1,32 @@
 class Solution {
+    List<Integer> res = new ArrayList<>();
+    
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
         if (root == null) return res;
+        helper(root, 0);
+        return res;
+    }
 
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
+    public void helper(TreeNode root, int currHeight) {
+        if (root == null) return;
 
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-                if (i == size - 1) {
-                    res.add(node.val);
-                }
-
-                if (node.left != null) queue.add(node.left);
-                if (node.right != null) queue.add(node.right);
-            }
+        // 在新的层级首次访问时添加节点值
+        if (currHeight == res.size()) {
+            res.add(root.val);
         }
 
-        return res;
+        // 首先尝试遍历右子树
+        if (root.right != null) {
+            helper(root.right, currHeight + 1);
+        }
+        // 如果右子树不存在，转向左子树
+        if (root.right == null || getHeight(root.left) > getHeight(root.right)) {
+            helper(root.left, currHeight + 1);
+        }
+    }
+
+    public int getHeight(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + Math.max(getHeight(root.left), getHeight(root.right));
     }
 }
