8a6f694a69c52964d936d8df42cee98f61e49cf1
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-01 16:48:21 -0700

    Time: 1 ms (26.09%), Space: 42.5 MB (74.66%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.java
@@ -2,20 +2,21 @@ class Solution {
     public int findKthPositive(int[] arr, int k) {
         if (k < arr[0]) return k;
 
-        int totalInterval = arr[0] - 1;
+        int totalFound = arr[0] - 1;
 
         for (int i = 1; i < arr.length; i++) {
             int start = arr[i - 1];
             int end = arr[i];
-            int currTotalMissing = end - start - 1;
+            
+            int currFound = end - start - 1;
 
-            if (totalInterval + currTotalMissing >= k) {
-                return (k - totalInterval) + start;
+            if (totalFound + currFound >= k) {
+                return (k - totalFound) + start;
             } 
                 
-            totalInterval += currTotalMissing;
+            totalFound += currFound;
         }
 
-        return arr[arr.length - 1] + (k - totalInterval);
+        return arr[arr.length - 1] + (k - totalFound);
     }
 }
