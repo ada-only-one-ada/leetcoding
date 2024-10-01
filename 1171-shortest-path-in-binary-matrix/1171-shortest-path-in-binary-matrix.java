85012fef293bbf74b984538739f9d92b0acd7a72
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-01 15:35:21 -0700

    Time: 24 ms (26.75%), Space: 45.5 MB (42.59%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.java
@@ -15,37 +15,37 @@ class Solution {
             Arrays.fill(row, Integer.MAX_VALUE);
         }
 
-        Queue<int[]> queue = new PriorityQueue<>((a, b) -> {
-            return a[2] - b[2];
-        });
+        Queue<int[]> queue = new LinkedList<>();
         // 走到起点本身占一个cell
         queue.add(new int[]{0, 0, 1});
 
         while (!queue.isEmpty()) {
-            int[] curr = queue.poll();
-            int currRow = curr[0];
-            int currCol = curr[1];
-            int usedCells = curr[2];
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int[] curr = queue.poll();
+                int currRow = curr[0];
+                int currCol = curr[1];
+                int usedCells = curr[2];
 
-            if (currRow == grid.length - 1 && currCol == grid[0].length - 1) return usedCells;
-            if (visited[currRow][currCol] == true) continue;
+                if (currRow == grid.length - 1 && currCol == grid[0].length - 1) return usedCells;
+                if (visited[currRow][currCol] == true) continue;
 
-            for (int [] direction: directions) {
-                int nextRow = currRow + direction[0];
-                int nextCol = currCol + direction[1];
+                for (int [] direction: directions) {
+                    int nextRow = currRow + direction[0];
+                    int nextCol = currCol + direction[1];
                
-                // 越界不走
-                if (nextRow < 0 || nextCol < 0 || nextRow >= grid.length || nextCol >= grid[0].length) continue;
-                // 被blocked住不走
-                if (grid[nextRow][nextCol] == 1) continue;
-                // 路径更远不走
-                if (usedCells + 1 > memo[nextRow][nextCol]) continue;
+                    // 越界不走
+                    if (nextRow < 0 || nextCol < 0 || nextRow >= grid.length || nextCol >= grid[0].length) continue;
+                    // 被blocked住不走
+                    if (grid[nextRow][nextCol] == 1) continue;
+                   // 路径更远不走
+                   if (usedCells + 1 > memo[nextRow][nextCol]) continue;
+                   queue.add(new int[]{nextRow, nextCol, usedCells + 1});
+                }
 
-                queue.add(new int[]{nextRow, nextCol, usedCells + 1});
+                visited[currRow][currCol] = true;
+                memo[currRow][currCol] = Math.min(usedCells, memo[currRow][currCol]);
             }
-
-            visited[currRow][currCol] = true;
-            memo[currRow][currCol] = Math.min(usedCells, memo[currRow][currCol]);
         }
 
         return -1;
