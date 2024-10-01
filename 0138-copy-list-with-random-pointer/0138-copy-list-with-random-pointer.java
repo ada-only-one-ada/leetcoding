731738dfa92a1c10205f9a2949eab8e12663a822
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-09-30 22:34:17 -0700

    Time: 0 ms (100%), Space: 44.4 MB (41.44%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -1,84 +1,48 @@
- /*
-// Definition for a Node.
-class Node {
-    int val;
-    Node next;
-    Node random;
-
-    public Node(int val) {
-        this.val = val;
-        this.next = null;
-        this.random = null;
-    }
-}
-*/
-
 class Solution {
     public Node copyRandomList(Node head) {
         if (head == null) return null;
 
-        // curr 指针遍历：
-        Node curr = head;
-        while (curr != null) {
-            // 下一个遍历节点先存起来
-            Node nextCurr = curr.next; 
+        Node origin = head;
+        while (origin != null) {
+            Node nextOrigin = origin.next;
 
-            // 复制一个curr的新节点
-            Node copyNode = new Node(curr.val);
+            Node copy = new Node(origin.val);
+            origin.next = copy;
+            copy.next = nextOrigin;
 
-            // curr -> copyNode -> nextCurr
-            // 原始1 -> copy1 -> 原始2 -> copy2.....
-            curr.next = copyNode;
-            copyNode.next = nextCurr;
-            curr = nextCurr;
+            origin = nextOrigin;
         }
 
-        // curr 指针遍历：
-        curr = head;
-        while (curr != null) {
-            // 之前复制的新节点是它的下一个，即next
-            Node copyNode = curr.next;
-
-            // 如果当前原节点 random 不为 null
-            if (curr.random != null) {
-                // 那么复制的新节点的random，是原节点的random的复制，即原节点.random.next
-                copyNode.random = curr.random.next;
+        origin = head;
+        while (origin != null) {
+            Node nextOrigin = origin.next.next;
+           
+            Node copy = origin.next;
+            if (origin.random != null) {
+                copy.random = origin.random.next;
             }
 
-            // curr 跳到下一个非复制节点
-            curr = curr.next.next;
+            origin = nextOrigin;
         }
 
-        // 创建一个新链表头，以便返回
         Node newHead = head.next;
-        // 新链表头指针遍历
-        Node newHeadCurr = newHead;
+        Node newCopy = newHead;
 
-        // curr 指针遍历：
-        curr = head;
-        while (curr != null) {
-            Node nextCurr = curr.next.next;
-            // 拿到当前节点的复制节点
-            Node copyNode = curr.next;
+        origin = head;
+        while (origin != null) {
+            Node nextOrigin = origin.next.next;
 
-            // 拿到当前节点的原始 next 节点，它被连到了复制节点的下一个
-            Node prevNext = copyNode.next; // 复原
-            // 当前节点的原始 next 节点不为空
-
-            // 复原原始节点和它的next的连接
-            curr.next = prevNext;
-
-            if (prevNext != null) {
-                // 新头节点的next即是原始 next的复制
-                newHeadCurr.next = prevNext.next;
-                newHeadCurr = newHeadCurr.next;
+            Node copy = origin.next;
+            Node originNext = copy.next; 
+            origin.next = originNext; // restore
+            if (originNext != null) {
+                newCopy.next = originNext.next;
+                newCopy = newCopy.next;
             } else {
-                newHeadCurr.next = null; // 避免最后一个节点要设置成null
+                newCopy.next = null; // the last node 
             }
 
-            // curr 跳到下一个非复制节点，使用原始链表的下一个节点继续
-            // 在一开始保存的，或者使用curr = prevNext（已被复原）
-            curr = nextCurr;
+            origin = nextOrigin;
         }
 
         return newHead;
