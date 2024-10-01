faeabd5c2e789091029caacd2979baebed1d6dbe
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-01 16:19:09 -0700

    Time: 0 ms (100%), Space: 41.9 MB (64.45%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -1,5 +1,6 @@
 class Solution {
     List<Integer> res = new ArrayList<>();
+    
     public List<Integer> rightSideView(TreeNode root) {
         if (root == null) return res;
         dfs(root, 0);
@@ -21,13 +22,6 @@ class Solution {
         // 再尝试遍历左子树
         if (root.left != null ) {
             dfs(root.left, currHeight + 1);
-        }
-
-        
-    }
-
-    public int getHeight(TreeNode root) {
-        if (root == null) return 0;
-        return 1 + Math.max(getHeight(root.left), getHeight(root.right));
+        }   
     }
 }
