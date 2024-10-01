1167e7600d5c6bfd06bb8dbe105c7ee50b2f88e2
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-01 16:18:38 -0700

    Time: 0 ms (100%), Space: 41.8 MB (90.63%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -1,20 +1,33 @@
 class Solution {
+    List<Integer> res = new ArrayList<>();
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        dfs(root, result, 0);
-        return result;
+        if (root == null) return res;
+        dfs(root, 0);
+        return res;
     }
 
-    private void dfs(TreeNode curr, List<Integer> result, int depth) {
-        if (curr == null) return;
-        
-        // 只有当到达一个新的层级时才添加
-        if (depth == result.size()) {
-            result.add(curr.val);
+    public void dfs(TreeNode root, int currHeight) {
+        if (root == null) return;
+
+        if (currHeight == res.size()) {
+            res.add(root.val);
         }
+
+        // 首先尝试遍历右子树
+        if (root.right != null) {
+            dfs(root.right, currHeight + 1);
+        } 
         
-        // 先右后左地递归
-        dfs(curr.right, result, depth + 1);
-        dfs(curr.left, result, depth + 1);
+        // 再尝试遍历左子树
+        if (root.left != null ) {
+            dfs(root.left, currHeight + 1);
+        }
+
+        
+    }
+
+    public int getHeight(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + Math.max(getHeight(root.left), getHeight(root.right));
     }
 }
