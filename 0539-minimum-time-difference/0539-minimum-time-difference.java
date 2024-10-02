10981c95e6ddc8bfdfb784e572abd5c48fbb98a0
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-01 23:35:39 -0700

    Time: 3 ms (88.65%), Space: 45 MB (84.21%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.java
+++ 0539-minimum-time-difference/0539-minimum-time-difference.java
@@ -1,32 +1,32 @@
 class Solution {
     public int findMinDifference(List<String> timePoints) {
-        //根据题意，一共有 24×60=1440 种不同的时间。
-        // 由鸽巢原理可知，如果 timePoints 的长度超过 1440，那么必然会有两个相同的时间，此时可以直接返回 0
         if (timePoints.size() > 24 * 60) return 0;
-        
-        List<Integer> minutes = new ArrayList<>();
 
-        for (String timePoint: timePoints) {
-            String[] time = timePoint.split(":");
-            int minute = Integer.valueOf(time[0]) * 60 + Integer.valueOf(time[1]);
-            minutes.add(minute);
+        int min = Integer.MAX_VALUE;
+        boolean[] minute = new boolean[24 * 60];
+
+        for (int i = 0; i < timePoints.size(); i++) {
+            String[] time = timePoints.get(i).split(":");
+            int total = Integer.valueOf(time[0]) * 60 + Integer.valueOf(time[1]);
+            if (minute[total]) return 0; // two same time
+            minute[total] = true;
         }
 
-        Collections.sort(minutes);
+        int prev = -1;
+        int first = -1;
 
-        int minDiff = Integer.MAX_VALUE;
-        for (int i = 1; i < minutes.size(); i++) {
-            int currMinute = minutes.get(i);
-            int lastMinute = minutes.get(i - 1);
-            minDiff = Math.min(minDiff, Math.abs(currMinute - lastMinute));
+        for (int i = 0; i < minute.length; i++) {
+            if (!minute[i]) continue;
+            if (first == -1) first = i;
 
-            // 最后一个要跟第 0 个比较，[00: 23, ... , 23:59]
-            if (i == minutes.size() - 1) {
-                currMinute -= 24 * 60;
-                minDiff = Math.min(minDiff, Math.abs(currMinute - minutes.get(0)));
+            if (prev != -1) {
+                min = Math.min(min, i - prev);
             }
+            prev = i;
         }
-  
-        return minDiff;
+
+        min = Math.min(min, first + 24 * 60 - prev);
+
+        return min;
     }
 }
