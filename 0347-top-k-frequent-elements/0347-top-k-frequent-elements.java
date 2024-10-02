9a207b5fa91e931b95091b67a626e96c87373285
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-01 21:14:56 -0700

    Time: 105 ms (5.14%), Space: 46.4 MB (82.27%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,22 +1,29 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
-        for (int num: nums) {
+
+        for (int num: nums){
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        Queue<Integer> heap = new PriorityQueue<>((n1, n2) -> map.get(n1) - map.get(n2));
-
-        for (int key: map.keySet()) {
-            heap.add(key);
-            if (heap.size() > k) {
-                heap.poll();
-            }
-        }
-
         int[] res = new int[k];
-        for (int i = res.length - 1; i >= 0; i--) {
-            res[i] = heap.poll();
+
+        
+        int max = Integer.MIN_VALUE;
+        int maxKey = 0;
+        int index = 0;
+        
+        while (index < k){
+            for (int num: map.keySet()){
+                if (map.get(num) > max){
+                    maxKey = num;
+                }
+                max = Math.max(max, map.get(num));
+            }
+            
+            res[index++] = maxKey;
+            map.remove(maxKey);
+            max = Integer.MIN_VALUE;
         }
 
         return res;
