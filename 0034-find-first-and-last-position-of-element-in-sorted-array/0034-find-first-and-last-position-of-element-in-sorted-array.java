48c3b9f7bb818a8e5a994fbf1f7a0f7107036a09
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-01 18:44:21 -0700

    Time: 0 ms (100%), Space: 45.9 MB (51.27%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -3,24 +3,23 @@ class Solution {
         int [] res = new int[]{-1, -1};
         if (nums.length == 0) return res;
 
-        // first pos of target 
         int left = 0;
         int right = nums.length;
         while (left < right) {
             int mid = left + (right - left) / 2;
-            // 说明第一个target肯定在 mid，或者 mid 之前，往前找
             if (nums[mid] == target) {
                 right = mid;
-            // 说明第一个target肯定在 mid 之后
             } else if (nums[mid] < target) {
                 left = mid + 1;
-            // 说明第一个target肯定在 mid之前
             } else if (nums[mid] > target) {
                 right = mid;
             }
         }
-        if (nums[left] != target) return res;
-        res[0] = left;
+        if (left >= 0 && left < nums.length && nums[left] == target) {
+            res[0] = left;
+        } else {
+            return res;
+        }
 
         left = 0;
         right = nums.length;
@@ -28,14 +27,11 @@ class Solution {
 
         while (left < right) {
             int mid = left + (right - left) / 2;
-            // 最后一个是mid，或者mid之后
             if (nums[mid] == target) {
                 rightMost = mid;
                 left = mid + 1;
-            // 最后一个肯定在mid之后
             } else if (nums[mid] < target) {
                 left = mid + 1;
-            // 最后一个肯定在mid之前
             } else if (nums[mid] > target) {
                 right = mid;
             }
