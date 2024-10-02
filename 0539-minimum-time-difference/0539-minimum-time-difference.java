6e35a072d554fb2af4194241113943ce75416f6f
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-01 23:43:02 -0700

    Time: 3 ms (88.65%), Space: 45.3 MB (68.49%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.java
+++ 0539-minimum-time-difference/0539-minimum-time-difference.java
@@ -1,32 +1,34 @@
 class Solution {
     public int findMinDifference(List<String> timePoints) {
-        if (timePoints.size() > 24 * 60) return 0;
+        if (timePoints.size() > 25 * 60) return 0;
 
-        int min = Integer.MAX_VALUE;
-        boolean[] minute = new boolean[24 * 60];
-
+        boolean[] validMinutes = new boolean[24 * 60];
         for (int i = 0; i < timePoints.size(); i++) {
             String[] time = timePoints.get(i).split(":");
-            int total = Integer.valueOf(time[0]) * 60 + Integer.valueOf(time[1]);
-            if (minute[total]) return 0; // two same time
-            minute[total] = true;
+            int index = Integer.valueOf(time[0]) * 60 + Integer.valueOf(time[1]);
+            
+            if (validMinutes[index]) return 0;
+            validMinutes[index] = true;
         }
 
-        int prev = -1;
-        int first = -1;
+        int res = Integer.MAX_VALUE;
 
-        for (int i = 0; i < minute.length; i++) {
-            if (!minute[i]) continue;
-            if (first == -1) first = i;
+        int firstValid = -1;
+        int prevValid = -1;
 
-            if (prev != -1) {
-                min = Math.min(min, i - prev);
+        for (int i = 0; i < validMinutes.length; i++) {
+            if (!validMinutes[i]) continue;
+
+            if (firstValid == -1) firstValid = i;
+
+            if (prevValid != -1) {
+                res = Math.min(res, i - prevValid);
             }
-            prev = i;
+
+            prevValid = i;
         }
 
-        min = Math.min(min, first + 24 * 60 - prev);
-
-        return min;
+        res = Math.min(res, firstValid + 24 * 60 - prevValid);
+        return res;
     }
 }
