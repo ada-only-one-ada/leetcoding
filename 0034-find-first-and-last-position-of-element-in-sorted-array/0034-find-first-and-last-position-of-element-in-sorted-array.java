a1afcdcfc11ab14fda245486e2593ec34956e412
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-01 18:25:30 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,37 +1,41 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        if (nums.length == 0) return new int[]{-1, -1};
+        int [] res = new int[]{-1, -1};
+        if (nums.length == 0) return res;
 
-        int leftMost = -1;
-        int startIndex = 0;
-        int endIndex = nums.length;
-        while (startIndex < endIndex) {
-            int mid = startIndex + (endIndex - startIndex) / 2;
+        // first pos of target 
+        int left = 0;
+        int right = nums.length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            // 说明第一个target肯定在 mid，或者 mid 之前，往前找
             if (nums[mid] == target) {
-                leftMost = mid;
-                endIndex = mid;
+                right = mid;
+            // 说明第一个target肯定在 mid 之后
             } else if (nums[mid] < target) {
-                startIndex = mid + 1;
+                left = mid + 1;
+            // 说明第一个target肯定在 mid之前
             } else if (nums[mid] > target) {
-                endIndex = mid;
+                right = mid - 1;
+            }
+        }
+        if (nums[left] != target) return res;
+        res[0] = left;
+
+        left = 0;
+        right = nums.length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                left = mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
             }
         }
 
-        int rightMost = -1;
-        startIndex = 0;
-        endIndex = nums.length;
-        while (startIndex < endIndex) {
-            int mid = startIndex + (endIndex - startIndex) / 2;
-            if (nums[mid] == target) {
-                rightMost = mid;
-                startIndex = mid + 1;
-            } else if (nums[mid] < target) {
-                startIndex = mid + 1;
-            } else if (nums[mid] > target) {
-                endIndex = mid;
-            }
-        }
-
-        return new int[]{leftMost, rightMost};
+        res[1] = left;
+        return res;
     }
 }
