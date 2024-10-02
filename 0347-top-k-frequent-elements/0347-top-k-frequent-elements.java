75d3c36dd008fcf3a0232150e74b2e15ae8e3558
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-01 21:04:48 -0700

    Time: 13 ms (74.84%), Space: 47.9 MB (80.38%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,31 +1,33 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
-        for (int num: nums) {
+
+        for (int num: nums){
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        List[] bucket = new List[nums.length + 1];
-        for (int num: map.keySet()) {
-            int freq = map.get(num);
-
-            if (bucket[freq] == null) {
-                bucket[freq] = new ArrayList<>();
+        List<Integer>[] bucket = new List[nums.length + 1];
+        for (int num: map.keySet()){
+            if (bucket[map.get(num)] == null){
+                bucket[map.get(num)] = new ArrayList<>();
             }
 
-            bucket[freq].add(num);
+            bucket[map.get(num)].add(num);
+        }
+        
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = bucket.length - 1; i >= 0; i--){
+            if (k > 0 && bucket[i] != null){
+                list.addAll(bucket[i]);
+                k -= bucket[i].size();
+            }
         }
 
-        int[] res = new int[k];
-        int index = res.length - 1;
-        for (int i = bucket.length - 1; i >= 0 & k > 0; i--) {
-            if (bucket[i] == null) continue;
-
-            List<Integer> list = bucket[i];
-            for (int num: list) {
-                res[index--] = num;
-                k--;
-            }
+        int i = 0;
+        int[] res = new int[list.size()];
+        for (int num: list){
+            res[i++] = num;
         }
 
         return res;
