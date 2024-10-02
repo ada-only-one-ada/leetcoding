f0e79872e945ca1a85250798f5ffc8547b429fec
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-01 21:05:47 -0700

    Time: 15 ms (38.62%), Space: 48.8 MB (24.06%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,33 +1,22 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
-
-        for (int num: nums){
+        for (int num: nums) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        List<Integer>[] bucket = new List[nums.length + 1];
-        for (int num: map.keySet()){
-            if (bucket[map.get(num)] == null){
-                bucket[map.get(num)] = new ArrayList<>();
-            }
+        Queue<Integer> heap = new PriorityQueue<>((n1, n2) -> map.get(n1) - map.get(n2));
 
-            bucket[map.get(num)].add(num);
-        }
-        
-        List<Integer> list = new ArrayList<>();
-
-        for (int i = bucket.length - 1; i >= 0; i--){
-            if (k > 0 && bucket[i] != null){
-                list.addAll(bucket[i]);
-                k -= bucket[i].size();
+        for (int key: map.keySet()) {
+            heap.add(key);
+            if (heap.size() > k) {
+                heap.poll();
             }
         }
 
-        int i = 0;
-        int[] res = new int[list.size()];
-        for (int num: list){
-            res[i++] = num;
+        int[] res = new int[k];
+        for (int i = res.length - 1; i >= 0; i--) {
+            res[i] = heap.poll();
         }
 
         return res;
