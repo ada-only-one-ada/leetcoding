7fe6410adce5897daba29cbe2d2735b0ccf8dbf1
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-01 22:10:23 -0700

    Time: 13 ms (22.74%), Space: 46.6 MB (6.53%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.java
+++ 0539-minimum-time-difference/0539-minimum-time-difference.java
@@ -1,52 +1,28 @@
 class Solution {
     public int findMinDifference(List<String> timePoints) {
-        int minDiff = Integer.MAX_VALUE;
+        List<Integer> minutes = new ArrayList<>();
 
-        Collections.sort(timePoints, (a, b) -> {
-            String[] timeA = a.split(":");
-            String[] timeB = b.split(":");
-
-            int hourA = Integer.valueOf(timeA[0]);
-            int hourB = Integer.valueOf(timeB[0]);
-            int minA = Integer.valueOf(timeA[1]);
-            int minB = Integer.valueOf(timeB[1]);
-
-            if (hourA == hourB) {
-                return minA - minB;
-            }
-
-            return hourA - hourB;
-        });
-        
-
-        for (int i = 0; i < timePoints.size(); i++) {
-            int left = i - 1;
-            boolean isFirst = false;
-            if (i == 0) {
-                isFirst = true;
-                left = timePoints.size() - 1;
-            }
-
-            minDiff = Math.min(minDiff, compare(timePoints.get(left), timePoints.get(i), isFirst));
-            if (minDiff == 0) return minDiff;
+        for (String timePoint: timePoints) {
+            String[] time = timePoint.split(":");
+            int minute = Integer.valueOf(time[0]) * 60 + Integer.valueOf(time[1]);
+            minutes.add(minute);
         }
 
+        Collections.sort(minutes);
+
+        int minDiff = Integer.MAX_VALUE;
+        for (int i = 1; i < minutes.size(); i++) {
+            int currMinute = minutes.get(i);
+            int lastMinute = minutes.get(i - 1);
+            minDiff = Math.min(minDiff, Math.abs(currMinute - lastMinute));
+
+            if (i == minutes.size() - 1) {
+                currMinute -= 24 * 60;
+                minDiff = Math.min(minDiff, Math.abs(currMinute - minutes.get(0)));
+            }
+        }
+
+        
         return minDiff;
-    }
-
-    public int compare(String s1, String s2, boolean isFirst) {
-        if (s1.equals(s2)) return 0;
-
-        String[] timeA = s1.split(":");
-        String[] timeB = s2.split(":");
-
-        int hourA = Integer.valueOf(timeA[0]);
-        int hourB = Integer.valueOf(timeB[0]);
-        int minA = Integer.valueOf(timeA[1]);
-        int minB = Integer.valueOf(timeB[1]);
-
-        if (isFirst) hourB += 24;
-
-        return Math.abs(hourA - hourB) * 60 + (minB - minA);
     }
 }
