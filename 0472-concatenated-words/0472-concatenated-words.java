eaef9e44b3b3c97353743a811d14d03f0fbe69dc
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-05 20:34:48 -0700

    Time: 31 ms (98.58%), Space: 59.5 MB (10.94%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.java
+++ 0472-concatenated-words/0472-concatenated-words.java
@@ -3,12 +3,14 @@ class Solution {
 
     public List<String> findAllConcatenatedWordsInADict(String[] words) {
         List<String> res = new ArrayList<>();
-        Arrays.sort(words, (a, b) -> a.length() - b.length());
 
+        // 把短的先加入字典，这样可以一边遍历一边判断
+        Arrays.sort(words, (w1, w2) -> w1.length() - w2.length());
         for (String word: words) {
-            if (dfs(word, 0, 1)) {
+            if (dfs(word, 0, 0)) {
                 res.add(word);
             } else {
+                // 不是concatenated单词，加入字典，比如cat，dod
                 insert(word);
             }
         }
@@ -16,36 +18,40 @@ class Solution {
         return res;
     }
 
-    public boolean dfs(String word, int index, int numOfWords) {
-        if (index == word.length() && numOfWords >= 2) {
+    public boolean dfs(String word, int index, int numOfValidWords) {
+        // 有效单词大于2个
+        if (index == word.length() && numOfValidWords >= 2) {
             return true;
         }
 
         TrieNode node = root;
         for (int i = index; i < word.length(); i++) {
-            if (node.children[word.charAt(i) - 'a'] == null) {
+            char c = word.charAt(i);
+            if (node.children[c - 'a'] == null) {
                 return false;
             }
-            if (node.children[word.charAt(i) - 'a'].isValid) {
-                if (dfs(word, i + 1, numOfWords + 1)) {
+
+            if (node.children[c - 'a'].isValid) {
+                if (dfs(word, i + 1, numOfValidWords + 1)) {
                     return true;
                 }
             }
-            node = node.children[word.charAt(i) - 'a'];
+            node = node.children[c - 'a'];
         }
 
         return false;
     }
 
+    // 构建字典树
     public void insert(String word) {
         TrieNode node = root;
-        for (int i = 0; i < word.length(); i++) {
-            char c = word.charAt(i);
+        for (char c: word.toCharArray()) {
             if (node.children[c - 'a'] == null) {
                 node.children[c - 'a'] = new TrieNode();
             }
             node = node.children[c - 'a'];
         }
+        // 设为有效单词
         node.isValid = true;
     }
 }
