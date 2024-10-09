7d98f4bff9c004c9e222c6f4a6d1ca8b55e41c54
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-09 16:00:56 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,49 +1,32 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        int n = matrix.length;
-
-        int startValue = matrix[0][0];
-        int endValue = matrix[n - 1][n - 1] + 1;
-
-        while (startValue < endValue) {
-            int midValue = startValue + (endValue - startValue) / 2;
-            // 在这个matrix中，有count个数字比midValue小
-            int count = countLessOrEqual(matrix, midValue);
-            // 在这个matrix中，有至少k个的数字比midValue小
-            // 说明第k小的数字肯定在前面
-            // 也就是说，第k小的数字肯定在 [startValue, midValue] 之间
-            if (count >= k) {
-                endValue = midValue;
-            // 第k小的数字肯定在后面
-            // 也就是说，第k小的数字肯定在 (midValue, endValue) 之间
-            } else {
-                startValue = midValue + 1;
-            }
-        }
-
-        // startValue肯定是matrix中的一个数字
-        // 因为我们的目标是找到满足条件的最小值startValue
-        // 即矩阵中恰好有k个元素小于或等于 startValue
-        return startValue;
+        
     }
 
-    // 数小于或等于 midValue 的数有几个
-    public int countLessOrEqual(int[][] matrix, int midValue) {
-        int count = 0;        
+    public int countSmaller(int[][] matrix, int value) {
+        int count = 0;
+        int rowNum = matrix.length;
+        int colNum = matrix[0].length;
 
-        int n = matrix.length;
-        int row = 0;
-        int col = n - 1;
-        while (row < n && col >= 0) {
-            if (matrix[row][col] <= midValue) {
-                // 因为当前行是递增的，所以当前行有col个数都小于等于midValue
-                count += col + 1; 
-                row++;
-            } else {
-                // 否则，往左移直到找到小于等于midValue的
-                // 如果到0了都没找到小于等于midValue的，说明也没有必要往下找了，因为下一行肯定比当前的数字大
-                col--; 
+        int startRow = 0;
+        int endRow = rowNum;
+        while (startRow < endRow) {
+            int midRow = startRow + (endRow - startRow) / 2;
+            
+            int startCol = 0;
+            int endCol = colNum;
+            while (startCol < endCol) {
+                int midCol = startCol + (endCol - startCol) / 2;
+
+                if (matrix[midRow][midCol] < value) {
+                    count += (midRow * colNum) + midCol;
+                    startCol = midCol + 1;
+                } else {
+                    endCol = midCol;
+                }
             }
+
+            
         }
 
         return count;
