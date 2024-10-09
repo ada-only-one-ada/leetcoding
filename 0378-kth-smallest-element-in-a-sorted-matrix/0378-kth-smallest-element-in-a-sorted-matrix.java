84fc733449d621a906a9bf7b8c5f0d688df4ed97
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-09 15:43:45 -0700

    Time: 0 ms (100%), Space: 49.7 MB (31.31%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,40 +1,45 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        int rowNum = matrix.length;
-        int colNum = matrix[0].length;
+        int n = matrix.length;
 
-        int candidate = -1;
         int startValue = matrix[0][0];
-        int endValue = matrix[rowNum - 1][colNum - 1] + 1;
+        int endValue = matrix[n - 1][n - 1] + 1;
 
         while (startValue < endValue) {
             int midValue = startValue + (endValue - startValue) / 2;
-            int lessOrEqual = count(matrix, midValue);
-
-            if (lessOrEqual >= k) {
-                candidate = midValue;
+            // 在这个matrix中，有count个数字比midValue小
+            int count = countLessOrEqual(matrix, midValue);
+            // 在这个matrix中，有至少k个的数字比midValue小
+            // 说明第k小的数字肯定在前面
+            // 也就是说，第k小的数字肯定在 [startValue, midValue] 之间
+            if (count >= k) {
                 endValue = midValue;
+            // 第k小的数字肯定在后面
+            // 也就是说，第k小的数字肯定在 (midValue, endValue) 之间
             } else {
                 startValue = midValue + 1;
             }
         }
 
-        return candidate;
+        return startValue;
     }
 
-    public int count(int[][] matrix, int value) {
-        int count = 0;
+    // 数小于或等于 midValue 的数有几个
+    public int countLessOrEqual(int[][] matrix, int midValue) {
+        int count = 0;        
 
+        int n = matrix.length;
         int row = 0;
-        int col = matrix[0].length - 1;
-        while (row < matrix.length && col >= 0) {
-            if (matrix[row][col] > value) {
-                // 注意：matrix每一行从左到右递增，每一列从上到下递增
-                col--;
-                // 小于或等于，因为一样的也在算k里面
-            } else {
-                count += col + 1;
+        int col = n - 1;
+        while (row < n && col >= 0) {
+            if (matrix[row][col] <= midValue) {
+                // 因为当前行是递增的，所以当前行有col个数都小于等于midValue
+                count += col + 1; 
                 row++;
+            } else {
+                // 否则，往左移直到找到小于等于midValue的
+                // 如果到0了都没找到小于等于midValue的，说明也没有必要往下找了，因为下一行肯定比当前的数字大
+                col--; 
             }
         }
 
