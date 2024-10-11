2ab89fd23861d6ee01615b4559bb99d043c63d2d
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-11 11:27:03 -0700

    Time: 0 ms (100%), Space: 41.5 MB (63.46%) - LeetHub

--- 0807-custom-sort-string/0807-custom-sort-string.java
+++ 0807-custom-sort-string/0807-custom-sort-string.java
@@ -1,30 +1,28 @@
 class Solution {
     public String customSortString(String order, String s) {
-        // 构建order字典，第一个char的order是0，第二个char的order是1...
-        Map<Character, Integer> charToOrder = new HashMap<>();
-        for (int i = 0; i < order.length(); i++) {
-            charToOrder.put(order.charAt(i), i);
+        int[] freq = new int[26];
+        for (char c: s.toCharArray()) {
+            freq[c - 'a']++;
         }
 
-        // 最后构建的字符，是按照order字典从小到大排序的
-        Queue<Pair<Character, Integer>> queue = new PriorityQueue<>((a, b) -> {
-            return a.getValue() - b.getValue();
-        });
-
-        // 一边遍历一边加未出现的char
         StringBuilder res = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (!charToOrder.containsKey(c)) {
-                res.append(c);
-            } else {
-                // 比如 abcda，a是2，b是1，c是0，d未出现，a是2
-                queue.add(new Pair<>(c, charToOrder.get(c)));
+        for (int i = 0; i < order.length(); i++) {
+            char o = order.charAt(i);
+            if (freq[o - 'a'] > 0) {
+                for (int j = 0; j < freq[o - 'a']; j++) {
+                    res.append(o);
+                }
+                freq[o - 'a'] = 0;
             }
         }
 
-        while (!queue.isEmpty()) {
-            res.append(queue.poll().getKey());
+        // 有些字符可能在 order 没有出现
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > 0) {
+                for (int j = 0; j < freq[i]; j++) {
+                    res.append((char) (i + 'a'));
+                }
+            }
         }
 
         return res.toString();
