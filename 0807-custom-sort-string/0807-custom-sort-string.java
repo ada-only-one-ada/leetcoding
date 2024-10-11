39ed2f0389f23e26d13786dfdc55e0950b6a6652
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-11 11:09:10 -0700

    Time: 0 ms (100%), Space: 41.2 MB (87.06%) - LeetHub

---
+++ 0807-custom-sort-string/0807-custom-sort-string.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String customSortString(String order, String s) {
+        int[] freq = new int[26];
+        for (char c: s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < order.length(); i++) {
+            char o = order.charAt(i);
+            if (freq[o - 'a'] > 0) {
+                for (int j = 0; j < freq[o - 'a']; j++) {
+                    res.append(o);
+                }
+                freq[o - 'a'] = 0;
+            }
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > 0) {
+                for (int j = 0; j < freq[i]; j++) {
+                    res.append((char) (i + 'a'));
+                }
+            }
+        }
+
+        return res.toString();
+    }
+}
