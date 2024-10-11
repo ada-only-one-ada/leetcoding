a40044f086cd5964c26a1655bb1e1a1131fd2fd8
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-11 16:54:45 -0700

    Time: 18 ms (57.16%), Space: 44.5 MB (37.05%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.java
@@ -1,36 +1,51 @@
-public class Solution {
-    public long solve(int[] nums) {
-        int n = nums.length;
-        int[] left = new int[n];  // left[i] 存储左侧严格大于 nums[i] 的最近元素的索引（如果不存在则为 -1）
-        int[] right = new int[n]; // right[i] 存储右侧大于等于 nums[i] 的最近元素的索引（如果不存在则为 n）
-        for (int i = 0; i < n; i++) {
-            right[i] = n;
-        }
-        
-        Stack<Integer> st = new Stack<>();
-        st.push(-1);
-        
-        for (int i = 0; i < n; i++) {
-            while (st.size() > 1 && nums[st.peek()] <= nums[i]) {
-                right[st.peek()] = i;
-                st.pop();
-            }
-            left[i] = st.peek();
-            st.push(i);
-        }
-        
-        long ans = 0;
-        for (int i = 0; i < n; i++) {
-            ans += (long) (i - left[i]) * (right[i] - i) * nums[i];
-        }
-        return ans;
-    }
-
+class Solution {
     public long subArrayRanges(int[] nums) {
-        long ans = solve(nums);
-        for (int i = 0; i < nums.length; i++) {  // 对所有元素取反，这样用相同的函数可以计算最小值的贡献
-            nums[i] = -nums[i];
+        int[] newNums = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            newNums[i] = nums[i];
         }
-        return ans + solve(nums);
+        nums = newNums;
+        nums[nums.length - 1] = Integer.MAX_VALUE;
+        long res = 0;
+
+        Stack<Integer> stack = new Stack();
+        stack.push(-1);
+        
+        // 左边第一个比它大的，右边第一个比它大的
+        // 那么计算当前 number 对最大值贡献了多少
+        for (int i = 0; i < nums.length; i++) {
+            if (stack.size() <= 1 || nums[i] <= nums[stack.peek()]) {
+                stack.push(i);
+            } else {
+                while (stack.size() >= 2 && nums[i] > nums[stack.peek()]) {
+                    int right = i;
+                    int mid = stack.pop();
+                    int left = stack.peek();
+                    res += (long) nums[mid] * (right - mid) * (mid - left);
+                }
+                stack.push(i);
+            }
+        }   
+
+        stack.clear();
+        nums[nums.length - 1] = Integer.MIN_VALUE;
+        stack.push(-1);
+        // 左边第一个比它小的，右边第一个比它小的
+        // 那么计算当前 number 对最小值贡献了多少
+        for (int i = 0; i < nums.length; i++) {
+            if (stack.size() <= 1 || nums[i] >= nums[stack.peek()]) {
+                stack.push(i);
+            } else {
+                while (stack.size() >= 2 && nums[i] < nums[stack.peek()]) {
+                    int right = i;
+                    int mid = stack.pop();
+                    int left = stack.peek();
+                    res -= (long) nums[mid] * (right - mid) * (mid - left);
+                }
+                stack.push(i);
+            }
+        }
+
+        return res;
     }
 }
