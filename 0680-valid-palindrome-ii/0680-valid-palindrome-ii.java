047c670d04f9c9af2c2162e114a91f9229f7b6b7
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-11 00:26:11 -0700

    Time: 4 ms (99.44%), Space: 45.4 MB (44.87%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,33 +1,30 @@
 class Solution {
     public boolean validPalindrome(String s) {
-        if (helper(s)) return true;
-
-        int left = 0;
-        int right = s.length() - 1;
-        while (left < right) {
-            // 这里不用记录改变过没有，因为一旦遇到不相等的字符，我们只能选择去掉左边/右边的再看是否全等
-            // 此时如果不全等的话就return false了，也就没有下一次的删除操作了（which means used might increase）
-            if (s.charAt(left) != s.charAt(right)) { 
-                return helper(s.substring(left, right)) || helper(s.substring(left + 1, right + 1));
+        int i = 0;
+        int j = s.length() - 1;
+        
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return helper(s.substring(i, j)) || helper(s.substring(i + 1, j + 1));
+            } else {
+                i++;
+                j--;
             }
-
-            left++;
-            right--;
-            
         }
-        return false;
+
+        return true;
     }
 
     public boolean helper(String s) {
-        int left = 0;
-        int right = s.length() - 1;
-
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
+        int i = 0;
+        int j = s.length() - 1;
+        
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
                 return false;
             }
-            left++;
-            right--;
+            i++;
+            j--;
         }
 
         return true;
