ecf3ac31d519e6dcca125fb7df7135975fcf013a
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-11 21:22:57 -0700

    Time: 0 ms (100%), Space: 44.3 MB (37.73%) - LeetHub

---
+++ 0777-toeplitz-matrix/0777-toeplitz-matrix.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        for (int row = 1; row < matrix.length; row++) {
+            for (int col = 1; col < matrix[0].length; col++) {
+                if (matrix[row][col] != matrix[row - 1][col - 1]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
