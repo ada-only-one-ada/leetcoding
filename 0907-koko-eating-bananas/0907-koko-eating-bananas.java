7e4a363f5bf959795802cccb359eca4b0c368e8c
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-11 21:11:31 -0700

    Time: 11 ms (85.64%), Space: 45.1 MB (47.8%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,9 +1,13 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
+        // 这里的sum如果太大会造成溢出，要用long
+        // 我们也可以直接设置 max 给 end
+        /*
         int sum = 0;
         for (int pile: piles) {
             sum += pile;
         }
+        */
 
         int start = 1;
         int end = Integer.MAX_VALUE;
