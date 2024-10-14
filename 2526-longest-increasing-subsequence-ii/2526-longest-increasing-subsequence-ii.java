5727c92a6e67098313194824c55f996ae14217f8
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-14 16:02:39 -0700

    Time: 88 ms (39.05%), Space: 56.1 MB (63.91%) - LeetHub

--- 2526-longest-increasing-subsequence-ii/2526-longest-increasing-subsequence-ii.java
+++ 2526-longest-increasing-subsequence-ii/2526-longest-increasing-subsequence-ii.java
@@ -1,45 +1,68 @@
 class Solution {
-
-    int N = 100001;
-    int[] seg = new int[2*N];
-    
-    void update(int pos, int val){  // update max
-        pos += N;
-        seg[pos] = val;
- 
-        while (pos > 1) {
-            pos >>= 1;
-            seg[pos] = Math.max(seg[2*pos], seg[2*pos+1]);
-        }
-    }
- 
-    int query(int lo, int hi){ // query max [lo, hi)
-        lo += N;
-        hi += N;
+    public int lengthOfLIS(int[] nums, int k) {
         int res = 0;
- 
-        while (lo < hi) {
-            if ((lo & 1)==1) {
-                res = Math.max(res, seg[lo++]);
-            }
-            if ((hi & 1)==1) {
-                res = Math.max(res, seg[--hi]);
-            }
-            lo >>= 1;
-            hi >>= 1;
+        // 找到nums中的最小和最大值，为了确定线段树的范围
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
         }
+
+        // 创建线段树
+        SegmentTree segTree = new SegmentTree(min, max);
+
+        // 遍历数组，更新线段树并查询最大值
+        for (int num : nums) {
+            // 查询当前num-k到num-1的最大LIS长度
+            int maxLIS = segTree.query(Math.max(min, num - k), num - 1) + 1;
+            // 更新当前数的LIS长度
+            segTree.update(num, maxLIS);
+            // 更新结果
+            res = Math.max(res, maxLIS);
+        }
+
         return res;
     }
     
-    public int lengthOfLIS(int[] A, int k) {
-        int ans = 0;
-        for (int i = 0; i < A.length; ++i){
-            int l = Math.max(0, A[i]-k);
-            int r = A[i];
-            int res = query(l, r) + 1; // best res for the current element
-            ans = Math.max(res, ans);
-            update(A[i], res); // and update it here
+    class SegmentTree {
+        int start, end;
+        SegmentTree left, right;
+        int max;
+
+        public SegmentTree(int start, int end) {
+            this.start = start;
+            this.end = end;
+            this.max = 0;
+            if (start != end) {
+                int mid = start + (end - start) / 2;
+                this.left = new SegmentTree(start, mid);
+                this.right = new SegmentTree(mid + 1, end);
+            }
         }
-        return ans;
+
+        public int query(int l, int r) {
+            if (l > end || r < start) {
+                return 0;
+            }
+            if (l <= start && end <= r) {
+                return max;
+            }
+            return Math.max(left.query(l, r), right.query(l, r));
+        }
+
+        public void update(int idx, int value) {
+            if (start == end) {
+                max = Math.max(max, value);
+                return;
+            }
+            int mid = start + (end - start) / 2;
+            if (idx <= mid) {
+                left.update(idx, value);
+            } else {
+                right.update(idx, value);
+            }
+            max = Math.max(left.max, right.max);
+        }
     }
 }
