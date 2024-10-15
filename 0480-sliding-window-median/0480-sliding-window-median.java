6cd291bc5ad45fb405fe244eb547af8a8f3f9a98
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-15 15:24:13 -0700

    Time: 54 ms (74.62%), Space: 56.1 MB (75.34%) - LeetHub

--- 0480-sliding-window-median/0480-sliding-window-median.java
+++ 0480-sliding-window-median/0480-sliding-window-median.java
@@ -52,13 +52,17 @@ class Solution {
                 balance--; // big 增加了一个元素
             }
 
+            // 调整平衡状态：
             // 理想状态下的 balance：
             // k 是偶数： balance = 0，表示 small 和 big 一样
             // k 是奇数： balance = 1，表示 small 比 big 多一个
-            if (balance > 0) {
+
+            if ((k%2==0 && balance > 0) || (k%2 != 0 && balance > 1)) {
                 big.add(small.poll());
                 balance -= 2; // small 移动一个元素到 big
             }
+
+            // 说明big比small多了
             if (balance < 0) {
                 small.add(big.poll());
                 balance += 2; // big 移动一个元素到 small
