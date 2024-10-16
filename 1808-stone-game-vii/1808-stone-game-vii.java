cde9b5463eb42c4605ed492591ec90aab4491d98
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-16 15:06:48 -0700

    Time: 32 ms (83.87%), Space: 54.8 MB (81.45%) - LeetHub

--- 1808-stone-game-vii/1808-stone-game-vii.java
+++ 1808-stone-game-vii/1808-stone-game-vii.java
@@ -1,22 +1,30 @@
 class Solution {
     public int stoneGameVII(int[] stones) {
         int n = stones.length;
-        int[] sum = new int[n+1];
-        // 构建前缀和数组
-        for (int i = 0; i < n; i++) {
-            sum[i+1] = sum[i] + stones[i];
+
+        int[] prefixSum = new int[n];
+        prefixSum[0] = stones[0];
+        for (int i = 1; i < n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + stones[i];
         }
-        
+
         int[][] dp = new int[n][n];
-        // 动态规划填表
         for (int len = 2; len <= n; len++) {
             for (int start = 0; start <= n - len; start++) {
                 int end = start + len - 1;
-                dp[start][end] = Math.max(sum[end+1] - sum[start+1] - dp[start+1][end], 
-                                          sum[end] - sum[start] - dp[start][end-1]);
+
+                int pickLeftSum = prefixSum[end] - prefixSum[start];
+                int pickLeftDp = dp[start + 1][end];
+
+                int prevSum = 0;
+                if (start > 0) prevSum = prefixSum[start - 1];
+                int pickRightSum = prefixSum[end - 1] - prevSum;
+                int pickRightDp = dp[start][end - 1];
+
+                dp[start][end] = Math.max(pickLeftSum - pickLeftDp,  pickRightSum - pickRightDp);
             }
         }
-        
-        return dp[0][n-1];
+
+        return dp[0][n - 1];
     }
 }
