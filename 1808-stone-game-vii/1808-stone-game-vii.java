d4dbddbc338aa59386e5efb4c56bed52fc159f1a
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-16 15:14:01 -0700

    Time: 36 ms (75.81%), Space: 55 MB (75%) - LeetHub

--- 1808-stone-game-vii/1808-stone-game-vii.java
+++ 1808-stone-game-vii/1808-stone-game-vii.java
@@ -8,13 +8,21 @@ class Solution {
             prefixSum[i] = prefixSum[i - 1] + stones[i];
         }
 
+        // Alice 想要最大化 (A - B), Bob 想要最小化 (A - B)
+        // 即 Alice 会让自己的分数尽可能大，Bob 分数尽可能小
+        // 最小化 (A - B)，即 A越小，B越大，所以 Bob 也会让自己分数尽可能大，Alice 分数尽可能小
+        // 定义 dp[start][end] 表示当只考虑 stones[start] 到 stones[end] 这部分数组时，
+        // 当前操作玩家（轮到操作的玩家）与另一个玩家之间的最大分数差
         int[][] dp = new int[n][n];
         for (int len = 2; len <= n; len++) {
             for (int start = 0; start <= n - len; start++) {
                 int end = start + len - 1;
 
+                // 拿最左边的石头以后，剩余石头的总和
                 int pickLeftSum = prefixSum[end] - prefixSum[start];
+                // 拿最左边的石头以后，剩下对手会挑选的最佳石头
                 int pickLeftDp = dp[start + 1][end];
+                // 当前对手可拿的分数就是：剩余石头的总和 减去 剩下对手会挑选的最佳石头
 
                 int pickRightSum = prefixSum[end - 1] - (start > 0? prefixSum[start - 1] : 0);
                 int pickRightDp = dp[start][end - 1];
