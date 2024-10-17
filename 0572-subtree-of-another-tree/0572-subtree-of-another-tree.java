de7c6c078e41142693e2a8ae39c4333438dbbd62
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-17 10:06:49 -0700

    Time: 2 ms (97.1%), Space: 44.6 MB (21.46%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -2,23 +2,18 @@ class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
         if (root == null && subRoot == null) return true;
         if (root == null || subRoot == null) return false;
+        // 这里是isSubtree的recursion！
         return helper(root, subRoot) || isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
     }
-
 
     public boolean helper(TreeNode root, TreeNode subRoot) {
         if (root == null && subRoot == null) return true;
         if (root == null || subRoot == null) return false;
 
-        if (root.val == subRoot.val) {
-            return helper(root.left, subRoot.left) && isSubtree(root.right, subRoot.right);
-        } 
+        if (root.val != subRoot.val) {
+            return false;
+        }
 
-        return false;
+        return helper(root.left, subRoot.left) && helper(root.right, subRoot.right);
     }
 }
-
-/*
-Wrong case: [3,4,5,1,null,2], [3,1,2] should return false
-需要在同一个递归树上
-*/
