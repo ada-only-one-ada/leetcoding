2c3df27782c96343a54421648f40969fa6f2afb1
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-18 16:01:39 -0700

    Time: 2 ms (79.83%), Space: 41.8 MB (31.15%) - LeetHub

--- 1095-two-city-scheduling/1095-two-city-scheduling.java
+++ 1095-two-city-scheduling/1095-two-city-scheduling.java
@@ -5,14 +5,21 @@ class Solution {
         });
 
         int total = 0;
-        for (int i = 0; i < costs.length / 2; i++) {
-            total += costs[i][0];
-        }
-
-        for (int i = costs.length / 2; i < costs.length; i++) {
-            total += costs[i][1];
+        for (int i = 0; i < costs.length; i++) {
+            if (i < costs.length / 2) {
+                total += costs[i][0]; // first half to A
+            } else {
+                total += costs[i][1]; // second half to B
+            }
         }
 
         return total;
     }
 }
+
+// [[70,311], [74,927], [732,711], [97,928]]
+// 选 A 的代价: [70-311]=-241, [74-927]=-853, [732,711]=21, [97-928]=-831
+// 按从低到高排序[74-927]=-853,[97-928]=-831, [70-311]=-241,[732,711]=21,
+// 所以前一半飞去代价最小的 A，[74-927]=-853,[97-928]=-831,总cost为74+97=171
+// 剩下的自然全都飞去 B了，[70-311]=-241,[732,711]=21,总cost为311+711=1022
+// total: 1193
