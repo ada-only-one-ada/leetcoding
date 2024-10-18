0fb046f2a174f48f124d8de22c198848524c67b4
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-18 15:59:52 -0700

    Time: 2 ms (79.83%), Space: 41.1 MB (98.89%) - LeetHub

---
+++ 1095-two-city-scheduling/1095-two-city-scheduling.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int twoCitySchedCost(int[][] costs) {
+        Arrays.sort(costs, (a, b) -> {
+            return (a[0] - a[1]) - (b[0] - b[1]);
+        });
+
+        int total = 0;
+        for (int i = 0; i < costs.length / 2; i++) {
+            total += costs[i][0];
+        }
+
+        for (int i = costs.length / 2; i < costs.length; i++) {
+            total += costs[i][1];
+        }
+
+        return total;
+    }
+}
