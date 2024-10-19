b0a5181deab2c5247b5f549fc9003bdd6b71ec64
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-18 23:47:08 -0700

    Time: 23 ms (100%), Space: 44.3 MB (7.04%) - LeetHub

--- 1362-airplane-seat-assignment-probability/1362-airplane-seat-assignment-probability.java
+++ 1362-airplane-seat-assignment-probability/1362-airplane-seat-assignment-probability.java
@@ -1,12 +1,14 @@
 class Solution {
     public double nthPersonGetsNthSeat(int n) {
+        // 如果一共只有1个人，那这个人怎么乱选都能选对它的位置
         if (n <= 1) return 1.0;
 
         double[] dp = new double[n + 1];
         dp[1] = 1.0;
-        System.out.println(1.0/2);
+
         for (int i = 2; i <= n; i++) {
-            dp[i] = (double)(1.0/i) + (double)(i-2)/(double)i * dp[i - 1];
+            dp[i] = (1.0 / i) 
+                  + (double) (i - 2) / i * dp[i - 1];
         }
 
         return dp[n];
