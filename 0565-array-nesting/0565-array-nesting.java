6283993cb68ffee3227b5f995a37899f61a81033
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-20 12:43:59 -0700

    Time: 10 ms (35.99%), Space: 63.8 MB (14.11%) - LeetHub

--- 0565-array-nesting/0565-array-nesting.java
+++ 0565-array-nesting/0565-array-nesting.java
@@ -1,21 +1,27 @@
 class Solution {
     int res = 0;
     public int arrayNesting(int[] nums) {
-        // A global set shared by all dfs() calls to avoid TLE.
-        Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < nums.length; i++) {
-            dfs(nums, i, set, 1);
+        for (int index = 0; index < nums.length; index++) {
+            // 只从没访问过的开始
+            if (nums[index] != -1) {
+                dfs(nums, index, 0);
+            }
         }
-        return res;
-    } 
 
-    public void dfs(int[] nums, int index, Set<Integer> set, int len) {
-        if (index < 0 || index >= nums.length || index == nums[index] || set.contains(nums[index])) {
+        return res;
+    }
+
+    public void dfs(int[] nums, int index, int len) {
+        // 超出边界，或者已经被访问过了，更新结果，返回
+        if (index < 0 || index >= nums.length || nums[index] == -1) {
             res = Math.max(res, len);
             return;
-        }
+        } 
 
-        set.add(index);
-        dfs(nums, nums[index], set, len + 1);
+        // temp 存起来
+        int nextIndex = nums[index];
+        //标记已访问
+        nums[index] = -1;
+        dfs(nums, nextIndex, len + 1);
     }
 }
