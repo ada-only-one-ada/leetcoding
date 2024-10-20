c128da405bec596040e65ae16b1b3ccf1b90a3e9
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-20 15:31:23 -0700

    Time: 18 ms (22.94%), Space: 51.5 MB (24.46%) - LeetHub

---
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.java
@@ -0,0 +1,27 @@
+class Solution {
+    Set<List<Integer>> res = new HashSet<>();
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        backtracking(nums, 0, new ArrayList<>());
+        List<List<Integer>> list = new ArrayList<>();
+        for (List<Integer> currRes: res) {
+            list.add(currRes);
+        }
+        return list;
+    }
+
+    public void backtracking(int[] nums, int start, List<Integer> currRes) {
+        if (currRes.size() >= 2) {
+            res.add(new ArrayList<>(currRes));
+
+            if (start == nums.length) return;
+        }
+
+        for (int i = start; i < nums.length; i++) {
+            if (currRes.size() == 0 || nums[i] >= currRes.get(currRes.size() - 1)) {
+                currRes.add(nums[i]);
+                backtracking(nums, i + 1, currRes);
+                currRes.remove(currRes.size() - 1); // 不一定要接着这个数字
+            }
+        }
+    }
+}
