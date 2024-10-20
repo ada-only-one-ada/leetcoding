f95f3ee4d80bee7e304f78b26394ad793908100c
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-19 19:39:51 -0700

    Time: 3 ms (100%), Space: 42.6 MB (7.01%) - LeetHub

--- 0284-peeking-iterator/0284-peeking-iterator.java
+++ 0284-peeking-iterator/0284-peeking-iterator.java
@@ -1,36 +1,32 @@
-// Java Iterator interface reference:
-// https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
-
 class PeekingIterator implements Iterator<Integer> {
-    Queue<Integer> queue;
+    Iterator<Integer> iterator;
+    Integer nextElement;
 	public PeekingIterator(Iterator<Integer> iterator) {
-	    queue = new LinkedList<>();
-        while (iterator.hasNext()) {
-            queue.add(iterator.next());
-        }
-	    
+	    this.iterator = iterator;
+        nextElement = iterator.next(); // 先把nextElement拿到，此时 iterator 的元素永远少一个
 	}
 	
     // Returns the next element in the iteration without advancing the iterator.
 	public Integer peek() {
-        if (!queue.isEmpty()) {
-            return queue.peek();
-        }
-        return null;
+        return nextElement; 
 	}
 	
 	// hasNext() and next() should behave the same as in the Iterator interface.
 	// Override them if needed.
 	@Override
 	public Integer next() {
-	    if (!queue.isEmpty()) {
-            return queue.poll();
+	    Integer returnedElement = nextElement;
+        if (iterator.hasNext()) {
+            nextElement = iterator.next();
+        } else {
+            nextElement = null;
         }
-        return null;
+
+        return returnedElement;
 	}
 	
 	@Override
 	public boolean hasNext() {
-	    return !queue.isEmpty();
+	    return nextElement != null; 
 	}
 }
