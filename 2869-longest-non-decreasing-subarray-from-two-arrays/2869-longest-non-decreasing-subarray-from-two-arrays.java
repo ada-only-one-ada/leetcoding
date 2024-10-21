241f36e78e333e85e203f9e91a3892005384db2d
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-20 22:45:18 -0700

    Time: 17 ms (28.77%), Space: 61.7 MB (40.41%) - LeetHub

--- 2869-longest-non-decreasing-subarray-from-two-arrays/2869-longest-non-decreasing-subarray-from-two-arrays.java
+++ 2869-longest-non-decreasing-subarray-from-two-arrays/2869-longest-non-decreasing-subarray-from-two-arrays.java
@@ -15,7 +15,7 @@ class Solution {
         int maxLen = 1;
         for (int i = 1; i < n; i++) {
             // 当前数字大于或等于自己的array的上一个数字，长度+1
-            if (nums1[i] >= nums1[i - 1]) dp[i][0] = dp[i - 1][0] + 1;
+            if (nums1[i] >= nums1[i - 1]) dp[i][0] = dp[i - 1][0] + 1; 
             if (nums2[i] >= nums2[i - 1]) dp[i][1] = dp[i - 1][1] + 1;
 
             // 不仅比自己的前一个数字大，也比对方的大，我们把当前结果接到长的串上
