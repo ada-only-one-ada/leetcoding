b7b2b300d7b9fb79d810f1960af9f8e71379e8f9
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-20 22:44:36 -0700

    Time: 17 ms (28.77%), Space: 62 MB (33.56%) - LeetHub

--- 2869-longest-non-decreasing-subarray-from-two-arrays/2869-longest-non-decreasing-subarray-from-two-arrays.java
+++ 2869-longest-non-decreasing-subarray-from-two-arrays/2869-longest-non-decreasing-subarray-from-two-arrays.java
@@ -3,33 +3,27 @@ class Solution {
         int n = nums1.length;
         if (n == 0) return 0;
 
+        // dp[i][0]: 以 nums1 的 i 结尾的max length
+        // dp[i][1]: 以 nums2 的 i 结尾的max length
         int[][] dp = new int[n][2];
+        // 初始化每个数字就是长度1
         for (int i = 0; i < n; i++) {
             dp[i][0] = 1;
             dp[i][1] = 1;
         }
-        int res = 1;
 
+        int maxLen = 1;
         for (int i = 1; i < n; i++) {
-            if (nums1[i] >= nums1[i - 1]) {
-                dp[i][0] = dp[i - 1][0] + 1;
-            }
+            // 当前数字大于或等于自己的array的上一个数字，长度+1
+            if (nums1[i] >= nums1[i - 1]) dp[i][0] = dp[i - 1][0] + 1;
+            if (nums2[i] >= nums2[i - 1]) dp[i][1] = dp[i - 1][1] + 1;
 
-            if (nums1[i] >= nums2[i - 1]) {
-                dp[i][0] = Math.max(dp[i][0], dp[i - 1][1] + 1);
-            }
-
-            if (nums2[i] >= nums2[i - 1]) {
-                dp[i][1] = dp[i - 1][1] + 1;
-            }
-
-            if (nums2[i] >= nums1[i - 1]) {
-                dp[i][1] = Math.max(dp[i][1], dp[i - 1][0] + 1);
-            }
-
-            res = Math.max(res, Math.max(dp[i][0], dp[i][1]));
+            // 不仅比自己的前一个数字大，也比对方的大，我们把当前结果接到长的串上
+            if (nums1[i] >= nums2[i - 1]) dp[i][0] = Math.max(dp[i][0], dp[i - 1][1] + 1);
+            if (nums2[i] >= nums1[i - 1]) dp[i][1] = Math.max(dp[i][1], dp[i - 1][0] + 1);
+            maxLen = Math.max(Math.max(dp[i][0], dp[i][1]), maxLen);
         }
 
-        return res;
+        return maxLen;
     }
 }
