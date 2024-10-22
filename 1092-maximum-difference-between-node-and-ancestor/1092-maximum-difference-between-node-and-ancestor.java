17306fcb6e2dd523d216267ada4284f96ef099a1
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-21 23:26:30 -0700

    Time: 0 ms (100%), Space: 41.7 MB (69.42%) - LeetHub

--- 1092-maximum-difference-between-node-and-ancestor/1092-maximum-difference-between-node-and-ancestor.java
+++ 1092-maximum-difference-between-node-and-ancestor/1092-maximum-difference-between-node-and-ancestor.java
@@ -8,9 +8,10 @@ class Solution {
             return Math.abs(max - min);
         }
 
+        // 更新最大最小值，递归计算左右子树的最大差值
         int left = helper(root.left, Math.min(min, root.val), Math.max(max, root.val));
         int right = helper(root.right, Math.min(min, root.val), Math.max(max, root.val));
-
+        
         return Math.max(left, right);
     }
 }
