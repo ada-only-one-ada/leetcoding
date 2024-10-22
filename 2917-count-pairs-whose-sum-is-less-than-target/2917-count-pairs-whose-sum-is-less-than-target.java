8bec7cf52f55ce81442359b94975b0b24d4032a0
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-21 18:24:55 -0700

    Time: 3 ms (32.8%), Space: 42 MB (82.72%) - LeetHub

--- 2917-count-pairs-whose-sum-is-less-than-target/2917-count-pairs-whose-sum-is-less-than-target.java
+++ 2917-count-pairs-whose-sum-is-less-than-target/2917-count-pairs-whose-sum-is-less-than-target.java
@@ -5,13 +5,25 @@ class Solution {
 
         int left = 0;
         int right = nums.size() - 1;
-        while (left < right) {
-            if (nums.get(left) + nums.get(right) < target) {
-                res += right - left;
-                left++;
-            } else {
-                right--;
+
+        while (left < right) { // 找最后一个和nums[left] 相加，小于target的数字
+            int start = left + 1; // 找右边界：右边界的最小
+            int end = right + 1;  // 找右边界： 右边界的最大
+            int candidate = -1; // 右边界的最小候选者
+
+            while (start < end) {
+                int mid = start + (end - start) / 2;
+                if (nums.get(left) + nums.get(mid) < target) {
+                    candidate = mid;
+                    start = mid + 1;
+                } else {
+                    end = mid;
+                }
             }
+
+            if (candidate > left) res += candidate - left;
+            left++;
+            right = candidate;
         }
 
         return res;
