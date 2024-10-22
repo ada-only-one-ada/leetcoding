4c00df8847d2d8467a39f742afbc276967510c77
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-21 17:13:31 -0700

    Time: 11 ms (32.93%), Space: 64.1 MB (13.62%) - LeetHub

--- 0565-array-nesting/0565-array-nesting.java
+++ 0565-array-nesting/0565-array-nesting.java
@@ -1,27 +1,28 @@
 class Solution {
     int res = 0;
     public int arrayNesting(int[] nums) {
-        for (int index = 0; index < nums.length; index++) {
-            // 只从没访问过的开始
-            if (nums[index] != -1) {
-                dfs(nums, index, 0);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != -1) {
+                helper(nums, i, 0);
             }
         }
 
         return res;
     }
 
-    public void dfs(int[] nums, int index, int len) {
-        // 超出边界，或者已经被访问过了，更新结果，返回
-        if (index < 0 || index >= nums.length || nums[index] == -1) {
+    public void helper(int[] nums, int index, int len) {
+        if (index < 0 || index >= nums.length) {
             res = Math.max(res, len);
             return;
-        } 
+        }
 
-        // temp 存起来
+        if (nums[index] == -1) {
+            res = Math.max(res, len);
+            return;
+        }
+
         int nextIndex = nums[index];
-        //标记已访问
         nums[index] = -1;
-        dfs(nums, nextIndex, len + 1);
+        helper(nums, nextIndex, len + 1);
     }
 }
