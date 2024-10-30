ac12a9acf83b051591c7fad00b8ca7157caf4846
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-29 20:34:07 -0700

    Time: 21 ms (25.51%), Space: 45.5 MB (79.38%) - LeetHub

--- 1275-validate-binary-tree-nodes/1275-validate-binary-tree-nodes.java
+++ 1275-validate-binary-tree-nodes/1275-validate-binary-tree-nodes.java
@@ -1,78 +1,51 @@
 class Solution {
-    int[] parent;
-    int[] rank;
-
     public boolean validateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild) {
-        parent = new int[n];
-        rank = new int[n];
-        boolean[] hasParent = new boolean[n];   
-
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
-            rank[i] = 1;
-        } 
-
-        for (int i = 0; i < n; i++) {
-            int leftChildNode = leftChild[i];
-            if (leftChildNode != -1) {
-                if (hasParent[leftChildNode]) return false; // Already have a parent
-                hasParent[leftChildNode] = true;
-
-                if (!connect(i, leftChildNode)) return false; // Already connected, a circle
-            }
-
-            int rightChildNode = rightChild[i];
-            if (rightChildNode != -1) {
-                if (hasParent[rightChildNode]) return false; // Already have a parent
-                hasParent[rightChildNode] = true;
-
-                if (!connect(i, rightChildNode)) return false;
-            }
+        int root = findRoot(n, leftChild, rightChild);
+        if (root == -1) {
+            return false;
         }
-
-        // find Root ancestor
-        int rootAncestor = 0;
-        int numberOfRootAncestor = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (findAncestor(i) == i) {
-                if (numberOfRootAncestor == 1) return false; // We can only have one ancestor that has no parent
-                numberOfRootAncestor++;
-                rootAncestor = i;
+        
+        Set<Integer> seen = new HashSet<>();
+        Queue<Integer> queue = new LinkedList<>();
+        seen.add(root);
+        queue.add(root);
+        
+        while (!queue.isEmpty()) {
+            int node = queue.remove();
+            int[] children = {leftChild[node], rightChild[node]};
+            
+            for (int child : children) {
+                if (child != -1) {
+                    if (seen.contains(child)) {
+                        return false;
+                    }
+                    
+                    queue.add(child);
+                    seen.add(child);
+                }
             }
+            
         }
-
-        for (int i = 0; i < n; i++) { // All node have same ancestor
-            if (findAncestor(i) != rootAncestor) {
-                return false;
-            }
-        }
-
-        return true;
+        
+        return seen.size() == n;
     }
-
-    public boolean connect(int node1,  int node2) {
-        int a1 = findAncestor(node1);
-        int a2 = findAncestor(node2);
-
-        if (a1 == a2) return false; // Already connected!
-
-        if (rank[a1] >= rank[a2]) {
-            parent[a2] = a1;
-            rank[a1] += rank[a2];
-        } else {
-            parent[a1] = a2;
-            rank[a2] += rank[a1];
+    
+    public int findRoot(int n, int[] left, int[] right) {
+        Set<Integer> children = new HashSet<>();
+        for (int node : left) {
+            children.add(node);
         }
-
-        return true;
-    }
-
-    public int findAncestor(int node) {
-        if (node != parent[node]) {
-            parent[node] = findAncestor(parent[node]);
+        
+        for (int node : right) {
+            children.add(node);
         }
-
-        return parent[node];
+        
+        for (int i = 0; i < n; i++) {
+            if (!children.contains(i)) {
+                return i;
+            }
+        }
+        
+        return -1;
     }
 }
