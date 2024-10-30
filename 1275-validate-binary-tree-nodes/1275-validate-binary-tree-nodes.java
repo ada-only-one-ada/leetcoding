917f6dc5c2902ffeb363d92b5ed245289f000be0
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-29 20:35:09 -0700

    Time: 20 ms (31.63%), Space: 45.7 MB (51.82%) - LeetHub

--- 1275-validate-binary-tree-nodes/1275-validate-binary-tree-nodes.java
+++ 1275-validate-binary-tree-nodes/1275-validate-binary-tree-nodes.java
@@ -1,15 +1,11 @@
 class Solution {
     public boolean validateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild) {
         int root = findRoot(n, leftChild, rightChild);
-        if (root == -1) {
-            return false;
-        }
-        
+        if (root == -1) return false;
         Set<Integer> seen = new HashSet<>();
         Queue<Integer> queue = new LinkedList<>();
         seen.add(root);
         queue.add(root);
-        
         while (!queue.isEmpty()) {
             int node = queue.remove();
             int[] children = {leftChild[node], rightChild[node]};
@@ -19,31 +15,19 @@ class Solution {
                     if (seen.contains(child)) {
                         return false;
                     }
-                    
                     queue.add(child);
                     seen.add(child);
                 }
             }
-            
         }
-        
         return seen.size() == n;
     }
-    
     public int findRoot(int n, int[] left, int[] right) {
         Set<Integer> children = new HashSet<>();
-        for (int node : left) {
-            children.add(node);
-        }
-        
-        for (int node : right) {
-            children.add(node);
-        }
-        
+        for (int node : left)  children.add(node);
+        for (int node : right) children.add(node);    
         for (int i = 0; i < n; i++) {
-            if (!children.contains(i)) {
-                return i;
-            }
+            if (!children.contains(i)) return i;
         }
         
         return -1;
