03065dea4f83ef11265187af88f38337fa90a4c0
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-11-06 21:01:47 -0800

    Time: 103 ms (5.71%), Space: 49.9 MB (5.98%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -0,0 +1,20 @@
+class Solution {
+    static Map<TreeNode, Integer> map = new HashMap();
+    public static int countNodes(TreeNode node) {
+        for (TreeNode curr: map.keySet()) {
+            System.out.println("Node: " + curr.val + "has size of " + map.get(curr));
+        }
+        return helper(node);
+    }
+    
+    public static int helper(TreeNode node) {
+        if (node == null) return 0;
+        if (node.left == null && node.right == null) return 1;
+        
+        int totalSize = 1 + helper(node.left) + helper(node.right);
+        
+        map.put(node, totalSize);
+        return totalSize;
+    }
+}
+       
