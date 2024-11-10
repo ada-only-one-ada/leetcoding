075a112bc895dd93338e43821b1d9e6d3548c14d
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-11-09 23:56:35 -0800

    Time: 1 ms (97.03%), Space: 49.5 MB (50.4%) - LeetHub

--- 1498-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1498-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.java
+++ 1498-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1498-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.java
@@ -1,18 +1,18 @@
 class Solution {
     public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
-        if (cloned != null && cloned.val == target.val) {
-            return cloned;
-        } 
-
         if (cloned == null) {
             return null;
         } 
 
-        TreeNode res1 = getTargetCopy(original, cloned.left, target);
-        TreeNode res2 = getTargetCopy(original, cloned.right, target);
+        if (cloned.val == target.val) {
+            return cloned;
+        } 
 
-        if (res1 != null) return res1;
-        if (res2 != null) return res2;
+        TreeNode left = getTargetCopy(original, cloned.left, target);
+        TreeNode right = getTargetCopy(original, cloned.right, target);
+
+        if (left != null) return left;
+        if (right != null) return right;
         return null;
     }
 }
