4352786a9fdbadc9eeffd573880fed2189fa99b6
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-11-12 12:42:29 -0800

    Time: 2 ms (38.56%), Space: 44.4 MB (71.72%) - LeetHub

---
+++ 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean hasIncreasingSubarrays(List<Integer> nums, int k) {
+        int[] dp = new int[nums.size()];
+        Arrays.fill(dp, 1);
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums.get(i) > nums.get(i - 1)) {
+                dp[i] = dp[i - 1] + 1;
+            }
+        }
+
+        for (int i = 0; i < dp.length; i++) {
+            if (i - k + 1 >= 0 && i + k <= dp.length - 1) {
+                if (dp[i] - dp[i-k+1] + 1 == k && dp[i+k] - dp[i+1] + 1== k) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
