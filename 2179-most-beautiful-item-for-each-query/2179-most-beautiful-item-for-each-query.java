880028b60ac9283c122891cccac0c672f24172b0
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-11-12 13:13:55 -0800

    Time: 81 ms (7.25%), Space: 79.9 MB (80.12%) - LeetHub

--- 2179-most-beautiful-item-for-each-query/2179-most-beautiful-item-for-each-query.java
+++ 2179-most-beautiful-item-for-each-query/2179-most-beautiful-item-for-each-query.java
@@ -1,31 +1,30 @@
 class Solution {
     public int[] maximumBeauty(int[][] items, int[] queries) {
         Arrays.sort(items, (a, b) -> {
-            return a[0] - b[0];
+            if (a[0] == b[0]) { // same price，higher beauty first
+                return b[1] - a[1];
+            };
+            return a[0] - b[0]; // lower price first
         });
 
-        TreeMap<Integer, Integer> map = new TreeMap<>();
+        TreeMap<Integer, Integer> map = new TreeMap<>(); // price to beauty
+        int maxBeautySoFar = 0;
         for (int i = 0; i < items.length; i++) {
             int price = items[i][0];
             int beauty = items[i][1];
-            int prevBeauty = map.containsKey(price) ? map.get(price) : 0;
-            map.put(price, Math.max(beauty, prevBeauty));
-        }
 
-        int maxBeauty = 0;
-        // 因为price在一开始按从小到大排序了，所以在这之前的item，价格肯定是低于或者等于的，它们的beuty如果更大可以适用当前item
-        for (int price: map.keySet()) {
-            maxBeauty = Math.max(maxBeauty, map.get(price));
-            map.put(price, maxBeauty);
+            // 因为price在一开始按从小到大排序了，所以在这之前的item，
+            // 价格肯定是低于或者等于的，它们的beuty如果更大可以适用当前item
+            maxBeautySoFar = Math.max(beauty, maxBeautySoFar);
+            map.put(price, maxBeautySoFar);
         }
 
         int[] res = new int[queries.length];
         for (int i = 0; i < queries.length; i++) {
-            int ts = queries[i]; 
+            Integer last = map.floorKey(queries[i]);
 
-            if (map.floorKey(ts) != null) {
-                res[i] = map.get(map.floorKey(ts));
-            } 
+            if (last == null) continue;
+            res[i] = map.get(last);
         }
 
         return res;
