a437c3cddd5ed8a636b4e68c90df27331fca1d9b
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-11-12 13:13:32 -0800

    Time: 125 ms (7.25%), Space: 79.8 MB (80.12%) - LeetHub

--- 2179-most-beautiful-item-for-each-query/2179-most-beautiful-item-for-each-query.java
+++ 2179-most-beautiful-item-for-each-query/2179-most-beautiful-item-for-each-query.java
@@ -1,28 +1,31 @@
 class Solution {
     public int[] maximumBeauty(int[][] items, int[] queries) {
         Arrays.sort(items, (a, b) -> {
-            if (a[0] == b[0]) { // same price，higher beauty first
-                return b[1] - a[1];
-            };
-            return a[0] - b[0]; // lower price first
+            return a[0] - b[0];
         });
 
-        TreeMap<Integer, Integer> map = new TreeMap<>(); // price to beauty
-        int maxBeautySoFar = 0;
+        TreeMap<Integer, Integer> map = new TreeMap<>();
         for (int i = 0; i < items.length; i++) {
             int price = items[i][0];
             int beauty = items[i][1];
+            int prevBeauty = map.containsKey(price) ? map.get(price) : 0;
+            map.put(price, Math.max(beauty, prevBeauty));
+        }
 
-            maxBeautySoFar = Math.max(beauty, maxBeautySoFar);
-            map.put(price, maxBeautySoFar);
+        int maxBeauty = 0;
+        // 因为price在一开始按从小到大排序了，所以在这之前的item，价格肯定是低于或者等于的，它们的beuty如果更大可以适用当前item
+        for (int price: map.keySet()) {
+            maxBeauty = Math.max(maxBeauty, map.get(price));
+            map.put(price, maxBeauty);
         }
 
         int[] res = new int[queries.length];
         for (int i = 0; i < queries.length; i++) {
-            Integer last = map.floorKey(queries[i]);
+            int ts = queries[i]; 
 
-            if (last == null) continue;
-            res[i] = map.get(last);
+            if (map.floorKey(ts) != null) {
+                res[i] = map.get(map.floorKey(ts));
+            } 
         }
 
         return res;
