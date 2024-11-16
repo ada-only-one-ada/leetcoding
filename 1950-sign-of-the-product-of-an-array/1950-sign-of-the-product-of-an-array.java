ca4ffb4d6ed3ae4e196ebbb78e20d80e8366d9f9
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-11-16 13:49:19 -0800

    Time: 1 ms (27.09%), Space: 44.2 MB (29.52%) - LeetHub

---
+++ 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int arraySign(int[] nums) {
+        int zero = 0;
+        int pos = 0;
+        int neg = 0;
+
+        for (int num: nums) {
+            if (num == 0) {
+                zero++;
+            } else if (num > 0) {
+                pos++;
+            } else if (num < 0) {
+                neg++;
+            }
+        }
+
+        if (zero > 0) {
+            return 0;
+        } else if (neg % 2 == 0) {
+            return 1;
+        } else if (neg % 2 != 0) {
+            return -1;
+        }
+
+        return -1;
+    }
+}
