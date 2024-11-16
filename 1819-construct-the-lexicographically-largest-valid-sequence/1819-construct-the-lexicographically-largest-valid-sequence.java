06899eaf2225b5ba7a099ceeafbc802c574bd217
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-11-16 12:08:47 -0800

    Time: 0 ms (100%), Space: 41.2 MB (15%) - LeetHub

--- 1819-construct-the-lexicographically-largest-valid-sequence/1819-construct-the-lexicographically-largest-valid-sequence.java
+++ 1819-construct-the-lexicographically-largest-valid-sequence/1819-construct-the-lexicographically-largest-valid-sequence.java
@@ -1,41 +1,67 @@
 class Solution {
-    int[] res;
     int totalLen;
+    int[] res;
 
     public int[] constructDistancedSequence(int n) {
-        totalLen = 2 * n - 1;
+        totalLen = n * 2 - 1;
         res = new int[totalLen];
-        boolean[] visited = new boolean[n + 1];
-        backtracking(0, n, visited);
+        int[] currRes = new int[totalLen];
+        boolean[] usedNums = new boolean[n + 1];
+        // 找到一个有效的，立刻返回，因为此时是字典序最大
+        if (backtracking(n, currRes, 0, usedNums)) {
+            return res;
+        }
+
         return res;
     }
 
-    private boolean backtracking(int index, int n, boolean[] visited) {
-        if (index == totalLen) return true; // 完成整个序列
-        if (res[index] != 0) return backtracking(index + 1, n, visited); // 当前位置已填充，跳过
+    public boolean backtracking(int n, int[] currRes, int index, boolean[] usedNums) {
+        // 找到一个有效答案
+        if (index == totalLen) {
+            System.arraycopy(currRes, 0, res, 0, totalLen);
+            return true;
+        }
 
-        for (int num = n; num > 0; num--) {
-            if (visited[num]) continue; // 数字已使用，跳过
+        // 当前位置已经被填过了
+        if (currRes[index] != 0) {
+            return backtracking(n, currRes, index + 1, usedNums);
+        }
 
-            // 尝试放置数字num
+        // 从最大的开始填
+        for (int num = n; num >= 1; num--) {
+            // 当前数字已经被填过了，找下一个最大的填
+            if (usedNums[num]) continue; 
+
+            // 当前数字没有被填过，看能不能填：
+            // 情况1：如果数字等于1，只要放在当前index就可以
             if (num == 1) {
-                res[index] = 1;
-                visited[num] = true;
-                if (backtracking(index + 1, n, visited)) return true;
-                // 回溯
-                res[index] = 0;
-                visited[num] = false;
-            } else if (index + num < totalLen && res[index + num] == 0) { // 确保有空间放置num，并且配对位置也为空
-                res[index] = num;
-                res[index + num] = num;
-                visited[num] = true;
-                if (backtracking(index + 1, n, visited)) return true;
-                // 回溯
-                res[index] = 0;
-                res[index + num] = 0;
-                visited[num] = false;
+                currRes[index] = num;
+                usedNums[num] = true;
+                // 这里一旦找到一个正确的立刻返回，因为需要字典序最大
+                if (backtracking(n, currRes, index + 1, usedNums)) {
+                    return true;
+                }
+                currRes[index] = 0;
+                usedNums[num] = false;
+            // 情况2：如果数字大于1，检查当前index和配对的index
+            } else if (num >= 2) {
+                // 如果配对的index越界，或者已经被占用了，这个数字填不了
+                if (index + num >= totalLen || currRes[index + num] != 0) continue;
+
+                currRes[index] = num;
+                currRes[index + num] = num;
+                usedNums[num] = true;
+                // 这里一旦找到一个正确的立刻返回，因为需要字典序最大
+                if (backtracking(n, currRes, index + 1, usedNums)) {
+                    return true;
+                }
+
+                currRes[index] = 0;
+                currRes[index + num] = 0;
+                usedNums[num] = false;
             }
         }
-        return false; // 无法放置任何数字
+
+        return false;
     }
 }
