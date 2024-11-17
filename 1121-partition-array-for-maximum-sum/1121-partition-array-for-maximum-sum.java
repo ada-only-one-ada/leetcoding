ca7be0ef35a1b7d5b70329659e6d12bdb1e3a625
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-11-16 16:36:31 -0800

    Time: 7 ms (49.9%), Space: 41.9 MB (77.68%) - LeetHub

--- 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.java
+++ 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.java
@@ -3,9 +3,11 @@ class Solution {
         int[] dp = new int[arr.length];  // 创建动态规划数组，用于存储到每个索引为止的最大和
 
         for (int end = 0; end < arr.length; end++) {
+            int max = Integer.MIN_VALUE;
             for (int len = 1; len <= k && end - len + 1 >= 0; len++) {
                 int start = end - len + 1; // 计算子数组的开始位置
-                int max = getMaxValue(arr, start, end); // 调用 getMaxValue 方法获取从 start 到 end 的最大值
+
+                max = Math.max(max, arr[start]); 
                 if (start > 0) {
                     // 如果不是数组的第一个元素，加上前一个状态的最大值乘以子数组长度
                     dp[end] = Math.max(dp[end], dp[start - 1] + max * len); 
@@ -17,15 +19,5 @@ class Solution {
         }
 
         return dp[arr.length - 1]; // 返回整个数组的最大划分和
-    }
-
-    private int getMaxValue(int[] arr, int start, int end) {
-        int maxValue = Integer.MIN_VALUE;
-
-        for (int i = start; i <= end; i++) { 
-            maxValue = Math.max(maxValue, arr[i]); 
-        }
-
-        return maxValue; 
     }
 }
