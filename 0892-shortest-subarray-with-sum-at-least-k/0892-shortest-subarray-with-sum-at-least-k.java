8ef97ca37a1b456c6beab388c1e220dcb6d91084
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-11-18 01:01:44 -0800

    Time: 36 ms (61.45%), Space: 62.7 MB (19.61%) - LeetHub

--- 0892-shortest-subarray-with-sum-at-least-k/0892-shortest-subarray-with-sum-at-least-k.java
+++ 0892-shortest-subarray-with-sum-at-least-k/0892-shortest-subarray-with-sum-at-least-k.java
@@ -1,25 +1,33 @@
 class Solution {
     public int shortestSubarray(int[] nums, int k) {
-        int res = nums.length + 1; // an impossible value as res
+        int res = nums.length + 1;
 
+        // long 防止溢出
         long[] prefixSum = new long[nums.length + 1];
-        //         nums = [2, -1, 2]
-        // prefixSum = [0, 2, 1, 3]
+        //         nums = [1, 4, 5]
+        // prefixSum = [0, 1, 5, 9]
         for (int i = 1; i < prefixSum.length; i++) {
             prefixSum[i] = prefixSum[i - 1] + nums[i - 1];
         }
 
-        Deque<Integer> queue = new ArrayDeque<>();
+        Deque<Integer> dequeue = new ArrayDeque<>();
+        // loop over the prefixSum array
         for (int end = 0; end < prefixSum.length; end++) {
-            while (!queue.isEmpty() && prefixSum[end] - prefixSum[queue.getFirst()] >= k) {
-                res = Math.min(res, end - queue.pollFirst());
+            // 如果queue里面有startIndex，且[start, end]之和大于等于k
+            // 我们可以不断缩进更新结果，看有没有更短的结果
+            //                  0  1  2  3
+            // 比如 prefixSum = [0, 1, 5, 9]， k = 5，现在 end 是2，deque是[0, 1]
+            //  5 - 0 = 5 >= k, update resule 
+            //  5 - 1 < k, not satisfy, exit loop
+            while (!dequeue.isEmpty() && prefixSum[end] - prefixSum[dequeue.getFirst()] >= k) {
+                res = Math.min(res, end - dequeue.pollFirst());
             }
 
-            while (!queue.isEmpty() && prefixSum[end] <= prefixSum[queue.getLast()]) {
-                queue.pollLast();
+            while (!dequeue.isEmpty() && prefixSum[end] <= prefixSum[dequeue.getLast()]) {
+                dequeue.pollLast();
             }
 
-            queue.addLast(end);
+            dequeue.addLast(end);
         }
 
         if (res == nums.length + 1) return -1;
