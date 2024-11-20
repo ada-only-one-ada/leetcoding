38b619496f8b212fb8a8a3072923184b47d98065
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-11-20 00:05:01 -0800

    Time: 41 ms (54.24%), Space: 43 MB (49.33%) - LeetHub

--- 0313-super-ugly-number/0313-super-ugly-number.java
+++ 0313-super-ugly-number/0313-super-ugly-number.java
@@ -1,24 +1,34 @@
 class Solution {
     public int nthSuperUglyNumber(int n, int[] primes) {
-        long[] ugly = new long[n]; // 存储第 n 个超级丑数
-        int[] indexes = new int[primes.length]; // 每个质数的索引
-        ugly[0] = 1; // 第一个丑数定义为 1
+        long[] uglyNums = new long[n + 1]; // 存储丑数，从1开始索引
+        uglyNums[1] = 1; // 第一个丑数是1
 
-        for (int i = 1; i < n; i++) {
+        int[] indices = new int[primes.length]; // 存储每个质数对应的丑数索引
+        Arrays.fill(indices, 1); // 初始化为第一个丑数的索引
+
+        // dp 依次填充第 2 到 n 个丑数
+        for (int num = 2; num <= n; num++) {
             long min = Long.MAX_VALUE;
-            // 寻找当前可生成的最小丑数
-            for (int j = 0; j < primes.length; j++) {
-                min = Math.min(min, primes[j] * ugly[indexes[j]]);
+
+            // 计算当前可用的最小丑数
+            for (int i = 0; i < primes.length; i++) {
+                long curr = uglyNums[indices[i]] * primes[i];
+                if (curr < min) {
+                    min = curr;
+                }
             }
-            ugly[i] = min;
-            // 更新所有质数的索引，只有当它们贡献了最小值时
-            for (int j = 0; j < primes.length; j++) {
-                if (primes[j] * ugly[indexes[j]] == min) {
-                    indexes[j]++;
+
+            // 更新丑数数组
+            uglyNums[num] = min;
+
+            // 更新所有生成了当前最小丑数的索引，避免重复
+            for (int i = 0; i < primes.length; i++) {
+                if (uglyNums[indices[i]] * primes[i] == min) {
+                    indices[i]++;
                 }
             }
         }
-
-        return (int) ugly[n-1];
+        
+        return (int)uglyNums[n];
     }
 }
