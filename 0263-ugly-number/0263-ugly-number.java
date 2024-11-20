9b8f8090a29e0340d6ebc1eefeddf1bf765400be
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-11-19 18:33:11 -0800

    Time: 1 ms (48.78%), Space: 40.9 MB (26.36%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.java
+++ 0263-ugly-number/0263-ugly-number.java
@@ -1,14 +1,18 @@
 class Solution {
     public boolean isUgly(int n) {
-        if (n <= 0) return false; // 不是positive，不是丑数
-        if (n == 1) return true;  // 除不下去了且等于1的时候，是丑数
+        if (n <= 0) return false;
+        if (n == 1) return true;
 
-        if (n % 2 == 0) {
-            return isUgly(n / 2);
-        } else if (n % 3 == 0) {
-            return isUgly(n / 3);
-        } else if (n % 5 == 0) {
-            return isUgly(n / 5);
+        while (n % 2 == 0 || n % 3 == 0 || n % 5 == 0) {
+            if (n == 2 || n == 3 || n == 5) return true;
+           
+            if (n % 2 == 0) {
+                n /= 2;
+            } else if (n % 3 == 0) {
+                n /= 3;
+            } else if (n % 5 == 0) {
+                n /= 5;
+            }   
         }
 
         return false;
