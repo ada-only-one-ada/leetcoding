80d1947352f71f9c469e6f986e7a16ca01d0dcd2
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-11-21 15:02:00 -0800

    Time: 1 ms (60.76%), Space: 43.4 MB (38.65%) - LeetHub

--- 0925-construct-binary-tree-from-preorder-and-postorder-traversal/0925-construct-binary-tree-from-preorder-and-postorder-traversal.java
+++ 0925-construct-binary-tree-from-preorder-and-postorder-traversal/0925-construct-binary-tree-from-preorder-and-postorder-traversal.java
@@ -9,25 +9,27 @@ class Solution {
             map.put(postorder[i], i);
         }
 
-        return helper(preorder, 0, preorder.length - 1, postorder, 0, postorder.length - 1);
+        return helper(preorder, 0, postorder, 0, postorder.length - 1);
     }
 
-    public TreeNode helper(int[] preorder, int preStart, int preEnd, int[] postorder, int postStart, int postEnd) {
-        if (preStart > preEnd || postStart > postEnd) return null;
+    public TreeNode helper(int[] preorder, int preStart, int[] postorder, int postStart, int postEnd) {
+        if (preStart >= preorder.length || postStart > postEnd) return null;
 
         TreeNode root = new TreeNode(preorder[preStart]);
 
         // 如果这两个索引相等，意味着在这个递归的子任务中，只考虑 preorder 数组中的一个元素
         // 这个元素是当前子树的根节点，并且由于没有其他元素，说明这个根节点没有左子树或右子树
-        if (preStart == preEnd) return root; 
+        if (preStart == preorder.length - 1 || postStart == postEnd) {
+            return root; 
+        }
 
         int leftVal = preorder[preStart + 1]; // 根-左
         int leftIndex = map.get(leftVal); // 找到postorder里面左的位置
         int leftSize = leftIndex - postStart + 1; // 计算左子树一共有几个孩子
 
         // 这里postEnd要包括leftIndex因为它是左子树最后一个孩子
-        root.left = helper(preorder, preStart + 1, preStart + leftSize, postorder, postStart, leftIndex); 
-        root.right = helper(preorder, preStart + leftSize + 1 , preEnd, postorder, leftIndex + 1, postEnd - 1);
+        root.left = helper(preorder, preStart + 1, postorder, postStart, leftIndex); 
+        root.right = helper(preorder, preStart + leftSize + 1, postorder, leftIndex + 1, postEnd - 1);
 
         return root;
     }
