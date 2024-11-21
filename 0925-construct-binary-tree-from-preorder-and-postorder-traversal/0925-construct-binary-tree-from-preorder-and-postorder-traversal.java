49c901b5bf681ba9d1cd0449624ecac904581403
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-11-21 15:03:18 -0800

    Time: 1 ms (60.76%), Space: 43.3 MB (62.47%) - LeetHub

--- 0925-construct-binary-tree-from-preorder-and-postorder-traversal/0925-construct-binary-tree-from-preorder-and-postorder-traversal.java
+++ 0925-construct-binary-tree-from-preorder-and-postorder-traversal/0925-construct-binary-tree-from-preorder-and-postorder-traversal.java
@@ -13,12 +13,11 @@ class Solution {
     }
 
     public TreeNode helper(int[] preorder, int preStart, int[] postorder, int postStart, int postEnd) {
-        if (preStart >= preorder.length || postStart > postEnd) return null;
+        if (preStart == preorder.length || postStart > postEnd) return null;
 
         TreeNode root = new TreeNode(preorder[preStart]);
 
-        // 如果这两个索引相等，意味着在这个递归的子任务中，只考虑 preorder 数组中的一个元素
-        // 这个元素是当前子树的根节点，并且由于没有其他元素，说明这个根节点没有左子树或右子树
+        // 如果这是最后一个节点，没有左或右子树
         if (preStart == preorder.length - 1 || postStart == postEnd) {
             return root; 
         }
@@ -27,7 +26,6 @@ class Solution {
         int leftIndex = map.get(leftVal); // 找到postorder里面左的位置
         int leftSize = leftIndex - postStart + 1; // 计算左子树一共有几个孩子
 
-        // 这里postEnd要包括leftIndex因为它是左子树最后一个孩子
         root.left = helper(preorder, preStart + 1, postorder, postStart, leftIndex); 
         root.right = helper(preorder, preStart + leftSize + 1, postorder, leftIndex + 1, postEnd - 1);
 
