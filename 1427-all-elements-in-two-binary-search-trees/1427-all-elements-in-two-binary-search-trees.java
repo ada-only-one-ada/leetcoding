d7cb15f2340597d1d6d050630017d9fe633e14b6
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-11-21 15:43:00 -0800

    Time: 16 ms (90.49%), Space: 46.2 MB (53.36%) - LeetHub

---
+++ 1427-all-elements-in-two-binary-search-trees/1427-all-elements-in-two-binary-search-trees.java
@@ -0,0 +1,41 @@
+class Solution {
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        inorder(root1, list1);
+        inorder(root2, list2);
+
+        List<Integer> res = new ArrayList<>();
+
+        int i = 0, j = 0;
+        while (i < list1.size() && j < list2.size()) {
+            if (list1.get(i) < list2.get(j)) {
+                res.add(list1.get(i));
+                i++;
+            } else {
+                res.add(list2.get(j));
+                j++;
+            }
+        }
+
+        while (i < list1.size()) {
+            res.add(list1.get(i));
+            i++;
+        }
+
+        while (j < list2.size()) {
+            res.add(list2.get(j));
+            j++;
+        }
+
+        return res;
+    }
+
+    public void inorder(TreeNode root, List<Integer> list) {
+        if (root == null) return;
+
+        inorder(root.left, list);
+        list.add(root.val);
+        inorder(root.right, list);
+    }
+}
