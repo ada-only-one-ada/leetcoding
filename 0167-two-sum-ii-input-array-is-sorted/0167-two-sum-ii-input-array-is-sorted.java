9eaa2019716766b2424d7413fa02d77b85c4069e
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-11-21 00:07:53 -0800

    Time: 4 ms (20.61%), Space: 47.2 MB (39.9%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,17 +1,25 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int i = 0, j = numbers.length - 1;
+        int i = 0;
 
-        while (i < j) {
-            int sum = numbers[i] + numbers[j];
-            if (sum == target) {
-                return new int[]{i + 1, j + 1};
-            } else if (sum < target) {
-                i++;
-            } else {
-                j--;
+        while (i < numbers.length - 1) {
+            int left = i + 1;
+            int right = numbers.length;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                int sum = numbers[i] + numbers[mid];
+
+                if (sum == target) {
+                    return new int[]{i + 1, mid + 1};
+                } else if (sum < target) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
             }
+            i++;
         }
+
 
         return new int[0];
     }
