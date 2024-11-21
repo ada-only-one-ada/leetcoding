b50d58db9ddf7b0d7df898f23fd802c37a805eeb
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-11-20 18:39:47 -0800

    Time: 0 ms (100%), Space: 41.7 MB (84.15%) - LeetHub

--- 1079-sum-of-root-to-leaf-binary-numbers/1079-sum-of-root-to-leaf-binary-numbers.java
+++ 1079-sum-of-root-to-leaf-binary-numbers/1079-sum-of-root-to-leaf-binary-numbers.java
@@ -7,6 +7,8 @@ class Solution {
         if (node == null) return 0;
         
         // 更新当前路径的值（将前一个值乘以2，再加上当前节点的值）
+        // 当前的（最后）总是2的0次方
+        // 前一个比当前多了一个2的次方
         currentSum = currentSum * 2 + node.val;
 
         // 如果是叶子节点，直接返回当前路径累积的值
