d000a3fc98f77e1f11c74c3251cd08743beb6d06
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-11-22 11:15:08 -0800

    Time: 0 ms (100%), Space: 41.3 MB (94.63%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -1,31 +1,21 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
         ListNode dummy = new ListNode(-1);
         dummy.next = head;
-
-        ListNode fast = dummy;
         ListNode slow = dummy;
+        ListNode fast = dummy;
 
-        for (int i = 0; i < n; i++) {
+        // 快指针走 n+1 步
+        for (int i = 0; i <= n; i++) {
             fast = fast.next;
         }
 
-        while (fast != null && fast.next != null) {
-            fast = fast.next;
+        while (fast != null ){
             slow = slow.next;
+            fast = fast.next;
         }
 
         slow.next = slow.next.next;
-        return dummy.next;
+        return dummy.next;     
     }
 }
