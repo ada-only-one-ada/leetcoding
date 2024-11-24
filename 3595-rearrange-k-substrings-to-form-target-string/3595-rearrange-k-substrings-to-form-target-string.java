1525ff4cee1bce4365209931fc8823112107676a
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-11-24 14:29:59 -0800

    Time: 72 ms (100%), Space: 49.6 MB (100%) - LeetHub

--- 3595-rearrange-k-substrings-to-form-target-string/3595-rearrange-k-substrings-to-form-target-string.java
+++ 3595-rearrange-k-substrings-to-form-target-string/3595-rearrange-k-substrings-to-form-target-string.java
@@ -4,21 +4,19 @@ class Solution {
         if (s.length() != t.length()) return false;
 
         Map<String, Integer> targetMap = new HashMap<>();
-        Map<String, Integer> map = new HashMap<>();
-
         int len = s.length() / k;
         
         for (int i = 0; i + len - 1 < s.length(); i += len) {
             String ts = t.substring(i, i + len);
-            String ss = s.substring(i, i + len);
-
             targetMap.put(ts, targetMap.getOrDefault(ts, 0) + 1);
-            map.put(ss, map.getOrDefault(ss, 0) + 1);
         }
 
-        for (String key: targetMap.keySet()) {
-            if (!map.containsKey(key)) return false;
-            if (!map.get(key).equals(targetMap.get(key))) return false;
+        for (int i = 0; i + len - 1 < s.length(); i += len) {
+            String ss = s.substring(i, i + len);
+            if (!targetMap.containsKey(ss)) return false;
+
+            targetMap.put(ss, targetMap.get(ss) - 1);
+            if (targetMap.get(ss) == 0) targetMap.remove(ss);
         }
 
         return true;
