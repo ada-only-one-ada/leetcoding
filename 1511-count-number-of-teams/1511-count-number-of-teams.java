8708f0605565cd6009dceeceb1094f9985cfd24d
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-11-24 18:43:31 -0800

    Time: 19 ms (49.24%), Space: 42.5 MB (45.07%) - LeetHub

--- 1511-count-number-of-teams/1511-count-number-of-teams.java
+++ 1511-count-number-of-teams/1511-count-number-of-teams.java
@@ -1,34 +1,37 @@
 class Solution {
     public int numTeams(int[] rating) {
+        int n = rating.length;
+        int[] lessLeft = new int[n];
+        int[] greaterLeft = new int[n];
+        int[] lessRight = new int[n];
+        int[] greaterRight = new int[n];
+        
+        // 从左到右计算每个元素的 lessLeft 和 greaterLeft
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (rating[j] < rating[i]) {
+                    lessLeft[i]++;
+                } else if (rating[j] > rating[i]) {
+                    greaterLeft[i]++;
+                }
+            }
+        }
+
+        // 从右到左计算每个元素的 lessRight 和 greaterRight
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j > i; j--) {
+                if (rating[j] < rating[i]) {
+                    lessRight[i]++;
+                } else if (rating[j] > rating[i]) {
+                    greaterRight[i]++;
+                }
+            }
+        }
+
+        // 计算结果
         int res = 0;
-
-        for (int i = 0; i < rating.length; i++) {
-            int[] freq = count(rating, i);
-            // 左边比他小的人数 × 右边比他大的人数 + 左边比他大的人数 × 右边比他小的人数
-            res += freq[0] * freq[3] + freq[1] * freq[2];
-        }
-       
-        return res;
-    }
-
-    public int[] count(int[] rating, int index) {
-        // 【左边比它小，左边比它大，右边比它小，右边比它大】
-        int[] res = new int[4];
-
-        for (int i = 0; i < index; i++) {
-            if (rating[i] < rating[index]) {
-                res[0]++;
-            } else if (rating[i] > rating[index]) {
-                res[1]++;
-            }
-        }
-
-        for (int i = index + 1; i < rating.length; i++) {
-            if (rating[i] < rating[index]) {
-                res[2]++;
-            } else if (rating[i] > rating[index]) {
-                res[3]++;
-            }
+        for (int i = 0; i < n; i++) {
+            res += lessLeft[i] * greaterRight[i] + greaterLeft[i] * lessRight[i];
         }
 
         return res;
