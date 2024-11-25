7d66a2798cc00d0d8f2d456c206e6f1186f6743c
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-11-24 17:19:57 -0800

    Time: 2 ms (100%), Space: 58.7 MB (92.09%) - LeetHub

--- 2233-number-of-smooth-descent-periods-of-a-stock/2233-number-of-smooth-descent-periods-of-a-stock.java
+++ 2233-number-of-smooth-descent-periods-of-a-stock/2233-number-of-smooth-descent-periods-of-a-stock.java
@@ -1,22 +1,19 @@
 class Solution {
     public long getDescentPeriods(int[] prices) {
-        // 递减数列by1:【4，3，2】，【3，2】，【2】
-
-        // 【3，2，1，4】
-        //  1  1  1  1
-        //     1  2  
-
-        int[] dp = new int[prices.length];
-        Arrays.fill(dp, 1);
-
-        long res = dp[0];
-        for (int i = 1; i < dp.length; i++) {
+        if (prices.length == 0) return 0;
+        
+        long res = 1;
+        int prev = 1;
+        
+        for (int i = 1; i < prices.length; i++) {
             // 当前数字可以和之前的每个字数组都组成新的递减字数组
             if (prices[i - 1] - 1 == prices[i]) {
-                dp[i] += dp[i-1];
+                res += prev + 1;
+                prev++;
+            } else {
+                res += 1;
+                prev = 1;
             }
-
-            res += dp[i];
         } 
 
         return res;
