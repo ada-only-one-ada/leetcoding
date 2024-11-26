6fbc237250873f796d16d255f02b4c041f5aa9ef
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-11-25 16:34:43 -0800

    Time: 1 ms (100%), Space: 41 MB (48.85%) - LeetHub

---
+++ 2561-number-of-distinct-averages/2561-number-of-distinct-averages.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int distinctAverages(int[] nums) {
+        Arrays.sort(nums);
+        Set<Integer> set = new HashSet<>();
+
+        int i = 0;
+        int j = nums.length - 1;
+        while (i < j) {
+            set.add(nums[i] + nums[j]);
+            i++;
+            j--;
+        }
+
+        return set.size();
+    }
+}
