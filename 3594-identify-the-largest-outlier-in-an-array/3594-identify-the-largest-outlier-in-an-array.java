a6eda8574cb45530cbe0565592c96d87207dfee3
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-01 00:22:46 -0800

    Time: 83 ms (100%), Space: 61.6 MB (100%) - LeetHub

--- 3594-identify-the-largest-outlier-in-an-array/3594-identify-the-largest-outlier-in-an-array.java
+++ 3594-identify-the-largest-outlier-in-an-array/3594-identify-the-largest-outlier-in-an-array.java
@@ -13,7 +13,10 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             int currSum = (sum - nums[i]) / 2;
             int remain = (sum - nums[i]) % 2;
-
+            
+            // [6,-31,50,-35,41,37,-42,13], output: 13, Expected: -35
+            // sum = 39
+            // 假设最后一个数字13是outlier, (39-13)/2 = 13, 这里是错的, 有一个sum存在, 不能是outlier同一个index
             if (remain == 0 && map.containsKey(currSum) && map.get(currSum) != i) {
                 maxOutlier = Math.max(maxOutlier, nums[i]);
             }
