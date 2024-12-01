7d99b143752773e6b86d3a5fe2e35ef116ce9152
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-01 00:11:11 -0800

    Time: 68 ms (100%), Space: 57.8 MB (100%) - LeetHub

--- 3594-identify-the-largest-outlier-in-an-array/3594-identify-the-largest-outlier-in-an-array.java
+++ 3594-identify-the-largest-outlier-in-an-array/3594-identify-the-largest-outlier-in-an-array.java
@@ -1,25 +1,26 @@
 class Solution {
     public int getLargestOutlier(int[] nums) {
-        // [x, y, z, x+y+z, outlier]
-        // 去掉outlier，整个数组的和就是 2(x+y+z)
+        /* 
+        Hint: What will be the value of array sum if we remove the outlier from it?
+        [x, y, z, x+y+z, outlier]，去掉outlier，整个数组的和就是 2(x+y+z)
+        比如，[-2,-1,-3,-6,4]，去掉4，[-2,-1,-3,-6]，sum is -12
 
-        // [-2,-1,-3,-6,4]
-        // [-2,-1,-3,-6] sum is -12
-
-        // edge case: [8,1,-8,-3,6], Expected: -8, Output: 1
+        edge case: [8,1,-8,-3,6], Expected: -8, Output: 1
+        */
 
         Map<Integer, Integer> map = new HashMap<>();
         int sum = 0;
 
         for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i] * 2, i);
+            map.put(nums[i] * 2, i); // 这里为了避免后面需要处理有余数的情况
             sum += nums[i];
         }
         
         int maxOutlier = Integer.MIN_VALUE;
 
         for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey((sum - nums[i])) && map.get(sum - nums[i]) != i) {
+            int doubleSum = sum - nums[i];
+            if (map.containsKey(doubleSum) && map.get(doubleSum) != i) {
                 maxOutlier = Math.max(maxOutlier, nums[i]);
             }
         }
