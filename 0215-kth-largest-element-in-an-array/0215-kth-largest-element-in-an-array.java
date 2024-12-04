95414767e93248c817a151b8022cd376c21f4cdc
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-04 13:29:24 -0800

    Time: 4 ms (97.04%), Space: 55.9 MB (97.86%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,32 +1,21 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        return quickSelect(nums, 0, nums.length - 1, nums.length - k);
-    }
+        int[] freq = new int[20001];
+        int offset = 10000;
 
-    // [1,2,6,100,  200   ,300,400], k = 3, 7 - 3 = 4
-
-    public int quickSelect(int[] nums, int left, int right, int kSmallest) {
-        int i = left;
-        int j = right;
-        int pivot = nums[left];
-
-        while (i <= j) {
-            while (i <= j && nums[i] < pivot) i++;  // 这里不能是<=
-            // 比如【3，3，3，3】在上面这个loop里，i会到数组长度，j不变，在上面这行就退出整个 whileloop了
-            while (i <= j && nums[j] > pivot) j--;
-
-            if (i <= j) {
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp; 
-                i++;
-                j--;
-            }  
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            freq[num + offset]++;
+            max = Math.max(max, num + offset);
         }
 
-        // 在这里的时候，left没变，j也没变，造成stack overflow了
-        if (kSmallest <= j) return quickSelect(nums, left, j, kSmallest);
-        if (kSmallest >= i) return quickSelect(nums, i, right, kSmallest);
-        return nums[kSmallest];
+        for (int i = max; i >= 0; i--) {
+            if (freq[i] == 0) continue;
+            k -= freq[i];
+            if (k <= 0) return i - offset;
+        }
+
+        return -1;
     }
 }
