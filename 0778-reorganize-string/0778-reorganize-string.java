4b3b1f97d9b4f1203e31adde792786e074f8b02e
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-04 13:46:09 -0800

    Time: 5 ms (47.31%), Space: 42 MB (17.89%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.java
+++ 0778-reorganize-string/0778-reorganize-string.java
@@ -1,41 +1,40 @@
 class Solution {
     public String reorganizeString(String s) {
         int[] freq = new int[26];
-        for (char c: s.toCharArray()) {
-            freq[c - 'a']++;
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
         }
 
-        Queue<Integer> queue = new PriorityQueue<>((a, b) -> freq[b] - freq[a]);
+        Queue<Pair<Character, Integer>> queue = new PriorityQueue<>((a, b) -> {
+            return b.getValue() - a.getValue();
+        });
+
         for (int i = 0; i < 26; i++) {
-            if (freq[i] > 0) {
-                queue.add(i);
-            }
+            if (freq[i] == 0) continue;
+            queue.add(new Pair((char)(i+'a') , freq[i]));
         }
+        StringBuilder sb = new StringBuilder();
 
-        StringBuilder res = new StringBuilder();
         while (!queue.isEmpty()) {
-            // 剩下一种字母，且数量超过一个，直接返回
-            if (queue.size() == 1 && freq[queue.peek()] > 1) {
-                return "";
+            if (queue.size() == 1 && queue.peek().getValue() > 1) return "";
+            List<Pair<Character, Integer>> wl = new ArrayList<>();
+
+            for (int i = 0; i < 2 && !queue.isEmpty(); i++) {
+                Pair<Character, Integer> curr = queue.poll();
+                char currChar = curr.getKey();
+                sb.append(currChar);
+
+                int currFreq = curr.getValue();
+                if (currFreq - 1 > 0) {
+                    wl.add(new Pair(currChar, currFreq - 1));
+                } 
             }
 
-            if (queue.size() == 1) {
-                res.append((char)(queue.poll() + 'a'));
-                break;
-            } else if (queue.size() >= 2) {
-                int i1 = queue.poll();
-                int i2 = queue.poll();
-
-                res.append((char)(i1 + 'a'));
-                res.append((char)(i2 + 'a'));
-
-                freq[i1]--;
-                freq[i2]--;
-                if (freq[i1] > 0) queue.add(i1);
-                if (freq[i2] > 0) queue.add(i2);
+            for (Pair<Character, Integer> pair: wl) {
+                queue.add(pair);
             }
         }
 
-        return res.toString();
+        return sb.toString();
     }
 }
