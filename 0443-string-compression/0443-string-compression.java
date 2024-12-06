e416f187ec044ab17f3fbe5d782c7736b2f08cd6
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-05 16:40:45 -0800

    Time: 1 ms (99.74%), Space: 44 MB (46.34%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int compress(char[] chars) {
+        int repeated = 1;
+        int filledIndex = 0;
+
+        for (int i = 1; i < chars.length; i++) {
+            if (chars[i] == chars[i - 1]) {
+                repeated++;
+            } else if (chars[i] != chars[i - 1]) {
+                chars[filledIndex] = chars[i - 1];
+                filledIndex++;
+                if (repeated == 1) continue;
+                
+                String strRepeated = String.valueOf(repeated);
+                for (int j = 0; j < strRepeated.length(); j++) {
+                    chars[filledIndex++] = strRepeated.charAt(j);
+                }
+
+                repeated = 1;
+            }
+        }
+
+        chars[filledIndex] = chars[chars.length - 1];
+        filledIndex++;
+        if (repeated > 1) {
+            String strRepeated = String.valueOf(repeated);
+            for (int j = 0; j < strRepeated.length() && filledIndex < chars.length; j++) {
+                chars[filledIndex++] = strRepeated.charAt(j);
+            }
+        }
+
+        return filledIndex;
+    }
+}
