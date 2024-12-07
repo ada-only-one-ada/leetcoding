bbe69476d72c16dda40773afba328051ebb98380
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-07 09:14:32 +0800

    Time: 1 ms (100%), Space: 42.1 MB (62.41%) - LeetHub

--- 0872-split-array-into-fibonacci-sequence/0872-split-array-into-fibonacci-sequence.java
+++ 0872-split-array-into-fibonacci-sequence/0872-split-array-into-fibonacci-sequence.java
@@ -16,10 +16,15 @@ class Solution {
 
         for (int i = index; i < nums.length; i++) {
             // i > index 说明超过一位数，且首位是0，不符合题目要求
+            // 使用 break 可以直接退出整个循环，因为后续的更长数字都将无效地以0开始
             if (i > index && nums[index] == 0) break;
 
             long num = getNum(nums, index, i);
+            
+            // 如果计算出来的数字超过了 Integer.MAX_VALUE，那么更长的数字肯定也会超过 Integer.MAX_VALUE。继续循环没有意义
             if (num > Integer.MAX_VALUE) break;
+            // 如果当前数字大于结果列表中最后两个数的和，这意味着任何更长的数字（即包含更多数字的组合）也会更大
+            // 因此，没有必要继续增加更多的数字来尝试
             if (res.size() >= 2 && num > res.get(res.size() - 1) + res.get(res.size() - 2)) break;
 
             if (res.size() < 2 || num == res.get(res.size() - 1) + res.get(res.size() - 2)) {
