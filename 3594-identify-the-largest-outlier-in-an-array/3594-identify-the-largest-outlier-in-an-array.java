7cc6cb356a31463b46f20e6e4a3920c91fe7da5f
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-08 10:56:53 -0800

    Time: 97 ms (54.89%), Space: 60.7 MB (27.92%) - LeetHub

--- 3594-identify-the-largest-outlier-in-an-array/3594-identify-the-largest-outlier-in-an-array.java
+++ 3594-identify-the-largest-outlier-in-an-array/3594-identify-the-largest-outlier-in-an-array.java
@@ -1,27 +1,25 @@
 class Solution {
     public int getLargestOutlier(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
         int sum = 0;
-
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], i); 
             sum += nums[i];
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
-        
-        int maxOutlier = Integer.MIN_VALUE;
 
+        int max = Integer.MIN_VALUE;
         for (int i = 0; i < nums.length; i++) {
-            int currSum = (sum - nums[i]) / 2;
-            int remain = (sum - nums[i]) % 2;
-            
-            // [6,-31,50,-35,41,37,-42,13], output: 13, Expected: -35
-            // sum = 39
-            // 假设最后一个数字13是outlier, (39-13)/2 = 13, 这里是错的, 有一个sum存在, 不能是outlier同一个index
-            if (remain == 0 && map.containsKey(currSum) && map.get(currSum) != i) {
-                maxOutlier = Math.max(maxOutlier, nums[i]);
+            int doubleSum = sum - nums[i];
+            int target = doubleSum / 2;
+
+            if (map.containsKey(target) && doubleSum % 2 == 0) {
+                if (target == nums[i]) {
+                    if (map.get(target) == 1) continue;
+                }
+                max = Math.max(max, nums[i]);
             }
         }
 
-        return maxOutlier;
+        return max;
     }
 }
