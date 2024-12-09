50dbef825eac4a70c18114a978e9129f570a0434
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-08 17:13:17 -0800

    Time: 2 ms (84.57%), Space: 44.7 MB (29.07%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,12 +1,13 @@
 class Solution {
+    List<List<Integer>> res;
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> res = new ArrayList<>();
-        backtracking(candidates, target, res, new ArrayList<>(), 0, 0);
+        res = new ArrayList<>();
+        backtracking(candidates, target, 0, new ArrayList<>(), 0);
         return res;
     }
 
-    public void backtracking(int[] candidates, int target, List<List<Integer>> res, List<Integer> currRes, int sum, int index) {
-        if (sum >= target) {
+    public void backtracking(int[] candidates, int target, int index, List<Integer> currRes, int sum) {
+        if (index == candidates.length || sum >= target) {
             if (sum == target) {
                 res.add(new ArrayList<>(currRes));
             }
@@ -14,12 +15,13 @@ class Solution {
         }
 
         for (int i = index; i < candidates.length; i++) {
-            currRes.add(candidates[i]);
             sum += candidates[i];
-            // i可以再取，但不可以往前取，所以start记录从每次开始的位置
-            backtracking(candidates, target, res, currRes, sum, i);
-            currRes.remove(currRes.size() - 1);
+            currRes.add(candidates[i]);
+
+            backtracking(candidates, target, i, currRes, sum);
+
             sum -= candidates[i];
+            currRes.remove(currRes.size() - 1);
         }
     }
 }
