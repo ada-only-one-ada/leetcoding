e66063dbb4880710ef625c68b99851dfdcd9b198
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-10 22:02:22 -0800

    Time: 0 ms (100%), Space: 40.8 MB (7.33%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.java
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.java
@@ -9,3 +9,21 @@ class Solution {
         return a;
     }
 }
+
+/*
+Initialization:
+a = 1 (in binary: 01)
+b = 2 (in binary: 10)
+
+1.temp = (a & b) << 1
+a & b = 01 & 10 = 00 (bitwise AND), 00 << 1 is 00 (shift left by one, still 00)
+
+2.a = a ^ b
+a ^ b is 01 ^ 10 = 11 (bitwise XOR, which gives the sum without carry)
+
+3.b = temp
+b becomes 00
+
+Loop Exit Condition: Since b is now 0, the loop exits.
+The final value of a is 11 in binary, which is 3 in decimal.
+*/
