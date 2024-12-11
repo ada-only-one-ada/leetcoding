61bf5c77cbc9839b4985c4daa3ae227ca130587a
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-11 14:32:37 -0800

    Time: 7 ms (93.71%), Space: 45.5 MB (31.76%) - LeetHub

--- 0962-flip-string-to-monotone-increasing/0962-flip-string-to-monotone-increasing.java
+++ 0962-flip-string-to-monotone-increasing/0962-flip-string-to-monotone-increasing.java
@@ -3,6 +3,8 @@ class Solution {
         // edge case: 1001110011
 
         /* 
+          当遍历到0 选择保留 那么之前的1设为0 选择不保留 反转次数+1 取两者较小值即可。
+
           根据题意，0左边不能有任何1，比如：0000111
           当前字符是0，保留这个0，那么之前的1都要反转；
           当前字符是0，不保留这个0，即反转这个0变成1，
@@ -12,10 +14,10 @@ class Solution {
         int res = 0;
 
         int ones = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '1') {
+        for (char num: s.toCharArray()) {
+            if (num == '1') {
                 ones++;
-            } else if (s.charAt(i) == '0') {
+            } else if (num == '0') {
                 // res 是之前都是妥善处理过的string，加上当前这次反转0为1
                 res = Math.min(res + 1, ones);   
             }
