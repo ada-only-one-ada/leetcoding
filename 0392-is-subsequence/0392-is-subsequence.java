101154b8a6280aa43a877b54d2fcba7767a5ac5a
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-12 00:05:16 -0800

    Time: 7 ms (5.93%), Space: 42.6 MB (6.19%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,17 +1,19 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        int checkIndex = 0;
+        //最长公共子序列
+        int[][] dp = new int[s.length()+1][t.length()+1];
 
-        int i = 0;
-        while (checkIndex < s.length() && i < t.length()) {
-            if (s.charAt(checkIndex) == t.charAt(i)) {
-                checkIndex++;
-                i++;
-            } else {
-                i++;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 1; j <= t.length(); j++) {
+                if (s.charAt(i-1) == t.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                } else{
+                    dp[i][j] = dp[i][j-1]; //删掉当前一个不相等的j
+                }
             }
         }
 
-        return checkIndex == s.length();
+        if (dp[s.length()][t.length()] == s.length()) return true;
+        return false; 
     }
 }
