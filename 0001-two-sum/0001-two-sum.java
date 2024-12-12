9fe3b782b034b4f6374a1dceeb634f6ea0b06df9
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-12 00:18:58 -0800

    Time: 2 ms (98.91%), Space: 45.3 MB (8.38%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,15 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
-        
         for (int i = 0; i < nums.length; i++) {
-            int val = target - nums[i];
-            if (map.containsKey(val)) {
-                return new int[]{i, map.get(val)};
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{i, map.get(target - nums[i])};
             }
             map.put(nums[i], i);
         }
 
-        return new int[]{};
+        return new int[]{-1, -1};
     }
 }
