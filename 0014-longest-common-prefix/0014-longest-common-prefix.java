0032ca38943ea02ce88e874dc689c8eb41c13894
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-11 23:04:43 -0800

    Time: 0 ms (100%), Space: 41.4 MB (59.94%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,14 +1,15 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        Arrays.sort(strs);
-      
-        String first = strs[0];
-        String last = strs[strs.length - 1];
-        
-        while (last.indexOf(first) != 0){
-            first = first.substring(0, first.length() - 1);
+        if (strs.length == 0) return "";
+
+        String candidate = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            String word = strs[i];
+            while (word.indexOf(candidate) != 0) {
+                candidate = candidate.substring(0, candidate.length() - 1);
+            }
         }
-        
-        return first;
+
+        return candidate;
     }
 }
