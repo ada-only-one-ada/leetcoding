6c645b9681ce39bb5b11a204ef093c4437db7a2d
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-11 20:18:06 -0800

    Time: 0 ms (100%), Space: 42.1 MB (14.98%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,15 +1,11 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
         int left = 0;
-        int right = 0;
-        while (right < nums.length) {
-            if (nums[right] == val) {
-                right++;
-            } else if (nums[right] != val) {
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] != val) {
                 nums[left] = nums[right];
                 left++;
-                right++;
-            }
+            } 
         }
 
         return left;
