55103cc0a0a9f68ded7e07b41e3eb082a7a91685
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-11 22:18:24 -0800

    Time: 2 ms (77.22%), Space: 61.5 MB (64.58%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -6,6 +6,7 @@ class Solution {
         int res = Integer.MIN_VALUE;
         int min = prices[0];        
 
+        // 注意不能更新max，因为不能保证max在min之后
         for (int i = 1; i < prices.length; i++) {
             res = Math.max(res, prices[i] - min);
             min = Math.min(min, prices[i]);
