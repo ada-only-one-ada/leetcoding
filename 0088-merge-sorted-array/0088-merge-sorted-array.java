ba37c847db4a0139cd307538e0419a94978852e2
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-11 20:14:43 -0800

    Time: 0 ms (100%), Space: 42 MB (82%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,23 +1,25 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i1 = m - 1;
-        int i2 = n - 1;
-        int index = m + n - 1;
+        int index = nums1.length - 1;
 
-        while (i1 >= 0 && i2 >= 0) {
-            if (nums1[i1] >= nums2[i2]) {
-                nums1[index--] = nums1[i1--];
+        while (m - 1 >= 0 && n - 1 >= 0) {
+            if (nums1[m - 1] >= nums2[n - 1]) {
+                nums1[index--] = nums1[m - 1];
+                m--;
             } else {
-                nums1[index--] = nums2[i2--];
+                nums1[index--] = nums2[n - 1];
+                n--;
             }
         }
 
-        while (i1 >= 0 && index >= 0) {
-            nums1[index--] = nums1[i1--];
+        while (m - 1 >= 0) {
+            nums1[index--] = nums1[m - 1];
+            m--;
         }
 
-        while (i2 >= 0 && index >= 0) {
-            nums1[index--] = nums2[i2--];
+        while (n - 1 >= 0) {
+            nums1[index--] = nums2[n - 1];
+            n--;
         }
     }
 }
