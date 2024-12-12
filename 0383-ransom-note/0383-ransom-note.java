68b2bd79a011b79af7c1e8e2b8f8f6dbb6435420
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-12 00:11:20 -0800

    Time: 1 ms (99.53%), Space: 45 MB (47.73%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,17 +1,16 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        
-        for (char c: magazine.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
+        int[] freq = new int[26];
+        for (char c: ransomNote.toCharArray()) {
+            freq[c - 'a']++;
         }
 
-        for (char c: ransomNote.toCharArray()){
-            if (map.containsKey(c) && map.get(c) > 0){
-                map.put(c, map.get(c) - 1);
-            } else {
-                return false;
-            }
+        for (char c: magazine.toCharArray()) {
+            freq[c - 'a']--;
+        }
+
+        for (int f: freq) {
+            if (f > 0) return false;
         }
 
         return true;
