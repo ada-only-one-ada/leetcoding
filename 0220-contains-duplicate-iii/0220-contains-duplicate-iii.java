fc84ac74078a4a17c92ae61fa3c26f757ec57eae
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-12 15:22:14 -0800

    Time: 164 ms (45.8%), Space: 63.9 MB (11.49%) - LeetHub

--- 0220-contains-duplicate-iii/0220-contains-duplicate-iii.java
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.java
@@ -1,38 +1,21 @@
 class Solution {
-    int bucketSize;
-
     public boolean containsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff) {
-        bucketSize = valueDiff + 1;
-        Map<Integer, Integer> map = new HashMap<>();
+        TreeSet<Integer> treeSet = new TreeSet<>();
 
         for (int i = 0; i < nums.length; i++) {
-            int currBucketId = getBucketId(nums[i]);
-            // 桶内已有元素了，说明存在差值范围在valueDiff内的另一个值，直接返回true
-            // 比如和我生日差30天的人，那么同一个月份的肯定是在这个范围内
-            if (map.containsKey(currBucketId)) return true;
+            Integer smallerEqual = treeSet.floor(nums[i]);
+            Integer largerEqual = treeSet.ceiling(nums[i]);
 
-            // 或者检查上/下一个月的，也有可能差值在30天内
-            int prevBucketId = currBucketId - 1;
-            int nextBucketId = currBucketId + 1;
-            if (map.containsKey(prevBucketId) && nums[i] - map.get(prevBucketId) <= valueDiff) return true;
-            if (map.containsKey(nextBucketId) && map.get(nextBucketId) - nums[i] <= valueDiff) return true;
+            if (smallerEqual != null && nums[i] - smallerEqual <= valueDiff) return true;
+            if (largerEqual != null && largerEqual - nums[i] <= valueDiff) return true;
+            treeSet.add(nums[i]);
 
-            map.put(currBucketId, nums[i]);
-
-            // 比如我遍历到3，这时候要移除index为0的，因为下一轮for loop是遍历到4的位置
+            // [11,8,4,11,19]，比如遍历到11，会把两个11都移除，不用担心因为移除11之前会检查所有符合的条件
             if (i >= indexDiff) {
-                int oldBucketId = getBucketId(nums[i - indexDiff]);
-                map.remove(oldBucketId);
+                treeSet.remove(nums[i - indexDiff]);
             }
         }
+        
         return false;
-    }
-
-    public int getBucketId(int num) {
-        if (num >= 0) {
-            return num / bucketSize;
-        } else {
-            return (num + 1) / bucketSize - 1;
-        }
     }
 }
