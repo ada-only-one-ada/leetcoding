18aa28cf73b3f01f0c260b78105d725640c0b0c9
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-11 23:10:01 -0800

    Time: 0 ms (100%), Space: 41.5 MB (57.49%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,6 +1,5 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        if (haystack.length() < needle.length()) return -1;
 
         return haystack.indexOf(needle);
     }
