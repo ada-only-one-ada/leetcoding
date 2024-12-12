9ad68e90595874cdc9312b0c0713cd93ba7b15f7
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-11 20:35:42 -0800

    Time: 1 ms (99.88%), Space: 53.6 MB (7.21%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,7 +1,22 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-       
-        return nums[nums.length/2];
+        // O(n) time and O(1) space
+
+        int candidate = nums[0];
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == candidate) {
+                count++;
+            } else {
+                count--;
+                if (count == -1) {
+                    candidate = nums[i];
+                    count = 0;
+                }
+            }
+        }
+
+        return candidate;
     }
 }
