7f5b3a95eefe1cb981357c7110e5d60804a5fef7
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-13 13:34:58 -0800

    Time: 0 ms (100%), Space: 41 MB (70.85%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -1,17 +1,14 @@
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        helper(root);
-        return root;
-    }
-
-    public void helper(TreeNode root) {
-        if (root == null) return;
+        if (root == null) return null;
 
         TreeNode temp = root.left;
         root.left = root.right;
         root.right = temp;
 
-        helper(root.left);
-        helper(root.right);
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;   
     }
 }
