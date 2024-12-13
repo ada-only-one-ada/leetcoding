ed5b6ece0655d15d7a54b1e37ef69255be38f4aa
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-13 00:02:51 -0800

    Time: 0 ms (100%), Space: 44.4 MB (58.86%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -1,18 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 public class Solution {
     public boolean hasCycle(ListNode head) {
         if (head == null) return false;
-       
-        Set<ListNode> visited = new HashSet<>();
-        
-        while (head != null){
-            if (visited.contains(head)){
-                return true;
-            }else{
-                visited.add(head);
-                head = head.next;
-            }
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) return true;
+            if (fast == null) return false;
         }
-        
+
         return false;
     }
 }
