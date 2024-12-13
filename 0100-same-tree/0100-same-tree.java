e32aa8b6ba9dd954fb2ecfdfe913594e8b34f7d2
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-13 00:31:52 -0800

    Time: 0 ms (100%), Space: 41 MB (49.19%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -3,10 +3,7 @@ class Solution {
         if (p == null && q == null) return true;
         if (p == null || q == null) return false;
 
-        if (p.val == q.val) {
-            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-        }
-        
-        return false;
+        if (p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
