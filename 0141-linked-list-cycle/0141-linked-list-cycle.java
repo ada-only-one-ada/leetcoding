151976f9e0218de28af6017428a3b94c8a2211bc
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-13 00:07:28 -0800

    Time: 0 ms (100%), Space: 44.5 MB (46.86%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -15,12 +15,11 @@ public class Solution {
 
         ListNode slow = head;
         ListNode fast = head;
-
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
 
-            if (fast == slow) return true;
+            if (slow == fast) return true;
         }
 
         return false;
