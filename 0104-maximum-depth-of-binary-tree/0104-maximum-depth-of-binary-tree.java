7db2abb64f5bca81498fad0bad9564a4d16af0b2
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-13 00:29:13 -0800

    Time: 0 ms (100%), Space: 42.7 MB (32.84%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -2,9 +2,6 @@ class Solution {
     public int maxDepth(TreeNode root) {
         if (root == null) return 0;
 
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
-
-        return 1 + Math.max(left, right);
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
     }
 }
