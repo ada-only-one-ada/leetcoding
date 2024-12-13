c75a22db148fa4b9687f6dadf5e3268a5c681c80
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-12 23:47:12 -0800

    Time: 2 ms (97.51%), Space: 42 MB (21.44%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -2,13 +2,12 @@ class Solution {
     public boolean isValid(String s) {
         Map<Character, Character> map = new HashMap<>();
         map.put('(', ')');
-        map.put('[', ']');
         map.put('{', '}');
+        map.put('[', ']');
 
         Stack<Character> stack = new Stack();
-
         for (char c: s.toCharArray()) {
-            if (c == '(' || c == '[' || c == '{') {
+            if (c == '(' || c == '{' || c == '[') {
                 stack.push(c);
             } else {
                 if (stack.isEmpty() || map.get(stack.pop()) != c) {
