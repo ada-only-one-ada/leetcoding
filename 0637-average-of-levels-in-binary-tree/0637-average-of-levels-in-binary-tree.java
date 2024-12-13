7952acc79838294c48550d584ddc9a2778b167ce
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-13 15:37:37 -0800

    Time: 2 ms (95.87%), Space: 45.7 MB (52.05%) - LeetHub

---
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> res = new ArrayList<>();
+        if (root == null) return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            double sum = 0;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                sum += node.val;
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            res.add(sum / size);
+        }
+
+        return res;
+    }
+}
