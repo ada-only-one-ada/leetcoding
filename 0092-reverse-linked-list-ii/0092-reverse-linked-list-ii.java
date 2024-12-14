592aaf94b9a7b1e2ba0d99d58aad829796ac0b33
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-14 00:39:04 -0800

    Time: 0 ms (100%), Space: 41.8 MB (5.53%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -1,52 +1,32 @@
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        // 如果没有需要反转的部分，直接返回原头结点
-        if (head == null || left == right) {
-            return head;
-        }
-
+        // 创建一个虚拟头节点，以便简化对头节点操作的复杂性
         ListNode dummy = new ListNode(-1);
         dummy.next = head;
-
-        // 找到反转区间的前一个节点
         ListNode prev = dummy;
-        for (int i = 0; i < left - 1; i++) {
-            prev = prev.next;
+        ListNode curr = head;
+
+        // 移动指针，找到反转区间的前一个节点
+        for (int i = 1; i < left; i++) {
+            prev = curr;
+            curr = curr.next;
+        }
+        // leftPart指向反转区间前的最后一个节点
+        ListNode leftPart = prev;
+
+        // 开始反转从left到right的节点
+        prev = null;
+        for (int i = left; i <= right; i++) {
+            ListNode nextHead = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextHead;
         }
 
-        // reverseStart 指向需要反转的第一个节点
-        ListNode reverseStart = prev.next;
-        // reverseEnd 指向需要反转的最后一个节点
-        ListNode reverseEnd = reverseStart;
-        // 第一个节点算一个，需要找 right-left+1 个
-        for (int i = 1; i < right - left + 1; i++) {
-            reverseEnd = reverseEnd.next;
-        }
-
-        // 记录反转部分的后一个节点
-        ListNode after = reverseEnd.next;
-        // 断开反转部分与后面的链表
-        reverseEnd.next = null;
-
-        // 反转从start到end的部分
-        prev.next = reverse(reverseStart);
-        // 连接反转后的部分与after
-        // 注意是从reverseStart连，比如例子里面的节点2，连接到5
-        reverseStart.next = after;
-
-        return dummy.next;
-    }
-
-    public ListNode reverse(ListNode head) {
-        ListNode prev = null;
-
-        while (head != null) {
-            ListNode nextHead = head.next;
-            head.next = prev;
-            prev = head;
-            head = nextHead;
-        }
-
-        return prev;
+        // 将反转后的部分连接回原链表
+        // leftPart.next指向反转后的第一个节点（原来的right位置的节点）
+        leftPart.next.next = curr; // 将反转部分的最后一个节点（原left位置的节点）连接到right后面的节点
+        leftPart.next = prev; // 将反转部分的开始连接到原链表中left之前的部分
+        return dummy.next; // 返回新的头节点
     }
 }
