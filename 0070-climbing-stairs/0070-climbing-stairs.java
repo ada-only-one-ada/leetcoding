e9dc23af37b7ab0192bbc42b71672b3647f77cba
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-13 17:06:37 -0800

    Time: 0 ms (100%), Space: 40.8 MB (9.67%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -7,7 +7,7 @@ class Solution {
         dp[2] = 2;
 
         for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+            dp[i] += dp[i - 1] + dp[i - 2];
         }
 
         return dp[n];
