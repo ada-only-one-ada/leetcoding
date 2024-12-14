cfac3988e7623540b8a9db71236fb1b7a0b3bd3f
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-13 18:19:23 -0800

    Time: 4 ms (100%), Space: 44 MB (57.7%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean isPalindrome(int x) {
         if (x < 0) return false;
-        // 1221
         int sum = 0;
         int copy = x;
 
         while (copy > 0) {
             sum = sum * 10 + copy % 10;
             copy /= 10;   
-            if (sum == x) return true;        
+            if (sum == x) return true;  
+            if (sum > x) return false;      
         }
 
         return sum == x;
