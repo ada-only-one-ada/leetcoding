3fc0ed754c1a4aeeac1515a65a9ed9f6f840c33f
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-13 23:08:59 -0800

    Time: 0 ms (100%), Space: 44.4 MB (74.05%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,9 +1,12 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
+        // 长度小于等于2，一定合法，返回长度即可
         if (nums.length <= 2) return nums.length;
 
         int replaceIndex = 2;
+        // 否则，从第三个数字开始loop
         for (int i = 2; i < nums.length; i++) {
+            // 如果当前数字跟即将替代的index的前一个或者前两个不一样，就可以取代
             if (nums[i] != nums[replaceIndex-1] || nums[i] != nums[replaceIndex-2]) {
                 nums[replaceIndex++] = nums[i];
             } 
