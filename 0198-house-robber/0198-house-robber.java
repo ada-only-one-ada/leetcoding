fab65e2e8c48c86b7e0dc976845fdf215f215aa7
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-14 02:26:52 -0800

    Time: 0 ms (100%), Space: 41 MB (43.43%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,14 +1,17 @@
 class Solution {
     public int rob(int[] nums) {
-        int[][] dp = new int[nums.length][2];
-        dp[0][0] = nums[0];
-        dp[0][1] = 0;
+        if (nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+        if (nums.length == 2) return Math.max(nums[0], nums[1]);
+        
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
 
-        for (int i = 1; i < dp.length; i++) {
-            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + nums[i]);
-            dp[i][1] = Math.max(dp[i-1][0], dp[i-1][1]);
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
         }
 
-        return Math.max(dp[nums.length - 1][0], dp[nums.length - 1][1]);
+        return dp[nums.length - 1];
     }
 }
