0cda760d136a8c55f67ae35cb23df2a860ae6c22
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-13 16:31:31 -0800

    Time: 0 ms (100%), Space: 43.4 MB (9.85%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -3,17 +3,21 @@ class Solution {
         int start = 0;
         int end = nums.length;
 
+        int res = -1;
         while (start < end) {
             int mid = start + (end - start) / 2;
             if (nums[mid] == target) {
                 return mid;
+            // 当前数字大于target，肯定在这之前
             } else if (nums[mid] > target) {
                 end = mid;
+            // 当前数字小于target，可能插入在当前位置之后一个，也可能之后
             } else if (nums[mid] < target) {
+                res = mid;
                 start = mid + 1;
             }
         }
 
-        return start;
+        return res + 1;
     }
 }
