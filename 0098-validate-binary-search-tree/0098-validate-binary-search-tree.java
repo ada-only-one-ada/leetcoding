b1195993ec40acefc0c72f9319171744fc028ebc
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-14 01:45:29 -0800

    Time: 0 ms (100%), Space: 44.3 MB (24.05%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -1,14 +1,12 @@
- class Solution {
+class Solution {
     public boolean isValidBST(TreeNode root) {
-        if (root == null) return true;
         return helper(root, null, null);
     }
 
     public boolean helper(TreeNode root, Integer min, Integer max) {
         if (root == null) return true;
-        if (min != null && root.val <= min) return false;
-        if (max != null && root.val >= max) return false;
 
+        if ((min != null && root.val <= min)|| (max != null && root.val >= max)) return false;
         return helper(root.left, min, root.val) && helper(root.right, root.val, max);
     }
 }
