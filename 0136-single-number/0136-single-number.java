70399b7116de7beee562b09c0071bbdf55ae5523
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-13 19:55:37 -0800

    Time: 1 ms (99.86%), Space: 46.7 MB (9.4%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,11 +1,13 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int num = nums[0];
-        
+        // 0 xor any = any;
+        // any xor any = 0;
+
+        int candidate = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            num ^= nums[i];
+            candidate = candidate ^ nums[i];
         }
 
-        return num;
+        return candidate;
     }
 }
