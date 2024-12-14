4c2eed420f473c5374ee4445b3207fc86b72349c
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-14 00:52:14 -0800

    Time: 8 ms (16.21%), Space: 44.8 MB (17.14%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String intToRoman(int num) {
+        int[] values =    {1000, 900, 500,  400,  100,  90,  50,   40,  10,  9,    5,   4,   1};
+        String[] romans = {"M", "CM", "D",  "CD", "C", "XC", "L", "XL", "X","IX", "V", "IV","I"};
+
+        TreeMap<Integer, String> treemap = new TreeMap<>();
+        treemap.put(1000, "M");
+        treemap.put(900, "CM");
+        treemap.put(500, "D");
+        treemap.put(400, "CD");
+        treemap.put(100, "C");
+        treemap.put(90, "XC");
+        treemap.put(50, "L");
+        treemap.put(40, "XL");
+        treemap.put(10, "X");
+        treemap.put(9, "IX");
+        treemap.put(5, "V");
+        treemap.put(4, "IV");
+        treemap.put(1, "I");
+
+        StringBuilder res = new StringBuilder();
+        while (num > 0) {
+            int lastEqualSmaller = treemap.floorKey(num);
+            int times = num / lastEqualSmaller;
+
+            for (int i = 0; i < times; i++) {
+                res.append(treemap.get(lastEqualSmaller));
+            }
+            num -= lastEqualSmaller * times;
+        }
+
+        return res.toString();
+    }
+}
