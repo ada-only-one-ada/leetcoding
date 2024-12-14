af24e0370624ced6249e02e052264bb533b9d4b7
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-13 17:25:40 -0800

    Time: 1 ms (86.94%), Space: 41.3 MB (6.21%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,23 +1,22 @@
 class Solution {
     public int mySqrt(int x) {
-        if (x <= 1) return x;
-
+        if (x == 0) return 0;
         int start = 1;
-        int end = x;
+        int end  = x;
 
-        int res = -1;
         while (start < end) {
             int mid = start + (end - start) / 2;
-            if (x / mid == mid) {
+
+            if (mid == x / mid) {       // mid * mid = x
                 return mid;
-            } else if (mid < x / mid) {
-                res = mid;
-                start = mid + 1;
-            } else if (mid > x / mid) {
-                end = mid;
+            } else if (mid < x / mid) { // mid * mid < x，有可能是这个mid 或者之后的mid
+               start = mid + 1;
+            } else if (mid > x / mid) { // mid * mid > x， 肯定不是这个mid
+               end = mid;
             }
         }
 
-        return res;
+        if (start > x / start) return start- 1; // start * start > x
+        return start;
     }
 }
