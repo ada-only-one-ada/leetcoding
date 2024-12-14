cc48073baa466a0995de7cf52512f011c25a6cd1
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-13 19:52:16 -0800

    Time: 2 ms (45.12%), Space: 42.1 MB (62.81%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,28 +1,24 @@
 class Solution {
     public String addBinary(String a, String b) {
-        StringBuilder res = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
 
         int i = a.length() - 1;
         int j = b.length() - 1;
-        int remainder = 0;
-       
-        while (i >= 0 || j >= 0) {
-            int numA = 0;
-            int numB = 0;
-            if (i >= 0) numA = Character.getNumericValue(a.charAt(i));
-            if (j >= 0) numB = Character.getNumericValue(b.charAt(j));
-            int sum = remainder + numA + numB;
 
-            res.append(sum % 2);
-            remainder = sum / 2;
+        int carry = 0;
+        while (i >= 0 || j >= 0) {
+            int digitA = 0;
+            int digitB = 0;
+            if (i >= 0) digitA = Character.getNumericValue(a.charAt(i));
+            if (j >= 0) digitB = Character.getNumericValue(b.charAt(j));
+
+            int sum = carry + digitA + digitB;
+            sb.append(sum % 2);
+            carry = sum / 2;
             i--;
             j--;
         }
-
-        if (remainder != 0) {
-            res.append(1);
-        }
-
-        return res.reverse().toString();
+        if (carry > 0) sb.append(carry);
+        return sb.reverse().toString();
     }
 }
