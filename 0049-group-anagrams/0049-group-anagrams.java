95a3b797e9c6a905d8fb1fbfeb49b6685fa4e990
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-13 23:23:41 -0800

    Time: 40 ms (7.82%), Space: 49.3 MB (5.91%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,32 +1,28 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
-        int[] freq = new int[26];
 
-        for (String str: strs) {                //O(n):n个string
-            Arrays.fill(freq, 0);
-            for (char c: str.toCharArray()) {         //O(k):each string takes k个时间统计freq（k是str的len）
-                freq[c - 'a']++;
+        for (String s: strs) {
+            int[] freq = new int[26];
+            for (int i = 0; i < s.length(); i++) {
+                freq[s.charAt(i) - 'a']++;
+            }
+            String key = "";
+            for (int i = 0; i < 26; i++) {
+                key += freq[i] + ",";
             }
 
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < 26; i++) {            //O(|Σ|) = (O(26)：遍历26个字母生成key
-                sb.append(freq[i]);
-                sb.append("#");
-             
-                //["bdddddddddd","bbbbbbbbbbc"], b1d10 and b10c1
-                // has #: 1#0#10 and 10#1#0
-                // no #: 1010 and 1010 -> wrong
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            } 
+            map.get(key).add(s);
+        }
 
-            }
-            String curr = sb.toString();
-
-            if (!map.containsKey(curr)) {             //O(1)：更新哈希表
-                map.put(curr, new ArrayList<>());
-            }
-            map.get(curr).add(str);
+        List<List<String>> res = new ArrayList<>();
+        for (List<String> vals: map.values()) {
+            res.add(vals);
         }
         
-        return new ArrayList<>(map.values());
+        return res;
     }
 }
