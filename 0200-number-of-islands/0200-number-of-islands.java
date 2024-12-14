1572501cd092995ec79b46183d8929a7a07f0f4c
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-14 01:02:15 -0800

    Time: 3 ms (86.88%), Space: 49.4 MB (58.37%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,29 +1,25 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int count = 0;
-        Set<String> set = new HashSet<>();
-
+        int res = 0;
         for (int row = 0; row < grid.length; row++) {
             for (int col = 0; col < grid[0].length; col++) {
-                if (grid[row][col] == '1' && !set.contains(row + " " + col)) {
-                    count++;
-                    dfs(grid, row, col, set);
+                if (grid[row][col] == '1') {
+                    res++;
+                    dfs(grid, row, col);
                 }
             }
         }
-
-        return count;
+        return res;
     }
 
-    public void dfs(char[][] grid, int row, int col, Set<String> set) {
-        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) return;
-        if (grid[row][col] == '0') return;
-        if (set.contains(row + " " + col)) return;
+    int[][] directions = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+    public void dfs(char[][] grid, int row, int col) {
+        if (row < 0 || row == grid.length || col < 0 || col == grid[0].length) return;
+        if (grid[row][col] != '1') return;
 
-        set.add(row + " "+ col);
-        dfs(grid, row + 1, col, set);
-        dfs(grid, row - 1, col, set);
-        dfs(grid, row, col + 1, set);
-        dfs(grid, row, col - 1, set);
+        grid[row][col] = '0';
+        for (int[] dir: directions) {
+            dfs(grid, row + dir[0], col + dir[1]);
+        }
     }
 }
