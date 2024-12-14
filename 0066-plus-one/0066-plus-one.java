1831e6f69a7edd0bd1caa3092a5d4ddc55f24c0c
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-13 17:36:51 -0800

    Time: 0 ms (100%), Space: 41.9 MB (26.64%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,20 +1,19 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int carry = 1;
-        int[] res = new int[digits.length];
-
+        int remain = 1;
+        
         for (int i = digits.length - 1; i >= 0; i--) {
-            int sum = digits[i] + carry;
-            carry = sum / 10;
-            int curr = sum % 10;
-            res[i] = curr;
+            int sum = digits[i] + remain;
+
+            digits[i] = sum % 10;
+            remain = sum / 10;
         }
 
-        if (carry == 1) {
-            res = new int[res.length + 1]; 
-            res[0] = 1;
+        if (remain > 0) {
+            digits = new int[digits.length + 1];
+            digits[0] = remain;
         }
 
-        return res;
+        return digits;
     }
 }
