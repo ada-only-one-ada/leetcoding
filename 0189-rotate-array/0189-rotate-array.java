6c50a8ba8c9055324ea5cbc689e2f6f057c99ef1
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-13 23:19:26 -0800

    Time: 0 ms (100%), Space: 57.6 MB (25.66%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,14 +1,23 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        int[] rotateNums = new int[nums.length];
+        // O(1) space
+        // [1,2,3,4,5,6,7]
+        // [7,6,5,4,3,2,1]
+        // [5,6,7][1,2,3,4]
         k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
 
-        for (int i = 0; i < nums.length; i++) {
-            rotateNums[(i + k) % nums.length] = nums[i];
-        }
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
 
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = rotateNums[i];
+            start++;
+            end--;
         }
     }
 }
