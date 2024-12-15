dc3f0f9d77750fc3742b7464cc852ea465816ff7
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-15 02:15:53 -0800

    Time: 2 ms (89.28%), Space: 55.4 MB (50.7%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,18 +1,28 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] prefix = new int[nums.length];
-        
+        int[] prefix = new int[nums.length + 1];
         prefix[0] = 1;
+        
+        //     nums = [1,2,3,4]
+        // prefix = [1,1,2,6,24]
         for (int i = 1; i < prefix.length; i++) {
             prefix[i] = prefix[i - 1] * nums[i - 1];
         }
 
+        //           0 1 2 3  4
+        // prefix = [1,1,2,6,24]
+        //              0 1 2 3
+        //      nums = [1,2,3,4]
+        //                       1
+                      
         int suffix = 1;
-        for (int i = prefix.length - 1; i >= 0; i--) {
-            prefix[i] = prefix[i] * suffix;
-            suffix = suffix * nums[i];
+        for (int i = nums.length - 1; i >= 0; i--) {
+            int num = nums[i];
+
+            nums[i] = suffix * prefix[i];
+            suffix *= num;
         }
 
-        return prefix;
+        return nums;
     }
 }
