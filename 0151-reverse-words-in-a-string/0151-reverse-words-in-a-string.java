d6978a555a6af32f314a778475bf2830df45ae69
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-15 01:41:52 -0800

    Time: 6 ms (86.04%), Space: 44.5 MB (31.13%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,40 +1,33 @@
 class Solution {
     public String reverseWords(String s) {
-        int left = 0;
-        while (left < s.length() && s.charAt(left) == ' ') {
-            left++;
-        }
-
-        int right = s.length() - 1;
-        while (right >= 0 && s.charAt(right) == ' ') {
-            right--;
-        }
-        s = s.substring(left, right + 1);
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            if (i > 0 && s.charAt(i) == ' ' && s.charAt(i-1) == ' ') continue;
-            sb.append(s.charAt(i));
-        }
-
-        sb = sb.reverse();
+        String wholeS = reverse(s);
         StringBuilder res = new StringBuilder();
-        
+
         int start = 0;
-        for (int end  = 0; end < sb.length(); end++) {
-            if (sb.charAt(end) == ' ') {
-                res.append(reverseSingle(sb.substring(start, end)));
-                res.append(' ');
+        for (int end = 0; end < wholeS.length(); end++) {
+            if (end > 0 && wholeS.charAt(end) == ' ' && wholeS.charAt(end - 1) != ' ') {
+                res.append(reverse(wholeS.substring(start, end)));
+                res.append(" ");
                 start = end + 1;
             }
         }
 
-        res.append(reverseSingle(sb.substring(start, sb.length())));
+        res.append(reverse(wholeS.substring(start, wholeS.length()))); // don't forget this
         return res.toString();
     }
 
-    public String reverseSingle(String word) {
-        StringBuilder sb = new StringBuilder(word);
+    public String reverse(String s) {
+        int left = 0;
+        while (left < s.length() && s.charAt(left) == ' ') {
+          left++;
+        }
+        
+        int right = s.length() - 1;
+        while (right >= 0 && s.charAt(right) == ' ') {
+           right--;
+        }
+
+        StringBuilder sb = new StringBuilder(s.substring(left, right + 1));
         return sb.reverse().toString();
     }
 }
