018ed2bfe96f050447a43a8d85ef1a90b4551f1e
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-15 01:40:46 -0800

    Time: 8 ms (44.76%), Space: 44.5 MB (30.28%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,49 +1,40 @@
 class Solution {
     public String reverseWords(String s) {
-        // 去掉leading spaces
-        int i = 0; 
-        while (i < s.length() && s.charAt(i) == ' ') i++;
-        // 去掉trailing spaces
-        
-        int j = s.length() - 1;
-        while (j >= 0 && s.charAt(j) == ' ') j--;
-
-        // 把 s 的内容粘贴到 sb 中
-        StringBuilder sb = new StringBuilder();
-        for (int index = i; index <= j; index++) {
-            // 忽略内部的多个空格
-            if (index > 0 && s.charAt(index) == ' ' && s.charAt(index - 1) == ' ') continue;
-            sb.append(s.charAt(index));
+        int left = 0;
+        while (left < s.length() && s.charAt(left) == ' ') {
+            left++;
         }
 
-        // 反转整个 sb
-        reverse(sb, 0, sb.length() - 1);
+        int right = s.length() - 1;
+        while (right >= 0 && s.charAt(right) == ' ') {
+            right--;
+        }
+        s = s.substring(left, right + 1);
 
-        // 内部一个个单词反转
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (i > 0 && s.charAt(i) == ' ' && s.charAt(i-1) == ' ') continue;
+            sb.append(s.charAt(i));
+        }
+
+        sb = sb.reverse();
+        StringBuilder res = new StringBuilder();
+        
         int start = 0;
-        for (int end = 0; end < sb.length(); end++) {
-            // 当前遇到空格，反转前一个单词
+        for (int end  = 0; end < sb.length(); end++) {
             if (sb.charAt(end) == ' ') {
-                reverse(sb, start, end - 1);
-                // 移动start到当前的下一个index
+                res.append(reverseSingle(sb.substring(start, end)));
+                res.append(' ');
                 start = end + 1;
             }
         }
 
-        // 反转最后一个单个单词
-        reverse(sb, start, sb.length() - 1);
-        return sb.toString();
+        res.append(reverseSingle(sb.substring(start, sb.length())));
+        return res.toString();
     }
 
-    public void reverse(StringBuilder sb, int start, int end) {
-        // setCharAt(int index, char ch)
-        while (start < end) {
-            char temp = sb.charAt(start);
-            sb.setCharAt(start, sb.charAt(end));
-            sb.setCharAt(end, temp);
-
-            start++;
-            end--;
-        }
+    public String reverseSingle(String word) {
+        StringBuilder sb = new StringBuilder(word);
+        return sb.reverse().toString();
     }
 }
