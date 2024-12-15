e25b9869b74249c13f5ea13c7f264c306d1a37bc
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-14 19:29:17 -0800

    Time: 5 ms (39.57%), Space: 45.2 MB (19.18%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.java
@@ -0,0 +1,50 @@
+class MinStack {
+    Stack<int[]> stack;
+    Stack<int[]> minStack;
+    int index;
+    Set<Integer> set;
+
+    public MinStack() {
+        stack = new Stack();
+        minStack = new Stack();
+        index = 0;
+        set = new HashSet<>();
+    }
+    
+    public void push(int val) {
+        stack.push(new int[]{index, val});
+        while (!minStack.isEmpty() && set.contains(minStack.peek()[0])) {
+            set.remove(minStack.pop()[0]);
+        }
+        if (minStack.isEmpty() || val <= minStack.peek()[1]) {
+            minStack.push(new int[]{index, val});
+        }
+        index++;
+    }
+    
+    public void pop() {
+        int popedIndex = stack.pop()[0];
+        set.add(popedIndex);
+    }
+    
+    public int top() {
+        return stack.peek()[1];
+    }
+    
+    public int getMin() {
+        while (!minStack.isEmpty() && set.contains(minStack.peek()[0])) {
+            set.remove(minStack.pop()[0]);
+        }
+        
+        return minStack.peek()[1];
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
