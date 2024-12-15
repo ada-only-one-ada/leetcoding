f831c79f408aae1d498be658246eb09cd693b2c7
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-14 20:29:56 -0800

    Time: 32 ms (94.72%), Space: 55.2 MB (67.06%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -0,0 +1,59 @@
+class Trie {
+    TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+    
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char c: word.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                node.children[c - 'a'] = new TrieNode();
+            }
+            node = node.children[c - 'a'];
+        }
+        node.isValid = true;
+    }
+    
+    public boolean search(String word) {
+        TrieNode node = root;
+        for (char c: word.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                return false;
+            }
+            node = node.children[c - 'a'];
+        }
+        return node.isValid;
+    }
+    
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+
+        for (char c: prefix.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                return false;
+            }
+            node = node.children[c - 'a'];
+        }
+        return true;
+    }
+}
+
+class TrieNode {
+    TrieNode[] children;
+    boolean isValid;
+
+    public TrieNode() {
+        children = new TrieNode[26];
+        isValid = false;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
