9884d53274906d991107d752f284519a7bae9b01
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-14 18:54:54 -0800

    Time: 4 ms (96.49%), Space: 54.9 MB (99.36%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -2,11 +2,15 @@ class Solution {
     public int findKthLargest(int[] nums, int k) {
         int[] bucket = new int[20001];
 
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
         for (int num: nums) {
             bucket[num + 10000]++;
+            max = Math.max(max, num + 10000);
+            min = Math.min(min, num + 10000);
         }
 
-        for (int i = bucket.length - 1; i >= 0; i--) {
+        for (int i = max; i >= min; i--) {
             k -= bucket[i];
 
             if (k <= 0) {
