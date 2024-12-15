11abf95291a4e9774caaec0d0e627ce222d92d04
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-14 18:47:16 -0800

    Time: 7 ms (39.07%), Space: 46.1 MB (34.41%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int[] indegree = new int[numCourses];
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+
+        for (int[] p: prerequisites) {
+            int toTake = p[0];
+            int mustTake = p[1];
+
+            map.putIfAbsent(mustTake, new HashSet<>());
+            map.get(mustTake).add(toTake);
+            indegree[toTake]++;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                queue.add(i);
+            }
+        }
+
+        int finished = 0;
+        int[] res = new int[numCourses];
+        int index = 0;
+        while (!queue.isEmpty()) {
+            int currTaking = queue.poll();
+            res[index++] = currTaking;
+            finished++;
+
+            Set<Integer> set = map.get(currTaking);
+            if (set == null) continue;
+
+            for (int nextCourse: set) {
+                indegree[nextCourse]--;
+                if (indegree[nextCourse] == 0) {
+                    queue.add(nextCourse);
+                }
+            }
+        }
+
+        if (finished < numCourses) return new int[0];
+        return res;
+    }
+}
