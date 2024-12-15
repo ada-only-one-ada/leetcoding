5123a2a33400c0cf3b9249ca55f5e8b250773b36
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-15 01:15:46 -0800

    Time: 7 ms (61.39%), Space: 43.3 MB (49.51%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,43 +1,23 @@
 class Solution {
     public String reverseWords(String s) {
-        char[] words = s.toCharArray();
-
-        int i = 0; 
-        while (i < words.length && words[i] == ' ') i++;
-        
-        int j = words.length - 1;
-        while (j >= 0 && words[j] == ' ') j--;
-
-        reverse(words, i, j);
-
-        int left = i;
-        for (int right = i; right <= j; right++) {
-            if (words[right] == ' ') {
-                reverse(words, left, right - 1);
-                left = right + 1;
-                while (left <= j && words[left] == ' ') left++;
-            }
+        String[] arr = s.trim().split("\\s+");
+        int left = 0;
+        int right = arr.length - 1;
+     
+        while (left < right) {
+            String temp = arr[left].trim();
+            arr[left] = arr[right].trim();
+            arr[right] = temp;
+            left++;
+            right--;
         }
 
-        reverse(words, left, j);
-
         StringBuilder res = new StringBuilder();
-        for (int index = i; index <= j; index++) {
-            if (index > 0 && words[index] == ' ' && words[index-1] == ' ') continue;
-            res.append(words[index]);
+        for (int i = 0; i < arr.length; i++) {
+            res.append(arr[i]);
+            if (i != arr.length - 1) res.append(" ");
         }
 
         return res.toString();
-    }
-
-    public void reverse(char[] s, int start, int end) {
-        while (start < end) {
-            char temp = s[start];
-            s[start] = s[end];
-            s[end] = temp;
-
-            start++;
-            end--;
-        }
     }
 }
