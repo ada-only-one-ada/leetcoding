1da86db33262dcb9826a4c729b1081b691dbb01b
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-15 01:37:37 -0800

    Time: 6 ms (86.04%), Space: 42.6 MB (93.18%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,51 +1,46 @@
 class Solution {
     public String reverseWords(String s) {
-        char[] words = s.toCharArray();
-
         // 去掉leading spaces
         int i = 0; 
-        while (i < words.length && words[i] == ' ') i++;
+        while (i < s.length() && s.charAt(i) == ' ') i++;
         // 去掉trailing spaces
-        int j = words.length - 1;
-        while (j >= 0 && words[j] == ' ') j--;
+        
+        int j = s.length() - 1;
+        while (j >= 0 && s.charAt(j) == ' ') j--;
 
-        // 反转从 i 到 j 的字符
-        reverse(words, i, j);
+        // 把 s 的内容粘贴到 sb 中
+        StringBuilder sb = new StringBuilder();
+        for (int index = i; index <= j; index++) {
+            // 忽略内部的多个空格
+            if (index > 0 && s.charAt(index) == ' ' && s.charAt(index - 1) == ' ') continue;
+            sb.append(s.charAt(index));
+        }
+
+        // 反转整个 sb
+        reverse(sb, 0, sb.length() - 1);
 
         // 内部一个个单词反转
-        int start = i;
-        for (int end = i; end <= j; end++) {
+        int start = 0;
+        for (int end = 0; end < sb.length(); end++) {
             // 当前遇到空格，反转前一个单词
-            if (words[end] == ' ') {
-                reverse(words, start, end - 1);
+            if (sb.charAt(end) == ' ') {
+                reverse(sb, start, end - 1);
                 // 移动start到当前的下一个index
                 start = end + 1;
-                // 可能会有连续的空格，继续移动start直到遇到一个字母（有效开始）
-                while (start <= j && words[start] == ' ') start++;
             }
         }
 
         // 反转最后一个单个单词
-        reverse(words, start, j);
-
-        // 拼接结果 
-        StringBuilder res = new StringBuilder();
-        // loop 从 i 到 j 范围的字符
-        for (int index = i; index <= j; index++) {
-            // 如果已经粘贴过了一个空格，又遇到了空格，则跳过当前遇到的空格
-            if (index > 0 && words[index] == ' ' && words[index-1] == ' ') continue;
-            // 否则，粘贴到结果
-            res.append(words[index]);
-        }
-
-        return res.toString();
+        reverse(sb, start, sb.length() - 1);
+        return sb.toString();
     }
 
-    public void reverse(char[] s, int start, int end) {
+    public void reverse(StringBuilder sb, int start, int end) {
+        // setCharAt(int index, char ch)
         while (start < end) {
-            char temp = s[start];
-            s[start] = s[end];
-            s[end] = temp;
+            char temp = sb.charAt(start);
+            sb.setCharAt(start, sb.charAt(end));
+            sb.setCharAt(end, temp);
 
             start++;
             end--;
