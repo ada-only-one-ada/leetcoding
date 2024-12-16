2a930a1aa6e49619282ab58f358275de1c86822b
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-15 18:49:47 -0800

    Time: 0 ms (100%), Space: 41.8 MB (8.37%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -6,22 +6,26 @@ class Solution {
         int leftCol = 0, rightCol = colNum - 1;
 
         while (res.size() < rowNum * colNum) {
-            for (int col = leftCol; col <= rightCol && topRow <= bottomRow; col++) {
+            for (int col = leftCol; col <= rightCol; col++) {
+                if (res.size() == rowNum * colNum) break;
                 res.add(matrix[topRow][col]);
             }
             topRow++;
 
-            for (int row = topRow; row <= bottomRow && leftCol <= rightCol; row++) {
+            for (int row = topRow; row <= bottomRow; row++) {
+                if (res.size() == rowNum * colNum) break;
                 res.add(matrix[row][rightCol]);
             }
             rightCol--;
 
-            for (int col = rightCol; col >= leftCol && topRow <= bottomRow; col--) {
+            for (int col = rightCol; col >= leftCol; col--) {
+                if (res.size() == rowNum * colNum) break;
                 res.add(matrix[bottomRow][col]);
             }
             bottomRow--;
 
-            for (int row = bottomRow; row >= topRow && leftCol <= rightCol; row--) {
+            for (int row = bottomRow; row >= topRow; row--) {
+                if (res.size() == rowNum * colNum) break;
                 res.add(matrix[row][leftCol]);
             }
             leftCol++;
