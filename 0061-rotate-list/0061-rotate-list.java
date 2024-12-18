1433d5a6dcfbaaa46113e2678fcfdefcc14a37a8
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-17 17:48:12 -0800

    Time: 0 ms (100%), Space: 42.7 MB (25.85%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -1,35 +1,48 @@
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
+        // 如果链表为空，直接返回
         if (head == null) return null; 
+
+        // 遍历一边得到节点的数量
         int size = 0;
-        ListNode curr = head;
-        while (curr != null) {
+        ListNode currNode = head;
+        while (currNode != null) {
             size++;
-            curr = curr.next;
+            currNode = currNode.next;
         }        
 
+        // 简化 k 
         k %= size;
+        // 如果 rotate 次数为0，直接返回
         if (k == 0) return head;
+
+        // 计算出新的头节点的位置（index 从0开始）
         int newHeadPos = size - k;
-        
-        ListNode prev = null;
-        curr = head;
-        int currIndex = 0;
-        
-        while (currIndex < newHeadPos) {
-            currIndex++;
-            prev = curr;
-            curr = curr.next;
+        // 把新的头节点的前一个节点存起来，方便后续断开
+        ListNode prevNode = null;
+
+        // 重置开始遍历
+        currNode = head;
+        int currPos = 0;
+        // 直到找到新的头节点的位置
+        while (currPos < newHeadPos) {
+            currPos++;
+            prevNode = currNode;
+            currNode = currNode.next;
         }
 
-        ListNode newHead = curr;
-        prev.next = null;
+        // 新的头节点
+        ListNode newHead = currNode;
+        // 断开前一段
+        prevNode.next = null;
 
-        while (curr.next != null) {
-            curr = curr.next;
+        // 遍历到新的头节点那一段的最后一个节点
+        while (currNode.next != null) {
+            currNode = currNode.next;
         }
-
-        curr.next = head;
+        // 把最后一个节点连到旧的头节点
+        currNode.next = head;
+        // 返回新的头节点
         return newHead;
     }
 }
