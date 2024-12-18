f7b27371c665e4a398de95099f4a9450a0e5b4ba
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-17 21:41:54 -0800

    Time: 8 ms (31.29%), Space: 45.6 MB (24.9%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,40 +1,40 @@
 class Solution {
-    int ROW_UP = -1;
-    int ROW_DOWN = 1;
-    int COL_RIGHT = 1;
-    int COL_STAY = 0;
-    
     public String convert(String s, int numRows) {
-        // "AB"，1，如果只能有一行，直接返回
-        if (numRows == 1) return s;
-
-        Map<Integer, StringBuilder> map = new HashMap<>();
-        helper(s, numRows, 0, ROW_DOWN, 0, COL_STAY, 0, map);
-
-        StringBuilder res = new StringBuilder();
+        if (numRows <= 1) return s; // "AB", 1
+        Map<Integer, List<Character>> map = new HashMap<>();
         for (int row = 0; row < numRows; row++) {
-            if (map.get(row) == null) continue; // A，2，不足两行
-
-            res.append(map.get(row));
+            map.put(row, new ArrayList<>());
         }
-        return res.toString();
-    }
 
-    public void helper(String s, int numRows, int currRow, int rowDir, int currCol, int colDir, int index, Map<Integer, StringBuilder> map) {
-        if (index == s.length()) return;
+        int currRow = 0;
+        int i = 0;
+        while (i < s.length()) {    
+            while (i < s.length() && currRow < numRows) {
+                char c = s.charAt(i);
+                map.get(currRow).add(c);
+                currRow++;
+                i++;
+            }
 
-        map.putIfAbsent(currRow, new StringBuilder());
-        map.get(currRow).append(s.charAt(index));
-
-        // 当前行是最后一行，行无法向下了，这时候改变：行数方向向上，列数方向向右
-        if (currRow == numRows - 1) {
-            helper(s, numRows, currRow - 1, ROW_UP, currCol + 1, COL_RIGHT, index + 1, map);
-        // 当前行是第一行，行无法向上了，这时候改变：行数方向向下，列数方向stay
-        } else if (currRow == 0) {
-            helper(s, numRows, currRow + 1, ROW_DOWN, currCol, COL_STAY, index + 1, map);
-        // 否则朝当前方向走
-        } else {
-            helper(s, numRows, currRow + rowDir, rowDir, currCol + colDir, colDir, index + 1, map);
+            currRow -= 2;
+            while (i < s.length() && currRow >= 0) {
+                char c = s.charAt(i);
+                map.get(currRow).add(c);
+                currRow--;
+                i++;
+            }
+            currRow += 2;
         }
+
+        StringBuilder sb = new StringBuilder();
+        for (int row = 0; row < numRows; row++) {
+            List<Character> list = map.get(row);
+            if (list == null) continue;
+            for (char c: list) {
+                sb.append(c);
+            }
+        }
+
+        return sb.toString();
     }
 }
