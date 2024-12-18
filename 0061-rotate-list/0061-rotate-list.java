c2869a7f62f3df2fee613274a5283ab495ef78b2
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-17 17:42:52 -0800

    Time: 0 ms (100%), Space: 42.6 MB (36%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -1,32 +1,35 @@
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if (head == null) return null;
-        
+        if (head == null) return null; // head=[], k=0
         int size = 0;
         ListNode curr = head;
         while (curr != null) {
             size++;
             curr = curr.next;
-        }
+        }        
+
         k %= size;
         if (k == 0) return head;
-
-        int newHeadPos = size - k; // zeroIndex
-        ListNode newHead = head;
+        int newHeadPos = size - k;
+        
         ListNode prev = null;
-
-        for (int i = 0; i < newHeadPos; i++) {
-            prev = newHead;
-            newHead = newHead.next;
-        }
-
-        prev.next = null;
-        curr = newHead;
-        while (curr != null && curr.next != null) {
+        curr = head;
+        int currIndex = 0;
+        
+        while (currIndex < newHeadPos) {
+            currIndex++;
+            prev = curr;
             curr = curr.next;
         }
 
-        if (curr != null) curr.next = head;
+        ListNode newHead = curr;
+        prev.next = null;
+
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+
+        curr.next = head;
         return newHead;
     }
 }
