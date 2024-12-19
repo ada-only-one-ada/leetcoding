984f5a23f612cd33f2af4c6a84b591744366a258
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-18 17:02:29 -0800

    Time: 0 ms (100%), Space: 42 MB (65.68%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -4,12 +4,12 @@ class Solution {
         if (nums[0] <= target && target <= nums[peakPos]){
             return binarySearch(nums, target, 0, peakPos);
         }
-        return binarySearch(nums, target, peakPos, nums.length-1);    
+        return binarySearch(nums, target, peakPos, nums.length - 1);    
     }
     
     public int binarySearch(int[] nums, int target, int left, int right){
         while (left + 1 < right){
-            int mid = left + (right-left)/2;
+            int mid = left + (right - left) / 2;
             if (nums[mid] == target){
                 return mid;
             }else if (nums[mid] < target){
@@ -25,14 +25,14 @@ class Solution {
     
     public int findPeak(int[] nums){
         if (nums.length == 1) return 0;
-        if (nums[0] < nums[nums.length-1]) return nums.length-1;
+        if (nums[0] < nums[nums.length - 1]) return nums.length - 1;
         
         int left = 0;
-        int right = nums.length-1;
+        int right = nums.length - 1;
         
         while (left + 1 < right){
-            int mid = left + (right - left) /2;
-            if (nums[mid] > nums[mid+1]){
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[mid + 1]){
                 return mid;
             }else if (nums[left] < nums[mid]){
                 left = mid;
