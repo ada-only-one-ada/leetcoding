0d86aca4d184478fc19834aa7120ec05bc8f0f6b
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-18 17:03:43 -0800

    Time: 0 ms (100%), Space: 42.2 MB (39.05%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,46 +1,38 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int peakPos = findPeak(nums);
-        if (nums[0] <= target && target <= nums[peakPos]){
-            return binarySearch(nums, target, 0, peakPos);
-        }
-        return binarySearch(nums, target, peakPos, nums.length - 1);    
-    }
-    
-    public int binarySearch(int[] nums, int target, int left, int right){
-        while (left + 1 < right){
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target){
+        int start = 0;
+        int end = nums.length;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            // mid直接找到了 target，直接返回 mid
+            if (nums[mid] == target) {
                 return mid;
-            }else if (nums[mid] < target){
-                left = mid;
-            }else{
-                right = mid;
+            // 否则，继续找，怎么找呢？
+            } else {
+                // 左半边是递增的：比如【4，5，6，7，0，1，2】，这里【4，5，6，7】是有序的
+                if (nums[start] <= nums[mid]) {
+                    //刚好target的范围又落在左半边，那答案肯定是在左半边
+                    if (nums[start] <= target && target < nums[mid]) {
+                        end = mid;
+                    // 否则，肯定不在左半边
+                    } else {
+                        start = mid + 1;
+                    }
+                // 否则，左半边不是递增有序的，但是右半边是递增有序的，比如【9，1，2，5，6，7，8】，这里【5，6，7，8】是有序的
+                } else if (nums[mid] <= nums[end - 1]) {
+                    // 刚好target的范围又落在右半边，那答案肯定是在右半边
+                    if (nums[mid] < target && target <= nums[end - 1]) {
+                        start = mid + 1;
+                    // 否则，肯定不在右半边
+                    } else {
+                        end = mid;
+                    }
+                }
             }
         }
-        if (nums[left] == target) return left;
-        if (nums[right] == target) return right;
-        return -1;
-    } 
-    
-    public int findPeak(int[] nums){
-        if (nums.length == 1) return 0;
-        if (nums[0] < nums[nums.length - 1]) return nums.length - 1;
-        
-        int left = 0;
-        int right = nums.length - 1;
-        
-        while (left + 1 < right){
-            int mid = left + (right - left) / 2;
-            if (nums[mid] > nums[mid + 1]){
-                return mid;
-            }else if (nums[left] < nums[mid]){
-                left = mid;
-            }else if (nums[left] > nums[mid]){
-                right = mid;
-            }
-        }
-        if (nums[right] > nums[left]) return right;
-        return left;    
+
+        if (start < 0 || start >= nums.length || nums[start] != target) return -1;
+        return start;
     }
 }
