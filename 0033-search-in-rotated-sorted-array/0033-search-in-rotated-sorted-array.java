4fd0269c015339a310b667a93cfd6a2b24ced9cd
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-18 17:01:39 -0800

    Time: 0 ms (100%), Space: 42.3 MB (28.48%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,60 +1,46 @@
 class Solution {
     public int search(int[] nums, int target) {
-        // 如果已经排序好了，就按照正常的二分去找
-        if (nums[0] <= nums[nums.length - 1]) {
-            int start = 0;
-            int end = nums.length;
-
-            while (start < end) {
-                int mid = start + (end - start) / 2;
-                if (nums[mid] == target) {
-                    return mid;
-                } else if (nums[mid] > target) {
-                    end = mid;
-                } else {
-                    start = mid + 1;
-                }
-            }    
-           
-            if (start >= 0 && start < nums.length && nums[start] == target) return start;
-            return -1; 
-        // 如果没有排序
-        } else {
-            int start = 0;
-            int end = nums.length;
-            while (start < end) {
-                int mid = start + (end - start) / 2;
-                // 找到了 target，直接返回
-                if (nums[mid] == target) {
-                    return mid;
-                }
-
-                // 否则，左边是 sorted 的
-                if (nums[mid] >= nums[start]) {
-                    // 且 target 在左边的范围
-                    if (nums[start] <= target && target <= nums[mid]) {
-                        // 去掉右边
-                        end = mid;
-                    } else {
-                        // 否则，肯定不在左边
-                        start = mid + 1;
-                    }
-                // 右边是 sorted 的
-                // 注意：这里要用 end - 1
-                } else if (nums[mid] <= nums[end - 1]) {
-                    // 且 target 在右边的范围
-                    if (nums[mid] <= target && target <= nums[end - 1]) {
-                        // 去掉左边
-                        start = mid + 1;
-                    } else {
-                        // 否则，肯定不在右边
-                        end = mid;
-                    }
-                }
-            }
-
-            if (start >= 0 && start < nums.length && nums[start] == target) return start;
-            return -1; 
+        int peakPos = findPeak(nums);
+        if (nums[0] <= target && target <= nums[peakPos]){
+            return binarySearch(nums, target, 0, peakPos);
         }
+        return binarySearch(nums, target, peakPos, nums.length-1);    
+    }
+    
+    public int binarySearch(int[] nums, int target, int left, int right){
+        while (left + 1 < right){
+            int mid = left + (right-left)/2;
+            if (nums[mid] == target){
+                return mid;
+            }else if (nums[mid] < target){
+                left = mid;
+            }else{
+                right = mid;
+            }
+        }
+        if (nums[left] == target) return left;
+        if (nums[right] == target) return right;
+        return -1;
+    } 
+    
+    public int findPeak(int[] nums){
+        if (nums.length == 1) return 0;
+        if (nums[0] < nums[nums.length-1]) return nums.length-1;
+        
+        int left = 0;
+        int right = nums.length-1;
+        
+        while (left + 1 < right){
+            int mid = left + (right - left) /2;
+            if (nums[mid] > nums[mid+1]){
+                return mid;
+            }else if (nums[left] < nums[mid]){
+                left = mid;
+            }else if (nums[left] > nums[mid]){
+                right = mid;
+            }
+        }
+        if (nums[right] > nums[left]) return right;
+        return left;    
     }
 }
