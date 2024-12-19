56738a24581a87348c4ab5295afcc392b2784d51
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-18 19:37:58 -0800

    Time: 0 ms (100%), Space: 42.1 MB (30.77%) - LeetHub

---
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int findMin(int[] nums) {
+        if (nums[0] <= nums[nums.length - 1]) return nums[0];
+
+        int start = 0;
+        int end = nums.length;
+        while (start < end) {
+            if (start + 1 == end - 1) {
+                return Math.min(nums[start], nums[end - 1]);
+            }
+
+            int mid = start + (end - start) / 2;
+
+            // 左边是递增的，意味着最小值可能是左边第一个，或者是 mid - end 的一个
+            // 右边递减的
+            if (nums[start] < nums[mid]) {
+                if (nums[start] < nums[end - 1]) {
+                    return nums[start];
+                } else {
+                    start = mid + 1;
+                }
+
+            // 右边是递增的，意味着最小值可能是mid，或者是 start - mid 的一个
+            // 左边是递减的
+            } else if (nums[mid] <= nums[end - 1]) {
+                if (mid - 1 >= 0 && nums[mid] < nums[mid - 1]) {
+                    return nums[mid];
+                } else {
+                    end = mid;
+                }
+            }
+        }
+
+        return nums[start];
+    }
+}
