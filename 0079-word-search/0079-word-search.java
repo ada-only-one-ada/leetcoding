4c424a9639ccb1f6e5a3c91c0913bbf2811e8dde
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-20 15:34:09 +0800

    Time: 133 ms (83.44%), Space: 40.7 MB (99.82%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[0].length; col++) {
+                if (board[row][col] == word.charAt(0)) {
+                    if (backtracking(board, word, 0, row, col)) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public boolean backtracking(char[][] board, String word, int index, int row, int col) {
+        if (index == word.length()) return true;
+        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length) return false;
+        if (board[row][col] != word.charAt(index)) return false;
+        if (board[row][col] == '!') return false;
+
+        char temp = board[row][col];
+        board[row][col] = '!';
+        boolean res = backtracking(board, word, index + 1, row - 1 , col) || 
+               backtracking(board, word, index + 1, row + 1, col) || 
+               backtracking(board, word, index + 1, row, col - 1) ||
+               backtracking(board, word, index + 1, row, col + 1);
+        board[row][col] = temp;
+
+        return res;
+    }
+}
