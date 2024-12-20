6684b906d6249688a2707884a07ea87937d61749
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-21 07:00:55 +0800

    Time: 7 ms (28.41%), Space: 41.8 MB (42.43%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -8,16 +8,12 @@ class Solution {
         for (int i = 1; i <= s1.length(); i++) {
             if (s1.charAt(i - 1) == s3.charAt(i - 1)) {
                 dp[i][0] = dp[ i- 1][0];
-            } else {
-                dp[i][0] = false;
             }
         }
         
         for (int j = 1; j <= s2.length(); j++) {
             if (s2.charAt(j - 1) == s3.charAt(j - 1)) {
                 dp[0][j] = dp[0][j - 1];
-            } else {
-                dp[0][j] = false;
             }
         }
 
@@ -27,7 +23,6 @@ class Solution {
                 char c2 = s2.charAt(j - 1);
 
                 char target = s3.charAt(i + j - 1);
-                char lastTarget = s3.charAt(i + j - 2);
 
                 if (c1 == target) {
                     dp[i][j] = dp[i-1][j];
