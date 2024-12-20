f48aa306ab7df566e8125e70d39cde60f1b6defc
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-20 10:25:19 +0800

    Time: 0 ms (100%), Space: 43.4 MB (26.15%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
@@ -1,31 +1,26 @@
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        if (head == null) return null;
-        
-        ListNode dummy = new ListNode(-1000);  // 创建一个哑节点作为链表的新头部
+        ListNode dummy = new ListNode(-1);  // 创建一个哑节点作为链表的新头部
         dummy.next = head;
         
-        ListNode prevNode = dummy;   // prevNode 是指向最后一个无重复节点的指针
-        ListNode currNode = head;    // currNode 是当前遍历到的节点
-        
-        while (currNode != null) {
-            // 判断当前节点是否有重复节点
-            boolean hasDuplicate = false;
-            while (currNode.next != null && currNode.val == currNode.next.val) {
-                hasDuplicate = true;
-                currNode = currNode.next; // 继续前进到重复序列的最后一个节点
-            }
+        ListNode prev = dummy;  // prev 起始位于哑节点，表示上一个添加到结果链表中的节点
+        ListNode curr = head;   // curr 为当前正在遍历的节点
 
-            if (hasDuplicate) {
-                // 如果存在重复，更新 prevNode.next 跳过所有重复节点
-                prevNode.next = currNode.next;
+        while (curr != null) {
+            // 检查当前节点是否是唯一的
+            if ((curr.next == null || curr.next.val != curr.val) && (prev.next == curr)) {
+                // 如果 curr 是唯一的，并且紧跟在 prev 后面，直接将 prev 前移至 curr
+                prev = curr;
             } else {
-                // 如果不存在重复，移动 prevNode 到 currNode
-                prevNode = currNode;
+                // 如果 curr 不是唯一的，找到下一个唯一的节点
+                while (curr.next != null && curr.next.val == curr.val) {
+                    curr = curr.next;  // 跳过所有重复的节点
+                }
+                // 更新 prev.next 指向 curr 的下一个节点（第一个不重复的节点）
+                prev.next = curr.next;
             }
-
-            // 移动 currNode 到下一个节点
-            currNode = currNode.next;
+            // 移动 curr 到下一个节点
+            curr = curr.next;
         }
         
         return dummy.next; // 返回哑节点的下一个节点，即更新后的链表头部
