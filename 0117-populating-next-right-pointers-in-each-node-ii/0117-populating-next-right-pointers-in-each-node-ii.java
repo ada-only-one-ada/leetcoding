3fce8cb7431af4c3d68a71218864197ad7851f6e
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-20 08:15:33 +0800

    Time: 0 ms (100%), Space: 44.6 MB (10.56%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.java
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.java
@@ -1,50 +1,38 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-    public Node next;
-
-    public Node() {}
-    
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, Node _left, Node _right, Node _next) {
-        val = _val;
-        left = _left;
-        right = _right;
-        next = _next;
-    }
-};
-*/
-
 class Solution {
     public Node connect(Node root) {
         if (root == null) return root;
+        
+        Node prePointer = null;
+        Node nextLevelHead = null;
+        Node curr = root;
 
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(root);
-
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            Node prevNode = null;
-
-            for (int i = 0; i < size; i++) {
-                Node currNode = queue.poll();
-
-                if (prevNode != null) {
-                    prevNode.next = currNode;
+        while (curr != null){
+            while (curr != null){
+                if (curr.left != null){
+                    if (nextLevelHead == null){
+                        nextLevelHead = curr.left;
+                        prePointer = curr.left;
+                    }else if (nextLevelHead != null){
+                        prePointer.next = curr.left;
+                        prePointer = prePointer.next;
+                    }
                 }
 
-                if (currNode.left != null) queue.add(currNode.left);
-                if (currNode.right != null) queue.add(currNode.right);
-                prevNode = currNode;
+                if (curr.right != null){
+                    if (nextLevelHead == null){
+                        nextLevelHead = curr.right;
+                        prePointer = curr.right;
+                    }else if (nextLevelHead != null){
+                        prePointer.next = curr.right;
+                        prePointer = prePointer.next;
+                    }
+                }
+                curr = curr.next;
             }
+            curr = nextLevelHead;
+            prePointer = null;
+            nextLevelHead = null;
         }
-
         return root;
     }
 }
