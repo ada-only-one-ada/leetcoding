f13fa46ba0894a06c3e536aff2f00ffc90b9c7de
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-20 15:00:17 +0800

    Time: 19 ms (50.09%), Space: 94.5 MB (69.03%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -5,7 +5,7 @@ class Solution {
         return res;
     }
 
-    public void backtracking(int n, int k, List<List<Integer>> res, List<Integer> currRes, int newC) {
+    public void backtracking(int n, int k, List<List<Integer>> res, List<Integer> currRes, int currNumber) {
         if (currRes.size() == k) {
             res.add(new ArrayList<>(currRes));
             return;
@@ -14,11 +14,12 @@ class Solution {
         // cut subtrees
         // k - currRes.size(): how many numbers we still need to look for 
         // e.g. n = 4, k = 3. currently currRes is 0, i can go to the right at most 2, so it will have 2,3,4
-        // it i goes to 3, it only have two numbers left, can't find three numbers. So we cut it here
+        // if i goes to 3, it only have two numbers left, can't find three numbers. So we cut it here
         // 4 - (3 - 0) + 1:
         // 4 - we still need to look for 3 numbers + 1
         // (4 - 3) + 1 = 1 + 1 = 2. 2 is included! [2,3,4]
-        for (int i = newC; i <= n - (k - currRes.size()) + 1; i++) {
+
+        for (int i = currNumber; i <= n - (k - currRes.size()) + 1; i++) {
             currRes.add(i);
             backtracking(n, k, res, currRes, i + 1);
             currRes.remove(currRes.size() - 1);
