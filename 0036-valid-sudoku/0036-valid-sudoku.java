9e5dfa3c6df43327fba25825632808b41f617713
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-20 16:26:00 +0800

    Time: 2 ms (78.22%), Space: 44.5 MB (57.63%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,23 +1,20 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        int rowNum = board.length;
-        int colNum = board[0].length;
+        boolean[][] rowUsed = new boolean[9][10];
+        boolean[][] colUsed = new boolean[9][10];
+        boolean[][][] gridUsed = new boolean[3][3][10];
 
-        boolean[][] rowTaken = new boolean[rowNum][10];
-        boolean[][] colTaken = new boolean[colNum][10];
-        boolean[][][] gridTaken = new boolean[3][3][10];
-
-        for (int row = 0; row < rowNum; row++) {
-            for (int col = 0; col < colNum; col++) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
                 if (board[row][col] == '.') continue;
+
                 int num = Character.getNumericValue(board[row][col]);
-                if (rowTaken[row][num] || colTaken[col][num] || gridTaken[row/3][col/3][num]) {
+                if (rowUsed[row][num] || colUsed[col][num] || gridUsed[row/3][col/3][num]) {
                     return false;
                 }
-
-                rowTaken[row][num] = true;
-                colTaken[col][num] = true;
-                gridTaken[row/3][col/3][num] = true;
+                rowUsed[row][num] = true;
+                colUsed[col][num] = true;
+                gridUsed[row/3][col/3][num] = true;
             }
         }
 
