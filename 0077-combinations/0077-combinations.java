e748ceaba344643faa423de11dc17d8abbf8629d
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-20 14:52:05 +0800

    Time: 17 ms (88.47%), Space: 94.4 MB (79.02%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -8,10 +8,13 @@ class Solution {
     }
 
     public void backtracking(int n, int k, List<Integer> currRes, int currNumber) {
+        // 先收获，不然包含最后一个数字 n 的组合不会加入结果集
         if (currRes.size() == k) {
             res.add(new ArrayList<>(currRes));
             return;
         }
+        
+        // 再判断当前数字能不能加
         if (currNumber == n + 1) return;
 
         for (int i = currNumber; i <= n; i++) {
