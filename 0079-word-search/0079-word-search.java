898c4b8788f75bfa9e263a845ecab1b04f3e337f
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-20 15:34:41 +0800

    Time: 135 ms (77.65%), Space: 41.7 MB (47.9%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -17,10 +17,12 @@ class Solution {
         if (index == word.length()) return true;
         if (row < 0 || row >= board.length || col < 0 || col >= board[0].length) return false;
         if (board[row][col] != word.charAt(index)) return false;
+        // 注意有可能会走回头路
         if (board[row][col] == '!') return false;
-
+     
         char temp = board[row][col];
         board[row][col] = '!';
+        
         boolean res = backtracking(board, word, index + 1, row - 1 , col) || 
                backtracking(board, word, index + 1, row + 1, col) || 
                backtracking(board, word, index + 1, row, col - 1) ||
