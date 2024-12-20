2ee5e4f2cd9b082a7b51fc2d4e4eeef5d841af4c
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-20 15:10:00 +0800

    Time: 2 ms (43.61%), Space: 44.5 MB (61.5%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,23 +1,24 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        backtracking(nums, res, new ArrayList<>(), 0, new HashSet<>());
+        backtracking(nums, res, new ArrayList<>(), new HashSet<>());
         return res;
     }
 
-    public void backtracking(int[] nums, List<List<Integer>> res, List<Integer> currRes, int start, Set<Integer> set) {
+    public void backtracking(int[] nums, List<List<Integer>> res, List<Integer> currRes, Set<Integer> set) {
         if (currRes.size() == nums.length) {
             res.add(new ArrayList<>(currRes));
             return;
         }
 
         for (int i = 0; i < nums.length; i++) {
-            if (set.contains(i)) continue;
+            if (set.contains(nums[i])) continue;
+            
             currRes.add(nums[i]);
-            set.add(i);
-            backtracking(nums, res, currRes, start + 1, set);
-            set.remove(i);
+            set.add(nums[i]);
+            backtracking(nums, res, currRes, set);
             currRes.remove(currRes.size() - 1);
+            set.remove(nums[i]);
         }
     }
 }
