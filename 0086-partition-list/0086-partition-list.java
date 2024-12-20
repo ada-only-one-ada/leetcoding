ca59b7ead6e6822e07d5e6b31f3a2ca34a0cd37e
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-20 13:29:56 +0800

    Time: 0 ms (100%), Space: 42 MB (56.45%) - LeetHub

--- 0086-partition-list/0086-partition-list.java
+++ 0086-partition-list/0086-partition-list.java
@@ -1,36 +1,26 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode partition(ListNode head, int x) {
+        if (head == null || head.next == null) return head;
+
         ListNode lessList = new ListNode(-1);
-        ListNode less = lessList;
-
         ListNode moreList = new ListNode(-1);
+        ListNode less = lessList;
         ListNode more = moreList;
-
+        
         ListNode curr = head;
-        while (curr != null) {
-            if (curr.val < x) {
-                less.next = curr;
+        while (curr != null){
+            ListNode copy = new ListNode(curr.val);
+            if (copy.val < x){
+                less.next = copy;
                 less = less.next;
-            } else {
-                more.next = curr;
+            }else if (copy.val >= x){
+                more.next = copy;
                 more = more.next;
             }
-
             curr = curr.next;
         }
 
         less.next = moreList.next;
-        more.next = null; // 要断开
         return lessList.next;
     }
 }
