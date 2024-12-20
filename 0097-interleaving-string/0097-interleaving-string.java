ce02f9a974b3d843709b14a9aa3b1ecb88653c8b
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-21 07:32:17 +0800

    Time: 1 ms (86.22%), Space: 42.1 MB (21.78%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,13 +1,11 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        if (s1.length() + s2.length() != s3.length()) return false;
-        
         Boolean[][] memo = new Boolean[s1.length() + 1][s2.length() + 1];
         return helper(s1, 0, s2, 0, s3, 0, memo);
     }
 
     public boolean helper(String s1, int p1, String s2, int p2, String s3, int index, Boolean[][] memo) {
-        if (index == s3.length()) {
+        if (index == s3.length())  {
             return p1 == s1.length() && p2 == s2.length();
         }
         
@@ -17,13 +15,14 @@ class Solution {
         char c2 = p2 < s2.length() ? s2.charAt(p2) : '-';
         char target = s3.charAt(index);
 
-        boolean res = false;
+        Boolean res = false;
         if (c1 == target) {
-            res = helper(s1, p1 + 1, s2, p2, s3, index + 1, memo);
+            res |= helper(s1, p1 + 1, s2, p2, s3, index + 1, memo);
         }
+
         if (c2 == target) {
             res |= helper(s1, p1, s2, p2 + 1, s3, index + 1, memo);
-        }
+        } 
 
         memo[p1][p2] = res;
         return res;
