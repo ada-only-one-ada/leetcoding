218bfcccd06e65d93ca9549f64f0e40eae244b81
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-20 16:30:27 +0800

    Time: 0 ms (100%), Space: 42.3 MB (39.8%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,17 +1,24 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int top = 0;
-        int bottom = matrix.length - 1;
-        while (top < bottom) {
-            int[] temp = matrix[top];
-            matrix[top] = matrix[bottom];
-            matrix[bottom] = temp;
-            top++;
-            bottom--;
+        /*
+        7 8 9
+        4 5 6
+        1 2 3
+        */
+
+        int i = 0;
+        int j = matrix.length - 1;
+        while (i < j) {
+            int[] temp = matrix[i];
+            matrix[i] = matrix[j];
+            matrix[j] = temp;
+
+            i++;
+            j--;
         }
 
         for (int row = 0; row < matrix.length; row++) {
-            for (int col = row+1; col < matrix[0].length; col++) {
+            for (int col = 0; col < row; col++) {
                 int temp = matrix[row][col];
                 matrix[row][col] = matrix[col][row];
                 matrix[col][row] = temp;
