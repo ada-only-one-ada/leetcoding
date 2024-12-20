17b1864ec5adbd33e3027b340e5896e734e3ed6d
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-12-21 07:28:37 +0800

    Time: 1 ms (86.22%), Space: 41.9 MB (33.2%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,33 +1,31 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        // 如果 s1 和 s2 的总长度不等于 s3 的长度，则直接返回 false
-        if (s1.length() + s2.length() != s3.length()) {
-            return false;
-        }
+        if (s1.length() + s2.length() != s3.length()) return false;
         
-        // 创建一个二维布尔数组 dp，用于存储动态规划的中间结果
-        // dp[i][j] 表示 s1 的前 i 个字符和 s2 的前 j 个字符是否能交错组成 s3 的前 i+j 个字符
-        boolean[][] dp = new boolean[s1.length() + 1][s2.length() + 1];
-        
-        // 初始化右下角的值为 true，假设表示两个字符串可以组成s3，之后loop验证正确性
-        dp[s1.length()][s2.length()] = true; 
+        Boolean[][] memo = new Boolean[s1.length() + 1][s2.length() + 1];
+        return helper(s1, 0, s2, 0, s3, 0, memo);
+    }
 
-        // 从右下角开始反向遍历 dp 数组
-        for (int i = dp.length - 1; i >= 0; i--) {
-            for (int j = dp[0].length - 1; j >= 0; j--) {
-                // 检查 s1 的第 i 个字符是否等于 s3 的第 i+j 个字符，并且 dp[i+1][j] 为 true
-                if (i < s1.length() && s1.charAt(i) == s3.charAt(i + j) && dp[i + 1][j]) {
-                    dp[i][j] = true;
-                }
-                
-                // 检查 s2 的第 j 个字符是否等于 s3 的第 i+j 个字符，并且 dp[i][j+1] 为 true
-                if (j < s2.length() && s2.charAt(j) == s3.charAt(i + j) && dp[i][j + 1]) {
-                    dp[i][j] = true;
-                }
-            }
+    public boolean helper(String s1, int p1, String s2, int p2, String s3, int index, Boolean[][] memo) {
+        if (index == s3.length()) {
+            return p1 == s1.length() && p2 == s2.length();
         }
         
-        // 返回 dp 数组的左上角值，即 s1 的全部字符和 s2 的全部字符是否能交错组成 s3 的全部字符
-        return dp[0][0];
+        if (memo[p1][p2] != null) return memo[p1][p2];
+
+        char c1 = p1 < s1.length() ? s1.charAt(p1) : '-';
+        char c2 = p2 < s2.length() ? s2.charAt(p2) : '-';
+        char target = s3.charAt(index);
+
+        boolean res = false;
+        if (c1 == target) {
+            res = helper(s1, p1 + 1, s2, p2, s3, index + 1, memo);
+        }
+        if (c2 == target) {
+            res |= helper(s1, p1, s2, p2 + 1, s3, index + 1, memo);
+        }
+
+        memo[p1][p2] = res;
+        return res;
     }
 }
