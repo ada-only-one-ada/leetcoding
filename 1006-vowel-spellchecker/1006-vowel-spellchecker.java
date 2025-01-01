bd032837449626e028da3f5ccb553dd81af47c35
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-02 07:41:58 +0800

    Time: 23 ms (74.38%), Space: 48.4 MB (64.46%) - LeetHub

--- 1006-vowel-spellchecker/1006-vowel-spellchecker.java
+++ 1006-vowel-spellchecker/1006-vowel-spellchecker.java
@@ -1,57 +1,58 @@
 class Solution {
     public String[] spellchecker(String[] wordlist, String[] queries) {
-        /* 
-          1、有匹配的立刻返回
-          2、优先检查大小写（有一个即可）
-          3、再检查忽略元音
-        */
         Set<String> perfect = new HashSet<>();
-        Map<String, String> ignoreCaseMap = new HashMap<>();
-        Map<String, String> ignoreVowelMap = new HashMap<>();
-        Set<Character> set = new HashSet<>();
-        set.addAll(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+        Map<String, String> deCaseMap = new HashMap<>();
+        Map<String, String> deVowelMap = new HashMap<>();
 
         for (String word: wordlist) {
             perfect.add(word);
- 
-            String lowerCaseWord = word.toLowerCase();
-            ignoreCaseMap.putIfAbsent(lowerCaseWord, word);
-
-            StringBuilder tempIgnoreVowel = new StringBuilder(lowerCaseWord);
-            for (int i = 0; i < tempIgnoreVowel.length(); i++) {
-                if (set.contains(tempIgnoreVowel.charAt(i))) {
-                    tempIgnoreVowel.setCharAt(i, '-');
-                }
-            }
-
-            ignoreVowelMap.putIfAbsent(tempIgnoreVowel.toString(), word);
+            String deCaseWord = deCase(word);
+            deCaseMap.putIfAbsent(deCaseWord, word);
+            deVowelMap.putIfAbsent(deVowel(deCaseWord), word);
         }
 
         String[] res = new String[queries.length];
-        Arrays.fill(res, "");
-
         for (int i = 0; i < queries.length; i++) {
             if (perfect.contains(queries[i])) {
                 res[i] = queries[i];
-            } else {
-                String lowerCaseWord = queries[i].toLowerCase();
-                if (ignoreCaseMap.containsKey(lowerCaseWord)) {
-                    res[i] = ignoreCaseMap.get(lowerCaseWord);
-                } else {
-                    StringBuilder noVowel = new StringBuilder(lowerCaseWord);
-                    for (int j = 0; j < noVowel.length(); j++) {
-                        if (set.contains(noVowel.charAt(j))) {
-                            noVowel.setCharAt(j, '-');
-                        }
-                    }
-                    
-                    if (ignoreVowelMap.containsKey(noVowel.toString())) {
-                        res[i] = ignoreVowelMap.get(noVowel.toString());
-                    }
-                }
+                continue;
+            }
+
+            String deCaseWord = deCase(queries[i]);
+            if (deCaseMap.containsKey(deCaseWord)) {
+                res[i] = deCaseMap.get(deCaseWord);
+                continue;
+            }
+
+            String deVowelWord = deVowel(deCaseWord);
+            if (deVowelMap.containsKey(deVowelWord)) {
+                res[i] = deVowelMap.get(deVowelWord);
+                continue;
+            }
+
+            res[i] = "";
+        }
+        return res;
+    }
+
+    public String deCase(String word) {
+        return word.toLowerCase();
+    }
+
+    public boolean isVowel(char letter) {
+        if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u') {
+            return true;
+        }
+        return false;
+    }
+
+    public String deVowel(String word) {
+        StringBuilder res = new StringBuilder(word);
+        for (int i = 0; i < res.length(); i++) {
+            if (isVowel(word.charAt(i))) {
+                res.setCharAt(i, '-');
             }
         }
-
-        return res;
+        return res.toString();
     }
 }
