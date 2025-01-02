fb96dc1efbccdaf775ef7c1b0351b94a18657656
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-03 07:21:29 +0800

    Time: 4 ms (71.33%), Space: 44.6 MB (32.86%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -1,36 +1,21 @@
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if (lists.length == 0) return null;
-        return divide(lists, 0, lists.length - 1);
-    }
-    
-    private ListNode divide(ListNode[] lists, int start, int end) {
-        if (start >= end) return lists[start];
-        
-        int mid = start + (end - start) / 2;
-        ListNode left = divide(lists, start, mid);
-        ListNode right = divide(lists, mid + 1, end);
-        
-        return mergeTwoLists(left, right);
-    }
-    
-     public ListNode mergeTwoLists(ListNode l1, ListNode l2) { 
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+        for (ListNode head: lists){
+            while (head != null){
+                queue.add(head.val);
+                head = head.next;
+            }
+        }
+
         ListNode dummy = new ListNode(-1);
         ListNode curr = dummy;
 
-        while (l1 != null && l2 != null) {
-            if (l1.val <= l2.val) {
-                curr.next = l1;
-                l1 = l1.next;
-            } else {
-                curr.next = l2;
-                l2 = l2.next;
-            }
+        while (!queue.isEmpty()){
+            curr.next = new ListNode(queue.poll());
             curr = curr.next;
         }
-
-        if (l1 == null) curr.next = l2;
-        if (l2 == null) curr.next = l1;
 
         return dummy.next;
     }
