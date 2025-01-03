987bbd3fed02e11c31c5276dab39fff85c77389e
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-04 03:45:39 +0800

    Time: 1 ms (99.06%), Space: 42.8 MB (5.94%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.java
+++ 0399-evaluate-division/0399-evaluate-division.java
@@ -1,50 +1,57 @@
-class Solution {
-    Map<String, Map<String, Double>> map = new HashMap<>();
-    
-    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-        for (int i = 0; i < equations.size(); i++) {
-            List<String> equation = equations.get(i);
+class Node {
+    String num2;
+    double value;
 
+    Node(String num2, double value) {
+        this.num2 = num2;
+        this.value = value;
+    }
+}
+
+class Solution {
+    Map<String, List<Node>> map = new HashMap<>();
+
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {        
+        for (int i = 0; i < equations.size(); i++) { 
+            List<String> equation = equations.get(i);
             String num1 = equation.get(0);
             String num2 = equation.get(1);
             double value = values[i];
 
-            map.putIfAbsent(num1, new HashMap<>());
-            map.putIfAbsent(num2, new HashMap<>());
+            map.putIfAbsent(num1, new ArrayList<>());
+            map.putIfAbsent(num2, new ArrayList<>());
 
-            map.get(num1).put(num2, value);
-            map.get(num2).put(num1, 1 / value);
-
-            // map.get(num1).put(num1, 1.0);
-            // map.get(num2).put(num2, 1.0);
+            map.get(num1).add(new Node(num2, value));
+            map.get(num2).add(new Node(num1, 1 / value)); // a/b = 5, b/a = 1/5
         }
 
         double[] res = new double[queries.size()];
+        
         for (int i = 0; i < queries.size(); i++) {
             List<String> query = queries.get(i);
             String queryNum1 = query.get(0);
             String queryNum2 = query.get(1);
-            res[i] = dfs(queryNum1, queryNum2, 1.0, new HashSet<>());
+
+            res[i] = dfs(queryNum1, queryNum2, 1, map, new HashSet<>());
         }
 
         return res;
     }
 
-    public double dfs(String num1, String num2, double value, Set<String> set) {
-        if (!map.containsKey(num1)) return -1.0;
-        if (num1.equals(num2)) return value;
-        if (set.contains(num1)) return -1.0;
-
-        Map<String, Double> neighbors = map.get(num1);
-        for (String next: neighbors.keySet()) {
-            set.add(num1);
-            double temp = dfs(next, num2, value * neighbors.get(next), set);
-            if (temp != -1.0) {
-                return temp;
-            } 
-            set.remove(num1);
-        }
+    public double dfs(String startDividend, String endDivisor, double value, Map<String, List<Node>> map, HashSet<String> set) {
+        if (!map.containsKey(startDividend)) return -1;
+        if (set.contains(startDividend)) return -1;
+        if (startDividend.equals(endDivisor)) return value;
         
-        return -1.0;
+        set.add(startDividend);
+        for (Node node: map.get(startDividend)) {
+            double res = dfs(node.num2, endDivisor, value * node.value, map, set); 
+            if (res != -1) {
+                return res;
+            }
+        }
+        set.remove(startDividend);
+      
+       return -1.0;
     }
 }
