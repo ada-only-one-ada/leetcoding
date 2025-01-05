f703b9ae83f9ea0594179cc5e641675f6faea95b
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-05 15:02:49 -0800

    Time: 7 ms (24.87%), Space: 45.5 MB (9.81%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,25 +1,48 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        int n = triangle.size();
-        // 使用一个数组来存储到当前行每个元素的最小路径和
-        int[] dp = new int[n];
-        dp[0] = triangle.get(0).get(0);  // 初始化最顶部元素
+        /*
+        0 1 2 3
+        ------------
+        2
+        3 4
+        6 5 7 （5可以由3过来，也可以由4过来）
+        4 1 8 3 （1可以由6过来，也可以由5过来；8可以由5过来，也可以由7过来）
 
-        // 从第二行开始遍历
-        for (int row = 1; row < n; row++) {
-            // 从右向左更新dp数组，以避免覆盖还未更新的值
-            dp[row] = dp[row - 1] + triangle.get(row).get(row);  // 最右侧元素的更新
-            for (int col = row - 1; col > 0; col--) {
-                dp[col] = Math.min(dp[col - 1], dp[col]) + triangle.get(row).get(col);
+        过程：
+         0  1  2  3 
+        [2]
+        [5,6]
+        [11,10,13]   
+        */
+
+        List<Integer> dp = new ArrayList<>();
+        dp.add(triangle.get(0).get(0));
+
+        for (int row = 1; row < triangle.size(); row++) {
+            List<Integer> temp = new ArrayList<>();
+            for (int col = 0; col <= row; col++) {
+                int number = triangle.get(row).get(col);
+                int minPathSum = Integer.MAX_VALUE;
+
+                if (col - 1 >= 0) {
+                    minPathSum = Math.min(minPathSum, number + dp.get(col - 1));
+                }
+
+                if (col < dp.size()) {
+                    minPathSum = Math.min(minPathSum, number + dp.get(col));
+                }
+
+                temp.add(minPathSum);
             }
-            dp[0] = dp[0] + triangle.get(row).get(0);  // 最左侧元素的更新
+
+            dp = temp;
         }
 
-        // 在dp数组中找到最小值，这就是从顶部到底部的最小路径和
-        int minTotal = dp[0];
-        for (int i = 1; i < n; i++) {
-            minTotal = Math.min(minTotal, dp[i]);
+        int res = Integer.MAX_VALUE;
+        for (int sum: dp) {
+            res = Math.min(res, sum);
         }
-        return minTotal;
+
+        return res;
     }
 }
