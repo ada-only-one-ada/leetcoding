f33d01ffa91f0a507fa417e461ddf693b382ec58
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-05 15:18:57 -0800

    Time: 3 ms (66.51%), Space: 44.1 MB (71.1%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,41 +1,31 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        /*
-        0 1 2 3
-        ------------
-        2
-        3 4
-        6 5 7 （5可以由3过来，也可以由4过来）
-        4 1 8 3 （1可以由6过来，也可以由5过来；8可以由5过来，也可以由7过来）
-
-        过程：
-         0  1  2  3 
-        [2]
-        [5,6]
-        [11,10,13]   
-        */
-
-        int[] dp = new int[triangle.size()];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[0] = triangle.get(0).get(0);
-
-        for (int row = 1; row < triangle.size(); row++) {
-            for (int col = row; col >= 0; col--) {
-                int number = triangle.get(row).get(col);
-                int minPathSum = Integer.MAX_VALUE;
-
-                if (col - 1 >= 0) minPathSum = Math.min(minPathSum, number + dp[col - 1]);
-                if (dp[col] != Integer.MAX_VALUE) minPathSum = Math.min(minPathSum, number + dp[col]);
-
-                dp[col] = minPathSum;
+        int[] dp = new int[triangle.size() + 1];
+        int sum = 0;
+        
+        for (int i = triangle.size()-1; i >= 0; i--){
+            for (int j = 0; j <= i; j++){
+                dp[j] = triangle.get(i).get(j) + Math.min(dp[j], dp[j+1]);
             }
         }
-
-        int min = Integer.MAX_VALUE;
-        for (int sum: dp) {
-            min = Math.min(min, sum);
-        }
-
-        return min;
+         
+        return dp[0];
     }
 }
+
+/*
+    2
+   3 4
+  6 5 7
+ 4 1 8 3
+0 0 0 0 0  
+
+initial dp array set to 0: dp[0, 0, 0, 0, 0] 
+row 4 1 8 3: dp[4, 1, 8,  3, -]
+row 6 5 7:   dp[7, 6, 10, -, -]
+row 3 4:     dp[9, 10, -, -, -]
+row 2:       dp[11, -, -, -, -]
+*/
+
+
+
