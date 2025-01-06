876c5e1a293369d12b3f4d3c59093b1f24a19ac0
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-05 23:54:44 -0800

    Time: 6 ms (74.47%), Space: 45 MB (26.36%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        // 只包含：+ - * / 和数字[-200, 200]
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < tokens.length; i++) {
+            String currToken = tokens[i];
+
+            if (currToken.equals("+") || currToken.equals("-") || currToken.equals("*") || currToken.equals("/")) {
+                int num2 = stack.pop();
+                int num1 = stack.pop();
+
+                if (currToken.equals("+")) {
+                    stack.push(num1 + num2);
+                } else if (currToken.equals("-")) {
+                    stack.push(num1 - num2);
+                } else if (currToken.equals("*")) {
+                    stack.push(num1 * num2);
+                } else if (currToken.equals("/")) {
+                    stack.push(num1 / num2);
+                }
+            } else {
+                stack.push(Integer.valueOf(currToken));
+            }
+        }
+
+        int sum = 0;
+        while (!stack.isEmpty()) {
+            sum += stack.pop();
+        }
+
+        return sum;
+    }
+}
