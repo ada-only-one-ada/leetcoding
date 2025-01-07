5db0a1b4bf5d536b4c8f7cfb6b7ee41008947b05
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-07 13:24:05 -0800

    Time: 2 ms (40.98%), Space: 44.7 MB (89.9%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,42 +1,40 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> res = new ArrayList<>();
-
-        int newStart = newInterval[0];
-        int newEnd = newInterval[1];
-       
-        //无脑把绝对不可能重叠的加入res   
-        int i = 0;
-        while (i < intervals.length && intervals[i][1] < newStart) {
-            res.add(intervals[i]);
-            i++;
-        }
-
-       // pre，【a，b，c，d....】
-       // 有可能newIntervals的起点先，或者a的起点先，把先的那个设置成prev
-        int[] prev = newInterval;
-        if (i < intervals.length && prev[0] < newStart){
-            int[] temp = prev;
-            intervals[i] = prev;
-            prev = temp;
-        }
-
-        if (i < intervals.length) {
-            prev[0] = Math.min(prev[0], intervals[i][0]);
-        }
+        // 读题目：You are given an array of non-overlapping intervals
         
-        for (int j = i; j < intervals.length; j++) {
-            int[] curr = intervals[j]; 
-            if (curr[0] > prev[1]) {
-                res.add(prev);
-                prev = curr;
-            } else {
-                // prev[0] = Math.min(prev[0], curr[0]);
-                prev[1] = Math.max(prev[1], curr[1]);
-            }
+        if (intervals.length == 0) return new int[][]{newInterval};
+        Arrays.sort(intervals, (a, b) -> {
+            return a[0] - b[0];
+        });
+
+        List<int[]> list = new ArrayList<>();
+        int insertStart = newInterval[0];
+        int insertEnd = newInterval[1];
+
+        int index = 0;
+        while (index < intervals.length && intervals[index][1] < insertStart) {
+            list.add(intervals[index]);
+            index++;
         }
 
-        res.add(prev);
-        return res.toArray(new int[res.size()][2]);
+      
+        while (index < intervals.length && intervals[index][0] <= insertEnd) {
+            insertStart = Math.min(insertStart, intervals[index][0]);
+            insertEnd = Math.max(insertEnd, intervals[index][1]);
+            index++;
+        }
+
+        list.add(new int[]{insertStart, insertEnd});
+        while (index < intervals.length) {
+            list.add(intervals[index]);
+            index++;
+        }
+
+        int[][] res = new int[list.size()][2];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = list.get(i);
+        }
+
+        return res;
     }
 }
