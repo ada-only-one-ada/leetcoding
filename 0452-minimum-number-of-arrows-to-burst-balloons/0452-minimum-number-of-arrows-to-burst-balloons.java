ebc1f6fdff941c640c82e661b80d2d78acd5b1ee
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-06 16:09:17 -0800

    Time: 54 ms (52.55%), Space: 69.2 MB (40.9%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
@@ -4,13 +4,13 @@ class Solution {
         if (points.length == 0) return 0;
 
         Arrays.sort(points, (a, b) -> {
-            if (a[0] == b[0]) return Integer.compare(b[1], a[1]);
+           // if (a[0] == b[0]) return Integer.compare(b[1], a[1]);
             return Integer.compare(a[0], b[0]);
         });
 
         int res = 1;
-        int prevStart = points[0][0]; // 尽量小
-        int prevEnd = points[0][1]; // 尽量大
+        int prevStart = points[0][0]; 
+        int prevEnd = points[0][1]; 
 
         for (int i = 1; i < points.length; i++) {
             int currStart = points[i][0];
@@ -19,6 +19,8 @@ class Solution {
             if (currStart <= prevEnd) {
                 prevEnd = Math.min(prevEnd, currEnd);
             } else {
+                // 没有重叠，需要额外的一支箭
+                // 射在重叠的较大点，因为越往后范围越大，射在较大点可以包括的范围更广
                 prevEnd = currEnd;
                 res++;
             }
