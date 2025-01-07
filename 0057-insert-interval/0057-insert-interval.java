f34fb36dde9ef571edda47d2539553f4ad0bbe99
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-07 13:25:42 -0800

    Time: 1 ms (99.05%), Space: 44.8 MB (81.23%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,10 +1,6 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         // 读题目：You are given an array of non-overlapping intervals
-        
-        if (intervals.length == 0) return new int[][]{newInterval};
-       
-
         List<int[]> list = new ArrayList<>();
         int insertStart = newInterval[0];
         int insertEnd = newInterval[1];
