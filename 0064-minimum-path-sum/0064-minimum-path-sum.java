c34b3b63969014481ce95ef595f9e979bf123b0b
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-07 16:26:02 -0800

    Time: 53 ms (5.25%), Space: 55.5 MB (5.59%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -2,12 +2,13 @@ class Solution {
     public int minPathSum(int[][] grid) {
         // 只能走 down 或者 right  
         // 左上走到右下
+        int[][] directions = new int[][]{{1,0}, {0,1}};
 
         Queue<int[]> queue = new PriorityQueue<>((a, b) -> {
             return a[2] - b[2];
         });
 
-        queue.add(new int[]{0, 0, 0});
+        queue.add(new int[]{0, 0, grid[0][0]});
         Map<String, Integer> map = new HashMap<>();
 
         while (!queue.isEmpty()) {
@@ -16,15 +17,18 @@ class Solution {
             int col = curr[1];
             int sum = curr[2];
 
-            if (row == grid.length - 1 && col == grid[0].length - 1) return sum + grid[row][col];
-            if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) continue;
+            if (row == grid.length - 1 && col == grid[0].length - 1) return sum;
             
-            String state = row + " " + col;
-            int nextSum = sum + grid[row][col];
+            for (int[] direction: directions) {
+                int nextRow = row + direction[0];
+                int nextCol = col + direction[1];
 
-            if (!map.containsKey(state) || nextSum < map.get(state)) {
-                queue.add(new int[]{row + 1, col, nextSum});
-                queue.add(new int[]{row, col + 1, nextSum});
+                if (nextRow >= grid.length || nextCol >= grid[0].length) continue;
+                String state = nextRow + " " + nextCol;
+                int nextSum = sum + grid[nextRow][nextCol];
+                if (map.containsKey(state) && nextSum >= map.get(state)) continue;
+
+                queue.add(new int[]{nextRow, nextCol, nextSum});
                 map.put(state, nextSum);
             }
         }
