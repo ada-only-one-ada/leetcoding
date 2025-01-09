e1db95e5185b89f7a9381219d6ea405a673a95f3
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-08 21:29:30 -0800

    Time: 0 ms (100%), Space: 42.4 MB (6.16%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -2,30 +2,23 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int rowNum = obstacleGrid.length;
         int colNum = obstacleGrid[0].length;
-        if (obstacleGrid[0][0] == 1 || obstacleGrid[rowNum - 1][colNum - 1] == 1) return 0; 
-
         int[][] dp = new int[rowNum][colNum];
-        
+
         for (int row = 0; row < rowNum; row++) {
             for (int col = 0; col < colNum; col++) {
                 if (obstacleGrid[row][col] == 1) {
                     dp[row][col] = 0;
-                    continue;
-                }
-
-                if (row == 0 && col == 0) {
-                    dp[row][col] = 1;
-                } else if (row > 0 && col > 0) {
-                    if (dp[row-1][col] == 0 && dp[row][col - 1] == 0) {
-                        dp[row][col] = 0;
-                    } else {
+                } else {
+                    if (row > 0 && col > 0) {
                         dp[row][col] = dp[row - 1][col] + dp[row][col - 1];
+                    } else if (row > 0) {
+                        dp[row][col] = dp[row - 1][col];
+                    } else if (col > 0){
+                        dp[row][col] = dp[row][col - 1];
+                    } else {
+                        dp[row][col] = 1;
                     }
-                } else if (row > 0) {
-                    dp[row][0] = dp[row - 1][0] == 0 ? 0 : 1;
-                } else if (col > 0) {
-                    dp[0][col] = dp[0][col - 1] == 0 ? 0 : 1;
-                }
+                } 
             }
         }
 
