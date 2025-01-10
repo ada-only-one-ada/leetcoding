ee822281658fab771ad9af14366d33c5cc757cd3
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-09 18:58:55 -0800

    Time: 782 ms (5.02%), Space: 50.3 MB (6.11%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -14,6 +14,7 @@ class Solution {
             if (currIndex >= nums.length - 1) return currCost;
             for (int i = currIndex + 1; i <= currIndex + nums[currIndex]; i++) {
                 int nextCost = currCost + 1;
+                // 如果之前没走过，或者这次走的花费更小，才需要更新
                 if (!map.containsKey(i) || nextCost < map.get(i)) {
                     queue.add(new int[]{i, nextCost});
                     map.put(i, nextCost);
