cbb27e7118249334d2ffe436c3961f4b5e777d67
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-09 17:44:42 -0800

    Time: 710 ms (5.02%), Space: 50.2 MB (6.11%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int jump(int[] nums) {
+        Queue<int[]> queue = new PriorityQueue<>((a, b) -> {
+            return a[1] - b[1];
+        });
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        // start at index 0, cost 0.
+        queue.add(new int[]{0, 0});
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            int currIndex = curr[0];
+            int currCost = curr[1];
+
+            if (currIndex >= nums.length - 1) return currCost;
+            for (int i = currIndex + 1; i <= currIndex + nums[currIndex]; i++) {
+                if (!map.containsKey(i) || currCost + 1 < map.get(i)) {
+                    queue.add(new int[]{i, currCost + 1});
+                    map.put(i, currCost + 1);
+                }
+            }
+        }
+
+        return -1;
+    }
+}
