37238dad962ef5d5c2196ebf5b8da18a86465df6
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-10 15:00:48 -0800

    Time: 1 ms (99.32%), Space: 45.4 MB (30.59%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,7 +1,7 @@
 class Solution {
     public int jump(int[] nums) {
         if (nums.length == 0) return 0; // 如果数组长度为0，直接返回0（虽然题目默认至少有一个元素）
-        int steps = 0;
+        int res = 0;
         
         int currentStart = 0;
         int currentEnd = 0;
@@ -18,11 +18,11 @@ class Solution {
             // 更新下一次的起始位置为当前覆盖范围的下一个位置
             currentStart = currentEnd + 1;
             // 完成一次跳跃，跳跃次数增加
-            steps++;
+            res++;
             // 更新当前覆盖的最远范围为下一次可以到达的最远范围
             currentEnd = nextEnd;
         }
 
-        return steps;
+        return res;
     }
 }
