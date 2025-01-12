59b2e59142a61d746f3433148ef2b372d0b6fb5d
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-12 15:43:20 -0800

    Time: 1 ms (99.33%), Space: 45.3 MB (38.67%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,28 +1,22 @@
 class Solution {
     public int jump(int[] nums) {
-        // 如果数组长度为0，直接返回 0（虽然题目默认至少有一个元素）
-        if (nums.length == 0) return 0; 
-
+        if (nums.length == 0) return 0;
         int res = 0;
-        
-        int currentStart = 0;
-        int currentEnd = 0;
+        int i = 0;
+        int currEnd = 0;
         int nextEnd = 0;
-        
-        // 遍历数组中的元素来计算跳跃步数，除了最后一个元素
-        while (currentEnd < nums.length - 1) {
-            // 遍历当前可以覆盖的范围
-            for (int index = currentStart; index <= currentEnd; index++) {
-                // 更新下一次跳跃可以达到的最远距离
-                nextEnd = Math.max(index + nums[index], nextEnd);
+
+        while (currEnd < nums.length - 1) {
+            nextEnd = Math.max(nextEnd, nums[i] + i);
+
+            if (i == currEnd) {
+                if (nextEnd <= currEnd) return -1;
+
+                currEnd = nextEnd;
+                res++;
             }
 
-            // 更新下一次的起始位置为当前覆盖范围的下一个位置
-            currentStart = currentEnd + 1;
-            // 完成一次跳跃，跳跃次数增加
-            res++;
-            // 更新当前覆盖的最远范围为下一次可以到达的最远范围
-            currentEnd = nextEnd;
+            i++;
         }
 
         return res;
