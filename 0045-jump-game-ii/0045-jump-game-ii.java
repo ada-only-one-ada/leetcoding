4264f9a1fae2ab476244e582653cb6409f5f9bcd
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-12 19:55:35 -0800

    Time: 1 ms (99.33%), Space: 45.7 MB (17.82%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,19 +1,31 @@
 class Solution {
     public int jump(int[] nums) {
-        if (nums.length == 0) return 0;
+        // 起点就是最后一个，不用跳跃，直接返回
+        if (nums.length <= 1) return 0;
+
         int res = 0;
+
         int i = 0;
         int currEnd = 0;
         int nextEnd = 0;
 
+        // 遍历当终点在最后一个之前
         while (currEnd < nums.length - 1) {
+            // 当前能走的最远的距离
             nextEnd = Math.max(nextEnd, nums[i] + i);
 
+            // 最到了当前能走的最远距离
             if (i == currEnd) {
-                if (nextEnd <= currEnd) return -1;
+                // 跳到了最后一个
+                if (currEnd >= nums.length - 1) return res;
 
-                currEnd = nextEnd;
+                // 下一个更远的不比当前的更远，结束遍历
+                if (nextEnd <= currEnd) {
+                    return -1;
+                }
+
                 res++;
+                currEnd = nextEnd;
             }
 
             i++;
