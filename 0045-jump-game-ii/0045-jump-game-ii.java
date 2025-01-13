6ee04d7034d696091c0035bc0a98ad67f0420aab
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-12 19:56:04 -0800

    Time: 1 ms (99.33%), Space: 45.1 MB (57.54%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,36 +1,31 @@
 class Solution {
     public int jump(int[] nums) {
-        // 起点就是最后一个，不用跳跃，直接返回
-        if (nums.length <= 1) return 0;
+        if (nums.length <= 1) return 0;  // 如果数组长度为1或更少，则不需要任何跳跃
+        
+        int res = 0;  // 用来记录跳跃次数
+        int currEnd = 0;  // 当前跳能达到的最远位置
+        int nextEnd = 0;  // 下一跳能达到的最远位置
+        int i = 0;  // 数组索引，用于遍历数组
 
-        int res = 0;
-
-        int i = 0;
-        int currEnd = 0;
-        int nextEnd = 0;
-
-        // 遍历当终点在最后一个之前
+        // 循环条件是当前最远位置小于数组最后一个元素的索引
         while (currEnd < nums.length - 1) {
-            // 当前能走的最远的距离
-            nextEnd = Math.max(nextEnd, nums[i] + i);
+            // 更新下一跳能达到的最远位置
+            nextEnd = Math.max(nextEnd, i + nums[i]);
 
-            // 最到了当前能走的最远距离
+            // 如果遍历到了当前跳的边界
             if (i == currEnd) {
-                // 跳到了最后一个
-                if (currEnd >= nums.length - 1) return res;
-
-                // 下一个更远的不比当前的更远，结束遍历
-                if (nextEnd <= currEnd) {
-                    return -1;
+                // 如果当前位置等于下一位置，且当前位置不是数组末尾，则无法到达数组末尾
+                if (currEnd == nextEnd) {
+                    return -1;  // 返回-1表示无法到达末尾
                 }
-
+                // 完成一次跳跃，更新跳数和当前跳的最远位置
                 res++;
                 currEnd = nextEnd;
             }
-
+            // 移动索引，继续向前探索
             i++;
         }
-
-        return res;
+        
+        return res;  // 返回需要的最小跳跃次数
     }
 }
