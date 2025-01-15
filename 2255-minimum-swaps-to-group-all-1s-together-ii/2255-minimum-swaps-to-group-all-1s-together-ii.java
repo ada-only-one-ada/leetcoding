2df45cb5071012d6554256e1cce9ee038499e123
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-14 22:32:37 -0800

    Time: 6 ms (73.54%), Space: 59.5 MB (40.31%) - LeetHub

--- 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.java
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.java
@@ -1,23 +1,26 @@
 class Solution {
     public int minSwaps(int[] nums) {       
-        int totalOnes = 0;
+        int ones = 0;
         for (int num: nums) {
-            if (num == 1) totalOnes++;
+            ones += num;
         }
 
-        int longestConsecutiveOnes = 0, currOnes = 0;
+        int longestConsecutiveOnes = 0;
+        int countOnes = 0;
+
         int left = 0;
         for (int right = 0; right < nums.length * 2; right++) {
             int num = nums[right % nums.length];
-            currOnes += num;
-
-            if (right - left + 1 == totalOnes) {
-                longestConsecutiveOnes = Math.max(longestConsecutiveOnes, currOnes);
-                currOnes -= nums[left % nums.length];
+            countOnes += num;
+            longestConsecutiveOnes = Math.max(longestConsecutiveOnes, countOnes);
+            if (right - left + 1 == ones) {
+                //longestConsecutiveOnes = Math.max(longestConsecutiveOnes, countOnes);
+               
+                countOnes -= nums[left % nums.length];
                 left++;
             }
         }
 
-        return totalOnes - longestConsecutiveOnes;
+        return ones - longestConsecutiveOnes;
     }
 }
