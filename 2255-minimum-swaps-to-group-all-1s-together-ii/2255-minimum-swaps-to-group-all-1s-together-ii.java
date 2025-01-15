efcab0a539368308d20511d414fc9332bde4dd87
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-14 22:36:35 -0800

    Time: 7 ms (66.46%), Space: 59.4 MB (40.31%) - LeetHub

--- 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.java
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.java
@@ -11,15 +11,20 @@ class Solution {
         int left = 0;
         for (int right = 0; right < nums.length * 2; right++) {
             int num = nums[right % nums.length];
+            // 累计统计当前的 1
             countOnes += num;
+            // 最长连续的 1 的长度
             longestConsecutiveOnes = Math.max(longestConsecutiveOnes, countOnes);
-           
+            
+            // 达到数量了，移动左窗口，即我们只关注包含 ones 个 1 的窗口
             if (right - left + 1 == ones) {
                 countOnes -= nums[left % nums.length];
                 left++;
             }
         }
 
+        // 总移动数就是要把剩下的 1 加入到这个最长串的 1 的 group
+        // 因为这表示我们需要将剩余的 1 移动到这个连续段中，以形成一个完整的连续 1 段
         return ones - longestConsecutiveOnes;
     }
 }
