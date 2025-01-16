ce6efc5f8aabc4764aafdf496aea250b7af2e883
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-15 17:25:10 -0800

    Time: 23 ms (46.03%), Space: 62.9 MB (38.12%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,18 +1,37 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        // [第一次买][第一次卖][第二次买][第二次卖]
-        int[][] dp = new int[prices.length][4];
-        dp[0][0] = -prices[0];
-        dp[0][2] = -prices[0]; // you need to initialize this
+        int[][] dp = new int[prices.length+1][5];
+        /*
+        不操作：dp[i][0] = dp[i-1][0]
 
-        for (int i = 1; i < prices.length; i++) {
-            dp[i][0] = Math.max(dp[i-1][0], -prices[i]);
-            dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] + prices[i]);
-            
-            dp[i][2] = Math.max(dp[i-1][2], dp[i-1][1] - prices[i]);
-            dp[i][3] = Math.max(dp[i-1][3], dp[i-1][2] + prices[i]);
+        第一次持有：dp[i][1]
+        1.之前没有，第i天买入：-prices[i-1]
+        2.之前就已经第一次持有了(但还没交易过,所以还是第一次持有的状态）：dp[i-1][1]
+
+        第一次不持有：dp[i][2] 
+        1.之前有，第i天卖掉：dp[i-1][1] + prices[i-1]
+        2.之前第一次不持有了：dp[i-1][2]
+
+        第二次持有：dp[i][3] 
+        1.在第一次不持有之后，第i天买入：dp[i-1][2] - prices[i-1]
+        2.之前就已经第二次持有了：dp[i-1][3]
+
+        第二次不持有：dp[i][4]
+        1.在第二次持有以后，第i天卖掉：dp[i-1][3] + prices[i-1]
+        2.之前就已经第二次不持有了：dp[i-1][4]
+        */
+        dp[1][1] = -prices[0];
+        dp[1][2] = 0;
+        dp[1][3] = -prices[0];
+        dp[1][4] = 0;
+
+        for (int i = 2; i <= prices.length; i++) {
+            dp[i][1] = Math.max(-prices[i-1], dp[i-1][1]);
+            dp[i][2] = Math.max(dp[i-1][1] + prices[i-1], dp[i-1][2]);
+            dp[i][3] = Math.max(dp[i-1][2] - prices[i-1], dp[i-1][3]);
+            dp[i][4] = Math.max(dp[i-1][3] + prices[i-1], dp[i-1][4]);
         }
-        
-        return Math.max(dp[dp.length - 1][1], dp[dp.length - 1][3]);
+
+        return dp[prices.length][4];
     }
 }
