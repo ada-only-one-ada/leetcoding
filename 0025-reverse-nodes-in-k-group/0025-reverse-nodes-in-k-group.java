a7fc41350a416de907c180939f99c6bc46809fc6
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-16 18:16:15 -0800

    Time: 0 ms (100%), Space: 44.1 MB (82.86%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -1,43 +1,29 @@
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
         ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+
         ListNode prev = dummy;
+        ListNode end = dummy;
 
-        int count = 0;
-        // 遍历链表
-        ListNode curr = head;
-        while (curr != null) {
-            // 把下一次遍历的头节点先保存下来
-            ListNode nextCurr = curr.next;
-            count++;
-
-            if (count == k) {
-                // 跟下一段断开
-                curr.next = null;
-                ListNode reverseCurrGroup = reverse(head);
-
-                // 把之前的段跟反转之后的相连
-                prev.next = reverseCurrGroup;
-
-                ListNode lastNode = reverseCurrGroup;
-                while (lastNode.next != null) {
-                    lastNode = lastNode.next;
-                }
-
-                // 把反转之后的跟下一段开头相连
-                lastNode.next = nextCurr;
-
-                // 更新 prev 节点
-                prev = lastNode;
-                
-                // 更新下一段的头节点
-                head = nextCurr;
-                count = 0;
+        while (prev != null) {
+            for (int count = 0; count < k && end != null; count++) {
+                end = end.next; // end = 2
             }
+            
+            if (end == null) break;
 
-            curr = nextCurr;
+            ListNode nextHead = end.next; // nextHead = 3,4,5
+            end.next = null; // 1,2,null
+
+            ListNode start = prev.next; // (start)1,2
+            prev.next = reverse(start); // dummy,2,1(start)
+            start.next = nextHead;
+            
+            prev = start;
+            end = start;
         }
-
+        
         return dummy.next;
     }
 
@@ -47,7 +33,6 @@ class Solution {
         while (head != null) {
             ListNode nextHead = head.next;
             head.next = prev;
-
             prev = head;
             head = nextHead;
         }
