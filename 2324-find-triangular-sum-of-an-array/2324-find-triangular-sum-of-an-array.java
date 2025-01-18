5f54234d052a7723f6b73ce44fa0e6defa591ce8
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-17 21:12:18 -0800

    Time: 45 ms (45.28%), Space: 45 MB (24.23%) - LeetHub

--- 2324-find-triangular-sum-of-an-array/2324-find-triangular-sum-of-an-array.java
+++ 2324-find-triangular-sum-of-an-array/2324-find-triangular-sum-of-an-array.java
@@ -1,21 +1,17 @@
 class Solution {
     public int triangularSum(int[] nums) {
-        List<Integer> dp = new ArrayList<>();
-        for (int num: nums) {
-            dp.add(num);
+        return dfs(nums);
+    }
+
+    public int dfs(int[] nums) {
+        if (nums.length == 1) return nums[0];
+
+        int[] temp = new int[nums.length - 1];
+
+        for (int i = 1; i < nums.length; i++) {
+            temp[i - 1] = (nums[i] + nums[i - 1]) % 10;
         }
 
-        while (dp.size() > 1) {
-            List<Integer> temp = new ArrayList<>();
-
-            for (int col = 0; col + 1 < dp.size(); col++) {
-                int sum = (dp.get(col) + dp.get(col + 1)) % 10;
-                temp.add(sum);
-            }
-
-            dp = temp;
-        }
-
-        return dp.get(0);
+        return dfs(temp);
     }
 }
