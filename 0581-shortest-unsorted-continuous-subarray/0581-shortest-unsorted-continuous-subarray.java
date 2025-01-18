2a9875d8fc9e574d953db9b0fda6d7ceee762211
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-17 22:16:13 -0800

    Time: 1 ms (96.32%), Space: 45 MB (95.8%) - LeetHub

--- 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.java
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.java
@@ -1,17 +1,17 @@
 class Solution {
     public int findUnsortedSubarray(int[] nums) {
-
         // 左边最大，右边最小
         // 要比左边最大大，要比右边最小小
-
         int[] max = new int[nums.length];
         int[] min = new int[nums.length];
 
+        // 注意这里初始化，不然是 0
         max[0] = nums[0];
         for (int i = 1; i < nums.length; i++) {
             max[i] = Math.max(max[i - 1], nums[i - 1]);   
         }
 
+        // 注意这里初始化，不然是 0
         min[min.length - 1] = nums[nums.length - 1];
         for (int i = nums.length - 2; i >= 0; i--){
             min[i] = Math.min(min[i + 1], nums[i + 1]);
@@ -20,18 +20,13 @@ class Solution {
         boolean[] wrong = new boolean[nums.length];
         // 要比左边最大大，要比右边最小小
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] < max[i]) {
-                wrong[i] = true;
-            }
-
-            if (nums[i] > min[i]) {
+            if (nums[i] < max[i] || nums[i] > min[i]) {
                 wrong[i] = true;
             }
         }
 
-        int start = -1;
-        int end = -1;
-
+        int start = 0;
+        int end = 0;
         for (int i = 0; i < wrong.length; i++) {
             if (wrong[i]) end = i;
         }
@@ -40,7 +35,7 @@ class Solution {
             if (wrong[i]) start = i;
         }
 
-        if (start == end) return 0;
+        if (start == end) return 0; 
         return end - start + 1;
     }
 }
