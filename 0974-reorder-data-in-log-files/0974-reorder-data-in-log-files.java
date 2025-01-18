c70cee645a43776821464e26f53c69a082c6b775
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-17 19:17:33 -0800

    Time: 8 ms (22.83%), Space: 44.9 MB (64.8%) - LeetHub

---
+++ 0974-reorder-data-in-log-files/0974-reorder-data-in-log-files.java
@@ -0,0 +1,45 @@
+class Solution {
+    public String[] reorderLogFiles(String[] logs) {
+        // iden + content
+        // 先根据 content 排序，然后根据 iden
+
+        List<Pair<String, String>> letterLogs = new ArrayList<>();
+        List<String> digitLogs = new ArrayList<>();
+
+        for (String log: logs) {
+            if (Character.isDigit(log.charAt(log.length() - 1))) {
+                digitLogs.add(log);
+            } else {
+                int space = 0;
+                while (space < log.length() && log.charAt(space) != ' ') space++;
+
+                String iden = log.substring(0, space);
+                String content = log.substring(space + 1);
+
+                Pair<String, String> pair = new Pair(iden, content);
+                letterLogs.add(pair);
+            }
+        }
+
+        Collections.sort(letterLogs, (a, b) -> {
+            if (!a.getValue().equals(b.getValue())) {
+                return a.getValue().compareTo(b.getValue());
+            } else {
+                return a.getKey().compareTo(b.getKey());
+            }
+        });
+
+        List<String> res = new ArrayList<>();
+        for (Pair<String, String> pair: letterLogs) {
+            res.add(pair.getKey() + " " + pair.getValue());
+        }
+
+        res.addAll(digitLogs);
+        String[] resArray = new String[res.size()];
+        for (int i = 0; i < resArray.length; i++) {
+            resArray[i] = res.get(i);
+        }
+
+        return resArray;
+    }
+}
