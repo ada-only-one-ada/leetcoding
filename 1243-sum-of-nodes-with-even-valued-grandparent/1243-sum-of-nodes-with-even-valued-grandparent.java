cc42de89bbf570630a4aae34af79efa7c542bc67
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-17 16:37:05 -0800

    Time: 1 ms (100%), Space: 46.7 MB (10.88%) - LeetHub

--- 1243-sum-of-nodes-with-even-valued-grandparent/1243-sum-of-nodes-with-even-valued-grandparent.java
+++ 1243-sum-of-nodes-with-even-valued-grandparent/1243-sum-of-nodes-with-even-valued-grandparent.java
@@ -1,16 +1,22 @@
 class Solution {
+    int sum = 0;
+
     public int sumEvenGrandparent(TreeNode root) {
-        return helper(root, null, null);
+        helper(root);
+        return sum;
     }
 
-    public int helper(TreeNode root, TreeNode parent, TreeNode grandparent) {
-        if (root == null) return 0;
+    public void helper(TreeNode root) {
+        if (root == null) return;
 
-        int subtree = helper(root.left, root, parent) + helper(root.right, root, parent);
-        if (grandparent != null && grandparent.val % 2 == 0) {
-            return root.val + subtree;
-        } else {
-            return subtree;
-        }
+        if (root.val % 2 == 0) {
+            if (root.left != null && root.left.left != null) sum += root.left.left.val;
+            if (root.left != null && root.left.right != null) sum += root.left.right.val;
+            if (root.right != null && root.right.left != null) sum += root.right.left.val;
+            if (root.right != null && root.right.right != null) sum += root.right.right.val;
+        } 
+
+        helper(root.left);
+        helper(root.right);
     }
 }
