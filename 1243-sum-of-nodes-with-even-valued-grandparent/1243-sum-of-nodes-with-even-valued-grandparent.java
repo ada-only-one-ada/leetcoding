2123d06fa99e01a934cecda1c913d338aed5a8e9
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-17 16:35:34 -0800

    Time: 1 ms (100%), Space: 46.3 MB (38.93%) - LeetHub

--- 1243-sum-of-nodes-with-even-valued-grandparent/1243-sum-of-nodes-with-even-valued-grandparent.java
+++ 1243-sum-of-nodes-with-even-valued-grandparent/1243-sum-of-nodes-with-even-valued-grandparent.java
@@ -1,32 +1,16 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    int sum = 0;
     public int sumEvenGrandparent(TreeNode root) {
-        helper(root, null, null);
-        return sum;
+        return helper(root, null, null);
     }
 
-    public void helper(TreeNode root, TreeNode parent, TreeNode grandparent) {
-        if (root == null) return;
-        if (grandparent != null && grandparent.val % 2 == 0) {
-            sum += root.val;
-        }
+    public int helper(TreeNode root, TreeNode parent, TreeNode grandparent) {
+        if (root == null) return 0;
 
-        helper(root.left, root, parent);
-        helper(root.right, root, parent);
+        int subtree = helper(root.left, root, parent) + helper(root.right, root, parent);
+        if (grandparent != null && grandparent.val % 2 == 0) {
+            return root.val + subtree;
+        } else {
+            return subtree;
+        }
     }
 }
