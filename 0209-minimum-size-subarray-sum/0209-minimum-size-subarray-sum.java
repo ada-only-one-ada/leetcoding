7f2822a67b8ae1fda3795707afd3fd7f4925afcf
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-20 18:54:43 -0800

    Time: 1 ms (99.86%), Space: 55.1 MB (94.51%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,19 +1,19 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        int minLen = nums.length + 1;
+        int res = Integer.MAX_VALUE;
 
-        int sum = 0;
+        int sum =  0;
         int left = 0;
         for (int right = 0; right < nums.length; right++) {
             sum += nums[right];
 
             while (sum >= target) {
-                minLen = Math.min(minLen, right - left + 1);
+                res = Math.min(res, right - left + 1);
                 sum -= nums[left];
                 left++;
             }
         }
 
-        return minLen == nums.length + 1? 0: minLen;
+        return res == Integer.MAX_VALUE ? 0 : res;
     }
 }
