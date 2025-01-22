3af420402bcba417ee892711ff2fe7e65c084063
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-21 17:05:53 -0800

    Time: 18 ms (6.33%), Space: 45.1 MB (87.66%) - LeetHub

--- 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.java
+++ 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.java
@@ -1,51 +1,38 @@
+
 class Solution {
     public int bestClosingTime(String customers) {
-        // 在某一时间点关门，计算那之前，没来的顾客
-        int[] prevNo = new int[customers.length()];
-        // 在0点关门，之前没有没来的顾客
-        for (int i = 1; i < customers.length(); i++) {
-            if (customers.charAt(i-1) == 'N') {
-                prevNo[i] = prevNo[i-1] + 1; 
-            } else {
-                prevNo[i] = prevNo[i-1];
-            }
-        }
-
-        // 在某一时间点关门，计算那之后，包括那一个点，来的顾客
-        int[] afterYes = new int[customers.length()];
-        // 最后一个点是 Y，那么那一个点来的顾客就是 1
-        afterYes[afterYes.length - 1] = customers.charAt(customers.length() - 1) == 'Y'? 1: 0;
-        for (int i = customers.length() - 2; i >= 0; i--) {
-            if (customers.charAt(i) == 'Y') {
-                afterYes[i] = afterYes[i+1] + 1;
-            } else {
-                afterYes[i] = afterYes[i+1];
-            }
-        }
-
-        int res = -1;
-        int minPenalty = Integer.MAX_VALUE;
-
-        for (int i = 0; i < customers.length(); i++) {
-            int no = prevNo[i]; // 关之前没来的
-            int yes = afterYes[i]; // 关之后来了的
-
-            if (no + yes < minPenalty) {
-                minPenalty = no + yes;
-                res = i;
-            }
-        }
-
-        // 一个顾客也没来
-        if (afterYes[0] < minPenalty) {
-            return 0;
-        }
-
-        // 一直有顾客来
-        if (prevNo[prevNo.length - 1] < minPenalty) {
-            return prevNo.length;
-        }
+        /* 规则：
+           1.关门之前有人最多，无人最少
+           2.关门之后无人最多，有人最少
+        */
         
+        int[] yesBefore = new int[customers.length() + 1];
+        for (int i = 1; i < yesBefore.length; i++) {
+            if (customers.charAt(i-1) == 'Y') {
+                yesBefore[i] = yesBefore[i-1] + 1;
+            } else {
+                yesBefore[i] = yesBefore[i-1];
+            }
+        }     
+
+        int res = yesBefore.length - 1;
+        int max = yesBefore[yesBefore.length - 1]; 
+        
+        int[] noAfter = new int[customers.length() + 1];
+        for (int i = noAfter.length - 2; i >= 0; i--) {
+            if (customers.charAt(i) == 'N') {
+                noAfter[i] = noAfter[i+1] + 1;
+            } else {
+                noAfter[i] = noAfter[i+1];
+            }
+
+            yesBefore[i] += noAfter[i];
+            if (yesBefore[i] >= max) {
+                res = i;
+                max = yesBefore[i];
+            }
+        }
+
         return res;
     }
 }
