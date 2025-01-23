fac64a452a1aeed37bf72b04f72a61d3b1f28b97
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-23 13:47:16 -0800

    Time: 3 ms (86.73%), Space: 49.6 MB (52.79%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,26 +1,37 @@
 class Solution {
     public int numIslands(char[][] grid) {
         int res = 0;
+        List<Integer> areas = new ArrayList<>();
+
         for (int row = 0; row < grid.length; row++) {
             for (int col = 0; col < grid[0].length; col++) {
                 if (grid[row][col] == '1') {
                     res++;
-                    dfs(grid, row, col);
+                    areas.add(dfs(grid, row, col));
                 }
             }
         }
 
-        return res;
+        return areas.size();
     }
 
-    public void dfs(char[][] grid, int row, int col) {
-        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) return;
-        if (grid[row][col] == '0') return;
+    public int dfs(char[][] grid, int row, int col) {
+        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) {
+            return 0;
+        };
+
+        if (grid[row][col] == '0') {
+            return 0;
+        }
 
         grid[row][col] = '0';
-        dfs(grid, row-1, col);
-        dfs(grid, row+1, col);
-        dfs(grid, row, col-1);
+        int sum = 1;
+        sum += 
+        dfs(grid, row-1, col) + 
+        dfs(grid, row+1, col) + 
+        dfs(grid, row, col-1) +
         dfs(grid, row, col+1);
+
+        return sum;
     }
 }
