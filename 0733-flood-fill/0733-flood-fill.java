858d844799c00c51e666c98aebb2932e9b29916f
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-23 16:43:11 -0800

    Time: 0 ms (100%), Space: 44.9 MB (80.87%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,20 +1,18 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        if (image[sr][sc] == color) return image;
-        
-        dfs(image, image[sr][sc], color, sr, sc);
+        dfs(image, sr, sc, color, image[sr][sc]);   
         return image;
     }
 
-    public void dfs(int[][] image, int originalColor, int newColor, int row, int col) {
-        if (row < 0 || col < 0 || row >= image.length || col >= image[0].length) return;
-        if (image[row][col] != originalColor) return;
-        // if (image[row][col] == newColor) return;
+    public void dfs(int[][] image, int sr, int sc, int color, int originalColor) {
+        if (sr < 0 || sr >= image.length || sc < 0 || sc >= image[0].length) return;
+        if (image[sr][sc] == color) return;
+        if (image[sr][sc] != originalColor) return;
 
-        image[row][col] = newColor;
-        dfs(image, originalColor, newColor, row - 1, col);
-        dfs(image, originalColor, newColor, row + 1, col);
-        dfs(image, originalColor, newColor, row, col - 1);
-        dfs(image, originalColor, newColor, row, col + 1);
+        image[sr][sc] = color;
+        dfs(image, sr - 1, sc, color, originalColor);
+        dfs(image, sr + 1, sc, color, originalColor);
+        dfs(image, sr, sc - 1, color, originalColor);
+        dfs(image, sr, sc + 1, color, originalColor);
     }
 }
