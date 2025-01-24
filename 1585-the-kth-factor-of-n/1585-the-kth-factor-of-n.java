ad77d1dc6cac7e16b829da38d2ff6b665a922069
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-23 18:26:32 -0800

    Time: 0 ms (100%), Space: 41.1 MB (8.74%) - LeetHub

--- 1585-the-kth-factor-of-n/1585-the-kth-factor-of-n.java
+++ 1585-the-kth-factor-of-n/1585-the-kth-factor-of-n.java
@@ -1,9 +1,9 @@
 class Solution {
     public int kthFactor(int n, int k) {
-        // O(n) complexity
+        // less than O(n) complexity
         int count = 0;
-        
-        for (int i = 1; i <= n; i++) {
+
+        for (int i = 1; i <= n/2; i++) {
             if (n % i == 0) {
                 count++;
                 if (count == k) {
@@ -11,6 +11,8 @@ class Solution {
                 }
             }
         }
+
+        if (count == k - 1) return n; 
 
         return -1;
     }
