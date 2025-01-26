603d6f355639a48b565f4fcb8db3ee204b97adfe
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-25 17:48:47 -0800

    Time: 8 ms (15.66%), Space: 49.1 MB (90.31%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,38 +1,42 @@
 class Solution {
-    public int numIslands(char[][] grid) {
-        int res = 0;
-        List<Integer> areas = new ArrayList<>();
+    public static int numIslands(char[][] grids) {
+        int count = 0;
+        int[][] directions = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+        Queue<int[]> queue = new LinkedList<>();
 
-        for (int row = 0; row < grid.length; row++) {
-            for (int col = 0; col < grid[0].length; col++) {
-                if (grid[row][col] == '1') {
-                    res++;
-                    areas.add(dfs(grid, row, col));
+        for (int row = 0; row < grids.length; row++) {
+            for (int col = 0; col < grids[0].length; col++) {
+                char state = grids[row][col];
+                
+                // an island
+                if (state == '1') {
+                    // marks as visited
+                    grids[row][col] = '2';
+                    count++;
+                    queue.add(new int[]{row, col});
+                   
+                    while (!queue.isEmpty()) {
+                        int[] curr = queue.poll();
+                        
+                        for (int[] d: directions) {
+                            int nextRow = curr[0] + d[0];
+                            int nextCol = curr[1] + d[1];
+
+                            if (nextRow >= 0 && nextRow < grids.length &&
+                            nextCol >= 0 && nextCol < grids[0].length) {
+                                // find neighbor valid island
+                                // will not be counted in the next iteration
+                                if (grids[nextRow][nextCol] == '1') {
+                                    grids[nextRow][nextCol] = '2';
+                                    queue.add(new int[]{nextRow, nextCol});
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
 
-        for (int area: areas) {
-            System.out.println(area);
-        }
-        return areas.size();
-    }
-
-    public int dfs(char[][] grid, int row, int col) {
-        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) {
-            return 0;
-        };
-
-        if (grid[row][col] == '0') {
-            return 0;
-        }
-
-        grid[row][col] = '0';
-        
-        return 1 + 
-        dfs(grid, row-1, col) + 
-        dfs(grid, row+1, col) + 
-        dfs(grid, row, col-1) +
-        dfs(grid, row, col+1);
+        return count;
     }
 }
