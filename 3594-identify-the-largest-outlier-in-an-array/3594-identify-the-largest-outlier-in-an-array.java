ced1113b2d1087d021483f1cb42e309690c983ba
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-28 15:52:55 -0800

    Time: 90 ms (62.55%), Space: 59.8 MB (40.15%) - LeetHub

--- 3594-identify-the-largest-outlier-in-an-array/3594-identify-the-largest-outlier-in-an-array.java
+++ 3594-identify-the-largest-outlier-in-an-array/3594-identify-the-largest-outlier-in-an-array.java
@@ -1,25 +1,25 @@
 class Solution {
     public int getLargestOutlier(int[] nums) {
+        int res = Integer.MIN_VALUE;
         int sum = 0;
         Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        for (int num: nums) {
+            sum += num;
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < nums.length; i++) {
-            int doubleSum = sum - nums[i];
-            int target = doubleSum / 2;
 
-            if (map.containsKey(target) && doubleSum % 2 == 0) {
-                if (target == nums[i]) {
-                    if (map.get(target) == 1) continue;
+        for (int num: nums) {
+            int doubleSum = sum - num;
+            if (doubleSum % 2 == 0 && map.containsKey(doubleSum / 2)) {
+                if (num == doubleSum / 2 && map.get(num) >= 2) {
+                    res = Math.max(res, num);
+                } else if (num != doubleSum / 2) {
+                    res = Math.max(res, num);
                 }
-                max = Math.max(max, nums[i]);
             }
         }
 
-        return max;
+        return res;
     }
 }
