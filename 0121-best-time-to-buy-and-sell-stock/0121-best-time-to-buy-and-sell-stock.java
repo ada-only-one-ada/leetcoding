9f22df541ab622deb90c0f9e43301791711a2c29
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-28 15:56:52 -0800

    Time: 2 ms (75.68%), Space: 62 MB (29.06%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,17 +1,20 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        // 低买高卖
-        if (prices.length == 1) return 0;
+        if (prices.length == 0) return 0;
 
-        int res = 0;
-        int min = prices[0];        
+        int res = Integer.MIN_VALUE;
 
-        // 注意不能更新max，因为不能保证max在min之后
+        // buy low sell high
+        int low = prices[0];
+
         for (int i = 1; i < prices.length; i++) {
-            res = Math.max(res, prices[i] - min);
-            min = Math.min(min, prices[i]);
+            res = Math.max(res, prices[i] - low);
+            low = Math.min(low, prices[i]);
         }
 
+        if (res < 0) {
+            return 0;
+        }
         return res;
     }
 }
