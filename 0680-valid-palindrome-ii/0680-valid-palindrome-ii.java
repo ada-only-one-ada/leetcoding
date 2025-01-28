6ef7f5b4757ce86619759699890d665db379c64e
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-28 12:48:08 -0800

    Time: 5 ms (23.29%), Space: 45.9 MB (6.31%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,18 +1,21 @@
 class Solution {
     public boolean validPalindrome(String s) {
+        if (helper(s)) return true;
+
         int i = 0;
         int j = s.length() - 1;
-        
+
         while (i < j) {
             if (s.charAt(i) != s.charAt(j)) {
-                return helper(s.substring(i, j)) || helper(s.substring(i + 1, j + 1));
-            } else {
-                i++;
-                j--;
-            }
+               return helper(s.substring(i + 1, j + 1)) || 
+                helper(s.substring(i, j));
+            } 
+
+            i++;
+            j--;
         }
 
-        return true;
+        return false;
     }
 
     public boolean helper(String s) {
