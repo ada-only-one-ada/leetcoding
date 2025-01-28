3f85ce24f2ee0a3d0541a6ca84a03f2d4cd44630
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-28 12:22:17 -0800

    Time: 2 ms (71.31%), Space: 42.9 MB (22.15%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,49 +1,64 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        Queue<int[]> queue = new LinkedList<>();
+       // if there is not rotten orange, but we have fresh, -1
+    int fresh = 0;
+    int rotten = 0;
 
-        int fresh = 0;
-        int rotten = 0;
-        for (int row = 0; row < grid.length; row++) {
-            for (int col = 0; col < grid[0].length; col++) {
-                if (grid[row][col] == 1) {
-                    fresh++;
-                } else if (grid[row][col] == 2) {
-                    rotten++;
-                    queue.add(new int[]{row, col});
-                }
+    int minutes = 0;
+    Queue<int[]> queue = new LinkedList<>();
+    int[][] directions = {{-1,0}, {1,0}, {0, -1}, {0, 1}};
+
+    for (int row = 0; row < grid.length; row++) {
+        for (int col = 0; col < grid[0].length; col++) {
+            int currOrange = grid[row][col];
+            if (currOrange == 1) {
+                fresh++;
+            } else if (currOrange == 2) {
+                /// we found a rotten orange, we can add it to the queue first 
+                rotten++;
+                queue.add(new int[]{row, col});
             }
         }
+    }
 
-        if (fresh == 0) return 0;
-        if (rotten == 0 && fresh != 0) return -1; 
+    // edge case:
+    // [2,2,2,2]
+    if (fresh > 0 && rotten == 0) return -1; 
+    
+    while (!queue.isEmpty()) {
+        // if there is not fresh orange, we quit the affected process.
+        if (fresh == 0) break;
 
-        int minutes = 0;
-        while (!queue.isEmpty()) {
-            if (fresh == 0) break;
-            minutes++;
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                int[] rottenPos = queue.poll();
-                int row = rottenPos[0];
-                int col = rottenPos[1];
+        // rotten organge row col index
+        int size = queue.size();
+        minutes++;
 
-                for (int[] dir: directions) {
-                    int nextRow = row + dir[0];
-                    int nextCol = col + dir[1];
-                    if (nextRow < 0 || nextCol < 0 || nextRow >= grid.length || nextCol >= grid[0].length) continue;
+        for (int i = 0; i < size; i++) {
+            int[] curr = queue.poll();
 
+            for (int[] direction: directions) {
+                int nextRow = curr[0] + direction[0];
+                int nextCol = curr[1] + direction[1];
+
+                if (nextRow >= 0 && nextCol >= 0 && nextRow < grid.length && nextCol < grid[0].length) {
+                    // your neighor is fresh
                     if (grid[nextRow][nextCol] == 1) {
-                        queue.add(new int[]{nextRow, nextCol});
-                        grid[nextRow][nextCol] = 2;
                         fresh--;
-                    }
+                        // turn into rotten
+                        grid[nextRow][nextCol] = 2;
+                        queue.add(new int[]{nextRow, nextCol});
+                    } 
+                    // if your neigbor is rotten already, means you are at the same level
                 }
             }
         }
+    }
 
-        if (fresh != 0) return -1;
-        return minutes;
+    // in the end, if you still have fresh orange, means it is not possible to affect all.
+    if (fresh > 0) return -1;
+
+    return minutes;
+
+    // time complexity: O(n^2), space complexity: O(n^2)
     }
 }
