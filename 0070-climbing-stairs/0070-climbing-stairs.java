127824c2b88c92bb33c2df1cbb1b6800dfbec39c
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-28 22:13:51 -0800

    Time: 0 ms (100%), Space: 40.7 MB (18.41%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -2,14 +2,16 @@ class Solution {
     public int climbStairs(int n) {
         if (n <= 2) return n;
 
-        int[] dp = new int[n + 1];
-        dp[1] = 1;
-        dp[2] = 2;
+        int prevPrev = 1;
+        int prev = 2;
 
         for (int i = 3; i <= n; i++) {
-            dp[i] += dp[i - 1] + dp[i - 2];
+            int temp = prev;
+
+            prev = prevPrev + prev;
+            prevPrev = temp;
         }
 
-        return dp[n];
+        return prev;
     }
 }
