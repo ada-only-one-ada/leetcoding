d5d9d72ed41311a2e010138ca9ab6018170ee475
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-28 17:56:34 -0800

    Time: 1 ms (100%), Space: 42.8 MB (92.92%) - LeetHub

--- 3684-substring-matching-pattern/3684-substring-matching-pattern.java
+++ 3684-substring-matching-pattern/3684-substring-matching-pattern.java
@@ -1,25 +1,27 @@
 class Solution {
     public boolean hasMatch(String s, String p) {
         // s = "l" and p = "*" return true
-        if (p.charAt(0) == '*') {
-            return s.contains(p.substring(1));
-        }
-
+        // 如果星号在最前面
+        if (p.charAt(0) == '*') return s.contains(p.substring(1));
+        // 如果星号在最后面
         if (p.charAt(p.length() - 1) == '*') {
             return s.contains(p.substring(0, p.length() - 1));
         }
 
+        // 找到星号位置
         int starIndex = p.indexOf("*");
-
         String before = p.substring(0, starIndex);
         String after = p.substring(starIndex + 1);
 
+        // 找到左边的substring
         int firstPartIndex = s.indexOf(before);
+        // 没找到左边，返回false
         if (firstPartIndex == -1) return false;
 
+        // 从左边以后的位置开始找右边的
         int secondPartIndex = s.indexOf(after, firstPartIndex + before.length());
 
         if (secondPartIndex == -1) return false;
-        return firstPartIndex < secondPartIndex;
+        return true;
     }
 }
