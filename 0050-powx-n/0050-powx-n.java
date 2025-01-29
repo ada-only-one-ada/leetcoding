0615696f9b7e3ec031906fa4d8427c86967cb54b
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-28 22:02:14 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,34 +1,13 @@
 class Solution {
     public double myPow(double x, int n) {
+        if (x == 1) return x;
         if (x == 0) return 0;
         if (n == 0) return 1;
-        if (n == 1 || x == 1) return x;
+        if (n == 1) return x;
+        if (n == -1) return 1/ x;
 
-        // 这个处理负次方的情况
-        if (n == -1) return 1/x;
+        double half = myPow(x, n / 2);
 
-        // 不要一直调用函数，一样的话存起来就好
-        double half = myPow(x, n/2);
-
-        if (n % 2 == 0) {
-            return half * half;
-        } else {
-            return half * half * myPow(x, n % 2);
-        }
+        return half * half * (n % 2 == 0? 1 : x);
     }
 }
-
-/*
-TLE case:
-x = 0.00001
-n = 2147483647
-
-StackOverflowError case:
-x = 1.00000
-n = -2147483648
-
-x = 2.00000
-n = -2147483648
-
-if (n < 0) return myPow(1/x, -n); -(min) will cause error        
-*/
