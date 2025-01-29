a6da86f346ab38116b1d18317b65eebc66a7129b
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-28 19:38:25 -0800

    Time: 19 ms (58.93%), Space: 44.8 MB (53.35%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -3,22 +3,17 @@ class Solution {
         String res = "";
 
         for (int i = 0; i < s.length(); i++) {
-            String res1 = check(s, i, i);
-            String res2 = check(s, i-1, i);
+            String res1 = validPalindrome(s, i, i);
+            String res2 = validPalindrome(s, i, i+1);
 
-            if (res1.length() > res.length()) {
-                res = res1;
-            } 
-
-            if (res2.length() > res.length()) {
-                res = res2;
-            }
+            if (res1.length() > res.length()) res = res1;
+            if (res2.length() > res.length()) res = res2;
         }
 
         return res;
     }
 
-    public String check(String s, int left, int right) {
+    public String validPalindrome(String s, int left, int right) {
         while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
             left--;
             right++;
