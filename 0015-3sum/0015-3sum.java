cd79683990297e9e500f2f6f015534e648c5d73c
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-28 22:21:38 -0800

    Time: 33 ms (38.34%), Space: 51.2 MB (96.6%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,16 +1,17 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
         
         int i1 = 0;
-        while (i1 <= nums.length - 1) {
+        while (i1 <= nums.length - 3) {
             int i2 = i1 + 1;
             int i3 = nums.length - 1;
+
             while (i2 < i3) {
                 int sum = nums[i1] + nums[i2] + nums[i3];
                 if (sum == 0) {
-                    res.add(new ArrayList<>(Arrays.asList(nums[i1], nums[i2], nums[i3])));
+                    res.add(new ArrayList(Arrays.asList(nums[i1], nums[i2], nums[i3])));
                     i2++;
                     i3--;
                     while (i2 < i3 && nums[i2] == nums[i2 - 1]) i2++;
@@ -23,7 +24,7 @@ class Solution {
             }
 
             i1++;
-            while (i1 <= nums.length - 3 && nums[i1] == nums[i1 - 1]) i1++; 
+            while (i1 <= nums.length - 3 && nums[i1] == nums[i1-1]) i1++;
         }
 
         return res;
