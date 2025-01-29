4388d3809c5265fef92921df79f2e248af43c0f0
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-28 16:37:21 -0800

    Time: 1 ms (95.42%), Space: 40.9 MB (97.03%) - LeetHub

--- 2436-make-array-zero-by-subtracting-equal-amounts/2436-make-array-zero-by-subtracting-equal-amounts.java
+++ 2436-make-array-zero-by-subtracting-equal-amounts/2436-make-array-zero-by-subtracting-equal-amounts.java
@@ -1,13 +1,20 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        for (int num: nums) {
-            if (num != 0) {
-                set.add(num);
-            }
+        // 选择小于等于nums里非0的最小数
+        // [0, 1, 3, 5, 5]
+        int res = 0;
+        Arrays.sort(nums);
 
+        // 之前减去的数要累加，因为每次减法影响都是累积的
+        int prev = 0;
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = nums[i] - prev;
+            if (nums[i] <= 0) continue;
+           
+            prev += nums[i];
+            res++;
         }
 
-        return set.size();
+        return res;
     }
 }
