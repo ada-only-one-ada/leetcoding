fe2e27847aa0818e54bcd341a843100349b57485
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-28 22:31:21 -0800

    Time: 11 ms (80.9%), Space: 44.9 MB (94.81%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,45 +1,34 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        // 这里的sum如果太大会造成溢出，要用long
-        // 我们也可以直接设置 max 给 end
-        // 最佳是设置 max(piles)
-        
-        int max = 0;
-        for (int pile: piles) {
-            max = Math.max(max, pile);
-        }
-        
-        int start = 1;
-        int end = max + 1;
-        while (start < end) {
-            int mid = start + (end - start) / 2;
+        int minSpeed = 1;
+        int maxSpeed = Integer.MAX_VALUE;
 
-            if (canFinish(piles, h, mid)) {
-                end = mid;
+        while (minSpeed < maxSpeed) {
+            int midSpeed = minSpeed + (maxSpeed - minSpeed) / 2;
+
+            if (canFinish(piles, h, midSpeed)) {
+                maxSpeed = midSpeed;
             } else {
-                start = mid + 1;
+                minSpeed = midSpeed + 1;
             }
         }
 
-        return start;
+        return minSpeed;
     }
 
-    public boolean canFinish(int[] piles, int h, int k) {
-        int hours = 0;
-        int i = 0;
-        while (i < piles.length) {
-            if (piles[i] <= k) {
-                hours++;
-                i++;
-            } else {
-                hours += piles[i] / k;
-                if (piles[i] % k != 0) {
-                    hours++;
-                }
-                i++;
+    public boolean canFinish(int[] piles, int h, int speed) {
+        int totalHours = 0;
+
+        for (int pile: piles) {
+            if (pile <= speed) {
+                totalHours++;
+            } else if (pile % speed == 0) {
+                totalHours += pile / speed;
+            } else if (pile % speed != 0) {
+                totalHours += pile / speed + 1;
             }
         }
 
-        return hours <= h;
+        return totalHours <= h;
     }
 }
