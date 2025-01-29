36e54a23153da647490e27b0d69ba77a1d0acc06
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-28 16:21:51 -0800

    Time: 8 ms (87.05%), Space: 46.9 MB (25.57%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,35 +1,32 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
+        if (intervals.length <= 1) return intervals;
+
         Arrays.sort(intervals, (a, b) -> {
-            if (a[0] != b[0]) return a[0] - b[0];
-            return a[1] - b[1];
+            if (a[0] == b[0]) {
+                return a[1] - b[1];
+            }
+            return a[0] - b[0];
         });
 
-        if (intervals.length <= 1) return intervals;
         List<int[]> list = new ArrayList<>();
-
         int prevStart = intervals[0][0];
         int prevEnd = intervals[0][1];
 
         for (int i = 1; i < intervals.length; i++) {
-            int currStart = intervals[i][0];
-            int currEnd = intervals[i][1];
-
-            if (currStart > prevEnd) {
+            if (intervals[i][0] > prevEnd) {
                 list.add(new int[]{prevStart, prevEnd});
-                prevStart = currStart;
-                prevEnd = currEnd;
-            } else {
-                prevEnd = Math.max(prevEnd, currEnd);
+                prevStart = intervals[i][0];
+                prevEnd = intervals[i][1];
+            } else if (intervals[i][0] <= prevEnd){
+                prevEnd = Math.max(prevEnd, intervals[i][1]);
             }
-        } 
+        }
 
         list.add(new int[]{prevStart, prevEnd});
-
         int[][] res = new int[list.size()][2];
-        int index = 0;
-        for (int[] interval: list) {
-            res[index++] = interval;
+        for (int i = 0; i < res.length; i++) {
+            res[i] = list.get(i);
         }
 
         return res;
