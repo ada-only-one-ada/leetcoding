de363f074b135e637750c8c986b64ef2b6ea9692
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-28 22:03:24 -0800

    Time: 0 ms (100%), Space: 43.7 MB (9.82%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -4,10 +4,10 @@ class Solution {
         if (x == 0) return 0;
         if (n == 0) return 1;
         if (n == 1) return x;
-        if (n == -1) return 1/ x;
+        if (n == -1) return 1 / x;
 
         double half = myPow(x, n / 2);
 
-        return half * half * (n % 2 == 0? 1 : x);
+        return half * half * (n % 2 == 0? 1 : myPow(x, n % 2));
     }
 }
