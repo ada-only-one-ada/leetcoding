3801ec3c286994afd9533c04180ac71b18a62f93
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-28 22:46:18 -0800

    Time: 0 ms (100%), Space: 42.1 MB (52.46%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,46 +1,42 @@
 class Solution {
-    public int search(int[] nums, int target) {        
-        int left = 0;
-        int right = nums.length - 1;
-        
-        while (left + 1 < right){
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target){
-                return mid;
-            }
-
-            /* normal binary search 
-            if (nums[left] < nums[right]){
-                if (nums[mid] < target){
-                    left = mid;
-                }else if (nums[mid] > target){
+    public int search(int[] nums, int target) {
+        if (nums[0] <= nums[nums.length - 1]) {
+            int left = 0;
+            int right = nums.length;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] == target) {
+                    return mid;
+                } else if (nums[mid] < target) {
+                    left = mid + 1;
+                } else {
                     right = mid;
                 }
             }
-            */
+            return -1;
+        } else {
+            int left = 0;
+            int right = nums.length;
 
-            // rotated binary search 
-            // mid to right: increasing
-            if (nums[mid] < nums[right]){
-                if (nums[mid] < target && target <= nums[right]){
-                    left = mid;
-                }else{
-                    right = mid;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] == target) {
+                    return mid;
+                } else if (nums[0] <= nums[mid]) {
+                    if (nums[0] <= target && target < nums[mid]) {
+                        right = mid;
+                    } else {
+                        left = mid + 1;
+                    }
+                } else if (nums[mid] <= nums[nums.length - 1]) {
+                    if (nums[mid] < target && target <= nums[nums.length - 1]) {
+                        left = mid + 1;
+                    } else {
+                        right = mid;
+                    }
                 }
             }
-
-            // left to mid: increasing
-            if (nums[left] < nums[mid]){
-                if (nums[left] <= target && target < nums[mid]){
-                    right = mid;
-                }else{
-                    left = mid;
-                }
-            }
+            return -1;
         }
-
-        if (nums[left] == target) return left;
-        if (nums[right] == target) return right;
-        return -1;
     }
 }
