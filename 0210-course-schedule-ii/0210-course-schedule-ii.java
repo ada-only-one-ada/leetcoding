3ccb7e3fd3403ce89a2cfc6f1129374e1af3b4c6
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-28 20:07:30 -0800

    Time: 7 ms (36.88%), Space: 45.9 MB (60.89%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,37 +1,34 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
         int[] indegree = new int[numCourses];
-        Map<Integer, Set<Integer>> map = new HashMap<>();
+        int[] res = new int[numCourses];
 
-        for (int[] p: prerequisites) {
-            int toTake = p[0];
-            int mustTake = p[1];
+        for (int[] req: prerequisites) {
+            int toTake = req[0];
+            int mustTake = req[1];
 
-            map.putIfAbsent(mustTake, new HashSet<>());
+            map.putIfAbsent(mustTake, new ArrayList<>());
             map.get(mustTake).add(toTake);
             indegree[toTake]++;
         }
 
         Queue<Integer> queue = new LinkedList<>();
-        for (int i = 0; i < numCourses; i++) {
+        for (int i = 0; i < indegree.length; i++) {
             if (indegree[i] == 0) {
                 queue.add(i);
             }
         }
-
-        int[] res = new int[numCourses];
         int index = 0;
         while (!queue.isEmpty()) {
             int currTaking = queue.poll();
             res[index++] = currTaking;
 
-            Set<Integer> set = map.get(currTaking);
-            if (set == null) continue;
-
-            for (int nextCourse: set) {
-                indegree[nextCourse]--;
-                if (indegree[nextCourse] == 0) {
-                    queue.add(nextCourse);
+            if (!map.containsKey(currTaking) || map.get(currTaking) == null) continue;
+            for (int nextTaking: map.get(currTaking)) {
+                indegree[nextTaking]--;
+                if (indegree[nextTaking] == 0) {
+                    queue.add(nextTaking);
                 }
             }
         }
