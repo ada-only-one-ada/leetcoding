b9ed9900c592ebc376443166e1d55d4141e68ed5
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-28 16:32:48 -0800

    Time: 148 ms (50.91%), Space: 41.7 MB (48.98%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -3,32 +3,38 @@ class Solution {
         for (int row = 0; row < board.length; row++) {
             for (int col = 0; col < board[0].length; col++) {
                 if (board[row][col] == word.charAt(0)) {
-                    if (backtracking(board, word, 0, row, col)) {
+                    if (dfs(board, word, 0, row, col)) {
                         return true;
                     }
                 }
             }
-        }
+        }   
 
         return false;
     }
 
-    public boolean backtracking(char[][] board, String word, int index, int row, int col) {
-        if (index == word.length()) return true;
-        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length) return false;
-        if (board[row][col] != word.charAt(index)) return false;
-        // 注意有可能会走回头路
-        if (board[row][col] == '!') return false;
-     
-        char temp = board[row][col];
-        board[row][col] = '!';
-        
-        boolean res = backtracking(board, word, index + 1, row - 1 , col) || 
-               backtracking(board, word, index + 1, row + 1, col) || 
-               backtracking(board, word, index + 1, row, col - 1) ||
-               backtracking(board, word, index + 1, row, col + 1);
-        board[row][col] = temp;
+    public boolean dfs(char[][] board, String word, int index, int row, int col) {
+        if (index == word.length()) {
+            return true;
+        }
 
-        return res;
+        if (row < 0 || col < 0 || row == board.length || col == board[0].length) {
+            return false;
+        }
+
+        if (board[row][col] == word.charAt(index)) {
+            board[row][col] = '-';
+
+            boolean res = dfs(board, word, index + 1, row - 1, col) || 
+            dfs(board, word, index + 1, row + 1, col) ||
+            dfs(board, word, index + 1, row, col - 1) ||
+            dfs(board, word, index + 1, row, col + 1);
+
+            if (res) return res;
+            board[row][col] = word.charAt(index);
+
+        }
+
+        return false;
     }
 }
