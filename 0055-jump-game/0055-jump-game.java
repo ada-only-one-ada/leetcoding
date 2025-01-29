04644f4e7852030057add41adb2450401a65ca31
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-28 19:57:02 -0800

    Time: 2 ms (79.46%), Space: 45.8 MB (55.24%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,13 +1,15 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        if (nums.length == 0) return false;
+        if (nums.length <= 1) return true;
 
-        int maxFar = nums[0];
-        int currPos = 0;
-       
-        for (int i = currPos; i <= maxFar; i++) {
-            if (maxFar >= nums.length - 1) return true;
-            maxFar = Math.max(maxFar, i + nums[i]);
+        int currFar = nums[0];
+        int nextMostFar = nums[0];
+
+        for (int i = 0; i <= currFar; i++) {
+            nextMostFar = Math.max(nextMostFar, nums[i] + i);
+
+            if (nextMostFar >= nums.length - 1) return true;
+            currFar = nextMostFar;
         }
 
         return false;
