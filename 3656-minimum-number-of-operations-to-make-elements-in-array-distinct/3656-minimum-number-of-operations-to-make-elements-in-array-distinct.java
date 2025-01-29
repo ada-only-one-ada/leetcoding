6e1804aa832e888c8f38a11cece62083f05e8bee
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-28 17:10:32 -0800

    Time: 2 ms (82.53%), Space: 44.6 MB (47.7%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.java
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.java
@@ -1,50 +1,18 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int num: nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
-        if (map.size() == nums.length) return 0;
-        int operations = 0;
+        // 从后往前找，一旦我们找到重复的，意味着前面的全部都要删掉
 
-        int removeIndex = 0;
-        while (map.size() != nums.length - removeIndex && removeIndex < nums.length) {
-            if (removeIndex + 2 < nums.length) {
-                int removeNum = nums[removeIndex];
-                int removeNum1 = nums[removeIndex + 1];
-                int removeNum2 = nums[removeIndex + 2];
+        Set<Integer> set = new HashSet<>();
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (set.contains(nums[i])) {
+                int numToDelete = i + 1;
 
-                map.put(removeNum, map.getOrDefault(removeNum, 0) -  1);
-                if (map.get(removeNum) <= 0) map.remove(removeNum);
-
-                map.put(removeNum1, map.getOrDefault(removeNum1, 0) -  1);
-                if (map.get(removeNum1) <= 0) map.remove(removeNum1);
-
-                map.put(removeNum2, map.getOrDefault(removeNum2, 0) -  1);
-
-                if (map.get(removeNum2) <= 0) map.remove(removeNum2);
-                removeIndex += 3;
-            } else if (removeIndex + 1 < nums.length) {
-                int removeNum = nums[removeIndex];
-                int removeNum1 = nums[removeIndex + 1];
-
-                map.put(removeNum, map.getOrDefault(removeNum, 0) -  1);
-                if (map.get(removeNum) <= 0) map.remove(removeNum);
-
-                map.put(removeNum1, map.getOrDefault(removeNum1, 0) -  1);
-                if (map.get(removeNum1) <= 0) map.remove(removeNum1);
-                removeIndex += 2;
-            } else if (removeIndex < nums.length) {
-                int removeNum = nums[removeIndex];
-        
-                map.put(removeNum, map.getOrDefault(removeNum, 0) -  1);
-               
-                if (map.get(removeNum) <= 0) map.remove(removeNum);
-                removeIndex += 1;
+                return numToDelete / 3 + (numToDelete % 3 == 0? 0:1);
             }
-            operations++;
+
+            set.add(nums[i]);
         }
 
-        return operations;
+        return 0;
     }
 }
