b95ec3e6722d8b43b47acbfa96d91dbd5828a7b1
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-28 22:38:17 -0800

    Time: 1 ms (84.31%), Space: 41.4 MB (15.85%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,21 +1,17 @@
 class Solution {
     public int reverse(int x) {
-        int sign = 1;
         if (x < 0) {
-            sign = -1;
-            x = -x;
+            if (x <= Integer.MIN_VALUE) return 0;
+            return -reverse(-x);
         }
+        int res = 0;
 
-        int MAX = Integer.MAX_VALUE; // 2,147,483,647
-
-        int sum = 0;
         while (x > 0) {
-            // if (sum > MAX / 10 || (sum == MAX / 10 && x % 10 > 7)) return 0;
-            if (sum > MAX / 10) return 0;
-            sum = sum * 10 + (x % 10);
-            x /= 10;
+            if (res > Integer.MAX_VALUE / 10 || res < Integer.MIN_VALUE / 10 ) return 0;
+            res = res * 10 + x % 10;
+            x = x / 10;
         }
 
-        return sum * sign;
+        return res;
     }
 }
