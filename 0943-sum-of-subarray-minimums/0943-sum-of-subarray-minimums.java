65fbfbe6a3cd9deb79aa5efb21d42c543df0d0f0
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-28 21:23:29 -0800

    Time: 588 ms (5.01%), Space: 50.4 MB (75.45%) - LeetHub

--- 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.java
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.java
@@ -1,27 +1,19 @@
 class Solution {
     public int sumSubarrayMins(int[] arr) {
-        int[] nums = new int[arr.length + 2];
-        nums[0] = nums[nums.length - 1] = Integer.MIN_VALUE;
-        for (int i = 0; i < arr.length; i++) {
-            nums[i + 1] = arr[i];
-        }
-
         long res = 0;
 
-        Stack<Integer> stack = new Stack();
-        for (int i = 0; i < nums.length; i++) {
-            if (stack.size() < 2 || nums[i] >= nums[stack.peek()]) {
-                stack.push(i);
-            } else {
-                while (stack.size() >= 2 && nums[i] < nums[stack.peek()]) {
-                    int right = i;
-                    int mid = stack.pop();
-                    int left = stack.peek();
-
-                    res += (long)(right - mid) * (mid - left) * nums[mid];
-                }
-                stack.push(i);
+        for (int i = 0; i < arr.length; i++) {
+            int left = i - 1;
+            while (left >= 0 && arr[left] >= arr[i]) {
+                left--;
             }
+            
+            int right = i + 1;
+            while (right < arr.length && arr[right] > arr[i]) {
+                right++;
+            }
+
+            res += (long)arr[i] * (i - left) * (right - i);
         }
 
         return (int)(res % 1000000007);
