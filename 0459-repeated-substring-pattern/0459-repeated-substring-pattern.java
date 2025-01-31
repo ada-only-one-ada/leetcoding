e7e2fb1e0f0a7b76700d6201355f66447134a166
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-01-31 11:51:35 -0800

    Time: 45 ms (58.66%), Space: 45.5 MB (28.67%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -2,9 +2,12 @@ class Solution {
     public boolean repeatedSubstringPattern(String s) {
         if (s.length() < 2) return false;
 
+        // pattern must repeat at least twice, i.e. pattern length is at most n/2, at least 1.
+
         for (int i = 0; i < s.length() / 2; i++) {
             String substring = s.substring(0, i + 1);
 
+            // s length must can be divided by the pattern length
             if (s.length() % substring.length() != 0) continue;
             int times = s.length() / substring.length();
 
