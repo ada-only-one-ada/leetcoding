365e9d8344da84dd3943df037a5f6f1540072149
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-03 14:43:40 -0800

    Time: 23 ms (51.67%), Space: 47 MB (17.65%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -9,22 +9,22 @@ class Solution {
 
             if (Character.isDigit(c)) {
                 num = num * 10 + c - '0';
-            } 
-                if (i == s.length() - 1) {
+            }
+            
+            if (i == s.length() - 1) {
                     if (prevSign == '+') stack.push(num);
                     else if (prevSign == '-') stack.push(-num);
                     else if (prevSign == '*') stack.push(stack.pop() * num);
                     else if (prevSign == '/') stack.push(stack.pop() / num);
 
-                } else if (c != ' ' && (c == '+' || c == '-' || c == '*' || c == '/')) {
+            } else if (c != ' ' && (c == '+' || c == '-' || c == '*' || c == '/')) {
                     if (prevSign == '+') stack.push(num);
                     else if (prevSign == '-') stack.push(-num);
                     else if (prevSign == '*') stack.push(stack.pop() * num);
                     else if (prevSign == '/') stack.push(stack.pop() / num);
                     prevSign = c;
                     num = 0;
-                }
-            
+            }
         }
 
         int sum = 0;
