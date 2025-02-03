a390fe66e320f9a671dece1a7e5ad1ff74457da4
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-02 21:50:14 -0800

    Time: 1 ms (99.38%), Space: 42.5 MB (79.92%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -1,41 +1,36 @@
 class Solution {
     public int longestValidParentheses(String s) {
         int res = 0;
+
         int open = 0;
         int close = 0;
-        
-        for (int i = 0; i < s.length(); i++) {
-            char curr = s.charAt(i);
-
-            if (curr == '(') {
-                open++;
-            } else if (curr == ')') {
-                close++;
-            }
+        for (char c: s.toCharArray()) {
+            if (c == '(') open++;
+            else if (c == ')') close++;
 
             if (close > open) {
                 open = 0;
                 close = 0;
-            } else if (open == close) {
+            }
+
+            if (open == close) {
                 res = Math.max(res, open + close);
-            } 
+            }
         }
 
+        // (()
         open = 0;
         close = 0;
         for (int i = s.length() - 1; i >= 0; i--) {
-            char curr = s.charAt(i);
+            if (s.charAt(i) == ')') close++;
+            else if (s.charAt(i) == '(') open++;
 
-            if (curr == '(') {
-                open++;
-            } else if (curr == ')') {
-                close++;
-            }
-
-            if (open > close ) {
+            if (open > close) {
                 open = 0;
                 close = 0;
-            } else if (open == close ) {
+            }
+
+            if (open == close) {
                 res = Math.max(res, open + close);
             }
         }
