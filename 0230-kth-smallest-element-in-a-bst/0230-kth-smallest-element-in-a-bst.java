ae89f2e1c462e27da0ab8ee4fa601ec33b6e6e0a
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-03 13:49:35 -0800

    Time: 1 ms (33.92%), Space: 44.1 MB (96.8%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -1,25 +1,25 @@
 class Solution {
-    int count = 0;
-    int k;
-    int res = 0;
-    boolean found = false;  // 新增一个标志位，用于指示是否已找到第k小的元素
-
     public int kthSmallest(TreeNode root, int k) {
-        this.k = k;
-        dfs(root);
-        return res;
-    }
+        Stack<TreeNode> stack = new Stack();
+        int count = 0;
 
-    public void dfs(TreeNode root) {
-        if (root == null || found) return;  // 如果节点为空或者已找到，直接返回
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            if (curr != null) {
+                stack.push(curr);
+                curr = curr.left; 
+            } else if (!stack.isEmpty()) {
+                TreeNode node = stack.pop();
+                count++;
 
-        dfs(root.left);
-        count++;
-        if (count == k) {
-            res = root.val;
-            found = true;  // 找到后将标志位设为true，后续递归调用会直接返回
-            return;
+                if (count == k) return node.val;
+
+                if (node.right != null) {
+                    curr = node.right;
+                }
+            }
         }
-        dfs(root.right);
+
+        return -1;
     }
 }
