6232057f2a77f92884104347cd633353865bf25a
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-03 15:51:05 -0800

    Time: 0 ms (100%), Space: 40.4 MB (63.36%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,16 +1,16 @@
 class Solution {
-    Map<Integer, Integer> memo = new HashMap<>();
-
     public int fib(int n) {
-        if (n == 0 || n == 1) return n;
+        if (n <= 1) return n;
 
-        if (memo.containsKey(n)) {
-            return memo.get(n);
+        int[] dp = new int[2];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            int temp = dp[1];
+            dp[1] = dp[0] + dp[1];
+            dp[0] = temp;
         }
 
-        int res = fib(n - 2) + fib(n - 1);
-        memo.put(n, res);
-
-        return res;
-    }   
+        return dp[1];
+    }
 }
