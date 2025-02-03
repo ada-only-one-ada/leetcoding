a02aeae5d75330d0590a5803aff3cc3e25b29c33
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-03 09:42:17 -0800

    Time: 1 ms (100%), Space: 44.4 MB (82.11%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -3,27 +3,28 @@ class Solution {
         ListNode dummy = new ListNode(-1);
         ListNode curr = dummy;
 
-        int prev = 0;
+        int remain = 0;
+
         while (l1 != null || l2 != null) {
             int num1 = 0;
             int num2 = 0;
-            
             if (l1 != null) {
                 num1 = l1.val;
                 l1 = l1.next;
             }
-
             if (l2 != null) {
                 num2 = l2.val;
                 l2 = l2.next;
             }
 
-            curr.next = new ListNode((num1 + num2 + prev) % 10);
-            prev = (num1 + num2 + prev) / 10;
+            int sum = remain + num1 + num2;
+            curr.next = new ListNode(sum % 10);
+            remain = sum / 10;
             curr = curr.next;
+           
         }
 
-        if (prev != 0) {
+        if (remain != 0) {
             curr.next = new ListNode(1);
         }
 
