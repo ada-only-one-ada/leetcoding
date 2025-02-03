aabe5071eac127e2b9d3698482ad1d0c70c7779e
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-03 09:59:26 -0800

    Time: 51 ms (8.62%), Space: 45.7 MB (65.1%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -5,39 +5,40 @@ class Solution {
             freq[task - 'A']++;
         }
 
-        Queue<Integer> queue = new PriorityQueue<>((a, b) -> (b - a));
-        for (int count: freq) {
-            if (count != 0) {
-                queue.add(count);
-            }
-        }
-        
-        int timeUsed = 0;
-        while (!queue.isEmpty()) {
-            List<Integer> wl = new ArrayList<>();
-            
-            // Try to schedule up to n + 1 tasks within one interval
-            for (int i = 0; i <= n; i++) {
-                // 这里如果到了最后一个task，那么做完以后，即使这个loop还没有结束，因为wl是空的，queue也是空的，直接退出就好了
-                // Break if no more tasks
-                if (queue.isEmpty() && wl.isEmpty()) break;
-                
-                if (!queue.isEmpty()) {
-                    int task = queue.poll();   
-                    // Add to waiting list if still remaining
-                    if (task - 1 > 0) {
-                        wl.add(task - 1);
-                    }
-                } 
-                timeUsed++;
-            }
-            
-            // Add waiting tasks back to queue
-            for (int task: wl) {
-                queue.add(task);
+        Queue<Integer> queue = new PriorityQueue<>((a, b) -> {
+            return freq[b] - freq[a];
+        });
+
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > 0) {
+                queue.add(i);
             }
         }
 
-        return timeUsed;
+        StringBuilder sb = new StringBuilder();
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> wl = new ArrayList<>();
+
+            for (int i = 0; i < n + 1; i++) {
+                if (queue.isEmpty() && wl.size() == 0) break;
+                if (!queue.isEmpty()) {
+                    int currTask = queue.poll();
+                    sb.append("T");
+                    freq[currTask]--;
+                    if (freq[currTask] > 0) wl.add(currTask);
+                } else {
+                    sb.append("I");
+                }
+            }
+
+            for (int t: wl) {
+                queue.add(t);
+            }
+        }
+
+        System.out.println(sb.toString());
+
+        return sb.length();
     }
 }
