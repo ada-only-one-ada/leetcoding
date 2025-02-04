c90e75a3e1b5065f516ee05f1c171ae4518aeb98
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-03 18:06:57 -0800

    Time: 0 ms (100%), Space: 42 MB (40.87%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -8,7 +8,9 @@ class Solution {
         if (left == null && right == null) return true;
         if (left == null || right == null) return false;
 
-        if (left.val != right.val) return false;
+        if (left.val != right.val) {
+            return false;
+        }
 
         return helper(left.left, right.right) && helper(left.right, right.left);
     }
