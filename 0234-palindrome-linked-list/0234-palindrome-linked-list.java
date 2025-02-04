ceb19c0b5380ee2d2cd4a7f863c81f25a1712f4a
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-04 13:35:45 -0800

    Time: 13 ms (17.51%), Space: 55.6 MB (99.55%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        StringBuilder sb = new StringBuilder();
+        while (head != null) {
+            sb.append(head.val);
+            head = head.next;
+        }
+
+        int left = 0;
+        int right = sb.length() - 1;
+        while (left < right) {
+            if (sb.charAt(left) != sb.charAt(right)) return false;
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
