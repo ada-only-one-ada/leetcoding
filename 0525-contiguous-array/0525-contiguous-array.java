d2d3a384bac0c35d806eca95f001fefe2cc3a9dd
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-04 15:45:50 -0800

    Time: 26 ms (14.54%), Space: 55.1 MB (71.99%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.java
+++ 0525-contiguous-array/0525-contiguous-array.java
@@ -1,25 +1,24 @@
 class Solution {
     public int findMaxLength(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        map.put(0, -1); // [0, 1, 1], [0, 1]
-        int maxLen = 0;
+        Map<Integer, Integer> sumToIndex = new HashMap<>();
+        sumToIndex.put(0, -1);
 
-        // [2, xxxxx, 2]: prefix 为 2 出现了两次，那么中间肯定是有0有1数量相等抵消了
-        int prefixSum = 0;
+        int res = 0;
+        int sum = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == 0) {
-                prefixSum--;
-            } else if (nums[i] == 1) {
-                prefixSum++;
+                sum--;
+            } else {
+                sum++;
             }
 
-            if (map.containsKey(prefixSum)) {
-                maxLen = Math.max(maxLen, i - map.get(prefixSum));
+            if (sumToIndex.containsKey(sum)) {
+                res = Math.max(res, i - sumToIndex.get(sum));
             } else {
-                map.put(prefixSum, i);
-            } 
+                sumToIndex.put(sum, i);
+            }
         }
 
-        return maxLen;
+        return res;
     }
 }
