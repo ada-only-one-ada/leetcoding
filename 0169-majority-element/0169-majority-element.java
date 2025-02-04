b10d690d599643e7b8aa68f8ac3d7e6d6442aeb9
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-03 22:57:29 -0800

    Time: 1 ms (99.8%), Space: 53.3 MB (29.48%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,6 +1,20 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length / 2];
+        int candidate = nums[0];
+        int count = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == candidate) {
+                count++;
+            } else {
+                count--;
+                if (count == -1) {
+                    candidate = nums[i];
+                    count = 1;
+                }
+            }
+        }
+
+        return candidate;
     }
 }
