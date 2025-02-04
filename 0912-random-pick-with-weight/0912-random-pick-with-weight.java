e03881dd4ae1c21f08a9ec38843240c810f97fb7
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-04 14:52:10 -0800

    Time: 82 ms (13.96%), Space: 50.5 MB (5.44%) - LeetHub

--- 0912-random-pick-with-weight/0912-random-pick-with-weight.java
+++ 0912-random-pick-with-weight/0912-random-pick-with-weight.java
@@ -1,42 +1,25 @@
 class Solution {
-    // 注意，Random是一个比较重的类，全局保留一个实例即可
-    // 或者使用 Math.random() 或 ThreadLocalRandom.nextInt() 方法
-    Random random;
     int[] prefixSum;
+    int[] w;
 
     public Solution(int[] w) {
-        random = new Random();
-      
+        this.w = w;
+        // [1, 3, 6] -> [1, 4, 10]
         prefixSum = new int[w.length];
         prefixSum[0] = w[0];
         for (int i = 1; i < w.length; i++) {
-            prefixSum[i] = prefixSum[i - 1] + w[i];
+            prefixSum[i] = prefixSum[i-1] + w[i];
         }
     }
     
-    /*
-    w = [1, 2, 3, 4]
-    prefixSum = [1, 3, 6, 10]
-    maxRange: 10
-    randNum is in range -> [0, 10) + 1 -> [1, 11) -> [1, 10]
-    */
     public int pickIndex() {
-        int maxRange = prefixSum[prefixSum.length - 1];
-        int randNum = random.nextInt(maxRange) + 1;
+        int maxNum = prefixSum[prefixSum.length - 1];
+        Random rand = new Random();
 
-        /*
-        randNum: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-        index: 0, 1, 2, 3
-        
-        判断randNum的范围落在哪个区间
-        randNum <= 1: 1 (prefix：1)
-        randNum <= 3: 2, 3 (prefix：3)
-        randNum <= 6: 4, 5, 6 (prefix：6)
-        randNum <= 10: 7, 8, 9, 10 (prefix：10)
-        */        
-        for (int index = 0; index < prefixSum.length; index++) {
-            if (randNum <= prefixSum[index]) {
-                return index;
+        int randNum = rand.nextInt(maxNum) + 1;
+        for (int i = 0; i < prefixSum.length; i++) {
+            if (randNum <= prefixSum[i]) {
+                return i;
             }
         }
 
