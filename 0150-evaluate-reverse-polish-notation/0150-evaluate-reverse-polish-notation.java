e7830540afae88ee1240359aa62c423429fec70f
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-03 18:21:08 -0800

    Time: 7 ms (28.96%), Space: 44.7 MB (52.51%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,29 +1,27 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        // 只包含：+ - * / 和数字[-200, 200]
+        Stack<Integer> stack = new Stack();
 
-        Stack<Integer> stack = new Stack<>();
         for (int i = 0; i < tokens.length; i++) {
-            String currToken = tokens[i];
+            String token = tokens[i];
 
-            if (currToken.equals("+") || currToken.equals("-") || currToken.equals("*") || currToken.equals("/")) {
+            if (!token.equals("+") && !token.equals("-") && !token.equals("*") && !token.equals("/")) {
+                stack.push(Integer.valueOf(token));
+            } else {
                 int num2 = stack.pop();
                 int num1 = stack.pop();
-
-                if (currToken.equals("+")) {
-                    stack.push(num1 + num2);
-                } else if (currToken.equals("-")) {
-                    stack.push(num1 - num2);
-                } else if (currToken.equals("*")) {
-                    stack.push(num1 * num2);
-                } else if (currToken.equals("/")) {
-                    stack.push(num1 / num2);
-                }
-            } else {
-                stack.push(Integer.valueOf(currToken));
+                if (token.equals("+")) stack.push(num1 + num2);
+                if (token.equals("-")) stack.push(num1 - num2);
+                if (token.equals("*")) stack.push(num1 * num2);
+                if (token.equals("/")) stack.push(num1 / num2);
             }
         }
 
-        return stack.peek();
+        int sum = 0;
+        while (!stack.isEmpty()) {
+            sum += stack.pop();
+        }
+
+        return sum;
     }
 }
