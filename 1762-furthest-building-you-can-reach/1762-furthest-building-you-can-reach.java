65b7fd2e81a74ab808752e23eedb96beeae7b008
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-04 13:30:05 -0800

    Time: 12 ms (97.52%), Space: 60.6 MB (5.5%) - LeetHub

--- 1762-furthest-building-you-can-reach/1762-furthest-building-you-can-reach.java
+++ 1762-furthest-building-you-can-reach/1762-furthest-building-you-can-reach.java
@@ -22,7 +22,7 @@ class Solution {
                 } else if (ladders > 0) {
                     if (!queue.isEmpty() && queue.peek() > diff) {
                         bricks += queue.poll();
-                        ladders -= 1;
+                        ladders -= 1; // 注意这里不要立刻前进，因为还要继续判断当前需要的砖块是否小于queue里面的
                         //bricks -= diff;
                        // currIndex++;
                     } else {
