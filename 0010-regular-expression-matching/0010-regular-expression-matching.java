9b76ecea444bd4ff7a907b35e7130a33902e74d8
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-04 10:41:39 -0800

    Time: 2 ms (64.29%), Space: 42 MB (78.72%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,17 +1,20 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        int sLen = s.length();
+        int pLen = p.length();
+
+        boolean[][] dp = new boolean[sLen + 1][pLen + 1];
         dp[0][0] = true;
 
-        for (int j = 1; j < p.length() + 1; j++) {
-            if (p.charAt(j-1) == '*') {
-                // ...*a,那么*前一个肯定可以被*吸收，关键看再前面的一个
+        for (int j = 1; j < pLen + 1; j++) {
+            char c = p.charAt(j-1);
+            if (c == '*') {
                 dp[0][j] = dp[0][j-2];
-            }
+            } 
         }
 
-        for (int i = 1; i < s.length() + 1; i++) {
-            for (int j = 1; j < p.length() + 1; j++) {
+        for (int i = 1; i < sLen + 1; i++) {
+            for (int j = 1; j < pLen + 1; j++) {
                 char c1 = s.charAt(i-1);
                 char c2 = p.charAt(j-1);
 
@@ -19,10 +22,8 @@ class Solution {
                     dp[i][j] = dp[i-1][j-1];
                 } else if (c2 == '*') {
                     char prevChar = p.charAt(j-2);
-                    // preChar匹配s当前字符，可选择匹配多次（dp[i-1][j]）或零次（dp[i][j-2]）
                     if (prevChar == c1 || prevChar == '.') {
-                        dp[i][j] = dp[i-1][j] || dp[i][j-2];
-                    // preChar不匹配，只能选择匹配零次，即跳过preChar和*
+                        dp[i][j] = dp[i][j-2] || dp[i-1][j];
                     } else {
                         dp[i][j] = dp[i][j-2];
                     }
@@ -30,6 +31,6 @@ class Solution {
             }
         }
 
-        return dp[s.length()][p.length()];
+        return dp[sLen][pLen];
     }
 }
