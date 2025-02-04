22e79f8856045bfd36f9459a46f7ad1b9b50fd17
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-04 11:21:55 -0800

    Time: 0 ms (100%), Space: 57.2 MB (76.18%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,10 +1,7 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        // O(1) space
-        // [1,2,3,4,5,6,7]
-        // [7,6,5,4,3,2,1]
-        // [5,6,7][1,2,3,4]
         k = k % nums.length;
+
         reverse(nums, 0, nums.length - 1);
         reverse(nums, 0, k - 1);
         reverse(nums, k, nums.length - 1);
@@ -15,7 +12,6 @@ class Solution {
             int temp = nums[start];
             nums[start] = nums[end];
             nums[end] = temp;
-
             start++;
             end--;
         }
