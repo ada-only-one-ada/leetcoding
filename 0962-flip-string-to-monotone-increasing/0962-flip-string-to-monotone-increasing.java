0e37d48babcfa35ec00e0c503aa87f9551cddba8
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-04 15:12:08 -0800

    Time: 7 ms (96.72%), Space: 45.3 MB (77.75%) - LeetHub

--- 0962-flip-string-to-monotone-increasing/0962-flip-string-to-monotone-increasing.java
+++ 0962-flip-string-to-monotone-increasing/0962-flip-string-to-monotone-increasing.java
@@ -1,25 +1,15 @@
 class Solution {
     public int minFlipsMonoIncr(String s) {
-        // edge case: 1001110011
-
-        /* 
-          当遍历到0 选择保留 那么之前的1设为0 选择不保留 反转次数+1 取两者较小值即可。
-
-          根据题意，0左边不能有任何1，比如：0000111
-          当前字符是0，保留这个0，那么之前的1都要反转；
-          当前字符是0，不保留这个0，即反转这个0变成1，
-          两者取小值
-        */
-
         int res = 0;
 
         int ones = 0;
-        for (char num: s.toCharArray()) {
-            if (num == '1') {
+        for (char c: s.toCharArray()) {
+            if (c == '0') {
+                // 保留 0，之前的 1 都要flip
+                // 不保留，反转加1
+                res = Math.min(ones, res + 1);
+            } else {
                 ones++;
-            } else if (num == '0') {
-                // res 是之前都是妥善处理过的string，加上当前这次反转0为1
-                res = Math.min(res + 1, ones);   
             }
         }
 
