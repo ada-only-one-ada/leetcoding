d18d9d2c7bf9428b5bd1e62a6ac9852c882395cf
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-05 10:33:20 -0800

    Time: 2 ms (89.84%), Space: 45.2 MB (15.71%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,56 @@
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> res = new ArrayList<>();
+        char[][] board = new char[n][n];
+        for (char[] row: board) {
+            Arrays.fill(row, '.');
+        }
+        backtracking(board, 0, res);
+        return res;
+    }
+
+    // 找到了一个solution，收集每一行的string，生成一个copy的solution，是给下面的方法辅助的
+    public List<String> collectOneSolution(char[][] board) {
+        List<String> res = new ArrayList<>();
+
+        for (char[] row: board) {
+            res.add(new String(row));
+        }
+
+        return res;
+    }
+
+    // backtracking核心方法
+    public void backtracking(char[][] board, int row, List<List<String>> res) {
+        if (row == board.length) {
+            res.add(collectOneSolution(board));
+            return;
+        }
+
+        for (int col = 0; col < board[0].length; col++) {
+            if (isValid(board, row, col)) {
+                board[row][col] = 'Q';
+                backtracking(board, row + 1, res);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    // 检查摆放是否合规，给上面的方法辅助用的
+    public boolean isValid(char[][] board, int row, int col) {
+        for (int aboveRow = row - 1; aboveRow >= 0; aboveRow--) {
+            if (board[aboveRow][col] == 'Q') return false;
+        }
+
+        for (int aboveRow = row - 1, leftCol = col - 1; aboveRow >= 0 && leftCol >= 0; aboveRow--, leftCol--) {
+            if (board[aboveRow][leftCol] == 'Q') return false;
+        }
+
+                for (int aboveRow = row - 1, rightCol = col + 1; aboveRow >= 0 && rightCol < board[0].length; aboveRow--, rightCol++) {
+            if (board[aboveRow][rightCol] == 'Q') return false;
+        }
+
+        return true;
+
+    }
+}
