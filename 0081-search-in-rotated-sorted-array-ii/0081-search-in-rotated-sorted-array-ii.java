e540ec07339d089bb153d4c7cb52319c1858bb1e
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-05 12:08:03 -0800

    Time: 0 ms (100%), Space: 43.3 MB (66.42%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,16 +1,14 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        // not necessarily with distinct values
         int start = 0;
         int end = nums.length;
-        
+
         while (start < end) {
             int mid = start + (end - start) / 2;
             if (nums[mid] == target) {
                 return true;
             } else {
-                // 由于存在重复元素，需要处理跳过重复的情况
-                if (nums[start] == nums[mid] && nums[mid] == nums[end-1]) {
+                if (nums[start] == nums[mid] && nums[end - 1] == nums[mid]) {
                     start++;
                     end--;
                 } else if (nums[start] <= nums[mid]) {
@@ -29,8 +27,6 @@ class Solution {
             }
         }
 
-        return false; 
+        return false;
     }
 }
-
-// [1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1], target = 2, expect return true
