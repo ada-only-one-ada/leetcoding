d6b4baa74368cedf4bd9267b22ae4e9f264c2f5f
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-05 11:42:52 -0800

    Time: 1 ms (74.28%), Space: 45.5 MB (82.24%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,47 +1,51 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        // in place
-        boolean topLeft = false;
-        boolean top = false;
-        boolean left = false;
+        int rowNum = matrix.length;
+        int colNum = matrix[0].length;
 
-        for (int row = 0; row < matrix.length; row++) {
-            for (int col = 0; col < matrix[0].length; col++) {
+        boolean firstRowHasZero = false;
+        boolean firstColHasZero = false;
+        for (int row = 0; row < rowNum; row++) {
+            if (matrix[row][0] == 0) {
+                firstColHasZero = true;
+                break;
+            }
+        }
+
+        for (int col = 0; col < colNum; col++) {
+            if (matrix[0][col] == 0) {
+                firstRowHasZero = true;
+                break;
+            }
+        }
+
+        for (int row = 1; row < rowNum; row++) {
+            for (int col = 1; col < colNum; col++) {
                 if (matrix[row][col] == 0) {
-                    if (row == 0 && col == 0) {
-                        topLeft = true;
-                    } else if (row == 0) {
-                        top = true;
-                    } else if (col == 0) {
-                        left = true;
-                    } else {
-                        matrix[0][col] = 0;
-                        matrix[row][0] = 0;
-                    }  
+                    matrix[0][col] = 0;
+                    matrix[row][0] = 0;
                 }
             }
         }
 
-        for (int row = 1; row < matrix.length; row++) {
-            for (int col = 1; col < matrix[0].length; col++) {
+        for (int row = 1; row < rowNum; row++) {
+            for (int col = 1; col < colNum; col++) {
                 if (matrix[0][col] == 0 || matrix[row][0] == 0) {
                     matrix[row][col] = 0;
                 }
             }
         }
 
-        if (topLeft || top || left) {
-            if (left || topLeft) {
-                for (int row = 0; row < matrix.length; row++) {
-                   matrix[row][0] = 0;
-                }
+        for (int row = 0; row < rowNum; row++) {
+            if (firstColHasZero) {
+                matrix[row][0] = 0;
             }
+        }
 
-            if (top || topLeft) {
-                for (int col = 0; col < matrix[0].length; col++) {
-                   matrix[0][col] = 0;
-                }
+        for (int col = 0; col < colNum; col++) {
+            if (firstRowHasZero) {
+                matrix[0][col] = 0;
             }
-        }   
+        }
     }
 }
