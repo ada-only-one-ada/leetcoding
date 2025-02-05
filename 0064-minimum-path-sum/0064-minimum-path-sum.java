9f98548ccacdf0d9e65794c2edf75333a1d04d16
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-05 11:21:51 -0800

    Time: 4 ms (62.43%), Space: 47.9 MB (33.22%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -2,26 +2,24 @@ class Solution {
     public int minPathSum(int[][] grid) {
         int rowNum = grid.length;
         int colNum = grid[0].length;
+
         int[][] dp = new int[rowNum][colNum];
-        
-        for (int row = 0; row < rowNum; row++) {
-            for (int col = 0; col < colNum; col++) {
-                // 左上第一个格子，花费是它本身
-                if (row == 0 && col == 0) {
-                    dp[row][col] = grid[row][col];
-                // 第一行，只能由左边走过来
-                } else if (row == 0) {
-                    dp[row][col] = dp[row][col - 1] + grid[row][col];
-                // 第一列，只能由上面走下来
-                } else if (col == 0) {
-                    dp[row][col] = dp[row - 1][col] + grid[row][col];
-                // 其他情况：可以由左边或者上面走下来
-                } else {
-                    dp[row][col] = Math.min(dp[row][col - 1] + grid[row][col], dp[row - 1][col] + grid[row][col]);
-                }
+        dp[0][0] = grid[0][0];
+
+        for (int row = 1; row < rowNum; row++) {
+            dp[row][0] = dp[row-1][0] + grid[row][0];
+        }
+
+        for (int col = 1; col < colNum; col++) {
+            dp[0][col] = dp[0][col-1] + grid[0][col];
+        }
+
+        for (int row = 1; row < rowNum; row++) {
+            for (int col = 1; col < colNum; col++) {
+                dp[row][col] = Math.min(dp[row-1][col], dp[row][col-1]) + grid[row][col];
             }
         }
 
-        return dp[rowNum - 1][colNum - 1];
-    } 
+        return dp[rowNum-1][colNum-1];
+    }
 }
