48279d26d0e03008ea84556a9eaa034ee734811d
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-05 12:11:22 -0800

    Time: 0 ms (100%), Space: 42.7 MB (99.2%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -3,16 +3,15 @@ class Solution {
         int start = 0;
         int end = nums.length;
 
+        while (start + 1 < nums.length && nums[start] == nums[start + 1]) start++;
+        while (end - 3 >= 0 && nums[end - 1] == nums[end - 2]) end--;
+
         while (start < end) {
             int mid = start + (end - start) / 2;
             if (nums[mid] == target) {
                 return true;
             } else {
-                // 由于存在重复元素，需要处理跳过重复的情况
-                if (nums[start] == nums[mid] && nums[end - 1] == nums[mid]) {
-                    start++;
-                    end--;
-                } else if (nums[start] <= nums[mid]) {
+                if (nums[start] <= nums[mid]) {
                     if (nums[start] <= target && target < nums[mid]) {
                         end = mid;
                     } else {
