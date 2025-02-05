a0104cca43a2788bea61343276aaf02aa421f0cb
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-04 23:37:05 -0800

    Time: 4 ms (20.28%), Space: 48 MB (46.98%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,58 +1,32 @@
 class Solution {
-    int[] parent;
-    int[] rank;
-
     public int findCircleNum(int[][] isConnected) {
-        int n = isConnected.length;
-        parent = new int[n];
-        rank = new int[n];
+        Set<Integer> visited = new HashSet<>();
+        int res = 0;
 
-        for (int node = 0; node < n; node++) {
-            parent[node] = node;
-            rank[node] = 1;
-        }
-
-        for (int node = 0; node < n; node++) {
-            for (int nei = 0; nei < n; nei++) {
-                if(node == nei) continue;
-                if (isConnected[node][nei] == 1) {
-                    connect(node, nei);
+        for (int node = 0; node < isConnected.length; node++) {
+            for (int nei = 0; nei < isConnected[0].length; nei++) {
+                
+                
+                if (isConnected[node][nei] == 1 && !visited.contains(node)) {
+                    res++;
+                    dfs(node, isConnected, visited);
                 }
             }
         }
 
-        for (int node = 0; node < n; node++) {
-            parent[node] = findParent(node);
-        }
-
-        Set<Integer> set = new HashSet<>();
-        for (int p: parent) {
-            set.add(p);
-        }
-        return set.size();
+        return res;
     }
 
-    public void connect(int node, int nei) {
-        int parentA = findParent(node);
-        int parentB = findParent(nei);
+    public void dfs(int node, int[][] isConnected, Set<Integer> visited) {
+        if (visited.contains(node)) return;
+        visited.add(node);
 
-        if (parentA == parentB) return;
-
-        if (rank[parentA] >= rank[parentB]) {
-            parent[parentB] = parentA;
-            rank[parentA] += rank[parentB];
-        } else {
-            parent[parentA] = parentB;
-            rank[parentB] += rank[parentA];
+        int[] neis = isConnected[node];
+        for (int nei = 0; nei < neis.length; nei++) {
+            if (node == nei) continue;
+            if (isConnected[node][nei] == 1) {
+                dfs(nei, isConnected, visited);
+            }
         }
-    }
-
-    public int findParent(int node) {
-        int p = parent[node];
-        if (parent[p] != p) {
-            parent[p] = findParent(p);
-        }
-
-        return parent[p];
     }
 }
