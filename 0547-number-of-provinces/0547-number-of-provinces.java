84f6619064de593a95dfd80e8a6893c7683981b2
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-04 23:21:11 -0800

    Time: 3 ms (33.23%), Space: 48.2 MB (29.09%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,56 +1,58 @@
 class Solution {
-    int[] root;
-    int[] count;
-    public int findCircleNum(int[][] isConnected) {
-        root = new int[isConnected.length];
-        count = new int[isConnected.length];
+    int[] parent;
+    int[] rank;
 
-        for (int i = 0; i < isConnected.length; i++) {
-            root[i] = i;
-            count[i] = 1;
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        parent = new int[n];
+        rank = new int[n];
+
+        for (int node = 0; node < n; node++) {
+            parent[node] = node;
+            rank[node] = 1;
         }
 
-        for (int i = 0; i < isConnected.length; i++) {
-            for (int nei = 0; nei < isConnected.length; nei++) {
-                if (isConnected[i][nei] == 1) {
-                    connect(i, nei);
+        for (int node = 0; node < n; node++) {
+            for (int nei = 0; nei < n; nei++) {
+                if(node == nei) continue;
+                if (isConnected[node][nei] == 1) {
+                    connect(node, nei);
                 }
             }
         }
 
-        for (int i = 0; i < isConnected.length; i++) {
-            root[i] = findRoot(i);
+        for (int node = 0; node < n; node++) {
+            parent[node] = findParent(node);
         }
 
-
         Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < root.length; i++) {
-            set.add(root[i]);
+        for (int p: parent) {
+            set.add(p);
         }
         return set.size();
     }
 
-    public void connect(int node1, int node2) {
-        int root1 = findRoot(node1);
-        int root2 = findRoot(node2);
+    public void connect(int node, int nei) {
+        int parentA = findParent(node);
+        int parentB = findParent(nei);
 
-        if (root1 == root2) return;
+        if (parentA == parentB) return;
 
-        if (count[root1] >= count[root2]) {
-            root[root2] = root1;
-            count[root1] += count[root2];
+        if (rank[parentA] >= rank[parentB]) {
+            parent[parentB] = parentA;
+            rank[parentA] += rank[parentB];
         } else {
-            root[root1] = root2; 
-            count[root2] += count[root1];
+            parent[parentA] = parentB;
+            rank[parentB] += rank[parentA];
         }
     }
 
-    public int findRoot(int node) {
-        int p = root[node];
-        if (root[p] != p) {
-            p = findRoot(root[p]);
+    public int findParent(int node) {
+        int p = parent[node];
+        if (parent[p] != p) {
+            parent[p] = findParent(p);
         }
 
-        return root[p];
+        return parent[p];
     }
 }
