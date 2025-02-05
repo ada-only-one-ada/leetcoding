658f68154a9ef019a117d7b248eef1013b99184f
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-05 13:22:41 -0800

    Time: 0 ms (100%), Space: 42.1 MB (29.79%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -7,11 +7,9 @@ class Solution {
     public boolean helper(TreeNode left, TreeNode right) {
         if (left == null && right == null) return true;
         if (left == null || right == null) return false;
-
-        if (left.val != right.val) {
-            return false;
-        }
+        if (left.val != right.val) return false;
 
         return helper(left.left, right.right) && helper(left.right, right.left);
+
     }
 }
