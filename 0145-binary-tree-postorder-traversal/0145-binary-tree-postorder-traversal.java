cb74a505d6c90bf6ea0465f3564fc8225470528c
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-05 12:35:55 -0800

    Time: 1 ms (7.84%), Space: 41.7 MB (64.44%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -1,18 +1,27 @@
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
-        if (root == null) return res;
         Stack<TreeNode> stack = new Stack();
-        stack.push(root);
 
-        while (!stack.isEmpty()) {
-            TreeNode curr = stack.pop();
-            res.add(0, curr.val);
-
-            if (curr.left != null) stack.push(curr.left);
-            if (curr.right != null) stack.push(curr.right);
+        while (root != null || !stack.isEmpty()) {
+            if (root != null) {
+                res.add(root.val);
+                stack.push(root);
+                root = root.right;
+            } else {
+                TreeNode node = stack.pop();
+                if (node.left != null) {
+                    root = node.left;
+                }
+            }
         }
-
+        Collections.reverse(res);
         return res;
     }
 }
+
+// preorder: root, left, right
+// inorder: left, root, right
+// postorder: left, right, root 
+
+// preorder变形：root，right，left，反转就是postorder
