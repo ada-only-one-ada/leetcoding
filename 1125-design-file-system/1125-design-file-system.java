a2ca50a5ca02c9387b15912660af48ef00e0f1d5
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-05 13:44:49 -0800

    Time: 91 ms (60.85%), Space: 55.1 MB (98.53%) - LeetHub

--- 1125-design-file-system/1125-design-file-system.java
+++ 1125-design-file-system/1125-design-file-system.java
@@ -1,34 +1,32 @@
-import java.util.*;
-
 class FileSystem {
     Map<String, Integer> map;
 
     public FileSystem() {
         map = new HashMap<>();
-        map.put("", 0); // Initialize root path
+        // the parent root of "/a" is ""
+        map.put("", -1);
     }
     
     public boolean createPath(String path, int value) {
-        if (path.equals("/") || map.containsKey(path)) {
-            return false; // Check if path already exists or is the root path
-        }
+        if (path.equals("/") || map.containsKey(path)) return false;
 
-        // Get the parent path by finding the last '/' and substring up to it
-        String parentPath = path.substring(0, path.lastIndexOf('/'));
-
-        // Check if the parent path exists
-        if (!map.containsKey(parentPath)) {
-            return false;
-        }
+        String parentPath = path.substring(0, path.lastIndexOf("/"));
+        if (!map.containsKey(parentPath)) return false;
 
         map.put(path, value);
         return true;
     }
     
     public int get(String path) {
-        if (!map.containsKey(path)) {
-            return -1;
-        }
+        if (!map.containsKey(path)) return -1;
+
         return map.get(path);
     }
 }
+
+/**
+ * Your FileSystem object will be instantiated and called as such:
+ * FileSystem obj = new FileSystem();
+ * boolean param_1 = obj.createPath(path,value);
+ * int param_2 = obj.get(path);
+ */
