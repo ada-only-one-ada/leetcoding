54e57933823aaefc0f6cc039f4c73422a1a561a8
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-05 15:40:19 -0800

    Time: 2 ms (56.32%), Space: 45.2 MB (31.76%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -2,22 +2,20 @@ class Solution {
     public int maxProduct(int[] nums) {
         int[] minDp = new int[nums.length];
         int[] maxDp = new int[nums.length];
-        
         minDp[0] = nums[0];
         maxDp[0] = nums[0];
         int res = nums[0];
 
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] >= 0) {
-                minDp[i] = Math.min(nums[i], minDp[i-1] * nums[i]);
-                maxDp[i] = Math.max(nums[i], maxDp[i-1] * nums[i]);
-            } else if (nums[i] < 0) {
-                minDp[i] = Math.min(nums[i], maxDp[i-1] * nums[i]);
-                maxDp[i] = Math.max(nums[i], minDp[i-1] * nums[i]);
+                minDp[i] = Math.min(minDp[i-1] * nums[i], nums[i]);
+                maxDp[i] = Math.max(maxDp[i-1] * nums[i], nums[i]);
+            } else {
+                minDp[i] = Math.min(maxDp[i-1] * nums[i], nums[i]);
+                maxDp[i] = Math.max(minDp[i-1] * nums[i], nums[i]);
             }
             res = Math.max(res, maxDp[i]);
         }
-
         return res;
     }
 }
