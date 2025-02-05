4eb13212de29ee22d3fbd8b38ddceea6eb62502f
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-04 17:21:36 -0800

    Time: 2 ms (99.96%), Space: 52.7 MB (38.79%) - LeetHub

--- 1396-count-servers-that-communicate/1396-count-servers-that-communicate.java
+++ 1396-count-servers-that-communicate/1396-count-servers-that-communicate.java
@@ -1,33 +1,48 @@
 class Solution {
-    public int countServers(int[][] grid) {
-        int rowNum = grid.length;
-        int colNum = grid[0].length;
+public int countServers(int[][] grid) {
+    int m = grid.length;
+    int n = grid[0].length;
+    int[] row = new int[m];
+    int[] col = new int[n];
+    int unCommu = 0;
+    int total = 0;
 
-        int[] checkRow = new int[rowNum];
-        int[] checkCol = new int[colNum];
-
-        int res = 0;
-
-        for (int row = 0; row < rowNum; row++) {
-            for (int col = 0; col < colNum; col++) {
-                if (grid[row][col] == 1) {
-                    checkRow[row]++;
-                    checkCol[col]++;
-                }
+    // initization
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (grid[i][j] == 1) {
+                // 4 * 4 matrix, row 1: 1 0 0 1, (rowHash set: <1>)
+                //               row 2: 0 1 0 0, row situations: [2, 1, 0, 1], col: [2, 1, 0, 1]
+                //               row 3: 0 0 0 0
+                //               row 4: 1 0 0 0 
+                // return 2 
+                row[i]++; // the i row has a server: int[] row 
+                col[j]++; // the j col has a server: int[] col
+                total++;
             }
         }
+    }
 
-        for (int row = 0; row < rowNum; row++) {
-            for (int col = 0; col < colNum; col++) {
-                if (grid[row][col] == 1) {
-                    if (checkRow[row] > 1 || checkCol[col] > 1) {
-                        res++;
+    // whether i should count the current server? grid[i][j] = 1: find a server
+    
+    // count: the same row has more than one server, or the same col has more than one server
+    // not count: the same row only has one and the same col only has one, which means the server 
+    // is the only server in its row and col
+
+    
+    for (int i = 0; i < m; i++) {
+        // row 1: 1 0 0 0
+        if (row[i] == 1) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    if (col[j] == 1) {
+                        unCommu++;
                     }
                 }
             }
         }
-
-
-        return res;
     }
+
+    return total - unCommu;
+}
 }
