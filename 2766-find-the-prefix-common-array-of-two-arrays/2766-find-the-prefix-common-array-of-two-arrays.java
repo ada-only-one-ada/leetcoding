4701eb5827229517aa4c105ffb37a8a662ca1f9f
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-05 16:25:48 -0800

    Time: 2 ms (88.13%), Space: 45.4 MB (78.11%) - LeetHub

--- 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.java
+++ 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.java
@@ -1,32 +1,22 @@
 class Solution {
     public int[] findThePrefixCommonArray(int[] A, int[] B) {
-        // It is guaranteed that A and B are both a permutation of n integers.
-        // 这意味着里面出现的数字一定是2次
-        
-        // 1 <= A[i], B[i] <= n
-        int n = A.length;
+        int[] res = new int[A.length];
 
-        int[] res = new int[n];
-        int[] freq = new int[n + 1];
-        int commonCount = 0;
+        int[] freq = new int[A.length + 1];
+        int common = 0;
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < A.length; i++) {
             int numA = A[i];
             int numB = B[i];
 
             freq[numA]++;
-            if (freq[numA] == 2) {
-                commonCount++;
-            }
+            if (freq[numA] == 2) common++;
 
             freq[numB]++;
-            if (freq[numB] == 2) {
-                commonCount++;
-            }
+            if (freq[numB] == 2) common++;
 
-            res[i] = commonCount;
+            res[i] = common;
         }
-
         return res;
     }
 }
