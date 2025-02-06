2238caae7de2bce070fb1662ff9591ce4f8e38a2
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-06 11:08:24 -0800

    Time: 137 ms (77.42%), Space: 57 MB (67.74%) - LeetHub

---
+++ 1640-design-a-file-sharing-system/1640-design-a-file-sharing-system.java
@@ -0,0 +1,66 @@
+class FileSharing {
+    Map<Integer, List<Integer>> map; // chunkId: <user1, user2>
+    PriorityQueue<Integer> queue; // previous available id
+    int id; // new id
+    Map<Integer, List<Integer>> userToChunks;
+
+    public FileSharing(int m) {
+        map = new HashMap<>(); 
+        queue = new PriorityQueue<>();
+        id = 1;
+        userToChunks = new HashMap<>();
+    }
+    
+    public int join(List<Integer> ownedChunks) {
+        int assignedId;
+
+        if (!queue.isEmpty()) {
+            assignedId = queue.poll();
+        } else {
+            assignedId = this.id++;
+        }
+
+        for (int chunk: ownedChunks) {
+            map.putIfAbsent(chunk, new ArrayList<>());
+
+            if (!map.get(chunk).contains(assignedId)) {
+                map.get(chunk).add(assignedId);
+            }
+        }
+
+        userToChunks.put(assignedId, ownedChunks);
+
+        return assignedId;
+    }
+    
+    public void leave(int userID) {
+        for (int chunk: userToChunks.get(userID)) {
+            if (map.get(chunk).contains(userID)) {
+                map.get(chunk).remove(Integer.valueOf(userID));
+            }
+        }
+        queue.add(userID);
+    }
+    
+    public List<Integer> request(int userID, int chunkID) {
+        if (!map.containsKey(chunkID)) return new ArrayList<>();
+
+        List<Integer> res = new ArrayList<>(map.get(chunkID));       
+        if (res.size() == 0) return res;
+
+        Collections.sort(res);
+        if (map.get(chunkID).size() > 0 && !map.get(chunkID).contains(userID)) {
+            map.get(chunkID).add(userID);
+            userToChunks.get(userID).add(chunkID);
+        }
+        return res;
+    }
+}
+
+/**
+ * Your FileSharing object will be instantiated and called as such:
+ * FileSharing obj = new FileSharing(m);
+ * int param_1 = obj.join(ownedChunks);
+ * obj.leave(userID);
+ * List<Integer> param_3 = obj.request(userID,chunkID);
+ */
