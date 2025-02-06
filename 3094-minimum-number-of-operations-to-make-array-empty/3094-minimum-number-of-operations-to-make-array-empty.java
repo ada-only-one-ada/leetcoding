b90027190d415c300b7769f7663350628053bcec
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-05 21:28:34 -0800

    Time: 20 ms (89.01%), Space: 62.8 MB (58.07%) - LeetHub

--- 3094-minimum-number-of-operations-to-make-array-empty/3094-minimum-number-of-operations-to-make-array-empty.java
+++ 3094-minimum-number-of-operations-to-make-array-empty/3094-minimum-number-of-operations-to-make-array-empty.java
@@ -1,37 +1,29 @@
 class Solution {
     public int minOperations(int[] nums) {
-        Map<Integer, Integer> frequencyMap = new HashMap<>();
-        for (int num : nums) {
-            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        // 选三个一样的删掉
+        // 选两个一样的删掉
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num: nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        int totalOperations = 0;
-        for (int freq : frequencyMap.values()) {
-            if (freq == 1) {
-                // 如果任何数字的频率为1，不能通过任何操作删除它，直接返回-1。
-                return -1;
-            }
-            // 首先尽可能使用“每三个一组”的操作
-            int groupsOfThree = freq / 3;
-            int remaining = freq % 3;
+        int res = 0;
+        for (int num: map.keySet()) {
+            int freq = map.get(num);
+            if (freq < 2) return -1;
 
-            // 计算使用“每三个一组”的操作次数
-            totalOperations += groupsOfThree;
+            int groupOfThree = freq / 3;
+            res += groupOfThree;
+            freq = freq % 3;
 
-            // 对剩余的部分，如果有两个，使用“每两个一组”的操作一次
-            if (remaining == 2) {
-                totalOperations += 1;
-            } else if (remaining == 1) {
-                // 如果剩余一个，且之前有至少一个“每三个一组”的操作，可以调整一个“每三个一组”变为“每两个一组”
-                if (groupsOfThree > 0) {
-                    totalOperations++;  // 使用一个额外的“每两个一组”
-                } else {
-                    // 如果没有足够的数量来调整，返回-1
-                    return -1;
-                }
+            if (freq >= 2) {
+                res += freq / 2;
+            } else if (freq == 1) {
+                res++;
             }
         }
 
-        return totalOperations;
+        return res;
     }
 }
