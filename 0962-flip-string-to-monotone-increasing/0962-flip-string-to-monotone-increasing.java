37093ca5028521f6a00f83dd6ac322610e2a7fd2
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-06 12:50:13 -0800

    Time: 7 ms (96.71%), Space: 45.5 MB (49.93%) - LeetHub

--- 0962-flip-string-to-monotone-increasing/0962-flip-string-to-monotone-increasing.java
+++ 0962-flip-string-to-monotone-increasing/0962-flip-string-to-monotone-increasing.java
@@ -1,13 +1,14 @@
 class Solution {
     public int minFlipsMonoIncr(String s) {
+        int res = 0;
+
         int ones = 0;
 
-        int res = 0;
         for (char c: s.toCharArray()) {
-            if (c == '0') {
-                res = Math.min(ones, res + 1);
-            } else {
+            if (c == '1') {
                 ones++;
+            } else if (c == '0') {
+                res = Math.min(ones, res + 1);
             }
         }
 
