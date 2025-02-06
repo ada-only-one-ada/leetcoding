e754d2104d60c96e1dc54642734ccf1a83e1b5fb
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-05 23:09:34 -0800

    Time: 172 ms (6.95%), Space: 122 MB (29.9%) - LeetHub

---
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.java
@@ -0,0 +1,33 @@
+class TimeMap {
+    Map<String, TreeMap<Integer, String>> map;
+    public TimeMap() {
+        map = new HashMap<>();
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        map.putIfAbsent(key, new TreeMap<>());
+        map.get(key).put(timestamp, value);
+    }
+    
+    public String get(String key, int timestamp) {
+        if (!map.containsKey(key)) {
+            return "";
+        }
+
+        TreeMap<Integer, String> values = map.get(key);
+        if (values.containsKey(timestamp)) {
+            return values.get(timestamp);
+        } 
+
+        Integer lower = values.lowerKey(timestamp);
+        if (lower == null || !values.containsKey(lower)) return "";
+        return values.get(lower);
+    }
+}
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap obj = new TimeMap();
+ * obj.set(key,value,timestamp);
+ * String param_2 = obj.get(key,timestamp);
+ */
