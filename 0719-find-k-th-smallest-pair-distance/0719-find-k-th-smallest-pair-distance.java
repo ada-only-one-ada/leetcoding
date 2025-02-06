45397b27733eaed73334cbeeefe6046f4a172236
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-06 15:03:13 -0800

    Time: 8 ms (79.4%), Space: 44.7 MB (41.32%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.java
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.java
@@ -1,36 +1,35 @@
 class Solution {
     public int smallestDistancePair(int[] nums, int k) {
-        // 对数组进行排序，以便于计算距离
         Arrays.sort(nums);
-        int n = nums.length;
+        
         // 确定二分搜索的范围，基于最小和最大距离
-        int left = 0, right = nums[n - 1] - nums[0];
-        int ans = 0;
+        int left = 0;
+        int right = nums[nums.length - 1] - nums[0] + 1;
+        int res = 0;
 
         // 使用二分搜索来找到第k小的距离
-        while (left <= right) {
+        while (left < right) {
             int mid = left + (right - left) / 2;
-            // 使用辅助函数来计算距离小于等于mid的对数
             int count = countPairsWithDistanceLessThan(nums, mid);
 
             // 根据对数来调整搜索范围
             if (count >= k) {
-                ans = mid; // 如果对数足够，则更新答案
-                right = mid - 1;
+                res = mid; // 如果对数足够，则更新答案
+                right = mid;
             } else {
                 left = mid + 1;
             }
         }
-        return ans;
+        return res;
     }
 
     // 辅助函数：计算距离小于等于给定值的对数
     private int countPairsWithDistanceLessThan(int[] nums, int maxDistance) {
         int count = 0;
         int i = 0;
-        int n = nums.length;
+
         // 使用双指针方法来计算距离
-        for (int j = 0; j < n; j++) {
+        for (int j = 0; j < nums.length; j++) {
             while (nums[j] - nums[i] > maxDistance) {
                 i++; // 移动左指针直到距离小于等于maxDistance
             }
