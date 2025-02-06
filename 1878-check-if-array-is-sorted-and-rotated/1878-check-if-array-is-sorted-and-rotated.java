4c0ab7b1178262ecf103bcbbc564170dc5b669a6
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-06 09:07:31 -0800

    Time: 0 ms (100%), Space: 41.5 MB (28.84%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
@@ -1,23 +1,15 @@
 class Solution {
     public boolean check(int[] nums) {
-        if (helper(nums, 0, nums.length - 1)) return true;
+        boolean rotated = false;
 
-        int line = 0;
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] < nums[i-1]) {
-                line = i;
-                break;
+                if (rotated) return false;
+                rotated = true;
             }
         }
 
-        return helper(nums, 0, line - 1) && helper(nums, line, nums.length - 1) && nums[nums.length - 1] <= nums[0];
-    }
-
-    public boolean helper(int[] nums, int start, int end) {
-        for (int i = start + 1; i <= end; i++) {
-            if (nums[i] < nums[i-1]) return false;
-        }
-
-        return true;
+        if (rotated) return nums[nums.length - 1] <= nums[0];
+        return nums[0] <= nums[nums.length -1];
     }
 }
