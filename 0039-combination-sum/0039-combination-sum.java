b0d6956262faa23ce882e9e522c1c476ce28f94f
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-05 21:49:48 -0800

    Time: 2 ms (82.54%), Space: 44.9 MB (32.11%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -5,22 +5,22 @@ class Solution {
         return res;
     }
 
-    public void backtracking(int[] candidates, int target, List<List<Integer>> res, List<Integer> currRes, int currSum, int index) {
-        // 所有数字都是正数
-        if (currSum >= target) {
-            if (currSum == target) {
-                res.add(new ArrayList<>(currRes));
+    public void backtracking(int[] candidates, int target, List<List<Integer>> res, List<Integer> currRes, int sum, int index) {
+        if (sum >= target) {
+            if (sum == target) {
+                res.add(new ArrayList(currRes));
             }
             return;
         }
 
-        // 注意读题，每个数字可以拿无数遍
         for (int i = index; i < candidates.length; i++) {
+            sum += candidates[i];
             currRes.add(candidates[i]);
-            currSum += candidates[i];
-            backtracking(candidates, target, res, currRes, currSum, i);
+
+            backtracking(candidates, target, res, currRes, sum, i);
+
+            sum -= candidates[i];
             currRes.remove(currRes.size() - 1);
-            currSum -= candidates[i];
         }
     }
 }
