3799d7261663347accf4afed71ead5715053594c
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-06 13:41:15 -0800

    Time: 7 ms (65.34%), Space: 42.7 MB (44.94%) - LeetHub

--- 0999-regions-cut-by-slashes/0999-regions-cut-by-slashes.java
+++ 0999-regions-cut-by-slashes/0999-regions-cut-by-slashes.java
@@ -1,35 +1,27 @@
 class Solution {
     public int regionsBySlashes(String[] grid) {
         int n = grid.length;
-        int[][] region = new int[n * 3][n * 3];
+        int[][] region = new int[n*3][n*3];
 
         for (int row = 0; row < n; row++) {
             for (int col = 0; col < n; col++) {
-                char curr = grid[row].charAt(col);
+                char c = grid[row].charAt(col);
 
-                // 3 x 3 网格
-                if (curr == '/') {
-                    // 第一行，第三个
-                    region[row * 3][col * 3 + 2] = 1;
-                    // 第二行，第二个（中间行，中间列）
-                    region[row * 3 + 1][col * 3 + 1] = 1;
-                    // 第三行，第一个
-                    region[row * 3 + 2][col * 3] = 1;
-                } else if (curr == '\\') {
-                    // 第一行，第一个
-                    region[row * 3][col * 3] = 1;
-                    // 第二行，第二个（中间行，中间列）
-                    region[row * 3 + 1][col * 3 + 1] = 1;
-                    // 第三行，第三个
-                    region[row * 3 + 2][col * 3 + 2] = 1;
+                if (c == '/') {
+                    region[row*3][col*3+2] = 1;
+                    region[row*3+1][col*3+1] = 1;
+                    region[row*3+2][col*3] = 1;
+                } else if (c == '\\') {
+                    region[row*3][col*3] = 1;
+                    region[row*3+1][col*3+1] = 1;
+                    region[row*3+2][col*3+2] = 1;
                 }
             }
         }
 
         int res = 0;
-        for (int row = 0; row < region.length; row++) {
-            for (int col = 0; col < region[0].length; col++) {
-                // 1是陆地分界线，0才是岛屿
+        for (int row = 0; row < n*3; row++) {
+            for (int col = 0; col < n*3; col++) {
                 if (region[row][col] == 0) {
                     res++;
                     dfs(region, row, col);
@@ -45,10 +37,9 @@ class Solution {
         if (region[row][col] == 1) return;
 
         region[row][col] = 1;
-        dfs(region, row - 1, col);
-        dfs(region, row + 1, col);
-        dfs(region, row, col - 1);
-        dfs(region, row, col + 1);
+        dfs(region, row-1, col);
+        dfs(region, row+1, col);
+        dfs(region, row, col-1);
+        dfs(region, row, col+1);
     }
 }
-   
