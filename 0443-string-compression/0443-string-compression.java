5d35e4cc553ac8b089697e189807cb5178c7c593
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-06 12:47:56 -0800

    Time: 1 ms (99.58%), Space: 44.3 MB (32.47%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,32 +1,34 @@
 class Solution {
     public int compress(char[] chars) {
-        int filled = 0;
+        int index = 0;
         int count = 1;
 
         for (int i = 1; i < chars.length; i++) {
-            if (chars[i] == chars[i - 1]) {
+            if (chars[i] == chars[i-1]) {
                 count++;
-            } else {
-                chars[filled++] = chars[i - 1];
-                if (count == 1) continue;
+            } else if (chars[i] != chars[i-1]) {
+                if (count == 1) {
+                    chars[index++] = chars[i-1];
+                } else if (count > 1) {
+                    String times = String.valueOf(count);
+                    chars[index++] = chars[i-1];
+                    for (int j = 0; j < times.length(); j++) {
+                        chars[index++] = times.charAt(j);
+                    }
 
-                String times = count + "";
-                for (char time: times.toCharArray()) {
-                    chars[filled++] = time;
+                    count = 1;
                 }
-
-                count = 1;
             }
         }
 
-        chars[filled++] = chars[chars.length - 1];
+        chars[index++] = chars[chars.length - 1];
         if (count > 1) {
-            String lastTimes = count + "";
-            for (char time: lastTimes.toCharArray()) {
-                chars[filled++] = time;
+            String times = String.valueOf(count);
+            for (int i = 0; i < times.length(); i++) {
+                chars[index++] = times.charAt(i);
             }
         }
 
-        return filled;
+        return index;
     }
 }
