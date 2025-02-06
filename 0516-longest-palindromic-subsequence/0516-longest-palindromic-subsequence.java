196d452f27067fd0ff708e498b6b891207ca570a
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-05 17:08:09 -0800

    Time: 37 ms (69.14%), Space: 55.3 MB (57.09%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -5,19 +5,21 @@ class Solution {
         for (int i = 0; i < s.length(); i++) {
             dp[i][i] = 1;
         }
+        int start = 0;
+        int end = 0;
 
-        for (int len = 2; len <= s.length(); len++) {  // 子序列的长度从2到n
+        for (int len = 2; len <= s.length(); len++) {
             for (int i = 0; i <= s.length() - len; i++) {
                 int j = i + len - 1;
 
                 if (s.charAt(i) == s.charAt(j)) {
-                    dp[i][j] = dp[i + 1][j - 1] + 2;  // 如果两端字符相同，则在内部子序列长度基础上加2
+                    dp[i][j] = dp[i+1][j-1] + 2;
                 } else {
-                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);  // 否则，取内部去掉一个字符后的最大值
+                    dp[i][j] = Math.max(dp[i+1][j], dp[i][j-1]);
                 }
             }
         }
 
-        return dp[0][s.length() - 1];  // 整个字符串的最长回文子序列长度
+        return dp[0][s.length()-1];
     }
 }
