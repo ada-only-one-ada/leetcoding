b78aaa34d46cdec82faa19470b8b5f62192db6e2
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-06 14:09:54 -0800

    Time: 31 ms (57.62%), Space: 50.8 MB (76.44%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
@@ -1,23 +1,21 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
         Queue<int[]> queue = new PriorityQueue<>((a, b) -> {
-            return (b[0] * b[0] + b[1] * b[1]) - (a[0] * a[0] + a[1] * a[1]);
+            return (b[0]*b[0]+b[1]*b[1]) - (a[0]*a[0]+a[1]*a[1]);
         });
 
         for (int[] point: points) {
-            queue.add(new int[]{point[0], point[1]});
-
-            while (queue.size() > k) {
+            queue.add(point);
+            if (queue.size() > k) {
                 queue.poll();
             }
         }
 
         int[][] res = new int[k][2];
-        int index = res.length - 1;
-        while (!queue.isEmpty()) {        
-            res[index--] = queue.poll();
+        int index = 0;
+        while (index < res.length && !queue.isEmpty()) {
+            res[index++] = queue.poll();
         }
-
         return res;
     }
 }
