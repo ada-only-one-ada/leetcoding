6dc9ea5dd484c5bd702b860dcbd5667bd46f0878
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-06 12:35:39 -0800

    Time: 61 ms (24.35%), Space: 55.5 MB (84.64%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,27 +1,33 @@
 class Solution {
-    private void countingSort(int[] arr) {
-        HashMap<Integer,Integer> counts = new HashMap<>();
-        int minVal = arr[0];
-        int maxVal = arr[0];
-        
-        for (int i = 0; i < arr.length; i++) {
-            minVal = Math.min(minVal, arr[i]);
-            maxVal = Math.max(maxVal, arr[i]);
-            counts.put(arr[i], counts.getOrDefault(arr[i], 0) + 1);
-        }
-        
-        int index = 0;
-        for (int val = minVal; val <= maxVal; val++) {
-            while (counts.getOrDefault(val, 0) > 0) {
-                arr[index] = val;
-                index += 1;
-                counts.put(val, counts.get(val) - 1);
-            }
-        }
-    }
-
     public int[] sortArray(int[] nums) {
         countingSort(nums);
         return nums;
+    }
+
+    public void countingSort(int[] nums) {
+        if (nums.length == 0) return;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int min = nums[0];
+        int max = nums[0];
+
+        for (int num: nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        int index = 0;
+        for (int num = min; num <= max && index < nums.length; num++) {
+            if (!map.containsKey(num)) continue;
+
+            while (map.containsKey(num) && map.get(num) > 0) {
+                nums[index++] = num;
+                map.put(num, map.get(num) - 1);
+                if (map.get(num) == 0) {
+                    map.remove(num);
+                } 
+            }
+        }
     }
 }
