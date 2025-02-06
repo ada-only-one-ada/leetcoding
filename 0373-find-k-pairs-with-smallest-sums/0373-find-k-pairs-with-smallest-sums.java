fa84797ca8386a9851652cb8bce1a79c7a217317
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-06 11:47:15 -0800

    Time: 37 ms (45.6%), Space: 60.2 MB (49.89%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
@@ -1,22 +1,24 @@
 class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
         List<List<Integer>> res = new ArrayList<>();
+
         Queue<int[]> queue = new PriorityQueue<>((a, b) -> {
             return (a[0]+a[1]) - (b[0]+b[1]);
         });
 
-        for (int i = 0; i < Math.min(nums2.length, k); i++) {
+        for (int i = 0; i < Math.min(k, nums2.length); i++) {
             queue.add(new int[]{nums1[0], nums2[i], 0});
         }
 
         while (res.size() < k && !queue.isEmpty()) {
             int[] pair = queue.poll();
+
             int num1 = pair[0];
             int num2 = pair[1];
             int i1 = pair[2];
 
-            res.add(new ArrayList(Arrays.asList(num1, num2)));
-            if (i1+1 < nums1.length) {
+            res.add(new ArrayList<>(Arrays.asList(num1, num2)));
+            if (i1 + 1 < nums1.length) {
                 queue.add(new int[]{nums1[i1+1], num2, i1+1});
             }
         }
