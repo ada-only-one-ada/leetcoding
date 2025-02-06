284e4798ca977e7b3b6a0d7205eb8b45995c48f2
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-05 20:51:11 -0800

    Time: 43 ms (30.77%), Space: 45.9 MB (40.94%) - LeetHub

--- 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,25 +1,26 @@
 class Solution {
     public String removeDuplicates(String s, int k) {
         StringBuilder sb = new StringBuilder(s);
-        Stack<Integer> counts = new Stack();
+        Stack<Integer> stack = new Stack();
 
         for (int i = 0; i < sb.length(); i++) {
             if (i == 0 || sb.charAt(i) != sb.charAt(i-1)) {
-                counts.push(1);
+                stack.push(1);
             } else if (sb.charAt(i) == sb.charAt(i-1)) {
-                int total = counts.pop() + 1;
+                int total = stack.pop() + 1;
 
                 if (total == k) {
-                    // [start, end)
-                    // aaa
-                    // 012 (0,3)
-                    sb.delete(i-k+1, i+1);
-                    i = i-k;
+                   // len = k
+                    //i - ?  +1 == k
+                   // i+1-k=?
+                    sb.delete(i+1-k, i+1);
+                    i=i-k;
                 } else {
-                    counts.push(total);
+                    stack.push(total);
                 }
             }
         }
+
 
         return sb.toString();
     }
