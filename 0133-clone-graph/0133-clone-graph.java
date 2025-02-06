8d4f16aa7ba743eea01cabde9c31643adbdaf6b8
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-06 15:43:57 -0800

    Time: 27 ms (33.69%), Space: 43 MB (22.4%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -1,39 +1,17 @@
 class Solution {
-    Map<Integer, Node> map;
-    Set<Integer> visited;
+    private Map<Integer, Node> map = new HashMap<>();
 
     public Node cloneGraph(Node node) {
         if (node == null) return null;
-        
-        map = new HashMap<>();
-
-        cloneNode(node);
-        visited = new HashSet<>();
-        cloneNeighbors(node);
-
-        return map.get(node.val);
-    }
-
-    public void cloneNode(Node node) {
-        if (!map.containsKey(node.val)) {
-            map.put(node.val, new Node(node.val, new ArrayList<>()));
-
-            for (Node nei: node.neighbors) {
-                cloneNode(nei);
-            }
+        if (map.containsKey(node.val)) {
+            return map.get(node.val);
         }
-    }
 
-    public void cloneNeighbors(Node node) {
-        if (node == null || node.neighbors == null || node.neighbors.size() == 0) return;
-
-        if (!visited.contains(node.val)) {
-            visited.add(node.val);    
-
-            for (Node oldNei: node.neighbors) {
-                map.get(node.val).neighbors.add(map.get(oldNei.val));
-                cloneNeighbors(oldNei);
-            }
+        Node clone = new Node(node.val, new ArrayList<>());
+        map.put(node.val, clone);
+        for (Node neighbor : node.neighbors) {
+            clone.neighbors.add(cloneGraph(neighbor));
         }
+        return clone;
     }
 }
