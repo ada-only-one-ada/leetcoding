891fda1870ab1b85465bede61a38315873a2ca65
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-06 13:01:49 -0800

    Time: 0 ms (100%), Space: 46 MB (75.95%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -1,8 +1,6 @@
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
-        if (root == null) {
-            return null;
-        }
+        if (root == null) return null;
 
         if (root.val < key) {
             root.right = deleteNode(root.right, key);
@@ -11,24 +9,20 @@ class Solution {
         } else {
             if (root.left == null && root.right == null) {
                 return null;
-            } else if (root.left == null) {
-                return root.right;
-            } else if (root.right == null) {
+            } else if (root.left != null && root.right == null) {
                 return root.left;
+            } else if (root.right != null && root.left == null) {
+                return root.right;
             } else {
                 TreeNode rightMin = root.right;
                 while (rightMin.left != null) {
                     rightMin = rightMin.left;
                 }
 
-                // 替代当前root的val
-                root.val = rightMin.val;
-                // 把最小那个删了
-                root.right = deleteNode(root.right, rightMin.val);
-                return root;
+                rightMin.left = root.left;
+                return root.right;
             }
         }
-
         return root;
     }
 }
