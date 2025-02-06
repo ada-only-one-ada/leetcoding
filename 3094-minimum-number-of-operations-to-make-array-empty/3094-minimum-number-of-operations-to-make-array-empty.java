d2a29af5410d350f43f2a229024a533b455b3b35
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-05 21:23:32 -0800

    Time: 22 ms (43.05%), Space: 63.1 MB (34.08%) - LeetHub

--- 3094-minimum-number-of-operations-to-make-array-empty/3094-minimum-number-of-operations-to-make-array-empty.java
+++ 3094-minimum-number-of-operations-to-make-array-empty/3094-minimum-number-of-operations-to-make-array-empty.java
@@ -1,42 +1,37 @@
 class Solution {
     public int minOperations(int[] nums) {
-        // 选两个一样的删除
-        // 选三个一样的删除
-
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int num: nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        for (int num : nums) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
         }
 
-        int res = 0;
+        int totalOperations = 0;
+        for (int freq : frequencyMap.values()) {
+            if (freq == 1) {
+                // 如果任何数字的频率为1，不能通过任何操作删除它，直接返回-1。
+                return -1;
+            }
+            // 首先尽可能使用“每三个一组”的操作
+            int groupsOfThree = freq / 3;
+            int remaining = freq % 3;
 
-        for (int key: map.keySet()) {
-            int freq = map.get(key);
-            if (freq <= 1) return -1;
+            // 计算使用“每三个一组”的操作次数
+            totalOperations += groupsOfThree;
 
-            if (freq % 3 == 0) {
-                res += freq / 3;
-            } else {
-                while (freq >= 2) {
-                    if (freq >= 3) {
-                        res += freq / 3;
-                        freq = freq - (freq / 3) * 3;
-                    } else if (freq >= 2) {
-                        res += freq / 2;
-                        freq = freq - (freq / 2) * 2;
-                    }
+            // 对剩余的部分，如果有两个，使用“每两个一组”的操作一次
+            if (remaining == 2) {
+                totalOperations += 1;
+            } else if (remaining == 1) {
+                // 如果剩余一个，且之前有至少一个“每三个一组”的操作，可以调整一个“每三个一组”变为“每两个一组”
+                if (groupsOfThree > 0) {
+                    totalOperations++;  // 使用一个额外的“每两个一组”
+                } else {
+                    // 如果没有足够的数量来调整，返回-1
+                    return -1;
                 }
-
-                if (freq > 0) res++;
             }
         }
 
-        return res;
+        return totalOperations;
     }
 }
-
-/*
-2:4 
-3:3
-4:2
-*/
