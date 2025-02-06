21282c051d865e3720869769dd3ca2b4ea307d27
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-06 15:12:50 -0800

    Time: 8 ms (79.4%), Space: 43.8 MB (99.35%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.java
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.java
@@ -1,40 +1,37 @@
 class Solution {
     public int smallestDistancePair(int[] nums, int k) {
         Arrays.sort(nums);
-        
-        // 确定二分搜索的范围，基于最小和最大距离
-        int left = 0;
-        int right = nums[nums.length - 1] - nums[0] + 1;
+
+        int start = 0;
+        int end = nums[nums.length - 1] - nums[0] + 1;
         int res = 0;
 
-        // 使用二分搜索来找到第k小的距离
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            int count = countPairsWithDistanceLessThan(nums, mid);
+        while (start < end) {
+            int maxDistance = start + (end - start) / 2;
 
-            // 根据对数来调整搜索范围
-            if (count >= k) {
-                res = mid; // 如果对数足够，则更新答案
-                right = mid;
+            if (countPairsLessThan(nums, maxDistance) >= k) {
+                res = maxDistance;
+                end = maxDistance;
             } else {
-                left = mid + 1;
+                start = maxDistance + 1;
             }
         }
+
         return res;
     }
 
-    // 辅助函数：计算距离小于等于给定值的对数
-    private int countPairsWithDistanceLessThan(int[] nums, int maxDistance) {
+    public int countPairsLessThan(int[] nums, int maxDistance) {
         int count = 0;
-        int i = 0;
 
-        // 使用双指针方法来计算距离
+        int i = 0;
         for (int j = 0; j < nums.length; j++) {
-            while (nums[j] - nums[i] > maxDistance) {
-                i++; // 移动左指针直到距离小于等于maxDistance
+            while (i < nums.length && nums[j] - nums[i] > maxDistance) {
+                i++;
             }
-            count += j - i; // 累计符合条件的对数
+
+            count += j - i;
         }
+
         return count;
     }
 }
