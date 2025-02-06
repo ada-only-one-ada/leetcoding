f77e16304aacb43058c65bbd10f5ce7adf62c21a
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-06 11:37:17 -0800

    Time: 37 ms (45.6%), Space: 60.3 MB (48.2%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
@@ -1,24 +1,23 @@
 class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        Queue<int[]> queue = new PriorityQueue<>((arrA, arrB) -> {
-            return (arrA[0]+arrA[1]) - (arrB[0]+arrB[1]);
+        List<List<Integer>> res = new ArrayList<>();
+        Queue<int[]> queue = new PriorityQueue<>((a, b) -> {
+            return (a[0]+a[1]) - (b[0]+b[1]);
         });
 
-        for (int i = 0; i < nums1.length; i++) {
-            queue.add(new int[]{nums1[i], nums2[0], 0});
-        };
+        for (int i = 0; i < Math.min(nums2.length, k); i++) {
+            queue.add(new int[]{nums1[0], nums2[i], 0});
+        }
 
-        List<List<Integer>> res = new ArrayList<>();
-        while (!queue.isEmpty() && k > 0) {
-            int[] curr = queue.poll();
-            int num1 = curr[0];
-            int num2 = curr[1];
-            int index2 = curr[2];
-            res.add(new ArrayList<>(Arrays.asList(num1, num2)));
-            k--;
+        while (res.size() < k && !queue.isEmpty()) {
+            int[] pair = queue.poll();
+            int num1 = pair[0];
+            int num2 = pair[1];
+            int i1 = pair[2];
 
-            if (index2 + 1 < nums2.length) {
-                queue.add(new int[]{num1, nums2[index2 + 1], index2 + 1});
+            res.add(new ArrayList(Arrays.asList(num1, num2)));
+            if (i1+1 < nums1.length) {
+                queue.add(new int[]{nums1[i1+1], num2, i1+1});
             }
         }
 
