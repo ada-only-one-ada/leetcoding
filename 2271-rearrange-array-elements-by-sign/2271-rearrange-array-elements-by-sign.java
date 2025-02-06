b2693f755c2e9e6e5497e03685f81439444a306e
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-05 16:41:30 -0800

    Time: 3 ms (100%), Space: 77.2 MB (42.02%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -1,29 +1,19 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        // every consecutive pair has diff signs
-        // same sign, order matters
-        // begin with positive
-
-        List<Integer> pos = new ArrayList<>();
-        List<Integer> neg = new ArrayList<>();
-        for (int num: nums) {
-            if (num >= 0) pos.add(num);
-            else neg.add(num);
-        }
-
+        int posIndex = 0;
+        int negIndex = 1;
         int[] res = new int[nums.length];
-        int posIndex = 1;
-        int negIndex = 0;
-        int resIndex = 1;
-        res[0] = pos.get(0);
 
-        while (resIndex < res.length) {
-            if (res[resIndex - 1] > 0) {
-                res[resIndex++] = neg.get(negIndex++);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= 0) {
+                res[posIndex] = nums[i];
+                posIndex += 2;
             } else {
-                res[resIndex++] = pos.get(posIndex++);
+                res[negIndex] = nums[i];
+                negIndex += 2;
             }
         }
+
         return res;
     }
 }
