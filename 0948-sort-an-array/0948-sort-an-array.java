c96021399c7899ed1566cfc723a358d17850d58d
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-06 12:31:49 -0800

    Time: 46 ms (25.63%), Space: 55.6 MB (78.22%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,11 +1,9 @@
 class Solution {
     private void countingSort(int[] arr) {
-        // Create the counting hash map.
         HashMap<Integer,Integer> counts = new HashMap<>();
-        int minVal = arr[0], maxVal = arr[0];
+        int minVal = arr[0];
+        int maxVal = arr[0];
         
-        // Find the minimum and maximum values in the array,
-        // and update it's count in the hash map.
         for (int i = 0; i < arr.length; i++) {
             minVal = Math.min(minVal, arr[i]);
             maxVal = Math.max(maxVal, arr[i]);
@@ -13,9 +11,7 @@ class Solution {
         }
         
         int index = 0;
-        // Place each element in its correct position in the array.
-        for (int val = minVal; val <= maxVal; ++val) {
-            // Append all 'val's together if they exist.
+        for (int val = minVal; val <= maxVal; val++) {
             while (counts.getOrDefault(val, 0) > 0) {
                 arr[index] = val;
                 index += 1;
