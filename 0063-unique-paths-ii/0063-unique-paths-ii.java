f6abdd325bb41bf4058cd8b1ac0db4b88c5a8915
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-05 21:59:25 -0800

    Time: 0 ms (100%), Space: 42.1 MB (28.48%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -2,32 +2,47 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int rowNum = obstacleGrid.length;
         int colNum = obstacleGrid[0].length;
-        int[][] dp = new int[rowNum][colNum];
 
-        for (int row = rowNum - 1; row >= 0; row--) {            
-            for (int col = colNum - 1; col >= 0; col--) {
-                // 有障碍
-                if (obstacleGrid[row][col] == 1) {                  
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[rowNum-1][colNum-1] == 1) return 0;
+
+        int[][] dp = new int[rowNum][colNum];
+        dp[0][0] = 1;
+
+        for (int col = 1; col < colNum; col++) {
+            if (obstacleGrid[0][col] == 1 || dp[0][col - 1] == 0) {
+                dp[0][col] = 0;
+            } else {
+                dp[0][col] = 1;
+            }
+        }
+
+        for (int row = 1; row < rowNum; row++) {
+            if (obstacleGrid[row][0] == 1 || dp[row - 1][0] == 0) {
+                dp[row][0] = 0;
+            } else {
+                dp[row][0] = 1;
+            }
+        }
+
+        for (int row = 1; row < rowNum; row++) {
+            for (int col = 1; col < colNum; col++) {
+                int fromTop = dp[row-1][col];
+                int fromLeft = dp[row][col-1];
+
+                if (fromTop == 0 && fromLeft == 0 || obstacleGrid[row][col] == 1) {
                     dp[row][col] = 0;
-                // 没障碍
-                } else {     
-                    // 终点                                       
-                    if (row == rowNum - 1 && col == colNum - 1){   
-                        dp[row][col] = 1; 
-                    // 最后一行
-                    } else if (row == rowNum - 1) {                 
-                        dp[row][col] = dp[row][col + 1];
-                    // 最后一列
-                    } else if (col == colNum - 1) {                 
-                        dp[row][col] = dp[row + 1][col];
-                    // 其他
-                    } else {
-                        dp[row][col] = dp[row + 1][col] + dp[row][col + 1];  
-                    }                
+                } else if (fromTop != 0 && fromLeft != 0) {
+                    dp[row][col] = fromTop + fromLeft;
+                } else if (fromTop != 0) {
+                    dp[row][col] = fromTop;
+                } else if (fromLeft != 0) {
+                    dp[row][col] = fromLeft;
+                } else {
+                    dp[row][col] = 0;
                 }
             }
         }
-        
-        return dp[0][0];
+
+        return dp[rowNum-1][colNum-1];
     }
 }
