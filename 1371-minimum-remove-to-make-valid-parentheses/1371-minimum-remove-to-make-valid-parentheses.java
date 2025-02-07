2138325aaf28ae9bb2df2e142321570b57d2f160
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-06 16:25:58 -0800

    Time: 20 ms (56.88%), Space: 45.6 MB (58.62%) - LeetHub

--- 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.java
+++ 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.java
@@ -1,46 +1,43 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        char[] parentheses = s.toCharArray();
-
-        // Track open and close parentheses
+        StringBuilder sb = new StringBuilder();
         int open = 0;
         int close = 0;
-        for (int i = 0; i < parentheses.length; i++) {
-            char curr = parentheses[i];
-            if (curr == '(') {
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c != '(' &&  c != ')') {
+                sb.append(c);
+            } else if (c == '(') {
                 open++;
-            } else if (curr == ')') {
-                if (close + 1 <= open) {
-                    close++;
-                } else {
-                    parentheses[i] = ' '; // Invalidate this ')' as no matching '('
-                }
+                sb.append(c);
+            } else if (c == ')') {
+                if (close == open) continue;
+                sb.append(c);
+                close++;
             }
         }
 
         open = 0;
         close = 0;
-        for (int i = parentheses.length - 1; i >= 0; i--) {
-            char curr = parentheses[i];
-            if (curr == ')') {
-                close++;       
-            } else if (curr == '(') {
-                if (open + 1 <= close) {
-                    open++;       
-                } else {
-                    parentheses[i] = ' '; // Invalidate this '(' as no matching ')'
-                } 
+
+        StringBuilder res = new StringBuilder();
+        for (int i = sb.length() - 1; i >= 0; i--) {
+            char c = sb.charAt(i);
+
+            if (c != '(' &&  c != ')') {
+                res.append(c);
+            } else if (c == ')') {
+                close++;
+                res.append(c);
+            } else if (c == '(') {
+                if (close == open) continue;
+                res.append(c);
+                open++;
             }
         }
-        
-        int left = 0;
-        for (int right = 0; right < parentheses.length; right++) {
-            if (parentheses[right] != ' ') {
-                parentheses[left] = parentheses[right];
-                left++;
-            } 
-        }
 
-        return new String(parentheses, 0, left);
+        return res.reverse().toString();
     }
 }
+
