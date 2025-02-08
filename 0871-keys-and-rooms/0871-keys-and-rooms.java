a950a06365e38b1fd89936c8d3f7e47b4627d789
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-07 16:39:16 -0800

    Time: 1 ms (74.33%), Space: 44.6 MB (25%) - LeetHub

---
+++ 0871-keys-and-rooms/0871-keys-and-rooms.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        // start from room 0
+
+        return dfs(rooms, 0, new HashSet());
+    }
+
+    public boolean dfs(List<List<Integer>> rooms, int currRoom, Set<Integer> visited) {
+        if (visited.size() == rooms.size() - 1) {
+            return true;
+        }
+       
+
+        visited.add(currRoom);
+        for (int nextRoom: rooms.get(currRoom)) {
+            if (!visited.contains(nextRoom) && dfs(rooms, nextRoom, visited)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
