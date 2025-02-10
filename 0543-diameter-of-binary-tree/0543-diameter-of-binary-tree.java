966448fb332116bd4ea5e2ed0d6693750b9112a0
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-10 15:23:49 -0800

    Time: 0 ms (100%), Space: 44.4 MB (96.06%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -1,19 +1,18 @@
 class Solution {
     int res = 0;
+
     public int diameterOfBinaryTree(TreeNode root) {
-        getHeight(root);
+        helper(root);
         return res;
     }
 
-    public int getHeight(TreeNode root) {
+    public int helper(TreeNode root) {
         if (root == null) return 0;
 
-        int left = getHeight(root.left);
-        int right = getHeight(root.right);
-       // res = Math.max(res, left);
-       // res = Math.max(res, right);
-        res = Math.max(res, left + right);
+        int left = helper(root.left);
+        int right = helper(root.right);
 
+        res = Math.max(res, left + right);
         return 1 + Math.max(left, right);
     }
 }
