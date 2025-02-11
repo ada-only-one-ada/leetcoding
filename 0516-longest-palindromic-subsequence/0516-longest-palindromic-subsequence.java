67013fcf4db2cc666afd5e7605f78006c1356acb
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-11 12:13:32 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,25 +1,34 @@
 class Solution {
     public int longestPalindromeSubseq(String s) {
-        int[][] dp = new int[s.length()][s.length()];
+        if (s.length() == 0) return 0;
 
+        int res = 1;
+        int[][] dp = new int[s.length()][s.length()]; // s.substring [i,j] 
+        
         for (int i = 0; i < s.length(); i++) {
             dp[i][i] = 1;
         }
-        int start = 0;
-        int end = 0;
 
-        for (int len = 2; len <= s.length(); len++) {
-            for (int i = 0; i <= s.length() - len; i++) {
-                int j = i + len - 1;
-
-                if (s.charAt(i) == s.charAt(j)) {
-                    dp[i][j] = dp[i+1][j-1] + 2;
-                } else {
-                    dp[i][j] = Math.max(dp[i+1][j], dp[i][j-1]);
-                }
+        for (int i = 0; i + 1 < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = 2;
+                res = Math.max(res, dp[i][i+1]);
             }
         }
 
-        return dp[0][s.length()-1];
+        for (int len = 2; len <= s.length(); len++) {
+            for (int left = 0; left <= s.length() - len - 1; left++) {
+                int right = left + len;
+
+                if (s.charAt(left) == s.charAt(right)) {
+                    dp[left][right] = dp[left+1][right-1] + 2;
+                } else {
+                    dp[left][right] = Math.max(dp[left+1][right], dp[left][right-1]);
+                }
+                res = Math.max(res, dp[left][right]);
+            }
+        }
+
+        return res;
     }
 }
