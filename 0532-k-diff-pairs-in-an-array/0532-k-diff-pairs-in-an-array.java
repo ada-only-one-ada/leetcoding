dacab4fee07af29a4e2879bbc1385355ae9bfff4
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-11 09:39:07 -0800

    Time: 14 ms (21%), Space: 45.3 MB (30.43%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.java
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.java
@@ -4,22 +4,17 @@ class Solution {
         int res = 0;
         Map<Integer, Integer> map = new HashMap<>();
 
-        for (int i = 0; i < nums.length; i++){
-            int target = nums[i] - k;
-            
-            if (map.containsKey(target)){
-                if (map.get(target) != -1){ 
+        for (int num: nums) {
+            int target = num - k;
+
+            if (map.containsKey(target)) {
+                if (map.get(target) != -1) {
                     res++;
                     map.put(target, -1);
                 }
             }
-            //注意这里，如果之前的数字是1，已经用完了，当前数字也是1，就要也是设置成-1
-            map.put(nums[i], map.getOrDefault(nums[i], 0));
-            
-            /*
-            map.put(nums[i], 0);
-            nums=[1,1,1,1,1], k=0. output:4, expected:1
-            */
+
+            map.put(num, map.getOrDefault(num, 0));
         }
 
         return res;
