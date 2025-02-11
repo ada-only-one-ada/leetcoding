eab192c59499923d9597a4ebf607a5cc0d0db227
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-11 09:40:54 -0800

    Time: 13 ms (26.54%), Space: 44.9 MB (57.62%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.java
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.java
@@ -10,14 +10,15 @@ class Solution {
             if (map.containsKey(target)) {
                 if (map.get(target) != -1) {
                     res++;
-                    map.put(target, -1); // 1, 1, 1
+                    map.put(target, -1); 
                 }
             }
 
+            // 避免覆盖上面的target
             if (num == target && map.containsKey(target)) {
                 map.put(num, map.get(target));
             } else {
-                map.put(num, 1);
+                map.put(num, 2);
             }
         }
 
