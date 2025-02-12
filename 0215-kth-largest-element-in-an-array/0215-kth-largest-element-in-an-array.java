614f07fdf28571efd88ccd38ebbef9a818ef802d
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-12 09:31:14 -0800

    Time: 11 ms (93.81%), Space: 58.1 MB (55.07%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -3,30 +3,27 @@ class Solution {
         return quickSelect(nums, 0, nums.length - 1, nums.length - k);
     }
 
-    // [1,2,6,100,  200   ,300,400], k = 3, 7 - 3 = 4
-
-    public int quickSelect(int[] nums, int left, int right, int kSmallest) {
+    public int quickSelect(int[] nums, int left, int right, int kSmall) {
         int i = left;
         int j = right;
         int pivot = nums[left];
 
         while (i <= j) {
-            while (i <= j && nums[i] < pivot) i++;  // 这里不能是<=
-            // 比如【3，3，3，3】在上面这个loop里，i会到数组长度，j不变，在上面这行就退出整个 whileloop了
+            while (i <= j && nums[i] < pivot) i++;
             while (i <= j && nums[j] > pivot) j--;
 
             if (i <= j) {
-                int temp = nums[i];
+                int temp= nums[i];
                 nums[i] = nums[j];
-                nums[j] = temp; 
+                nums[j] = temp;
                 i++;
                 j--;
-            }  
+            }
         }
 
-        // 在这里的时候，left没变，j也没变，造成stack overflow了
-        if (kSmallest <= j) return quickSelect(nums, left, j, kSmallest);
-        if (kSmallest >= i) return quickSelect(nums, i, right, kSmallest);
-        return nums[kSmallest];
+        if (kSmall <= j) return quickSelect(nums, left, j, kSmall);
+        if (kSmall >= i) return quickSelect(nums, i, right, kSmall);
+
+        return nums[kSmall];
     }
 }
