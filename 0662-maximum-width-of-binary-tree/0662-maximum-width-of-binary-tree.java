28009917f7d9428fce12b63b764394099ff135c7
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-11 22:02:12 -0800

    Time: 2 ms (36.88%), Space: 43.2 MB (87.96%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -1,36 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public int widthOfBinaryTree(TreeNode root) {
-        if (root == null) return 0;
-        int res = 0;
-        
-        Queue<Pair<TreeNode, Integer>> queue = new LinkedList<>();
-        queue.add(new Pair(root, 1));
+    public int  widthOfBinaryTree(TreeNode root) {
+  if (root == null) return 0;
 
-        while (!queue.isEmpty()) {
-            int left = -1;
-            int right = -1;
-            int size = queue.size();
+  int res = 0;
+  Queue<Pair<TreeNode, Integer>> queue = new LinkedList<>();
+  queue.add(new Pair(root, 1));
 
-            for (int i = 0; i < size; i++) {
-                Pair<TreeNode, Integer> pair = queue.poll();
-                TreeNode node = pair.getKey();
-                int id = pair.getValue();
-                
-                if (left == -1) left = id;
-                if (i == size - 1) right = id;
+  while (!queue.isEmpty()) {
+    int size = queue.size();
+    int leftMost = -1;
+    int rightMost = -1;
 
-                if (node.left != null) {
-                    queue.add(new Pair(node.left, 2 * id));
-                }
+    for (int i = 0; i < size; i++) {
+      Pair<TreeNode, Integer> pair = queue.poll();
+      TreeNode node = pair.getKey();
+      int id = pair.getValue();
 
-                if (node.right != null) {
-                    queue.add(new Pair(node.right, 2 * id + 1));
-                }
-            }
+      if (i == 0) leftMost = id;
+      if (i == size - 1) rightMost = id;
 
-            res = Math.max(res, right - left + 1);
-        }
+      if (node.left != null) {
+        queue.add(new Pair(node.left, id * 2));
+      }
 
-        return res;
+      if (node.right != null) {
+        queue.add(new Pair(node.right, id * 2 + 1));
+      }
     }
+
+    res = Math.max(res, rightMost - leftMost + 1);
+  }
+
+  return res;
 }
+}
+
