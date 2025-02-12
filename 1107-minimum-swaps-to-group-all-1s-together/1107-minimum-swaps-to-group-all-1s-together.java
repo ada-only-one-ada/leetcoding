89a2b8f215e466200cdd319e5207a995e74679d0
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-11 20:33:14 -0800

    Time: 6 ms (69.69%), Space: 61.2 MB (5.75%) - LeetHub

---
+++ 1107-minimum-swaps-to-group-all-1s-together/1107-minimum-swaps-to-group-all-1s-together.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minSwaps(int[] data) {
+        int res = Integer.MAX_VALUE;
+
+        int len = 0;
+        for (int d: data) {
+            len += d;
+        }
+        if (len <= 1) return 0;
+
+        int zeros = 0;
+        int left = 0;
+        for (int right = 0; right < data.length; right++) {
+            if (data[right] == 0) zeros++;
+
+            if (right - left + 1 == len) {
+                res = Math.min(res, zeros);
+
+                if (data[left] == 0) {
+                zeros--;
+            }
+            left ++;
+            }
+            
+        }        
+
+        return res;
+    }
+}
