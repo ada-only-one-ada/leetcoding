6a786d0084ce99ae99c899ba605868c965d80290
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2025-02-12 09:12:15 -0800

    Time: 15 ms (38.96%), Space: 49.3 MB (11.12%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,30 +1,24 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        List<Integer>[] bucket = new List[nums.length + 1];
-
         Map<Integer, Integer> map = new HashMap<>();
         for (int num: nums) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
-
-        for (int num: map.keySet()) {
-            int freq = map.get(num);
-
-            if (bucket[freq] == null) {
-                bucket[freq] = new ArrayList<>();
-            }
-            bucket[freq].add(num);
-        }
-
+        
         int[] res = new int[k];
-        int index = 0;
-        for (int i = bucket.length - 1; i > 0 && index < res.length; i--) {
-            if (bucket[i] == null) continue;
-            for (int num: bucket[i]) {
-                res[index++] = num;
-            }
-        }
+        Queue<Integer> queue = new PriorityQueue<>((a, b) -> {
+            return map.get(a) - map.get(b);
+        });
 
+        for (int key: map.keySet()) {
+            queue.add(key);
+
+            while (!queue.isEmpty() &&queue.size() > k) queue.poll();
+        }
+        int index = k - 1;
+        while (!queue.isEmpty()) {
+            res[index--] = queue.poll();
+        }
         return res;
     }
 }
